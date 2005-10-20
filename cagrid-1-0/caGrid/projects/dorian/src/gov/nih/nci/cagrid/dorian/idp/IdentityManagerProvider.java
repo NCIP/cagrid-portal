c2930df731e5340920d20feae9643b5ad9cca550
langella <langella>
2005-10-20 21:01:23 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@49 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/idp/IdentityManagerProvider.java
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/idp/IdentityManagerProvider.java
@@ -1,11 +1,17 @@
 package gov.nih.nci.cagrid.gums.idp;
 
 import gov.nih.nci.cagrid.gums.bean.GUMSInternalFault;
+import gov.nih.nci.cagrid.gums.common.Crypt;
 import gov.nih.nci.cagrid.gums.common.Database;
 import gov.nih.nci.cagrid.gums.common.GUMSObject;
 import gov.nih.nci.cagrid.gums.idp.bean.Application;
+import gov.nih.nci.cagrid.gums.idp.bean.BasicAuthCredential;
+import gov.nih.nci.cagrid.gums.idp.bean.InvalidLoginFault;
 import gov.nih.nci.cagrid.gums.idp.bean.InvalidUserPropertyFault;
+import gov.nih.nci.cagrid.gums.idp.bean.NoSuchUserFault;
+import gov.nih.nci.cagrid.gums.idp.bean.PermissionDeniedFault;
 import gov.nih.nci.cagrid.gums.idp.bean.User;
+import gov.nih.nci.cagrid.gums.idp.bean.UserFilter;
 
 import org.globus.wsrf.utils.FaultHelper;
 
@@ -17,15 +23,16 @@ import org.globus.wsrf.utils.FaultHelper;
  *          Exp $
  */
 
-public class IdentityManagerProvider extends GUMSObject{
-	
+public class IdentityManagerProvider extends GUMSObject {
+
 	private IdPProperties properties;
+
 	private UserManager userManager;
-	
+
 	public IdentityManagerProvider(Database db) throws GUMSInternalFault {
 		try {
 			this.properties = new IdPProperties(db);
-			this.userManager = new UserManager(db,this.properties);
+			this.userManager = new UserManager(db, this.properties);
 		} catch (Exception e) {
 			logError(e.getMessage(), e);
 			GUMSInternalFault fault = new GUMSInternalFault();
@@ -37,19 +44,91 @@ public class IdentityManagerProvider extends GUMSObject{
 			throw fault;
 		}
 	}
-	
-	public String register(Application application)throws GUMSInternalFault, InvalidUserPropertyFault{
+
+	public String register(Application application) throws GUMSInternalFault,
+			InvalidUserPropertyFault {
 		IdPRegistrationPolicy policy = properties.getRegistrationPolicy();
 		User u = policy.register(application);
 		userManager.addUser(u);
-		if(u.getStatus().equals(UserManager.PENDING)){
-			return "Your application will be reviewed by an administrator and you will be contacted at "+u.getEmail()+" upon a decision.";
+		if (u.getStatus().equals(UserManager.PENDING)) {
+			return "Your application will be reviewed by an administrator and you will be contacted at "
+					+ u.getEmail() + " upon a decision.";
+		} else if (u.getStatus().equals(UserManager.ACTIVE)) {
+			return "Your account was approved, your current account status is "
+					+ u.getStatus().getValue() + " you mail use "
+					+ u.getEmail() + " to access your account";
+		}else if (u.getStatus().equals(UserManager.REJECTED)) {
+			return "Your account was rejected.";
 		}else{
-			return "Your account was approved, your current account status is "+u.getStatus().getValue()+" you mail use "+u.getEmail()+" to access your account";
+			return "Contact an administrator for the status of your account.";
+		}
+}
+
+
+	public User[] findUsers(BasicAuthCredential credential, UserFilter filter)
+			throws GUMSInternalFault, InvalidLoginFault, PermissionDeniedFault {
+		User requestor = verifyUser(credential);
+		verifyAdministrator(requestor);
+		return this.userManager.getUsers(filter, true);
+	}
+
+	public void updateUser(BasicAuthCredential credential, User u)
+			throws GUMSInternalFault, InvalidLoginFault, PermissionDeniedFault, NoSuchUserFault {
+		User requestor = verifyUser(credential);
+		verifyAdministrator(requestor);
+		this.userManager.updateUser(u);
+	}
+
+	private void verifyAdministrator(User u) throws PermissionDeniedFault {
+		if (!u.getRole().equals(UserManager.ADMINISTRATOR)) {
+			PermissionDeniedFault fault = new PermissionDeniedFault();
+			fault.setFaultString("You are NOT an administrator.");
+			throw fault;
 		}
 	}
-	
-	
+
+	private User verifyUser(BasicAuthCredential credential)
+			throws GUMSInternalFault, InvalidLoginFault {
+		try {
+			User u = this.userManager.getUser(credential.getEmail());
+			if (!u.getPassword().equals(Crypt.crypt(credential.getPassword()))) {
+				InvalidLoginFault fault = new InvalidLoginFault();
+				fault
+						.setFaultString("The mmail address or password is incorrect.");
+				throw fault;
+			}
+			if (!u.getStatus().equals(UserManager.ACTIVE)) {
+				if (u.getStatus().equals(UserManager.SUSPENDED)) {
+					InvalidLoginFault fault = new InvalidLoginFault();
+					fault.setFaultString("The account has been suspended.");
+					throw fault;
+
+				} else if (u.getStatus().equals(UserManager.REJECTED)) {
+					InvalidLoginFault fault = new InvalidLoginFault();
+					fault
+							.setFaultString("The application for the account was rejected.");
+					throw fault;
+
+				} else if (u.getStatus().equals(UserManager.PENDING)) {
+					InvalidLoginFault fault = new InvalidLoginFault();
+					fault
+							.setFaultString("The application for this account has not yet been reviewed.");
+					throw fault;
+				} else {
+					InvalidLoginFault fault = new InvalidLoginFault();
+					fault.setFaultString("Unknown Reason");
+					throw fault;
+				}
+			}
+
+			return u;
+		} catch (NoSuchUserFault e) {
+			InvalidLoginFault fault = new InvalidLoginFault();
+			fault.setFaultString("Email address or password is incorrect");
+			throw fault;
+		}
+
+	}
 
 	public IdPProperties getProperties() {
 		return properties;
