ebf746ac22de8bf1e999267814d344df6ae82e6e
langella <langella>
2005-10-20 15:04:11 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@42 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/common/AddressValidator.java
@@ -0,0 +1,62 @@
+package gov.nih.nci.cagrid.gums.common;
+
+import java.util.regex.*;
+
+/**
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public class AddressValidator {
+
+	// precompiled patterns
+	// notice double escape of special characters
+	private static Pattern phonePattern = Pattern
+			.compile("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d");
+
+	private static Pattern zipPattern = Pattern.compile("\\d\\d\\d\\d\\d");
+
+	public static void validatePhone(String phone)
+			throws IllegalArgumentException {
+		if (phone == null) {
+			throw new IllegalArgumentException(
+					"No phone number specified, correct format is 555-555-5555");
+		}
+		Matcher phoneMat = phonePattern.matcher(phone);
+		if (!phoneMat.matches()) {
+			throw new IllegalArgumentException(
+					"Invalid phone number, correct format is 555-555-5555");
+		}
+	}
+
+	public static void validateEmail(String email)
+			throws IllegalArgumentException {
+		if (email.indexOf("@") == -1) {
+			throw new IllegalArgumentException(
+					"Invalid email address specified.");
+		}
+	}
+
+	public static void validateState(String state)
+			throws IllegalArgumentException {
+		if (!USStates.isValidStateAbbreviation(state)) {
+			throw new IllegalArgumentException(
+					"Invalid state specified, use state abbreviations.");
+		}
+	}
+
+	public static void validateZipCode(String zip)
+			throws IllegalArgumentException {
+		if (zip == null) {
+			throw new IllegalArgumentException(
+					"No zip code specified, correct format is 55555");
+		}
+		Matcher zipMat = zipPattern.matcher(zip);
+		if (!zipMat.matches()) {
+			throw new IllegalArgumentException(
+					"Invalid phone number, correct format is 55555");
+		}
+	}
+}
---
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/common/USStates.java
@@ -0,0 +1,84 @@
+package gov.nih.nci.cagrid.gums.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public class USStates {
+
+	private static Map abrToState = new HashMap();
+
+	
+	public static  boolean isValidStateAbbreviation(String s){
+		if(abrToState.containsKey(s)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	static {
+		abrToState.put("AK", "Alaska");
+		abrToState.put("AL", "Alabama");
+		abrToState.put("AR", "Arkansas");
+		abrToState.put("AZ", "Arizona");
+		abrToState.put("CA", "California");
+		abrToState.put("CO", "Colorado");
+		abrToState.put("CT", "Connecticut");
+		abrToState.put("DC", "Dist of Columbia");
+		abrToState.put("DE", "Delaware");
+		abrToState.put("FL", "Florida");
+		abrToState.put("GA", "Georgia");
+		abrToState.put("GU", "Guam");
+		abrToState.put("HI", "Hawaii");
+		abrToState.put("IA", "Iowa");
+		abrToState.put("ID", "Idaho");
+		abrToState.put("IL", "Illinois");
+		abrToState.put("IN", "Indiana");
+		abrToState.put("KS", "Kansas");
+		abrToState.put("KY", "Kentucky");
+		abrToState.put("LA", "Louisiana");
+		abrToState.put("MA", "Massachusetts");
+		abrToState.put("MD", "Maryland");
+		abrToState.put("ME", "Maine");
+		abrToState.put("MI", "Michigan");
+		abrToState.put("MN", "Minnesota");
+		abrToState.put("MO", "Missouri");
+		abrToState.put("MS", "Mississippi");
+		abrToState.put("MT", "Montana");
+		abrToState.put("NC", "North Carolina");
+		abrToState.put("ND", "North Dakota");
+		abrToState.put("NE", "Nebraska");
+		abrToState.put("NH", "New Hampshire");
+		abrToState.put("NJ", "New Jersey");
+		abrToState.put(" NM", "New Mexico");
+		abrToState.put("NV", "Nevada");
+		abrToState.put("NY", "New York");
+		abrToState.put("OH", "Ohio");
+		abrToState.put("OK", "Oklahoma");
+		abrToState.put("OR", "Oregon");
+		abrToState.put("PA", "Pennsylvania");
+		abrToState.put("PR", "Puerto Rico");
+		abrToState.put("RI", "Rhode Island");
+		abrToState.put("SC", "South Carolina");
+		abrToState.put("SD", "South Dakota");
+		abrToState.put("TN", "Tennessee");
+		abrToState.put("TX", "Texas");
+		abrToState.put("UT", "Utah");
+		abrToState.put("VA", "Virginia");
+		abrToState.put("VI", "Virgin Islands");
+		abrToState.put("VT", "Vermont");
+		abrToState.put("WA", "Washington");
+		abrToState.put("WI", "Wisconsin");
+		abrToState.put("WV", "West Virginia");
+		abrToState.put("WY", "Wyoming");
+	}
+
+
+}
