40ffa89599f81335184ebf09538317054d3773a3
langella <langella>
2005-11-14 06:51:36 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@96 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/idp/TestIdPConfiguration.java
+++ cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/idp/TestIdPConfiguration.java
@@ -1,6 +1,8 @@
 package gov.nih.nci.cagrid.gums.idp;
 
 import gov.nih.nci.cagrid.gums.common.FaultUtil;
+import gov.nih.nci.cagrid.gums.common.ca.CertUtil;
+import gov.nih.nci.cagrid.gums.common.ca.KeyUtil;
 import gov.nih.nci.cagrid.gums.test.TestResourceManager;
 
 import java.io.File;
@@ -36,8 +38,11 @@ public class TestIdPConfiguration extends TestCase {
 
 	public static String IDP_CONFIG = "resources" + File.separator
 	+ "general-test" + File.separator + "idp-config.xml";
+	
+	public static String IDP_CONFIG2 = "resources" + File.separator
+	+ "general-test" + File.separator + "idp-config2.xml";
 
-	public void testDefaultProperties() {
+	public void testAutoConfiguration() {
 		try {
 			 TestResourceManager trm = new TestResourceManager(IDP_CONFIG);
 			 IdPConfiguration conf = (IdPConfiguration)trm.getResource(IdPConfiguration.RESOURCE);
@@ -57,6 +62,33 @@ public class TestIdPConfiguration extends TestCase {
 			assertEquals(true,conf.isAutoRenewAssertingCredentials());
 			assertEquals(null,conf.getAssertingCertificate());
 			assertEquals(null,conf.getAssertingKey());
+			
+		
+		} catch (Exception e) {
+			FaultUtil.printFault(e);
+			assertTrue(false);
+		} 
+	}
+	public void testConfiguration() {
+		try {
+			 TestResourceManager trm = new TestResourceManager(IDP_CONFIG2);
+			 IdPConfiguration conf = (IdPConfiguration)trm.getResource(IdPConfiguration.RESOURCE);
+		
+			assertEquals(DEFAULT_MIN_PASSWORD_LENGTH, conf
+					.getMinimumPasswordLength());
+			assertEquals(DEFAULT_MAX_PASSWORD_LENGTH, conf
+					.getMaximumPasswordLength());
+			assertEquals(DEFAULT_MIN_UID_LENGTH, conf
+					.getMinimumUIDLength());
+			assertEquals(DEFAULT_MAX_UID_LENGTH, conf
+					.getMaximumUIDLength());
+			assertEquals(DEFAULT_REGISTRATION_POLICY, conf.getRegistrationPolicy().getClass().getName());
+			assertEquals(null,conf.getKeyPassword());
+		
+			assertEquals(false,conf.isAutoCreateAssertingCredentials());
+			assertEquals(false,conf.isAutoRenewAssertingCredentials());
+			assertEquals(CertUtil.loadCertificate("resources/ca-test/gums-cert.pem"),conf.getAssertingCertificate());
+			assertEquals(KeyUtil.loadPrivateKey("resources/ca-test/gums-key.pem",conf.getKeyPassword()),conf.getAssertingKey());
 			
 		
 		} catch (Exception e) {
