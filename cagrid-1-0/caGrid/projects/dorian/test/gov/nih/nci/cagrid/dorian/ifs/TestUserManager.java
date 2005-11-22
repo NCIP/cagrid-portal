7dfd4efb733d0ef5903c5f40e70caef425f4f9fb
langella <langella>
2005-11-22 20:53:02 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@173 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/ifs/TestUserManager.java
+++ cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/ifs/TestUserManager.java
@@ -35,6 +35,7 @@ public class TestUserManager extends TestCase {
 			IFSUser user = new IFSUser();
 			user.setIdPId(1);
 			user.setUID("user");
+			user.setEmail("user@user.com");
 			user = um.addUser(user);
 			assertNotNull(user.getCertificate());
 			assertNotNull(user.getGridId());
@@ -61,6 +62,68 @@ public class TestUserManager extends TestCase {
 			l2 = um.getUsers(f2);
 			assertEquals(1,l2.length);
 			assertEquals(user,l2[0]);
+			
+			//Test querying by IdP_Id
+			IFSUserFilter f3 = new IFSUserFilter();
+			f3.setIdPId(Long.MAX_VALUE);
+			IFSUser[] l3 = um.getUsers(f3);
+			assertEquals(0,l3.length);
+			f3.setIdPId(user.getIdPId());
+			l3 = um.getUsers(f2);
+			assertEquals(1,l3.length);
+			assertEquals(user,l3[0]);
+			
+           //Test querying by GID
+			IFSUserFilter f4 = new IFSUserFilter();
+			f4.setGridId("nobody");
+			IFSUser[] l4 = um.getUsers(f4);
+			assertEquals(0,l4.length);
+			f4.setGridId(user.getGridId());
+			l4 = um.getUsers(f4);
+			assertEquals(1,l4.length);
+			assertEquals(user,l4[0]);
+			
+			//Test querying by Email
+			IFSUserFilter f5 = new IFSUserFilter();
+			f5.setEmail("nobody");
+			IFSUser[] l5 = um.getUsers(f5);
+			assertEquals(0,l5.length);
+			f5.setEmail(user.getEmail());
+			l5 = um.getUsers(f5);
+			assertEquals(1,l5.length);
+			assertEquals(user,l5[0]);
+			
+			//Test querying by Role
+			IFSUserFilter f6 = new IFSUserFilter();
+			f6.setUserRole(IFSUserRole.Administrator);
+			IFSUser[] l6 = um.getUsers(f6);
+			assertEquals(0,l6.length);
+			f6.setUserRole(IFSUserRole.Non_Administrator);
+			l6 = um.getUsers(f6);
+			assertEquals(1,l6.length);
+			assertEquals(user,l6[0]);
+			
+			//Test querying by Status
+			IFSUserFilter f7 = new IFSUserFilter();
+			f7.setUserStatus(IFSUserStatus.Suspended);
+			IFSUser[] l7 = um.getUsers(f7);
+			assertEquals(0,l7.length);
+			f7.setUserStatus(user.getUserStatus());
+			l7 = um.getUsers(f7);
+			assertEquals(1,l7.length);
+			assertEquals(user,l7[0]);
+			
+			//Test All
+			IFSUserFilter f8 = new IFSUserFilter();
+			f8.setIdPId(user.getIdPId());
+			f8.setUID(user.getUID());
+			f8.setGridId(user.getGridId());
+			f8.setEmail(user.getEmail());
+			f8.setUserRole(user.getUserRole());
+			f8.setUserStatus(user.getUserStatus());
+			IFSUser[] l8 = um.getUsers(f8);
+			assertEquals(1,l8.length);
+			
 		
 		} catch (Exception e) {
 			FaultUtil.printFault(e);
