7acd16c13a1dbc397a200396c68c2cc4d5148e21
langella <langella>
2005-11-22 20:26:47 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@171 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/ifs/UserManager.java
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/ifs/UserManager.java
@@ -6,21 +6,22 @@ import gov.nih.nci.cagrid.gums.common.Database;
 import gov.nih.nci.cagrid.gums.common.GUMSObject;
 import gov.nih.nci.cagrid.gums.common.ca.CertUtil;
 import gov.nih.nci.cagrid.gums.common.ca.KeyUtil;
-import gov.nih.nci.cagrid.gums.idp.portal.UserRolesComboBox;
 import gov.nih.nci.cagrid.gums.ifs.bean.CredentialsFault;
 import gov.nih.nci.cagrid.gums.ifs.bean.IFSUser;
+import gov.nih.nci.cagrid.gums.ifs.bean.IFSUserFilter;
 import gov.nih.nci.cagrid.gums.ifs.bean.IFSUserRole;
 import gov.nih.nci.cagrid.gums.ifs.bean.IFSUserStatus;
-import gov.nih.nci.cagrid.gums.ifs.bean.InvalidUserFault;
 
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.bouncycastle.jce.PKCS10CertificationRequest;
 import org.globus.wsrf.utils.FaultHelper;
@@ -41,12 +42,13 @@ public class UserManager extends GUMSObject {
 	private boolean dbBuilt = false;
 
 	private CredentialsManager credentialsManager;
-	
+
 	private IFSConfiguration conf;
-	
+
 	private CertificateAuthority ca;
-	
-	public UserManager(Database db,IFSConfiguration conf,CertificateAuthority ca) {
+
+	public UserManager(Database db, IFSConfiguration conf,
+			CertificateAuthority ca) {
 		this.db = db;
 		this.credentialsManager = new CredentialsManager(db);
 		this.conf = conf;
@@ -88,37 +90,36 @@ public class UserManager extends GUMSObject {
 		}
 		return exists;
 	}
-	
-	private String getCredentialsManagerUID(long idpId, String uid){
-		return "[IdPId="+idpId+", UID="+uid+"]";
+
+	private String getCredentialsManagerUID(long idpId, String uid) {
+		return "[IdPId=" + idpId + ", UID=" + uid + "]";
 	}
-	
-	
-	
-	public synchronized X509Certificate createUserCredentials(long idpId, String uid)
-			throws GUMSInternalFault, CredentialsFault {
+
+	public synchronized X509Certificate createUserCredentials(long idpId,
+			String uid) throws GUMSInternalFault, CredentialsFault {
 		try {
-			
+
 			String caSubject = ca.getCACertificate().getSubjectDN().getName();
 			int caindex = caSubject.lastIndexOf(",");
 			String caPreSub = caSubject.substring(0, caindex);
 			String sub = caPreSub + ",OU=IdP [" + idpId + "],CN=" + uid;
 			Calendar c = new GregorianCalendar();
 			Date start = c.getTime();
-			c.roll(Calendar.YEAR,conf.getCredentialsValidYears());
-			c.roll(Calendar.MONTH,conf.getCredentialsValidMonths());
-			c.roll(Calendar.DAY_OF_MONTH,conf.getCredentialsValidDays());
+			c.roll(Calendar.YEAR, conf.getCredentialsValidYears());
+			c.roll(Calendar.MONTH, conf.getCredentialsValidMonths());
+			c.roll(Calendar.DAY_OF_MONTH, conf.getCredentialsValidDays());
 			Date end = c.getTime();
-			if(end.after(ca.getCACertificate().getNotAfter())){
+			if (end.after(ca.getCACertificate().getNotAfter())) {
 				end = ca.getCACertificate().getNotAfter();
 			}
-			
+
 			KeyPair pair = KeyUtil.generateRSAKeyPair1024();
-			
-			PKCS10CertificationRequest req = CertUtil.generateCertficateRequest(
-					sub, pair);
-			X509Certificate cert = ca.requestCertificate(req,start,end);
-			this.credentialsManager.addCredentials(getCredentialsManagerUID(idpId,uid),null,cert,pair.getPrivate());
+
+			PKCS10CertificationRequest req = CertUtil
+					.generateCertficateRequest(sub, pair);
+			X509Certificate cert = ca.requestCertificate(req, start, end);
+			this.credentialsManager.addCredentials(getCredentialsManagerUID(
+					idpId, uid), null, cert, pair.getPrivate());
 			return cert;
 		} catch (Exception e) {
 			logError(e.getMessage(), e);
@@ -131,40 +132,148 @@ public class UserManager extends GUMSObject {
 		}
 	}
 
-	public synchronized IFSUser addUser(IFSUser user) throws GUMSInternalFault, CredentialsFault {
+	private StringBuffer appendWhereOrAnd(boolean firstAppended,
+			StringBuffer sql) {
+		if (firstAppended) {
+			sql.append(" AND ");
+		} else {
+			sql.append(" WHERE");
+		}
+		return sql;
+	}
+
+	public IFSUser[] getUsers(IFSUserFilter filter) throws GUMSInternalFault {
+
+		this.buildDatabase();
+		Connection c = null;
+		List users = new ArrayList();
+		try {
+			c = db.getConnectionManager().getConnection();
+			Statement s = c.createStatement();
+
+			StringBuffer sql = new StringBuffer();
+			sql.append("select * from " + USERS_TABLE);
+			if (filter != null) {
+				boolean firstAppended = false;
+
+				if (filter.getIdPId() > 0) {
+					sql = appendWhereOrAnd(firstAppended, sql);
+					firstAppended = true;
+					sql.append(" IDP_ID =" + filter.getIdPId());
+				}
+
+				if (filter.getUID() != null) {
+					sql = appendWhereOrAnd(firstAppended, sql);
+					firstAppended = true;
+					sql.append(" UID LIKE '%" + filter.getUID() + "%'");
+				}
+
+				if (filter.getGridId() != null) {
+					sql = appendWhereOrAnd(firstAppended, sql);
+					firstAppended = true;
+					sql.append(" GID LIKE '%" + filter.getGridId() + "%'");
+				}
+
+				if (filter.getEmail() != null) {
+					sql = appendWhereOrAnd(firstAppended, sql);
+					firstAppended = true;
+					sql.append(" EMAIL LIKE '%" + filter.getEmail() + "%'");
+				}
+
+				if (filter.getUserStatus() != null) {
+					sql = appendWhereOrAnd(firstAppended, sql);
+					firstAppended = true;
+					sql.append(" STATUS='" + filter.getUserStatus() + "'");
+				}
+
+				if (filter.getUserRole() != null) {
+					sql = appendWhereOrAnd(firstAppended, sql);
+					firstAppended = true;
+					sql.append(" ROLE='" + filter.getUserRole() + "'");
+				}
+			}
+
+			ResultSet rs = s.executeQuery(sql.toString());
+			while (rs.next()) {
+				IFSUser user = new IFSUser();
+				user.setIdPId(rs.getLong("IDP_ID"));
+				user.setUID(rs.getString("UID"));
+				user.setGridId(rs.getString("GID"));
+				String email = rs.getString("EMAIL");
+				if ((email != null) && (!email.equals("null"))) {
+					user.setEmail(email);
+				}
+				user.setUserStatus(IFSUserStatus.fromValue(rs
+						.getString("STATUS")));
+				String role = rs.getString("ROLE");
+				user.setUserRole(IFSUserRole.fromValue(role));
+				X509Certificate cert = credentialsManager
+						.getCertificate(getCredentialsManagerUID(user
+								.getIdPId(), user.getUID()));
+				user.setCertificate(CertUtil.writeCertificateToString(cert));
+				users.add(user);
+			}
+			rs.close();
+			s.close();
+
+			IFSUser[] list = new IFSUser[users.size()];
+			for (int i = 0; i < list.length; i++) {
+				list[i] = (IFSUser) users.get(i);
+			}
+			return list;
+
+		} catch (Exception e) {
+			logError(e.getMessage(), e);
+			GUMSInternalFault fault = new GUMSInternalFault();
+			fault
+					.setFaultString("Unexpected Error, could not obtain a list of users");
+			FaultHelper helper = new FaultHelper(fault);
+			helper.addFaultCause(e);
+			fault = (GUMSInternalFault) helper.getFault();
+			throw fault;
+		} finally {
+			db.getConnectionManager().releaseConnection(c);
+		}
+	}
+
+	public synchronized IFSUser addUser(IFSUser user) throws GUMSInternalFault,
+			CredentialsFault {
 		this.buildDatabase();
 		if (!determineIfUserExists(user.getIdPId(), user.getUID())) {
-			X509Certificate cert = createUserCredentials(user.getIdPId(),user.getUID());
+			X509Certificate cert = createUserCredentials(user.getIdPId(), user
+					.getUID());
 			try {
 				// Write method for creating and setting a users credentials
 				user.setCertificate(CertUtil.writeCertificateToString(cert));
 				user.setGridId(cert.getSubjectDN().toString());
 				user.setUserRole(IFSUserRole.Non_Administrator);
 				user.setUserStatus(IFSUserStatus.Pending);
-				db.update("INSERT INTO " + USERS_TABLE
-						+ " SET IDP_ID='" + user.getIdPId() + "',UID='"
-						+ user.getUID() + "', GID='"
-						+ user.getGridId() + "',STATUS='"
-						+ user.getUserStatus().toString() + "',ADMIN='"
+				db.update("INSERT INTO " + USERS_TABLE + " SET IDP_ID='"
+						+ user.getIdPId() + "',UID='" + user.getUID()
+						+ "', GID='" + user.getGridId() + "',STATUS='"
+						+ user.getUserStatus().toString() + "',ROLE='"
 						+ user.getUserRole().toString() + "',EMAIL='"
 						+ user.getEmail() + "'");
 			} catch (Exception e) {
 				try {
 					this.removeUser(user.getIdPId(), user.getUID());
 				} catch (Exception ex) {
-					
+
 				}
-				
+
 				try {
-					this.credentialsManager.deleteCredentials(getCredentialsManagerUID(user.getIdPId(),user.getUID()));
+					this.credentialsManager
+							.deleteCredentials(getCredentialsManagerUID(user
+									.getIdPId(), user.getUID()));
 				} catch (Exception ex) {
-					
+
 				}
 				logError(e.getMessage(), e);
 				GUMSInternalFault fault = new GUMSInternalFault();
 				fault
 						.setFaultString("Error adding the user "
-								+ getCredentialsManagerUID(user.getIdPId(),user.getUID())
+								+ getCredentialsManagerUID(user.getIdPId(),
+										user.getUID())
 								+ " to the IFS, an unexpected database error occurred.");
 				FaultHelper helper = new FaultHelper(fault);
 				helper.addFaultCause(e);
@@ -174,12 +283,11 @@ public class UserManager extends GUMSObject {
 
 		} else {
 			GUMSInternalFault fault = new GUMSInternalFault();
-			fault
-					.setFaultString("Error adding the user, "
-							+ getCredentialsManagerUID(user.getIdPId(),user.getUID())
-							+ ", the user already exists!!!");
+			fault.setFaultString("Error adding the user, "
+					+ getCredentialsManagerUID(user.getIdPId(), user.getUID())
+					+ ", the user already exists!!!");
 			throw fault;
-			
+
 		}
 		return user;
 	}
@@ -197,8 +305,8 @@ public class UserManager extends GUMSObject {
 				String users = "CREATE TABLE " + USERS_TABLE + " ("
 						+ "IDP_ID INT NOT NULL," + "UID VARCHAR(255) NOT NULL,"
 						+ "GID VARCHAR(255) NOT NULL,"
-						+ "STATUS VARCHAR(10) NOT NULL,"
-						+ "ADMIN VARCHAR(5) NOT NULL, "
+						+ "STATUS VARCHAR(50) NOT NULL,"
+						+ "ROLE VARCHAR(50) NOT NULL, "
 						+ "EMAIL VARCHAR(255) NOT NULL, "
 						+ "INDEX document_index (UID));";
 				db.update(users);
--- cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/ifs/TestUserManager.java
+++ cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/ifs/TestUserManager.java
@@ -1,29 +1,18 @@
 package gov.nih.nci.cagrid.gums.ifs;
 
-import gov.nih.nci.cagrid.gums.bean.GUMSInternalFault;
 import gov.nih.nci.cagrid.gums.common.Database;
 import gov.nih.nci.cagrid.gums.common.FaultUtil;
 import gov.nih.nci.cagrid.gums.common.ca.CertUtil;
-import gov.nih.nci.cagrid.gums.common.ca.KeyUtil;
-import gov.nih.nci.cagrid.gums.idp.portal.UserRolesComboBox;
-import gov.nih.nci.cagrid.gums.ifs.CredentialsManager;
 import gov.nih.nci.cagrid.gums.ifs.bean.IFSUser;
+import gov.nih.nci.cagrid.gums.ifs.bean.IFSUserFilter;
 import gov.nih.nci.cagrid.gums.ifs.bean.IFSUserRole;
 import gov.nih.nci.cagrid.gums.ifs.bean.IFSUserStatus;
-import gov.nih.nci.cagrid.gums.ifs.bean.InvalidPasswordFault;
 import gov.nih.nci.cagrid.gums.test.TestUtils;
 
-import java.io.File;
 import java.io.StringReader;
-import java.security.KeyPair;
-import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-import java.util.Date;
 
 import junit.framework.TestCase;
-
-import org.bouncycastle.asn1.x509.X509Name;
-import org.bouncycastle.jce.PKCS10CertificationRequest;
 
 /**
  * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
@@ -43,6 +32,8 @@ public class TestUserManager extends TestCase {
 			UserManager um = new UserManager(db, getOneYearConf(), TestUtils
 					.getCA(db));
 			
+			//Test adding user
+			
 			IFSUser user = new IFSUser();
 			user.setIdPId(1);
 			user.setUID("user");
@@ -57,6 +48,21 @@ public class TestUserManager extends TestCase {
 			X509Certificate cert = CertUtil.loadCertificate(ureader);
 			assertEquals(user.getGridId(),cert.getSubjectDN().getName());
 			
+			//Test Querying for users
+			IFSUserFilter f1 = new IFSUserFilter();
+			IFSUser[] l1 = um.getUsers(f1);
+			assertEquals(1,l1.length);
+			assertEquals(user,l1[0]);
+			
+			//Test querying by uid
+			IFSUserFilter f2 = new IFSUserFilter();
+			f2.setUID("nobody");
+			IFSUser[] l2 = um.getUsers(f2);
+			assertEquals(0,l2.length);
+			f2.setUID("use");
+			l2 = um.getUsers(f2);
+			assertEquals(1,l2.length);
+			assertEquals(user,l2[0]);
 		
 		} catch (Exception e) {
 			FaultUtil.printFault(e);
