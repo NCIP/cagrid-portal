7793fc8d3b99cbcfd66234476869f9c7be1e0595
hastings <hastings>
2005-12-07 17:48:02 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@283 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/Archive.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.cagrid.introduce;
+
+import java.io.File;
+
+
+public class Archive {
+
+	public static synchronized void create(String baseDir) {
+		File baseDir = new File();
+		
+		
+	}
+
+	public static synchronized void restoreFromLast(String baseDir) {
+
+	}
+
+}
