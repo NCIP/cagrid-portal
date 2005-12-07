39554618fb79184a1080cb6c70e6ced885498dc7
christman <christman>
2005-12-07 21:25:36 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@296 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/service/TestGUMS.java
+++ cagrid-1-0/caGrid/projects/dorian/test/gov/nih/nci/cagrid/dorian/service/TestGUMS.java
@@ -1,7 +1,11 @@
 package gov.nih.nci.cagrid.gums.service;
 
 import gov.nih.nci.cagrid.gums.common.FaultUtil;
+import gov.nih.nci.cagrid.gums.common.SimpleResourceManager;
+import gov.nih.nci.cagrid.gums.idp.IdPConfiguration;
+import gov.nih.nci.cagrid.gums.ifs.IFSConfiguration;
 import gov.nih.nci.cagrid.gums.test.TestUtils;
+import gov.nih.nci.cagrid.gums.ca.GUMSCertificateAuthorityConf;
 
 import java.io.File;
 
@@ -20,7 +24,6 @@ public class TestGUMS extends TestCase{
 	
     public void testGUMSManager(){
     	try{
-    		TestUtils.getCA();
     		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
     		assertNotNull(jm.getGUMSConfiguration());
     		assertNotNull(jm.getDatabase());
@@ -32,6 +35,121 @@ public class TestGUMS extends TestCase{
 		}
     }
     
-
-
+    public void testGUMSgetResource(){
+    	try{
+    		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
+    		assertNotNull(jm.getGUMSConfiguration());
+    		assertNotNull(jm.getDatabase());
+    		
+    		assertNotNull(jm.getResource(IdPConfiguration.RESOURCE));
+    		assertNotNull(jm.getResource(IFSConfiguration.RESOURCE));
+    		assertNotNull(jm.getResource(GUMSCertificateAuthorityConf.RESOURCE));
+    		
+    		assertEquals(0,jm.getDatabase().getUsedConnectionCount());
+    		jm.getDatabase().destroyDatabase();
+    	}catch (Exception e) {
+    		FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+    }
+    
+    public void testGUMSauthenticate(){
+    	try{
+    		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
+    		assertNotNull(jm.getGUMSConfiguration());
+    		assertNotNull(jm.getDatabase());
+    		
+    		//add in code to test authenticate
+    		
+    		
+    		
+    		assertEquals(0,jm.getDatabase().getUsedConnectionCount());
+    		jm.getDatabase().destroyDatabase();
+    	}catch (Exception e) {
+    		FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+    }
+ 
+    public void testGUMSfindIdPUsers(){
+    	try{
+    		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
+    		assertNotNull(jm.getGUMSConfiguration());
+    		assertNotNull(jm.getDatabase());
+    		
+    		//add in code to test findIdPUsers
+    		
+    		
+    		
+    		assertEquals(0,jm.getDatabase().getUsedConnectionCount());
+    		jm.getDatabase().destroyDatabase();
+    	}catch (Exception e) {
+    		FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+    }
+    
+    public void testGUMSregisterWithIdP(){
+    	try{
+    		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
+    		assertNotNull(jm.getGUMSConfiguration());
+    		assertNotNull(jm.getDatabase());
+    		
+    		//add in code to test registerWithIdP
+    		
+    		
+    		
+    		assertEquals(0,jm.getDatabase().getUsedConnectionCount());
+    		jm.getDatabase().destroyDatabase();
+    	}catch (Exception e) {
+    		FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+    }
+    
+    public void testGUMSremoveIdPUser(){
+    	try{
+    		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
+    		assertNotNull(jm.getGUMSConfiguration());
+    		assertNotNull(jm.getDatabase());
+    		
+    		//add in code to test removeIdPUser
+    
+    		
+    		
+    		assertEquals(0,jm.getDatabase().getUsedConnectionCount());
+    		jm.getDatabase().destroyDatabase();
+    	}catch (Exception e) {
+    		FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+    }
+    
+    public void testGUMSupdateIdPUser(){
+    	try{
+    		GUMS jm = new GUMS(RESOURCES_DIR+File.separator+"gums-conf.xml","localhost");
+    		assertNotNull(jm.getGUMSConfiguration());
+    		assertNotNull(jm.getDatabase());
+    		
+    		//add in code to test updateIdPUsers
+    		
+    		
+    		
+    		assertEquals(0,jm.getDatabase().getUsedConnectionCount());
+    		jm.getDatabase().destroyDatabase();
+    	}catch (Exception e) {
+    		FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+    }
+    
+    
+/*   protected void setUp() throws Exception {
+		super.setUp();
+	}
+    
+    protected void tearDown() throws Exception {
+		super.setUp();
+	}
+*/	
 }
