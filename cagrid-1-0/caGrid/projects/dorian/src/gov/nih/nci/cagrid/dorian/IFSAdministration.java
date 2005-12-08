3d71e23556a33b40037d0ca90fb0c2057af5aade
langella <langella>
2005-12-08 20:29:38 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@319 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/IFSAdministration.java
@@ -0,0 +1,20 @@
+package gov.nih.nci.cagrid.gums;
+
+import gov.nih.nci.cagrid.gums.bean.GUMSInternalFault;
+import gov.nih.nci.cagrid.gums.bean.PermissionDeniedFault;
+import gov.nih.nci.cagrid.gums.common.GUMSFault;
+import gov.nih.nci.cagrid.gums.ifs.bean.InvalidUserFault;
+import gov.nih.nci.cagrid.gums.ifs.bean.TrustedIdP;
+
+/**
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public interface IFSAdministration {
+	public TrustedIdP[] getTrustedIdPs() throws GUMSFault,
+			PermissionDeniedFault, InvalidUserFault, GUMSInternalFault;
+
+}
---
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/client/IFSAdministrationClient.java
@@ -0,0 +1,67 @@
+package gov.nih.nci.cagrid.gums.client;
+
+import gov.nih.nci.cagrid.gums.IFSAdministration;
+import gov.nih.nci.cagrid.gums.bean.GUMSInternalFault;
+import gov.nih.nci.cagrid.gums.bean.PermissionDeniedFault;
+import gov.nih.nci.cagrid.gums.common.FaultHelper;
+import gov.nih.nci.cagrid.gums.common.FaultUtil;
+import gov.nih.nci.cagrid.gums.common.GUMSFault;
+import gov.nih.nci.cagrid.gums.common.IOUtils;
+import gov.nih.nci.cagrid.gums.ifs.bean.InvalidUserFault;
+import gov.nih.nci.cagrid.gums.ifs.bean.TrustedIdP;
+import gov.nih.nci.cagrid.gums.wsrf.GUMSPortType;
+import gov.nih.nci.cagrid.gums.wsrf.IFSFindTrustedIdPs;
+import gov.nih.nci.cagrid.security.commstyle.CommunicationStyle;
+
+/**
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public class IFSAdministrationClient extends GUMSBaseClient implements
+		IFSAdministration {
+
+	CommunicationStyle style;
+
+	public IFSAdministrationClient(String serviceURI, CommunicationStyle style) {
+		super(serviceURI);
+		this.style = style;
+	}
+
+	public TrustedIdP[] getTrustedIdPs() throws GUMSFault,
+			PermissionDeniedFault, InvalidUserFault, GUMSInternalFault {
+
+		GUMSPortType port = null;
+		try {
+			port = this.getPort(style);
+		} catch (Exception e) {
+			GUMSFault fault = new GUMSFault();
+			fault.setFaultString(e.getMessage());
+			FaultHelper helper = new FaultHelper(fault);
+			helper.addFaultCause(e);
+			fault = (GUMSFault) helper.getFault();
+			throw fault;
+		}
+		try {
+			return port.getTrustedIdPs(new IFSFindTrustedIdPs()).getIdps();
+		} catch (GUMSInternalFault gie) {
+			throw gie;
+		} catch (PermissionDeniedFault f) {
+			throw f;
+		} catch (InvalidUserFault f) {
+			throw f;
+		} catch (Exception e) {
+			FaultUtil.printFault(e);
+			GUMSFault fault = new GUMSFault();
+			fault
+					.setFaultString(simplifyMessage(IOUtils
+							.getExceptionMessage(e)));
+			FaultHelper helper = new FaultHelper(fault);
+			helper.addFaultCause(e);
+			fault = (GUMSFault) helper.getFault();
+			throw fault;
+		}
+	}
+}
