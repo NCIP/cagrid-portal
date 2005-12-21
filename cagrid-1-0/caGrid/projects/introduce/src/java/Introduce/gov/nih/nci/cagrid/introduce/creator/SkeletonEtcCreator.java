2a47c15267a9b47d7462a0e650af59bd4ee9e469
hastings <hastings>
2005-12-21 02:19:57 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@459 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/creator/SkeletonEtcCreator.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/creator/SkeletonEtcCreator.java
@@ -1,31 +1,41 @@
 package gov.nih.nci.cagrid.introduce.creator;
 
 import gov.nih.nci.cagrid.introduce.templates.etc.RegistationTemplate;
+import gov.nih.nci.cagrid.introduce.templates.etc.SecurityDescTemplate;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Properties;
-
 
 public class SkeletonEtcCreator {
 
 	public SkeletonEtcCreator() {
 	}
 
-
 	public void createSkeleton(Properties properties) throws Exception {
-		File baseDirectory = new File(properties.getProperty("introduce.skeleton.destination.dir"));
+		File baseDirectory = new File(properties
+				.getProperty("introduce.skeleton.destination.dir"));
 
-		File etcDir = new File(baseDirectory.getAbsolutePath() + File.separator + "etc");
+		File etcDir = new File(baseDirectory.getAbsolutePath() + File.separator
+				+ "etc");
 		etcDir.mkdir();
-		
+
 		RegistationTemplate registrationT = new RegistationTemplate();
 		String registrationS = registrationT.generate(properties);
-		File registrationF = new File(etcDir.getAbsolutePath() + File.separator + "registration.xml");
-
+		File registrationF = new File(etcDir.getAbsolutePath() + File.separator
+				+ "registration.xml");
 		FileWriter registrationFW = new FileWriter(registrationF);
 		registrationFW.write(registrationS);
 		registrationFW.close();
+
+		SecurityDescTemplate securityDescT = new SecurityDescTemplate();
+		String securityDescS = securityDescT.generate(properties);
+		File securityDescF = new File(etcDir.getAbsolutePath() + File.separator
+				+ "security-desc.xml");
+		FileWriter securityDescFW = new FileWriter(securityDescF);
+		securityDescFW.write(securityDescS);
+		securityDescFW.close();
+
 	}
 
 }
