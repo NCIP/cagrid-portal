00414b75d4c84a0f1f2d919219ec1feeefada637
oster <oster>
2006-01-10 20:09:23 +0000

    start of templating multiple resource properties
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@522 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/codegen/metadata/MetadataTemplateUtils.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.cagrid.introduce.codegen.metadata;
+
+import gov.nih.nci.cagrid.introduce.beans.metadata.ServiceMetadataListType;
+import gov.nih.nci.cagrid.introduce.beans.metadata.ServiceMetadataType;
+
+
+public class MetadataTemplateUtils {
+
+	/**
+	 * Define a unique name for use as a variable for the metadata at the
+	 * specified index given the scope of the ServiceMetadataListType.
+	 * 
+	 * @param metadataList
+	 *            the list of metadata
+	 * @param index
+	 *            the index into the metadata list of the targeted metadata item
+	 * @return the variable name to use
+	 */
+	public static String getResourcePropertyVariableName(ServiceMetadataListType metadataList, int index) {
+		String baseName = metadataList.getMetadata(index).getQName().getLocalPart();
+
+		int previousNumber = 0;
+		for (int i = 0; (i < index && i < metadataList.getMetadata().length); i++) {
+			ServiceMetadataType metadata = metadataList.getMetadata()[i];
+			if (metadata.getQName().getLocalPart().equalsIgnoreCase(baseName)) {
+				// the qname local parts are the same for multiple qnames
+				// resolve the issue by appending a number
+				previousNumber++;
+			}
+		}
+
+		// return the orginal name, if it is unique, otherwise append a number
+		return baseName + ((previousNumber > 0) ? String.valueOf(previousNumber) : "");
+
+	}
+
+}
---
+++ cagrid-1-0/caGrid/projects/introduce/test/src/java/Introduce/gov/nih/nci/cagrid/introduce/codegen/metadata/MetadataTemplateUtilsTest.java
@@ -0,0 +1,67 @@
+package gov.nih.nci.cagrid.introduce.codegen.metadata;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import gov.nih.nci.cagrid.common.CommonTools;
+import gov.nih.nci.cagrid.introduce.beans.metadata.ServiceMetadataListType;
+import junit.framework.TestCase;
+
+
+public class MetadataTemplateUtilsTest extends TestCase {
+	public static String GOLD_DIRECTORY = "/test/resources/gold/";
+	public static String GOLD_FILE = "ServiceMetadata_Example.xml";
+
+	private ServiceMetadataListType metadataList = null;
+
+/*
+    <Metadata xmlns:a="http://ns1" className="AType" namespace="http://ns1" type="A" location="." populateFromFile="true" register="true" QName="a:A"/>
+	<Metadata xmlns:a="http://ns2" className="AType" namespace="http://ns2" type="A" location="." populateFromFile="true" register="true" QName="a:A"/>
+	<Metadata xmlns:a="http://ns1" className="BType" namespace="http://ns1" type="B" location="." populateFromFile="true" register="true" QName="b:B"/>
+	<Metadata xmlns:a="http://ns2" className="BType" namespace="http://ns2" type="B" location="." populateFromFile="false" register="false" QName="b:B"/>
+	<Metadata xmlns:a="http://ns3" className="BType" namespace="http://ns3" type="B" location="." populateFromFile="false" register="true" QName="b:B"/>
+*/
+	
+	
+	public void setUp() {
+		String pathtobasedir = System.getProperty("basedir", ".");
+		try {
+			metadataList = (ServiceMetadataListType) CommonTools.deserializeDocument(pathtobasedir + GOLD_DIRECTORY
+				+ GOLD_FILE, ServiceMetadataListType.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Error in setup:" + e.getMessage());
+		}
+		
+		assertNotNull(metadataList);
+		assertNotNull(metadataList.getMetadata());
+	}
+
+
+	public void testGetResourcePropertyVariableName() {
+		//make sure the pattern is right
+		String computedVarName1 = MetadataTemplateUtils.getResourcePropertyVariableName(metadataList, 0);
+		assertNotNull(computedVarName1);
+		assertTrue(computedVarName1.matches("([a-zA-Z])*"));
+		
+		//make sure the name is uniq when only the name space is different
+		String computedVarName2 = MetadataTemplateUtils.getResourcePropertyVariableName(metadataList, 1);
+		assertNotNull(computedVarName2);
+		assertFalse(computedVarName1.equals(computedVarName2));
+		assertTrue(computedVarName2.matches("([a-zA-Z])*[1-9]+"));
+		
+		//store all the names in a set to check for uniqueness 
+		Set names= new HashSet();
+		for (int i = 0; i < metadataList.getMetadata().length; i++) {
+			names.add(MetadataTemplateUtils.getResourcePropertyVariableName(metadataList, i));
+		}
+		//makes sure we got a unique name for all items
+		assertEquals(names.size(),metadataList.getMetadata().length);
+	}
+
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(MetadataTemplateUtilsTest.class);
+	}
+
+}
