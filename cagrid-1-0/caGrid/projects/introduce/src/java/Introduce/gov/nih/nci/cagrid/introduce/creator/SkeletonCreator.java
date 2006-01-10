e8cf76882be1afbf6cb3e8f331efe9481fb906c9
oster <oster>
2006-01-10 22:04:47 +0000

    ant is really nice.... and really crappy
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@531 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/creator/SkeletonCreator.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/creator/SkeletonCreator.java
@@ -11,57 +11,53 @@ import java.util.Properties;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
+
 public class SkeletonCreator extends Task {
 
 	public SkeletonCreator() {
 	}
 
+
 	public void execute() throws BuildException {
 		super.execute();
+
+		Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+
 		Properties properties = new Properties();
 		properties.putAll(this.getProject().getProperties());
-		
-		File baseDirectory = new File(properties.getProperty(
-		"introduce.skeleton.destination.dir"));
-		
+
+		File baseDirectory = new File(properties.getProperty("introduce.skeleton.destination.dir"));
+
 		ServiceMetadataListType metadatas = null;
 		MethodsType methods = null;
 		try {
-			methods = (MethodsType) CommonTools
-			.deserializeDocument(baseDirectory + File.separator
-					+ "introduceMethods.xml",
-					MethodsType.class);
-			metadatas = (ServiceMetadataListType) CommonTools
-			.deserializeDocument(baseDirectory + File.separator
-					+ "introduceMetadata.xml",
-					ServiceMetadataListType.class);
+			methods = (MethodsType) CommonTools.deserializeDocument(baseDirectory + File.separator
+				+ "introduceMethods.xml", MethodsType.class);
+			metadatas = (ServiceMetadataListType) CommonTools.deserializeDocument(baseDirectory + File.separator
+				+ "introduceMetadata.xml", ServiceMetadataListType.class);
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-		
+
 		ServiceInformation info = new ServiceInformation(methods, metadatas, properties);
-		
+
 		SkeletonBaseCreator sbc = new SkeletonBaseCreator();
 		SkeletonSourceCreator ssc = new SkeletonSourceCreator();
 		SkeletonSchemaCreator sscc = new SkeletonSchemaCreator();
 		SkeletonEtcCreator sec = new SkeletonEtcCreator();
 		SkeletonDocsCreator sdc = new SkeletonDocsCreator();
 
-		String service = properties
-				.getProperty("introduce.skeleton.service.name");
+		String service = properties.getProperty("introduce.skeleton.service.name");
 		if (!service.matches("[A-Z]++[A-Za-z0-9\\_\\$]*")) {
-			System.err
-					.println("Service Name can only contain [A-Z]++[A-Za-z0-9\\_\\$]*");
+			System.err.println("Service Name can only contain [A-Z]++[A-Za-z0-9\\_\\$]*");
 			return;
 		}
-		if (service.substring(0, 1).toLowerCase().equals(
-				service.substring(0, 1))) {
-			System.err
-					.println("Service Name cannnot start with lower case letters.");
+		if (service.substring(0, 1).toLowerCase().equals(service.substring(0, 1))) {
+			System.err.println("Service Name cannnot start with lower case letters.");
 			return;
 		}
 
-		//create the dirs to the basedir if needed
+		// create the dirs to the basedir if needed
 		baseDirectory.mkdirs();
 
 		// Generate the source
