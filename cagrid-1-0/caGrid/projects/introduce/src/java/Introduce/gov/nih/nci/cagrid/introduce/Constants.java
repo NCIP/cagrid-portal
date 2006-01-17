3da856bb4f79b7cf5f480b3a31c5d0632173dc47
hastings <hastings>
2006-01-17 18:53:43 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@577 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/Constants.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/Constants.java
@@ -4,6 +4,10 @@ import java.io.File;
 
 public class Constants {
 	
+	public final static String CACHE_POSTFIX = "_backup.zip";
+	
+	public final static String LAST_DIR_RESOURCE_FILE = "";
+	
 	public final static String getResourcePath(){
 		String userHome = System.getProperty("user.home");
 		File userHomeF = new File(userHome);
