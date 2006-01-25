6b515631e3aed7efe86404582104f4e45ed35032
hastings <hastings>
2006-01-25 14:44:10 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@660 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/introduce/test/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/IntroducePortalTest.java
@@ -0,0 +1,100 @@
+package gov.nih.nci.cagrid.introduce.portal;
+
+import gov.nih.nci.cagrid.introduce.TestCaseInfo;
+import gov.nih.nci.cagrid.introduce.portal.steps.CreateServiceStep;
+import gov.nih.nci.cagrid.introduce.portal.steps.ModifyServiceStep;
+
+import java.io.File;
+import java.util.Vector;
+
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.portal.GridPortal;
+import org.uispec4j.Trigger;
+import org.uispec4j.UISpec4J;
+import org.uispec4j.Window;
+import org.uispec4j.interception.WindowInterceptor;
+
+import com.atomicobject.haste.framework.Story;
+
+public class IntroducePortalTest extends Story {
+	private TestCaseInfo tci;
+
+	private Window mainWindow = null;
+
+	private GridPortal myApp;
+
+	private CreateServiceStep createService;
+	
+	private ModifyServiceStep modifyService;
+
+	protected Vector steps() {
+		System.out.println("TRYING TO SET THIS TEST UP.......................................");
+		UISpec4J.init();
+
+		mainWindow = WindowInterceptor.run(new Trigger() {
+			public void run() {
+				try {
+					
+					String pathtobasedir = System.getProperty("basedir");
+					System.out.println(pathtobasedir);
+					if (pathtobasedir == null) {
+						System.err.println("pathtobasedir system property not set");
+						throw new Exception("pathtobasedir system property not set");
+					}
+					
+					System.out.println("TRYING TO CREATE THE PORTAL WITH NO PROBLEMS...................");
+					System.out.println(pathtobasedir + File.separator +
+							"conf\\introduce\\introduce-portal-conf.xml");
+					System.err.println(new File(pathtobasedir + File.separator +
+							"conf\\introduce\\introduce-portal-conf.xml").exists());
+					myApp = new GridPortal(pathtobasedir + File.separator +
+							"conf\\introduce\\introduce-portal-conf.xml");
+					System.out.println("CREATED THE PORTAL WITH NO PROBLEMS...................");
+					myApp.show();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		System.out.println("DONE SETTING THIS TEST UP.......................................");
+
+		createService = new CreateServiceStep(mainWindow);
+		modifyService = new ModifyServiceStep(mainWindow);
+		this.tci = new TestCaseInfo();
+		Vector steps = new Vector();
+		steps.add(createService);
+		//steps.add(modifyService);
+		return steps;
+	}
+
+	public String getDescription() {
+		return "Tests the code generation tools";
+	}
+
+	protected void storyTearDown() throws Throwable {
+		//myApp.setVisible(false);
+	}
+
+	// used to make sure that if we are going to use a junit testsuite to test
+	// this
+	// that the test suite will not error out looking for a single test......
+	public void testDummy() throws Throwable {
+	}
+
+	/**
+	 * Convenience method for running all the Steps in this Story.
+	 */
+	public static void main(String args[]) {
+		TestRunner runner = new TestRunner();
+		TestResult result = runner.doRun(new TestSuite(
+				IntroducePortalTest.class));
+		System.exit(result.errorCount() + result.failureCount());
+	}
+
+}
---
+++ cagrid-1-0/caGrid/projects/introduce/test/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/steps/CreateServiceStep.java
@@ -0,0 +1,63 @@
+package gov.nih.nci.cagrid.introduce.portal.steps;
+
+import java.awt.Component;
+
+import org.projectmobius.portal.GridPortal;
+import org.projectmobius.portal.PortalResourceManager;
+import org.uispec4j.Panel;
+import org.uispec4j.Trigger;
+import org.uispec4j.Window;
+import org.uispec4j.finder.ComponentMatcher;
+import org.uispec4j.interception.WindowHandler;
+import org.uispec4j.interception.WindowInterceptor;
+
+import com.atomicobject.haste.framework.Step;
+
+public class CreateServiceStep extends Step {
+	Window mainWindow;
+
+	public CreateServiceStep(Window mainWindow) {
+		this.mainWindow = mainWindow;
+	}
+
+	public void runStep() throws Throwable {
+		System.out.println("Creating a service");
+		mainWindow.getMenuBar().getMenu("Tools").getSubMenu(
+				"Create New Service").click();
+		Thread.sleep(3000);
+		GridPortal portal = PortalResourceManager.getInstance().getGridPortal();
+		Panel panel = new Panel(portal.getLastComponent());
+		System.out.println(panel.getDescription());
+		WindowInterceptor.init(panel.getButton("Create").triggerClick())
+				.process(new WindowHandler() {
+					public Trigger process(Window window) {
+						int count = 0;
+						while (window.isVisible()) {
+							try {
+								Thread.sleep(3);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							count++;
+							if (count >= 100) {
+								assertFalse(
+										"Service Creation took longer than 300 seconds....",
+										false);
+							}
+						}
+						// return empty trigger because this modal dialog closes
+						// itself
+						return new Trigger() {
+							public void run() throws Exception {
+							}
+						};
+					}
+				}).run();
+
+		//close out the modification panel the pops up.....
+		Panel modificationPanel = new Panel(portal.getLastComponent());
+		modificationPanel.getButton("Cancel").click();
+	}
+
+}
