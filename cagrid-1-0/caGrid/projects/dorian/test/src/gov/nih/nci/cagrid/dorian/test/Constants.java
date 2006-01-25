4af8c4df915b10c3c80b9618de7e76a1851392bb
christman <christman>
2006-01-25 15:35:50 +0000

    Added class to hold all constants for all the junit tests.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@669 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/dorian/test/src/gov/nih/nci/cagrid/dorian/test/Constants.java
@@ -0,0 +1,29 @@
+package gov.nih.nci.cagrid.dorian.test;
+
+public class Constants {
+	
+	public static String BMI_CACERT = "/ca-test/bmi-cacert.pem";
+	
+	public static String BMI_CAKEY = "/ca-test/bmi-cakey.pem";
+	
+	public static String DORIAN_CERT = "/ca-test/dorian-cert.pem";
+	
+	public static String DORIAN_KEY = "/ca-test/dorian-key.pem";
+	
+	public static String SIMPLECA_CACERT = "/ca-test/simpleca-cacert.pem";
+	
+	public static String SIMPLECA_CAKEY = "/ca-test/simpleca-cakey.pem";
+	
+	public static String DB_CONFIG = "/general-test/db-config.xml";
+	
+	public static String DORIAN_CONF = "/general-test/dorian-conf.xml";
+	
+	public static String IDP_CONFIG = "/general-test/idp-config.xml";
+	
+	public static String IDP_CONFIG2 = "/general-test/idp-config2.xml";
+	
+	public static String IFS_CONF = "/general-test/ifs-conf.xml";
+	
+	public static String BAD_KEY = "/general-test/BADKEY";
+
+}
