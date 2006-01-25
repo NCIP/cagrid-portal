832aa89c76b53b6b27cecf4cca70ab7c61d99e55
oster <oster>
2006-01-25 21:59:49 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@699 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dorian/test/src/gov/nih/nci/cagrid/dorian/idp/TestIdPConfiguration.java
+++ cagrid-1-0/caGrid/projects/dorian/test/src/gov/nih/nci/cagrid/dorian/idp/TestIdPConfiguration.java
@@ -6,10 +6,10 @@ import gov.nih.nci.cagrid.dorian.common.ca.CertUtil;
 import gov.nih.nci.cagrid.dorian.common.ca.KeyUtil;
 import gov.nih.nci.cagrid.dorian.test.Constants;
 
-import java.io.File;
 import java.io.InputStream;
 
 import junit.framework.TestCase;
+
 
 /**
  * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
@@ -19,83 +19,69 @@ import junit.framework.TestCase;
  *          Exp $
  */
 public class TestIdPConfiguration extends TestCase {
-	
+
 	public final static int DEFAULT_MIN_PASSWORD_LENGTH = 6;
 
-	
 	public final static int DEFAULT_MAX_PASSWORD_LENGTH = 10;
 
-	
 	public final static int DEFAULT_MIN_UID_LENGTH = 4;
 
-	
 	public final static int DEFAULT_MAX_UID_LENGTH = 10;
-	
+
 	public final static String DEFAULT_KEY_PASSWORD = "idpkey";
 
+	private final static String DEFAULT_REGISTRATION_POLICY = ManualRegistrationPolicy.class.getName();
 
-	
-	private final static String DEFAULT_REGISTRATION_POLICY = ManualRegistrationPolicy.class
-			.getName();
 
 	public void testAutoConfiguration() {
 		try {
 			InputStream resource = TestCase.class.getResourceAsStream(Constants.IDP_CONFIG);
 			SimpleResourceManager trm = new SimpleResourceManager(resource);
-			IdPConfiguration conf = (IdPConfiguration)trm.getResource(IdPConfiguration.RESOURCE);
-		
-			assertEquals(DEFAULT_MIN_PASSWORD_LENGTH, conf
-					.getMinimumPasswordLength());
-			assertEquals(DEFAULT_MAX_PASSWORD_LENGTH, conf
-					.getMaximumPasswordLength());
-			assertEquals(DEFAULT_MIN_UID_LENGTH, conf
-					.getMinimumUIDLength());
-			assertEquals(DEFAULT_MAX_UID_LENGTH, conf
-					.getMaximumUIDLength());
+			IdPConfiguration conf = (IdPConfiguration) trm.getResource(IdPConfiguration.RESOURCE);
+
+			assertEquals(DEFAULT_MIN_PASSWORD_LENGTH, conf.getMinimumPasswordLength());
+			assertEquals(DEFAULT_MAX_PASSWORD_LENGTH, conf.getMaximumPasswordLength());
+			assertEquals(DEFAULT_MIN_UID_LENGTH, conf.getMinimumUIDLength());
+			assertEquals(DEFAULT_MAX_UID_LENGTH, conf.getMaximumUIDLength());
 			assertEquals(DEFAULT_REGISTRATION_POLICY, conf.getRegistrationPolicy().getClass().getName());
-			assertEquals(DEFAULT_KEY_PASSWORD,conf.getKeyPassword());
-		
-			assertEquals(true,conf.isAutoCreateAssertingCredentials());
-			assertEquals(true,conf.isAutoRenewAssertingCredentials());
-			assertEquals(null,conf.getAssertingCertificate());
-			assertEquals(null,conf.getAssertingKey());
-			
-		
+			assertEquals(DEFAULT_KEY_PASSWORD, conf.getKeyPassword());
+
+			assertEquals(true, conf.isAutoCreateAssertingCredentials());
+			assertEquals(true, conf.isAutoRenewAssertingCredentials());
+			assertEquals(null, conf.getAssertingCertificate());
+			assertEquals(null, conf.getAssertingKey());
+
 		} catch (Exception e) {
 			FaultUtil.printFault(e);
 			assertTrue(false);
 		}
 	}
+
+
 	public void testConfiguration() {
 		try {
 			InputStream resource = TestCase.class.getResourceAsStream(Constants.IDP_CONFIG2);
 			SimpleResourceManager trm = new SimpleResourceManager(resource);
-			IdPConfiguration conf = (IdPConfiguration)trm.getResource(IdPConfiguration.RESOURCE);
-		
-			assertEquals(DEFAULT_MIN_PASSWORD_LENGTH, conf
-					.getMinimumPasswordLength());
-			assertEquals(DEFAULT_MAX_PASSWORD_LENGTH, conf
-					.getMaximumPasswordLength());
-			assertEquals(DEFAULT_MIN_UID_LENGTH, conf
-					.getMinimumUIDLength());
-			assertEquals(DEFAULT_MAX_UID_LENGTH, conf
-					.getMaximumUIDLength());
+			IdPConfiguration conf = (IdPConfiguration) trm.getResource(IdPConfiguration.RESOURCE);
+
+			assertEquals(DEFAULT_MIN_PASSWORD_LENGTH, conf.getMinimumPasswordLength());
+			assertEquals(DEFAULT_MAX_PASSWORD_LENGTH, conf.getMaximumPasswordLength());
+			assertEquals(DEFAULT_MIN_UID_LENGTH, conf.getMinimumUIDLength());
+			assertEquals(DEFAULT_MAX_UID_LENGTH, conf.getMaximumUIDLength());
 			assertEquals(DEFAULT_REGISTRATION_POLICY, conf.getRegistrationPolicy().getClass().getName());
-			assertEquals(null,conf.getKeyPassword());
-		
-			assertEquals(false,conf.isAutoCreateAssertingCredentials());
-			assertEquals(false,conf.isAutoRenewAssertingCredentials());
+			assertEquals(null, conf.getKeyPassword());
+
+			assertEquals(false, conf.isAutoCreateAssertingCredentials());
+			assertEquals(false, conf.isAutoRenewAssertingCredentials());
 			resource = TestCase.class.getResourceAsStream(Constants.DORIAN_CERT);
-			assertEquals(CertUtil.loadCertificate(resource),conf.getAssertingCertificate());
+			assertEquals(CertUtil.loadCertificate(resource), conf.getAssertingCertificate());
 			resource = TestCase.class.getResourceAsStream(Constants.DORIAN_KEY);
-			assertEquals(KeyUtil.loadPrivateKey(resource,conf.getKeyPassword()),conf.getAssertingKey());
-			
-		
+			assertEquals(KeyUtil.loadPrivateKey(resource, conf.getKeyPassword()), conf.getAssertingKey());
+
 		} catch (Exception e) {
 			FaultUtil.printFault(e);
 			assertTrue(false);
-		} 
+		}
 	}
-	
 
 }
