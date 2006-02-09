d544f9f8241544857fc13de8f929b2b1e10811e2
hastings <hastings>
2006-02-09 17:29:10 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@799 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/IntroducePortalConf.java
@@ -0,0 +1,28 @@
+package gov.nih.nci.cagrid.introduce.portal;
+
+import org.jdom.Element;
+import org.projectmobius.common.AbstractMobiusConfiguration;
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.common.MobiusResourceManager;
+
+/**
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public class IntroducePortalConf implements AbstractMobiusConfiguration {
+
+	public static String RESOURCE = "IntroducePortalConf";
+
+
+	public IntroducePortalConf() {
+	}
+
+	public void parse(MobiusResourceManager resourceManager, Element config)
+			throws MobiusException {
+
+	}
+
+}
