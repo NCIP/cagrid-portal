9016cc669904c65e45974343dedff228f28a350f
hastings <hastings>
2006-02-09 17:09:14 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@797 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/modification/CADSRClassDescriptor.java
@@ -0,0 +1,59 @@
+package gov.nih.nci.cagrid.introduce.portal.modification;
+
+public class CADSRClassDescriptor {
+	
+	private String context;
+	private String classificationScheme;
+	private String version;
+	private String schemeItem;
+
+	public CADSRClassDescriptor(String context, String classificationScheme, String version, String schemeItem) {
+		super();
+		// TODO Auto-generated constructor stub
+		this.context = context;
+		this.classificationScheme = classificationScheme;
+		this.version = version;
+		this.schemeItem = schemeItem;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getClassificationScheme() {
+		return classificationScheme;
+	}
+
+	public void setClassificationScheme(String classificationScheme) {
+		this.classificationScheme = classificationScheme;
+	}
+
+	public String getContext() {
+		return context;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+	}
+
+	public String getSchemeItem() {
+		return schemeItem;
+	}
+
+	public void setSchemeItem(String schemeItem) {
+		this.schemeItem = schemeItem;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+}
