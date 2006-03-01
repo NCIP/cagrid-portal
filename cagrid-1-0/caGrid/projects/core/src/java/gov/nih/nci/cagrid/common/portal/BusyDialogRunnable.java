d329aa240bbbb8cb33e3f712df0c9e1143c195c3
hastings <hastings>
2006-03-01 15:54:14 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@969 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
@@ -3,21 +3,25 @@ package gov.nih.nci.cagrid.common.portal;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+
 public abstract class BusyDialogRunnable implements Runnable {
 
 	final private BusyDialog dialog;
+
 
 	public BusyDialogRunnable(JFrame owner, String title) {
 		dialog = new BusyDialog(owner, "Progress (" + title + ")", this);
 	}
 
+
 	public void setProgressText(final String text) {
 		SwingUtilities.invokeLater(new Runnable() {
-	        public void run() {
-	        	dialog.getProgress().setString(text);
-	        }
-	    });
+			public void run() {
+				dialog.getProgress().setString(text);
+			}
+		});
 	}
+
 
 	public void run() {
 		Thread thread = new Thread(new Runnable() {
@@ -32,6 +36,7 @@ public abstract class BusyDialogRunnable implements Runnable {
 		setProgressText("");
 		dialog.setVisible(false);
 	}
+
 
 	public abstract void process();
 
