83852b38e3865d2cea32cee1246f5a79f054d4c0
langella <langella>
2006-03-14 18:41:04 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1131 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/service/PermissionManager.java
+++ cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/service/PermissionManager.java
@@ -1,8 +1,14 @@
 package gov.nih.nci.cagrid.gts.service;
 
+import gov.nih.nci.cagrid.gts.bean.Permission;
 import gov.nih.nci.cagrid.gts.common.Database;
 import gov.nih.nci.cagrid.gts.stubs.GTSInternalFault;
+import gov.nih.nci.cagrid.gts.stubs.IllegalPermissionFault;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -23,10 +29,95 @@ public class PermissionManager {
 
 	private Database db;
 
+	private String ALL_TRUST_AUTHORITIES = "*";
+
 
 	public PermissionManager(Database db) {
 		logger = Logger.getLogger(this.getClass().getName());
 		this.db = db;
+	}
+
+
+	public synchronized void addPermission(Permission p) throws GTSInternalFault, IllegalPermissionFault {
+
+		// This method assumes that any Trusted Authorites associated with a
+		// permission is valid
+		this.buildDatabase();
+
+		if (this.doesPermissionExist(p)) {
+			IllegalPermissionFault fault = new IllegalPermissionFault();
+			fault.setFaultString("The permission " + formatPermission(p) + " cannot be added, it already exists.");
+			throw fault;
+		}
+
+		if (p.getGridIdentity() == null) {
+			IllegalPermissionFault fault = new IllegalPermissionFault();
+			fault.setFaultString("The permission " + formatPermission(p) + " no grid identity specified.");
+			throw fault;
+		}
+
+		if (p.getRole() == null) {
+			IllegalPermissionFault fault = new IllegalPermissionFault();
+			fault.setFaultString("The permission " + formatPermission(p) + " no role specified.");
+			throw fault;
+		}
+
+		if (p.getTrustedAuthorityName() == null) {
+			p.setTrustedAuthorityName(ALL_TRUST_AUTHORITIES);
+		}
+
+		StringBuffer insert = new StringBuffer();
+		try {
+			insert.append("INSERT INTO " + PERMISSIONS_TABLE + " SET GRID_IDENTITY='" + p.getGridIdentity()
+				+ "',ROLE='" + p.getRole().getValue() + "',TRUSTED_AUTHORITY='" + p.getTrustedAuthorityName() + "'");
+
+			db.update(insert.toString());
+
+		} catch (Exception e) {
+			this.logger.log(Level.SEVERE, "Unexpected database error incurred in adding the permission "+formatPermission(p)
+				+ ", the following statement generated the error: \n"
+				+ insert.toString() + "\n", e);
+			GTSInternalFault fault = new GTSInternalFault();
+			fault.setFaultString("Unexpected error adding the permission " + formatPermission(p)
+				+ "!!!");
+			throw fault;
+		}
+	}
+
+
+	public synchronized boolean doesPermissionExist(Permission p) throws GTSInternalFault {
+		String sql = "select count(*) from " + PERMISSIONS_TABLE + " where GRID_IDENTITY='" + p.getGridIdentity()
+			+ "' AND ROLE='" + p.getRole().getValue() + "' AND TRUSTED_AUTHORITY='" + p.getTrustedAuthorityName() + "'";
+		Connection c = null;
+		boolean exists = false;
+		try {
+			c = db.getConnection();
+			Statement s = c.createStatement();
+			ResultSet rs = s.executeQuery(sql);
+			if (rs.next()) {
+				int count = rs.getInt(1);
+				if (count > 0) {
+					exists = true;
+				}
+			}
+			rs.close();
+			s.close();
+		} catch (Exception e) {
+			String perm = formatPermission(p);
+			this.logger.log(Level.SEVERE, "Unexpected database error incurred in determining if the permission " + perm
+				+ " exists, the following statement generated the error: \n" + sql + "\n", e);
+			GTSInternalFault fault = new GTSInternalFault();
+			fault.setFaultString("Unexpected error in determining if the permission " + perm + " exists.");
+			throw fault;
+		} finally {
+			db.releaseConnection(c);
+		}
+		return exists;
+	}
+
+
+	private String formatPermission(Permission p) {
+		return "[" + p.getGridIdentity() + "," + p.getRole().getValue() + "," + p.getTrustedAuthorityName() + "]";
 	}
 
 
--- cagrid-1-0/caGrid/projects/gts/test/src/gov/nih/nci/cagrid/gts/service/TestPermissionManager.java
+++ cagrid-1-0/caGrid/projects/gts/test/src/gov/nih/nci/cagrid/gts/service/TestPermissionManager.java
@@ -1,6 +1,8 @@
 package gov.nih.nci.cagrid.gts.service;
 
 import gov.nih.nci.cagrid.common.FaultUtil;
+import gov.nih.nci.cagrid.gts.bean.Permission;
+import gov.nih.nci.cagrid.gts.bean.Role;
 import gov.nih.nci.cagrid.gts.common.Database;
 import gov.nih.nci.cagrid.gts.test.Utils;
 import junit.framework.TestCase;
@@ -21,6 +23,27 @@ public class TestPermissionManager extends TestCase {
 			PermissionManager pm = new PermissionManager(db);
 			pm.buildDatabase();
 			pm.destroy();
+		} catch (Exception e) {
+			FaultUtil.printFault(e);
+			assertTrue(false);
+		}
+	}
+	
+	public void testAddPermission() {
+		try {
+			PermissionManager pm = new PermissionManager(db);
+			Permission p1 = new Permission();
+			p1.setGridIdentity("O=Test Organization,OU=Test Unit,CN=User");
+			p1.setRole(Role.TrustServiceAdmin);
+			pm.addPermission(p1);
+			assertTrue(pm.doesPermissionExist(p1));
+			
+			Permission p2 = new Permission();
+			p2.setGridIdentity("O=Test Organization,OU=Test Unit,CN=User");
+			p2.setRole(Role.TrustAuthorityManager);
+			p2.setTrustedAuthorityName("O=Test Organization,OU=Test Unit,CN=CA");
+			pm.addPermission(p2);
+			assertTrue(pm.doesPermissionExist(p2));
 		} catch (Exception e) {
 			FaultUtil.printFault(e);
 			assertTrue(false);
