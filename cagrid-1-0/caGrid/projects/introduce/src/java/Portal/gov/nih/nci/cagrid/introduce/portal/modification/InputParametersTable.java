314ab898f557d066a58bbf73cab4d2ed608fe565
oster <oster>
2006-03-22 20:14:54 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1273 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/modification/InputParametersTable.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/modification/InputParametersTable.java
@@ -1,42 +1,28 @@
 package gov.nih.nci.cagrid.introduce.portal.modification;
 
-import gov.nih.nci.cagrid.common.portal.PortalBaseTable;
 import gov.nih.nci.cagrid.introduce.beans.method.MethodType;
 import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputsInput;
 
 import java.util.Vector;
 
-import javax.swing.DefaultCellEditor;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 import javax.xml.namespace.QName;
-
-import org.uispec4j.ComboBox;
 
 
 /**
  * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Hastings </A>
  * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster </A>
  * @author <A HREF="MAILTO:langella@bmi.osu.edu">Stephen Langella </A>
- * 
  */
 public class InputParametersTable extends JTable {
 
 	public static String NAME = "Name";
-	
 	public static String ISARRAY = "Is Array";
-
 	public static String NAMESPACE = "Namespace";
-
 	public static String TYPE = "Type";
-
 	public static String DATA1 = "DATA1";
-
+	
 	private MethodType method;
 
 
@@ -44,12 +30,12 @@ public class InputParametersTable extends JTable {
 		super();
 		this.setModel(createTableModel());
 		this.method = method;
-		//this.setColumnSelectionAllowed(true);
-		//this.setRowSelectionAllowed(true);
-		//this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		//TableColumn col = getColumnModel().getColumn(1);
-	    //col.setCellEditor(new DefaultCellEditor(new JCheckBox()));
-	    initialize();
+		// this.setColumnSelectionAllowed(true);
+		// this.setRowSelectionAllowed(true);
+		// this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		// TableColumn col = getColumnModel().getColumn(1);
+		// col.setCellEditor(new DefaultCellEditor(new JCheckBox()));
+		initialize();
 	}
 
 
@@ -79,7 +65,7 @@ public class InputParametersTable extends JTable {
 		}
 		Vector v = (Vector) getValueAt(getSelectedRow(), 4);
 		v.set(0, input.getName());
-		v.set(1,new Boolean(input.isIsArray()));
+		v.set(1, new Boolean(input.isIsArray()));
 		v.set(2, input.getQName().getNamespaceURI());
 		v.set(3, input.getQName().getLocalPart());
 		v.set(4, v);
@@ -107,19 +93,19 @@ public class InputParametersTable extends JTable {
 			throw new Exception("invalid row");
 		}
 		MethodTypeInputsInput input = new MethodTypeInputsInput();
-		
+
 		String name = ((String) getValueAt(row, 0));
 		boolean isArray = ((Boolean) getValueAt(row, 1)).booleanValue();
 		String namespace = ((String) getValueAt(row, 2));
 		String type = ((String) getValueAt(row, 3));
 
-			input.setIsArray(isArray);
-	
+		input.setIsArray(isArray);
+
 		if (name != null && !name.equals("")) {
 			input.setName(name);
 		}
 		if (namespace != null && !namespace.equals("") && type != null && !type.equals("")) {
-			input.setQName(new QName(namespace,type));
+			input.setQName(new QName(namespace, type));
 		}
 
 		return input;
@@ -159,10 +145,11 @@ public class InputParametersTable extends JTable {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+
 	public class MyDefaultTableModel extends DefaultTableModel {
-		
-		public MyDefaultTableModel(){
+
+		public MyDefaultTableModel() {
 			super();
 			addColumn(NAME);
 			addColumn(ISARRAY);
@@ -170,8 +157,6 @@ public class InputParametersTable extends JTable {
 			addColumn(TYPE);
 			addColumn(DATA1);
 		}
-		
-		
+
 	}
 }
-
