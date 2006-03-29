436558e9b215541f6ffdaa310b61cd378c855ae4
langella <langella>
2006-03-29 21:28:11 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1436 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/portal/PermissionPanel.java
+++ cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/portal/PermissionPanel.java
@@ -17,7 +17,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
+/**
+ * @author <A HREF="MAILTO:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Langella </A>
+ * @version $Id: TrustedAuthoritiesWindow.java,v 1.2 2006/03/27 19:05:40
+ *          langella Exp $
+ */
 public class PermissionPanel extends JPanel {
 
 	public final static String ANY = "Any";
