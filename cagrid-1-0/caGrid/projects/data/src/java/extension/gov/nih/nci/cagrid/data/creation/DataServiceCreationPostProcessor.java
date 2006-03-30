05d747d6e08c220444502b34999aa7bd52811cb7
dervin <dervin>
2006-03-30 18:56:22 +0000

    getting a start on the data service post-creation processor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1454 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/DataServiceCreationPostProcessor.java
@@ -0,0 +1,147 @@
+package gov.nih.nci.cagrid.data.creation;
+
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.common.portal.PortalUtils;
+import gov.nih.nci.cagrid.introduce.IntroduceConstants;
+import gov.nih.nci.cagrid.introduce.beans.ServiceDescription;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodType;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputs;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputsInput;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeOutput;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodsType;
+import gov.nih.nci.cagrid.introduce.beans.namespace.NamespaceType;
+import gov.nih.nci.cagrid.introduce.beans.namespace.NamespacesType;
+import gov.nih.nci.cagrid.introduce.common.CommonTools;
+import gov.nih.nci.cagrid.introduce.creator.extension.CreationExtensionException;
+import gov.nih.nci.cagrid.introduce.creator.extension.CreationExtensionPostProcessor;
+import gov.nih.nci.cagrid.introduce.portal.IntroducePortalConf;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
+import javax.xml.namespace.QName;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+import org.projectmobius.client.gme.ImportInfo;
+import org.projectmobius.common.GridServiceResolver;
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.common.Namespace;
+import org.projectmobius.gme.XMLDataModelService;
+import org.projectmobius.gme.client.GlobusGMEXMLDataModelServiceFactory;
+import org.projectmobius.portal.PortalResourceManager;
+
+/** 
+ *  DataServiceCreationPostProcessor
+ *  Creation post-processor for data services
+ * 
+ * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
+ * 
+ * @created Mar 29, 2006 
+ * @version $Id$ 
+ */
+public class DataServiceCreationPostProcessor extends CreationExtensionPostProcessor {
+	
+	public static final String CQL_QUERY_URI = "http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery";
+	public static final String CQL_RESULT_SET_URI = "http://CQL.caBIG/3/gov.nih.nci.cagrid.CQLResultSet";
+	
+	public DataServiceCreationPostProcessor(Properties serviceProperties) {
+		super(serviceProperties);
+	}
+
+
+	public void postCreate() throws CreationExtensionException {
+		// builds on the introduce template file to add the required
+		// query method for all data services
+		
+	}
+	
+	
+	private ServiceDescription getDataServiceDescription() throws Exception {
+		ServiceDescription dataService = new ServiceDescription();
+		// grab cql query and result set schemas from GME
+		String schemaDir = getServiceSchemaDir();
+		String cqlQuerySchemaLocation = cacheSchema(CQL_QUERY_URI, schemaDir);
+		String cqlResultSetSchemaLocation = cacheSchema(CQL_RESULT_SET_URI, schemaDir);
+		// namespaces
+		NamespacesType namespaces = new NamespacesType();
+		NamespaceType[] namespaceTypes = new NamespaceType[2];
+		namespaceTypes[0] = CommonTools.createNamespaceType(schemaDir + File.separator + cqlQuerySchemaLocation);
+		namespaceTypes[0].setLocation("." + File.separator + cqlQuerySchemaLocation);
+		namespaceTypes[1] = CommonTools.createNamespaceType(schemaDir + File.separator + cqlResultSetSchemaLocation);
+		namespaceTypes[1].setLocation("." + File.separator + cqlResultSetSchemaLocation);
+		namespaces.setNamespace(namespaceTypes);
+		dataService.setNamespaces(namespaces);
+		
+		// query method
+		MethodsType methods = new MethodsType();
+		MethodType queryMethod = new MethodType();
+		queryMethod.setName("query");
+		// method input parameters
+		MethodTypeInputs inputs = new MethodTypeInputs();
+		MethodTypeInputsInput queryInput = new MethodTypeInputsInput();
+		queryInput.setName("cqlQuery");
+		queryInput.setIsArray(false);
+		QName queryQname = new QName(namespaceTypes[0].getNamespace(), namespaceTypes[0].getSchemaElement(0).getType());
+		queryInput.setQName(queryQname);
+		inputs.setInput(new MethodTypeInputsInput[] {queryInput});
+		queryMethod.setInputs(inputs);
+		// method output
+		MethodTypeOutput output = new MethodTypeOutput();
+		output.setIsArray(false);
+		QName resultSetQName = new QName(namespaceTypes[1].getNamespace(), namespaceTypes[1].getSchemaElement(0).getType());
+		output.setQName(resultSetQName);
+		queryMethod.setOutput(output);
+		methods.setMethod(new MethodType[] {queryMethod});
+		dataService.setMethods(methods);
+		
+		return dataService;
+	}
+	
+	
+	private String getServiceSchemaDir() {
+		return getServiceProperties().getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR) + File.separator + "schema";
+	}
+	
+	
+	private String cacheSchema(String uri, String directory) throws Exception {
+		System.out.println("Retrieving schema " + uri);
+		String cachedFilename = null;
+		IntroducePortalConf conf = (IntroducePortalConf) PortalResourceManager.getInstance().getResource(
+			IntroducePortalConf.RESOURCE);
+		GridServiceResolver.getInstance().setDefaultFactory(new GlobusGMEXMLDataModelServiceFactory());
+		try {
+			XMLDataModelService handle = (XMLDataModelService) GridServiceResolver.getInstance().getGridService(
+				conf.getGME());
+			List retrievedNamespaces = handle.cacheSchema(new Namespace(uri), new File(directory));
+			// determine which of those files is the one we originally asked for
+			for (int i = 0; i < retrievedNamespaces.size(); i++) {
+				ImportInfo nsInfo = new ImportInfo((Namespace) retrievedNamespaces.get(i));
+				String xsdFilename = directory + File.separator + nsInfo.getFileName();
+				String targetNamespace = extractTargetNamespace(xsdFilename);
+				Namespace requestedNs = new Namespace(uri);
+				Namespace targetNs = new Namespace(targetNamespace);
+				if (requestedNs.getNamespace().equals(targetNs.getNamespace())) {
+					cachedFilename = xsdFilename;
+					break;
+				}
+			}
+		} catch (MobiusException ex) {
+			ex.printStackTrace();
+			PortalUtils.showErrorMessage("Could not retrieve schema from GME!  Please check the GME URL and make sure that you have the appropriate credentials!");
+		}
+		return cachedFilename;
+	}
+	
+	
+	private String extractTargetNamespace(String filename) throws Exception {
+		String fileContents = Utils.fileToStringBuffer(new File(filename)).toString();
+		ByteArrayInputStream stream = new ByteArrayInputStream(fileContents.getBytes());
+		SAXBuilder builder = new SAXBuilder(false);
+		Document doc = builder.build(stream);
+		stream.close();
+		return doc.getRootElement().getAttributeValue("targetNamespace");
+	}
+}
