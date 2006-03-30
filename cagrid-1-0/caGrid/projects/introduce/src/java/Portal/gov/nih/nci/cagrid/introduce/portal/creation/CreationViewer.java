09ef56ba9c8b3b6b88f816d5f9a3d9e0e1acda7d
dervin <dervin>
2006-03-30 21:05:45 +0000

    removed unused constants
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1471 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/creation/CreationViewer.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/creation/CreationViewer.java
@@ -44,11 +44,9 @@ import org.projectmobius.portal.PortalResourceManager;
  */
 public class CreationViewer extends GridPortalComponent {
 
+	public static final String ALLOWED_SERVICE_NAME_REGEX = "[A-Z]++[A-Za-z0-9\\_\\$]*";
+
 	public static final String SCHEMA_DIR = "schema";
-
-	public static final String CQL_RESULT_SET_SCHEMA = "3_gov.nih.nci.cagrid.CQLResultSet.xsd";
-
-	public static final String CQL_QUERY_SCHEMA = "1_gov.nih.nci.cagrid.CQLQuery.xsd";
 
 	private static String DEFAULT_NAME = "HelloWorld";
 
@@ -572,8 +570,8 @@ public class CreationViewer extends GridPortalComponent {
 						// String templateFilename =
 						// getMethodsTemplateFile().getText();
 						if (serviceName.length() > 0) {
-							if (!serviceName.matches("[A-Z]++[A-Za-z0-9\\_\\$]*")) {
-								PortalUtils.showMessage("Service Name can only contain [A-Z]++[A-Za-z0-9\\_\\$]*");
+							if (!serviceName.matches(ALLOWED_SERVICE_NAME_REGEX)) {
+								PortalUtils.showMessage("Service Name can only contain " + ALLOWED_SERVICE_NAME_REGEX);
 								return;
 							}
 							if (serviceName.substring(0, 1).toLowerCase().equals(serviceName.substring(0, 1))) {
