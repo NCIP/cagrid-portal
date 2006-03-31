c2ef5d48997a9520658c37465e71ed0605073d14
oster <oster>
2006-03-31 00:13:05 +0000

    removed support for default namespace, and changed to using a basic map
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1481 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/XPathUtils.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/XPathUtils.java
@@ -1,65 +1,38 @@
 package gov.nih.nci.cagrid.common;
 
 import java.util.Iterator;
-
-import org.projectmobius.common.Namespace;
-import org.projectmobius.common.NamespaceManager;
+import java.util.Map;
 
 
 public class XPathUtils {
 
-	/*
-	 * [28] ExprToken ::= '(' | ')' | '[' | ']' | '.' | '..' | '@' | ',' | '::' |
-	 * NameTest | NodeType | Operator | FunctionName | AxisName | Literal |
-	 * Number | VariableReference [29] Literal ::= '"' [^"]* '"' | "'" [^']* "'"
-	 * [30] Number ::= Digits ('.' Digits?)? | '.' Digits [31] Digits ::= [0-9]+
-	 * [32] Operator ::= OperatorName | MultiplyOperator | '/' | '//' | '|' |
-	 * '+' | '-' | '=' | '!=' | '<' | '<=' | '>' | '>=' [33] OperatorName ::=
-	 * 'and' | 'or' | 'mod' | 'div' [34] MultiplyOperator ::= '*' [35]
-	 * FunctionName ::= QName - NodeType [36] VariableReference ::= '$' QName
-	 * [37] NameTest ::= '*' | NCName ':' '*' | QName [38] NodeType ::=
-	 * 'comment' | 'text' | 'processing-instruction' | 'node' [39]
-	 * ExprWhitespace ::= S
-	 */
-
-	public static String translateXPath(String prefixedXpath, NamespaceManager namespaces) {
+	public static String translateXPath(String prefixedXpath, Map namespaces) {
 		// don't process an empty Xpath, or one with not ns prefixes
 		if (prefixedXpath == null || prefixedXpath.trim().length() == 0 || prefixedXpath.indexOf(":") < 0) {
 			return prefixedXpath;
-		} else if (namespaces == null
-			|| (namespaces.getDefaultNamespace() == null && namespaces.getAllPrefixes().size() == 0)) {
+		} else if (namespaces == null || namespaces.keySet().size() == 0) {
 			throw new IllegalArgumentException(
 				"You specified an XPath with prefixes, yet didn't define any prefix mappings.");
 		}
 
 		// process all the replacements based on prefixes
-		Iterator iterator = namespaces.getAllPrefixes().iterator();
+		Iterator iterator = namespaces.keySet().iterator();
 		while (iterator.hasNext()) {
 			String prefix = (String) iterator.next();
-			Namespace ns = namespaces.lookupNamespace(prefix);
+			String ns = (String) namespaces.get(prefix);
 
-			// replace the /a:A stuff
-			prefixedXpath = prefixedXpath.replaceAll("/" + prefix + ":(\\w)?[\\s]*$", "/*[namespace-uri()='" + ns.getRaw()
+			// replace the a:A$ stuff
+			prefixedXpath = prefixedXpath.replaceAll(prefix + ":([a-zA-Z])?[\\s]*$", "*[namespace-uri()='" + ns
 				+ "' and local-name()='$1']");
 
-			// replace the /a:A/ stuff
-			prefixedXpath = prefixedXpath.replaceAll("/" + prefix + ":(\\w)?/", "/*[namespace-uri()='" + ns.getRaw()
+			// replace the a:A/ stuff
+			prefixedXpath = prefixedXpath.replaceAll(prefix + ":([a-zA-Z])?/", "*[namespace-uri()='" + ns
 				+ "' and local-name()='$1']/");
 
 			// replace the /a:A[* stuff
-			prefixedXpath = prefixedXpath.replaceAll("/" + prefix + ":(\\w)?\\[", "/*[namespace-uri()='"
-				+ ns.getRaw() + "' and local-name()='$1' and ");
+			prefixedXpath = prefixedXpath.replaceAll(prefix + ":([a-zA-Z])?\\[", "*[namespace-uri()='" + ns
+				+ "' and local-name()='$1' and ");
 
-
-		}
-		// process the default namespace
-		if (namespaces.getDefaultNamespace() != null) {
-			// prefixedXpath = prefixedXpath.replaceAll("(\\w)?",
-			// "*[namespace-uri()='"
-			// + namespaces.getDefaultNamespace().getRaw() + "' and
-			// local-name()='$1']");
-		} else {
-			// TODO: check for need and throw exception
 		}
 
 		return prefixedXpath;
--- cagrid-1-0/caGrid/projects/core/test/src/java/gov/nih/nci/cagrid/common/XPathUtilsTest.java
+++ cagrid-1-0/caGrid/projects/core/test/src/java/gov/nih/nci/cagrid/common/XPathUtilsTest.java
@@ -1,15 +1,14 @@
 package gov.nih.nci.cagrid.common;
 
-import junit.framework.TestCase;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.projectmobius.common.MalformedNamespaceException;
-import org.projectmobius.common.Namespace;
-import org.projectmobius.common.NamespaceManager;
+import junit.framework.TestCase;
 
 
 public class XPathUtilsTest extends TestCase {
 
-	private NamespaceManager namespaces;
+	private Map prefixMap;
 
 
 	public static void main(String[] args) {
@@ -19,14 +18,9 @@ public class XPathUtilsTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		try {
-			namespaces = new NamespaceManager();
-			namespaces.addNamespace("a", new Namespace("http://foo/d"));
-			namespaces.setDefaultNamespace(new Namespace("gme://abc/def"));
-		} catch (MalformedNamespaceException e) {
-			e.printStackTrace();
-			fail("Error setting up namespaces:" + e.getMessage());
-		}
+
+		prefixMap = new HashMap();
+		prefixMap.put("a", "http://DOMAIN.COM/SCHEMA");
 	}
 
 
@@ -38,12 +32,13 @@ public class XPathUtilsTest extends TestCase {
 		assertNoModification("/a/b/c");
 		assertNoModification("/a[@b!='foo']");
 		assertNoModification("/a[@b!='foo']/*[namespace-uri()='http://somedomain.com/200/foobar' and local-name()='SomeElementName']");
+		assertNoModification("/child::node()/child::node()[@id='a']");
 	}
 
 
 	public void assertNoModification(String xpath) {
 		try {
-			String translated = XPathUtils.translateXPath(xpath, namespaces);
+			String translated = XPathUtils.translateXPath(xpath, prefixMap);
 			assertEquals(translated, xpath);
 		} catch (IllegalArgumentException e) {
 			fail(e.getMessage());
@@ -51,13 +46,18 @@ public class XPathUtilsTest extends TestCase {
 	}
 
 
-	public void assertRepeatable(String xpath) {
+	public void assertReplacedAndRepeatable(String xpath) {
 		try {
-			String translated = XPathUtils.translateXPath(xpath, namespaces);
+			String translated = XPathUtils.translateXPath(xpath, prefixMap);
 			System.out.println();
-			System.out.println("ORIGINAL:"+xpath);
-			System.out.println("TRANSLATED:"+translated);
-			assertEquals(translated, XPathUtils.translateXPath(translated, namespaces));
+			System.out.println("ORIGINAL:" + xpath);
+			System.out.println("TRANSLATED:" + translated);
+
+			assertTrue(translated.length() > xpath.length());
+			assertTrue(translated.indexOf("][") < 0);
+			assertTrue(translated.indexOf("namespace-uri") >= 0);
+			// make sure if we run it again, it doesn't alter it
+			assertEquals(translated, XPathUtils.translateXPath(translated, prefixMap));
 		} catch (IllegalArgumentException e) {
 			fail(e.getMessage());
 		}
@@ -65,11 +65,12 @@ public class XPathUtilsTest extends TestCase {
 
 
 	public void testValidPaths() {
-		
-		assertRepeatable("/a:B");
-		assertRepeatable("/a:B/a:C");
-		assertRepeatable("/a:B[@b!='foo']");
-		assertRepeatable("/a:B[@b!='foo']/a:C");
-		assertRepeatable("/a:B[@b!='foo' and a:C]/a:C");
+		assertReplacedAndRepeatable("/a:B");
+		assertReplacedAndRepeatable("/a:B/a:C");
+		assertReplacedAndRepeatable("/a:B[@b!='foo']");
+		assertReplacedAndRepeatable("/a:B[@b!='foo']/a:C");
+		assertReplacedAndRepeatable("/a:B[@b!='foo' and a:C/text()='a:b']/a:C");
+		assertReplacedAndRepeatable("/a:B[@b!='foo' and a:C or a:B/[@b='a']]/a:C");
+		assertReplacedAndRepeatable("/a:B[@b!='foo' and a:C or a:B/[@b='a']]/*[namespace-uri()='http://DOMAIN.COM:80/SCHEMA' and local-name()='B']");
 	}
 }
