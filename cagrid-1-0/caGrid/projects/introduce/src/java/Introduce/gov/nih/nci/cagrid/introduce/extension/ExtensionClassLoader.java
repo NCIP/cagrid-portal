cd2f9379f704bbbf49ceb80b872577147eb5b3ff
dervin <dervin>
2006-04-03 15:24:40 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1542 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/extension/ExtensionClassLoader.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/extension/ExtensionClassLoader.java
@@ -5,59 +5,57 @@ import java.io.FileFilter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ExtensionClassLoader {
 	private File root;
+	private Map loadedClasses;
 
 
 	public ExtensionClassLoader(File rootDir) {
 		if (rootDir == null)
 			throw new IllegalArgumentException("Null root directory");
 		root = rootDir;
+		loadedClasses = new HashMap();
 	}
 
 
 	protected Class loadClass(String name) throws ClassNotFoundException {
-		Class c = null;
-		// Class loaded yet?
+		Class c = (Class) loadedClasses.get(name);
 		if (c == null) {
 			// Create a new class loader with the directory
 			ClassLoader cl = null;
 			try {
-				File libDir = new File(root.getAbsolutePath() + "/lib/");
+				File libDir = new File(root.getAbsolutePath() + File.separator + "lib" + File.separator);
+				System.out.println("Extension lib dir: " + libDir.getAbsolutePath());
 				File[] theirjars = libDir.listFiles(new FileFilter() {
 					public boolean accept(File pathname) {
-						if (pathname.getName().endsWith(".jar")) {
-							return true;
-						}
-						return false;
+						return pathname.getName().toLowerCase().endsWith(".jar");
 					}
-
 				});
-
+				
 				if (theirjars != null) {
 					URL[] urls = new URL[theirjars.length];
 					for (int i = 0; i < theirjars.length; i++) {
-						urls[i] = new URL("file:/" + theirjars[i].getAbsolutePath());
+						System.out.println("Using jar: " + theirjars[i].getAbsolutePath());
+						urls[i] = theirjars[i].toURL();
 					}
 					cl = new URLClassLoader(urls, this.getClass().getClassLoader());
 				} else {
+					System.out.println("No extension jars found, using default classloader");
 					cl = this.getClass().getClassLoader();
-				}
-
+				}				
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			// Load in the class
 			System.out.println("Loading in extension class: " + name);
-			Class cls = cl.loadClass(name);
-			return cls;
-
-		} else {
-			return c;
+			c = cl.loadClass(name);
+			loadedClasses.put(name, c);
 		}
-
+		return c;
 	}
 }
