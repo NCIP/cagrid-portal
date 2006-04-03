50dde888c972f9097972bc2294b95f07c3a4103b
dervin <dervin>
2006-04-03 18:53:31 +0000

    can we make implemting this thing optional??
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1562 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/DataServiceCreationDialog.java
+++ cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/DataServiceCreationDialog.java
@@ -3,16 +3,9 @@ package gov.nih.nci.cagrid.data.creation;
 import gov.nih.nci.cagrid.introduce.extension.CreationExtensionUIDialog;
 import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
-
 /** 
  *  DataServiceCreationDialog
- *  TODO:DOCUMENT ME
- * 
+ *  
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
  * 
  * @created Apr 3, 2006 
@@ -22,18 +15,7 @@ public class DataServiceCreationDialog extends CreationExtensionUIDialog {
 
 	public DataServiceCreationDialog(ServiceInformation info) {
 		super(info);
-		initialize();
-	}
-	
-	
-	private void initialize() {
-		getContentPane().add(new JLabel("Here I am!"));
-		JButton ok = new JButton("OK");
-		ok.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-		getContentPane().add(ok);
+		// nothing to see here, please move along
+		dispose();
 	}
 }
