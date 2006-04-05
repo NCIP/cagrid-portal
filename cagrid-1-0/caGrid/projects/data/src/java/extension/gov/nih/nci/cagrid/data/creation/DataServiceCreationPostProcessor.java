8de064bcc392255964910791433e52d6b383c149
dervin <dervin>
2006-04-05 16:54:36 +0000

    fixed file copy
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1611 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/DataServiceCreationPostProcessor.java
+++ cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/DataServiceCreationPostProcessor.java
@@ -15,11 +15,15 @@ import gov.nih.nci.cagrid.introduce.extension.CreationExtensionException;
 import gov.nih.nci.cagrid.introduce.extension.CreationExtensionPostProcessor;
 import gov.nih.nci.cagrid.introduce.extension.ExtensionsLoader;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -155,15 +159,15 @@ public class DataServiceCreationPostProcessor implements CreationExtensionPostPr
 	
 	
 	private void copyFiles(File inputFile, File outputFile) throws FileNotFoundException, IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-		char[] buff = new char[1024];
+		BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(inputFile));
+		BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(outputFile));
+		byte[] buff = new byte[1024];
 		int len = -1;
-		while ((len = reader.read(buff)) != -1) {
-			writer.write(buff, 0, len);
+		while ((len = inputStream.read(buff)) != -1) {
+			outputStream.write(buff, 0, len);
 		}
-		reader.close();
-		writer.flush();
-		writer.close();
+		inputStream.close();
+		outputStream.flush();
+		outputStream.close();
 	}
 }
