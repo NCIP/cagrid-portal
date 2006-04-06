5d9887365848de9201a1b06b1dd961a1581e72e3
oster <oster>
2006-04-06 01:11:27 +0000

    now typemappings are generated by introduce
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1644 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/cadsr/src/gov/nih/nci/cagrid/cadsr/common/CaDSRServiceI.java
+++ cagrid-1-0/caGrid/projects/cadsr/src/gov/nih/nci/cagrid/cadsr/common/CaDSRServiceI.java
@@ -10,37 +10,48 @@ import java.rmi.RemoteException;
  */
 public interface CaDSRServiceI {
 
-	public gov.nih.nci.cadsr.umlproject.domain.Project[] findAllProjects() throws RemoteException;
 
 
-	public gov.nih.nci.cadsr.umlproject.domain.Project[] findProjects(java.lang.String context) throws RemoteException;
 
 
-	public gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata[] findPackagesInProject(
-		gov.nih.nci.cadsr.umlproject.domain.Project project) throws RemoteException;
 
 
-	public gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata[] findClassesInProject(
-		gov.nih.nci.cadsr.umlproject.domain.Project project) throws RemoteException;
 
 
-	public gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata[] findClassesInPackage(
-		gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata pkg) throws RemoteException;
 
 
-	public java.lang.String generateMetadataExtractForProject(gov.nih.nci.cadsr.umlproject.domain.Project project)
-		throws RemoteException;
 
 
-	public java.lang.String generateMetadataExtractForPackages(
-		gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata[] packages) throws RemoteException;
 
 
-	public java.lang.String generateMetadataExtractForClasses(
-		gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata[] classes) throws RemoteException;
 
 
-	public gov.nih.nci.cadsr.umlproject.domain.UMLAttributeMetadata[] findAttributesInClass(
-		gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata clazz) throws RemoteException;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     public gov.nih.nci.cadsr.umlproject.domain.Project[] findAllProjects() throws RemoteException ;
+     public gov.nih.nci.cadsr.umlproject.domain.Project[] findProjects(java.lang.String context) throws RemoteException ;
+     public gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata[] findPackagesInProject(gov.nih.nci.cadsr.umlproject.domain.Project project) throws RemoteException ;
+     public gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata[] findClassesInProject(gov.nih.nci.cadsr.umlproject.domain.Project project) throws RemoteException ;
+     public gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata[] findClassesInPackage(gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata pkg) throws RemoteException ;
+     public java.lang.String generateMetadataExtractForProject(gov.nih.nci.cadsr.umlproject.domain.Project project) throws RemoteException ;
+     public java.lang.String generateMetadataExtractForPackages(gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata[] packages) throws RemoteException ;
+     public java.lang.String generateMetadataExtractForClasses(gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata[] classes) throws RemoteException ;
+     public gov.nih.nci.cadsr.umlproject.domain.UMLAttributeMetadata[] findAttributesInClass(gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata clazz) throws RemoteException ;
 
 }
