bd536fedd35a64a1c9b22fe2faec4c567e28cdfb
oster <oster>
2006-04-19 01:58:07 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1808 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/cadsr/Portal/src/gov/nih/nci/cagrid/cadsr/portal/discovery/CaDSRTypeSelectionComponent.java
+++ cagrid-1-0/caGrid/projects/cadsr/Portal/src/gov/nih/nci/cagrid/cadsr/portal/discovery/CaDSRTypeSelectionComponent.java
@@ -4,24 +4,38 @@ import gov.nih.nci.cadsr.umlproject.domain.Project;
 import gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata;
 import gov.nih.nci.cagrid.cadsr.portal.CaDSRBrowserPanel;
 import gov.nih.nci.cagrid.cadsr.portal.PackageSelectedListener;
-import gov.nih.nci.cagrid.introduce.IntroduceConstants;
+import gov.nih.nci.cagrid.common.Utils;
 import gov.nih.nci.cagrid.introduce.beans.extension.DiscoveryExtensionDescriptionType;
+import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionDescription;
 import gov.nih.nci.cagrid.introduce.beans.namespace.NamespaceType;
+import gov.nih.nci.cagrid.introduce.common.CommonTools;
 import gov.nih.nci.cagrid.introduce.extension.ExtensionTools;
 import gov.nih.nci.cagrid.introduce.portal.modification.discovery.NamespaceTypeDiscoveryComponent;
 
+import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.projectmobius.client.gme.ImportInfo;
 import org.projectmobius.common.GridServiceResolver;
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.common.Namespace;
+import org.projectmobius.common.XMLUtilities;
+import org.projectmobius.common.gme.NoSuchSchemaException;
 import org.projectmobius.gme.XMLDataModelService;
 import org.projectmobius.gme.client.GlobusGMEXMLDataModelServiceFactory;
+import org.projectmobius.protocol.gme.SchemaNode;
 
 
 public class CaDSRTypeSelectionComponent extends NamespaceTypeDiscoveryComponent implements PackageSelectedListener {
@@ -84,45 +98,85 @@ public class CaDSRTypeSelectionComponent extends NamespaceTypeDiscoveryComponent
 
 
 	public NamespaceType createNamespaceType(File schemaDestinationDir) {
-		// NamespaceType input = new NamespaceType();
-		// try {
-		// // set the package name
-		// String packageName =
-		// CommonTools.getPackageName(gmePanel.currentNamespace);
-		// input.setPackageName(packageName);
-		//
-		// if (this.gmePanel.currentNamespace != null) {
-		// input.setNamespace(this.gmePanel.currentNamespace.getRaw());
-		// ImportInfo ii = new ImportInfo(this.gmePanel.currentNamespace);
-		// input.setLocation("./" + ii.getFileName());
-		// } else {
-		// return null;
-		// }
-		//
-		// ExtensionTools.setSchemaElements(input,
-		// XMLUtilities.stringToDocument(gmePanel.currentNode
-		// .getSchemaContents()));
-		// cacheSchema(schemaDestinationDir, input.getNamespace());
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// return null;
-		// }
+		NamespaceType input = new NamespaceType();
+		try {
+			String ns = getNsTextField().getText();
+			if (ns.equals("") || ns.equals("unavailable")) {
+				return null;
+			}
+			Namespace namespace = new Namespace(ns);
+			List namespaceDomainList = getGME().getNamespaceDomainList();
+			if (!namespaceDomainList.contains(namespace.getDomain())) {
+				// prompt for alternate
+				String alternativeDomain = (String) JOptionPane.showInputDialog(this,
+					"Unable to locate schema for the selected caDSR package.\n"
+						+ "The GME does not appear to contain schemas under the specified domain: "
+						+ namespace.getDomain() + "\nSelect an alternative domain, or cancel.",
+					"Schema Location Error", JOptionPane.ERROR_MESSAGE, null, namespaceDomainList.toArray(), null);
 
-		return null;
+				if (alternativeDomain != null) {
+					namespace = new Namespace(namespace.getProtocol() + "://" + alternativeDomain + "/"
+						+ namespace.getNamespace());
+					getNsTextField().setText(namespace.getRaw());
+				} else {
+					return null;
+				}
+			}
+			String schemaContents = null;
+			try {
+				schemaContents = getSchema(namespace);
+			} catch (NoSuchSchemaException e) {
+				// prompt for alternate
+				List schemas = getGME().getSchemaListForNamespaceDomain(namespace.getDomain());
+				Namespace alternativeSchema = (Namespace) JOptionPane.showInputDialog(this,
+					"Unable to locate schema for the selected caDSR package.\n"
+						+ "This package may not have a published Schema."
+						+ "\nSelect an alternative Schema, or cancel.", "Schema Location Error",
+					JOptionPane.ERROR_MESSAGE, null, schemas.toArray(), null);
+
+				if (alternativeSchema != null) {
+					namespace = alternativeSchema;
+					getNsTextField().setText(namespace.getRaw());
+				} else {
+					return null;
+				}
+				schemaContents = getSchema(namespace);
+			}
+
+			// set the package name
+			String packageName = CommonTools.getPackageName(namespace);
+			input.setPackageName(packageName);
+
+			input.setNamespace(ns);
+			ImportInfo ii = new ImportInfo(namespace);
+			input.setLocation("./" + ii.getFileName());
+
+			// popualte the schema elements
+			gov.nih.nci.cagrid.introduce.portal.ExtensionTools.setSchemaElements(input, XMLUtilities
+				.stringToDocument(schemaContents));
+			// write the schema and its imports to the filesystem
+			getGME().cacheSchema(namespace, schemaDestinationDir);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return input;
 	}
 
 
-	private void cacheSchema(File dir, String namespace) throws Exception {
-		if (namespace.equals(IntroduceConstants.W3CNAMESPACE)) {
-			// this is "natively supported" so we don't need to cache it
-			return;
-		}
+	private String getSchema(Namespace namespace) throws Exception {
+		SchemaNode schema = getGME().getSchema(namespace, false);
+		return schema.getSchemaContents();
 
+	}
+
+
+	private XMLDataModelService getGME() throws MobiusException {
 		GridServiceResolver.getInstance().setDefaultFactory(new GlobusGMEXMLDataModelServiceFactory());
 		XMLDataModelService handle = (XMLDataModelService) GridServiceResolver.getInstance()
 			.getGridService(this.gmeURL);
-		handle.cacheSchema(new org.projectmobius.common.Namespace(namespace), dir);
-
+		return handle;
 	}
 
 
@@ -144,7 +198,7 @@ public class CaDSRTypeSelectionComponent extends NamespaceTypeDiscoveryComponent
 			gridBagConstraints1.gridy = 0;
 			gridBagConstraints1.gridx = 0;
 			nsLabel = new JLabel();
-			nsLabel.setText("Derived Namespace:");
+			nsLabel.setText("Namespace:");
 			nsPanel = new JPanel();
 			nsPanel.setLayout(new GridBagLayout());
 			nsPanel.add(nsLabel, gridBagConstraints1);
@@ -170,12 +224,36 @@ public class CaDSRTypeSelectionComponent extends NamespaceTypeDiscoveryComponent
 
 
 	public static void main(String[] args) {
-		CaDSRTypeSelectionComponent panel = new CaDSRTypeSelectionComponent(new DiscoveryExtensionDescriptionType());
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(panel);
-		frame.pack();
-		frame.setVisible(true);
+		try {
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			ExtensionDescription ext = (ExtensionDescription) Utils.deserializeDocument(
+				"extensions/caDSR-discovery/extension.xml", ExtensionDescription.class);
+			final CaDSRTypeSelectionComponent panel = new CaDSRTypeSelectionComponent(ext
+				.getDiscoveryExtensionDescription());
+			frame.getContentPane().setLayout(new BorderLayout());
+			frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+			JButton createButton = new JButton("Test Create");
+			createButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					NamespaceType createdNs = panel.createNamespaceType(new File("."));
+					if (createdNs != null) {
+						System.out.println("Created Namespace:" + createdNs.getNamespace() + " at location:"
+							+ createdNs.getLocation());
+					} else {
+						System.out.println("Problem creating namespace");
+					}
+				}
+			});
+			frame.getContentPane().add(createButton, BorderLayout.SOUTH);
+
+			frame.pack();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 
