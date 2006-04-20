beb619160d0638d2c667e53776d71c29e7fac1d8
dervin <dervin>
2006-04-20 19:12:01 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1857 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/ui/types/TargetTypeSelectionPanel.java
+++ cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/ui/types/TargetTypeSelectionPanel.java
@@ -1,13 +1,35 @@
 package gov.nih.nci.cagrid.data.ui.types;
 
+import gov.nih.nci.cadsr.umlproject.domain.Project;
+import gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata;
 import gov.nih.nci.cagrid.cadsr.portal.CaDSRBrowserPanel;
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionDescription;
+import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
+import gov.nih.nci.cagrid.introduce.beans.namespace.NamespaceType;
+import gov.nih.nci.cagrid.introduce.common.CommonTools;
+import gov.nih.nci.cagrid.introduce.extension.ExtensionTools;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.io.File;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+
+import org.projectmobius.client.gme.ImportInfo;
+import org.projectmobius.common.GridServiceResolver;
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.common.Namespace;
+import org.projectmobius.common.XMLUtilities;
+import org.projectmobius.common.gme.NoSuchSchemaException;
+import org.projectmobius.gme.XMLDataModelService;
+import org.projectmobius.gme.client.GlobusGMEXMLDataModelServiceFactory;
+import org.projectmobius.protocol.gme.SchemaNode;
 
 /** 
  *  TargetTypeSelectionPanel
@@ -28,38 +50,42 @@ public class TargetTypeSelectionPanel extends JPanel {
 	private JScrollPane typesTableScrollPane = null;
 	private JPanel serializationPanel = null;
 	private JPanel typeSelectionPanel = null;
+	private JButton setModelButton = null;
 	
-	public TargetTypeSelectionPanel() {
+	private XMLDataModelService gmeHandle = null;
+	private ServiceExtensionDescriptionType serviceDesc;
+	
+	public TargetTypeSelectionPanel(ServiceExtensionDescriptionType serviceDescription) {
 		super();
+		this.serviceDesc = serviceDescription;
 		initialize();
 	}
 	
 	
 	private void initialize() {
-        GridBagConstraints gridBagConstraints5 = new GridBagConstraints();
-        gridBagConstraints5.gridx = 1;
-        gridBagConstraints5.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints5.weightx = 1.0D;
-        gridBagConstraints5.weighty = 1.0D;
-        gridBagConstraints5.gridy = 0;
-        GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
-        gridBagConstraints4.gridx = 0;
-        gridBagConstraints4.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints4.weighty = 1.0D;
-        gridBagConstraints4.weightx = 1.0D;
-        gridBagConstraints4.anchor = java.awt.GridBagConstraints.NORTH;
-        gridBagConstraints4.gridy = 0;
-        this.setLayout(new GridBagLayout());
-        this.setSize(new java.awt.Dimension(622,548));
-        this.add(getTypeSelectionPanel(), gridBagConstraints4);
-        this.add(getSerializationPanel(), gridBagConstraints5);
-		
+		GridBagConstraints gridBagConstraints5 = new GridBagConstraints();
+		gridBagConstraints5.gridx = 1;
+		gridBagConstraints5.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints5.weightx = 1.0D;
+		gridBagConstraints5.weighty = 1.0D;
+		gridBagConstraints5.gridy = 0;
+		GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
+		gridBagConstraints4.gridx = 0;
+		gridBagConstraints4.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints4.weighty = 1.0D;
+		gridBagConstraints4.weightx = 1.0D;
+		gridBagConstraints4.anchor = java.awt.GridBagConstraints.NORTH;
+		gridBagConstraints4.gridy = 0;
+		this.setLayout(new GridBagLayout());
+		this.setSize(new java.awt.Dimension(1017,548));
+		this.add(getTypeSelectionPanel(), gridBagConstraints4);
+		this.add(getSerializationPanel(), gridBagConstraints5);	
 	}
 	
 	
 	private CaDSRBrowserPanel getDomainBrowserPanel() {
 		if (domainBrowserPanel == null) {
-			domainBrowserPanel = new CaDSRBrowserPanel();
+			domainBrowserPanel = new CaDSRBrowserPanel(true, false);
 		}
 		return domainBrowserPanel;
 	}
@@ -71,7 +97,7 @@ public class TargetTypeSelectionPanel extends JPanel {
 		}
 		return typesTree;
 	}
-		
+	
 	
 	/**
 	 * This method initializes jScrollPane	
@@ -126,8 +152,8 @@ public class TargetTypeSelectionPanel extends JPanel {
 		}
 		return typesTableScrollPane;
 	}
-
-
+	
+	
 	/**
 	 * This method initializes jPanel	
 	 * 	
@@ -154,8 +180,8 @@ public class TargetTypeSelectionPanel extends JPanel {
 		}
 		return serializationPanel;
 	}
-
-
+	
+	
 	/**
 	 * This method initializes jPanel	
 	 * 	
@@ -163,9 +189,12 @@ public class TargetTypeSelectionPanel extends JPanel {
 	 */
 	private JPanel getTypeSelectionPanel() {
 		if (typeSelectionPanel == null) {
+			GridBagConstraints gridBagConstraints31 = new GridBagConstraints();
+			gridBagConstraints31.gridx = 0;
+			gridBagConstraints31.gridy = 1;
 			GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
 			gridBagConstraints1.fill = GridBagConstraints.BOTH;
-			gridBagConstraints1.gridy = 1;
+			gridBagConstraints1.gridy = 2;
 			gridBagConstraints1.weightx = 1.0;
 			gridBagConstraints1.weighty = 1.0;
 			gridBagConstraints1.gridx = 0;
@@ -178,15 +207,138 @@ public class TargetTypeSelectionPanel extends JPanel {
 			typeSelectionPanel.setLayout(new GridBagLayout());
 			typeSelectionPanel.add(getDomainBrowserPanel(), gridBagConstraints);
 			typeSelectionPanel.add(getTypesTreeScrollPane(), gridBagConstraints1);
+			typeSelectionPanel.add(getSetModelButton(), gridBagConstraints31);
 		}
 		return typeSelectionPanel;
 	}
-
-
+	
+	
+	/**
+	 * This method initializes jButton	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getSetModelButton() {
+		if (setModelButton == null) {
+			setModelButton = new JButton();
+			setModelButton.setText("Set Model");
+			setModelButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					UMLPackageMetadata pack = getDomainBrowserPanel().getSelectedPackage();
+					if (pack != null) {
+						NamespaceType nsType = createNamespaceType(pack);
+						getTypesTree().setNamespace(nsType);
+					}
+				}
+			});
+		}
+		return setModelButton;
+	}
+	
+	
+	private NamespaceType createNamespaceType(UMLPackageMetadata pack) {
+		String namespaceString = null;
+		Project proj = getDomainBrowserPanel().getSelectedProject();
+		if (proj != null) {
+			// TODO: need to get Context
+			String version = proj.getVersion();
+			if (version.indexOf(".") < 0) {
+				version += ".0";
+			}
+			namespaceString = "gme://" + proj.getShortName() + ".caBIG/" + version + "/" + pack.getName();
+			NamespaceType nsType = new NamespaceType();
+			try {
+				Namespace namespace = new Namespace(namespaceString);
+				List namespaceDomainList = getGME().getNamespaceDomainList();
+				if (!namespaceDomainList.contains(namespace.getDomain())) {
+					// prompt for alternate
+					String alternativeDomain = (String) JOptionPane.showInputDialog(this,
+						"The GME does not appear to contain schemas under the specified domain.\n"
+						+ "Select an alternative domain, or cancel if no viable option is available.\n"
+						+ "\nExpected domain: " + namespace.getDomain(), "Schema Location Error",
+						JOptionPane.ERROR_MESSAGE, null, namespaceDomainList.toArray(), null);
+					
+					if (alternativeDomain != null) {
+						namespace = new Namespace(namespace.getProtocol() + "://" + alternativeDomain + "/"
+							+ namespace.getName());
+					} else {
+						return null;
+					}
+				}
+				String schemaContents = null;
+				try {
+					schemaContents = getSchema(namespace);
+				} catch (NoSuchSchemaException e) {
+					// prompt for alternate
+					List schemas = getGME().getSchemaListForNamespaceDomain(namespace.getDomain());
+					Namespace alternativeSchema = (Namespace) JOptionPane.showInputDialog(this,
+						"Unable to locate schema for the selected caDSR package.\n"
+						+ "This package may not have a published Schema."
+						+ "\nSelect an alternative Schema, or cancel.\n\nExpected schema: " + namespace.getName(),
+						"Schema Location Error", JOptionPane.ERROR_MESSAGE, null, schemas.toArray(), null);
+					
+					if (alternativeSchema != null) {
+						namespace = alternativeSchema;
+					} else {
+						return null;
+					}
+					schemaContents = getSchema(namespace);
+				}
+				
+				// set the package name
+				String packageName = CommonTools.getPackageName(namespace);
+				nsType.setPackageName(packageName);
+				
+				nsType.setNamespace(namespaceString);
+				ImportInfo ii = new ImportInfo(namespace);
+				nsType.setLocation("./" + ii.getFileName());
+				
+				// popualte the schema elements
+				gov.nih.nci.cagrid.introduce.portal.ExtensionTools.setSchemaElements(nsType, 
+					XMLUtilities.stringToDocument(schemaContents));
+				// write the schema and its imports to the filesystem
+				getGME().cacheSchema(namespace, getSchemaDir());
+				return nsType;
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	
+	private XMLDataModelService getGME() throws MobiusException {
+		if (gmeHandle == null) {
+			GridServiceResolver.getInstance().setDefaultFactory(new GlobusGMEXMLDataModelServiceFactory());
+			gmeHandle = (XMLDataModelService) GridServiceResolver.getInstance()
+				.getGridService(ExtensionTools.getProperty(serviceDesc.getProperties(), "GME_URL"));
+		}
+		return gmeHandle;
+	}
+	
+	
+	private String getSchema(Namespace namespace) throws Exception {
+		SchemaNode schema = getGME().getSchema(namespace, false);
+		return schema.getSchemaContents();
+	}
+	
+	
+	private File getSchemaDir() {
+		return null;
+	}
+	
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(new TargetTypeSelectionPanel());
+		try {
+			ExtensionDescription desc = (ExtensionDescription) Utils.deserializeDocument("extension.xml",
+				ExtensionDescription.class);
+			frame.setContentPane(new TargetTypeSelectionPanel(desc.getServiceExtensionDescription()));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
 		frame.setVisible(true);
 	}
-}  //  @jve:decl-index=0:visual-constraint="22,46"
+}
