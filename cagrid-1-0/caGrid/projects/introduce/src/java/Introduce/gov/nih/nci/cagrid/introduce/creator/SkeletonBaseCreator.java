fa43f9099e41bd6dc9b7be6631d087082e8bc059
oster <oster>
2006-04-21 20:13:13 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1885 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/creator/SkeletonBaseCreator.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/creator/SkeletonBaseCreator.java
@@ -3,7 +3,6 @@ package gov.nih.nci.cagrid.introduce.creator;
 import gov.nih.nci.cagrid.introduce.IntroduceConstants;
 import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
 import gov.nih.nci.cagrid.introduce.templates.ClasspathTemplate;
-import gov.nih.nci.cagrid.introduce.templates.ClientConfigTemplate;
 import gov.nih.nci.cagrid.introduce.templates.DeployPropertiesTemplate;
 import gov.nih.nci.cagrid.introduce.templates.JNDIConfigTemplate;
 import gov.nih.nci.cagrid.introduce.templates.NamespaceMappingsTemplate;
@@ -12,6 +11,7 @@ import gov.nih.nci.cagrid.introduce.templates.ServerConfigTemplate;
 
 import java.io.File;
 import java.io.FileWriter;
+
 
 /**
  * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Hastings </A>
@@ -26,43 +26,45 @@ public class SkeletonBaseCreator {
 
 
 	public void createSkeleton(ServiceInformation info) throws Exception {
-		File baseDirectory = new File(info.getIntroduceServiceProperties().getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR));
-		
+		File baseDirectory = new File(info.getIntroduceServiceProperties().getProperty(
+			IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR));
+
 		ServerConfigTemplate serverConfigT = new ServerConfigTemplate();
 		String serverConfigS = serverConfigT.generate(info);
 		File serverConfigF = new File(baseDirectory.getAbsolutePath() + File.separator + "server-config.wsdd");
 		FileWriter serverConfigFW = new FileWriter(serverConfigF);
 		serverConfigFW.write(serverConfigS);
 		serverConfigFW.close();
-		
+
 		JNDIConfigTemplate jndiConfigT = new JNDIConfigTemplate();
 		String jndiConfigS = jndiConfigT.generate(info);
 		File jndiConfigF = new File(baseDirectory.getAbsolutePath() + File.separator + "jndi-config.xml");
 		FileWriter jndiConfigFW = new FileWriter(jndiConfigF);
 		jndiConfigFW.write(jndiConfigS);
 		jndiConfigFW.close();
-		
+
 		DeployPropertiesTemplate deployPropertiesT = new DeployPropertiesTemplate();
 		String deployPropertiesS = deployPropertiesT.generate(info);
 		File deployPropertiesF = new File(baseDirectory.getAbsolutePath() + File.separator + "deploy.properties");
 		FileWriter deployPropertiesFW = new FileWriter(deployPropertiesF);
 		deployPropertiesFW.write(deployPropertiesS);
 		deployPropertiesFW.close();
-		
+
 		NamespaceMappingsTemplate namespaceMappingsT = new NamespaceMappingsTemplate();
 		String namespaceMappingsS = namespaceMappingsT.generate(info);
-		File namespaceMappingsF = new File(baseDirectory.getAbsolutePath() + File.separator + IntroduceConstants.NAMESPACE2PACKAGE_MAPPINGS_FILE);
+		File namespaceMappingsF = new File(baseDirectory.getAbsolutePath() + File.separator
+			+ IntroduceConstants.NAMESPACE2PACKAGE_MAPPINGS_FILE);
 		FileWriter namespaceMappingsFW = new FileWriter(namespaceMappingsF);
 		namespaceMappingsFW.write(namespaceMappingsS);
 		namespaceMappingsFW.close();
-		
+
 		ClasspathTemplate classpathT = new ClasspathTemplate();
 		String classpathS = classpathT.generate(info);
 		File classpathF = new File(baseDirectory.getAbsolutePath() + File.separator + ".classpath");
 		FileWriter classpathFW = new FileWriter(classpathF);
 		classpathFW.write(classpathS);
 		classpathFW.close();
-		
+
 		ProjectTemplate projectT = new ProjectTemplate();
 		String projectS = projectT.generate(info);
 		File projectF = new File(baseDirectory.getAbsolutePath() + File.separator + ".project");
