bf5fa0192af824be7c663b646b35563b8e82c586
hastings <hastings>
2006-04-24 18:00:00 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1916 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/IntroducePortalInitializer.java
@@ -0,0 +1,22 @@
+package gov.nih.nci.cagrid.introduce.portal;
+import gov.nih.nci.cagrid.introduce.portal.help.IntroduceHelp;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import org.projectmobius.portal.GridPortalInitializer;
+import org.projectmobius.portal.PortalResourceManager;
+
+public class IntroducePortalInitializer implements GridPortalInitializer {
+	private static final int HELP_MENU = 4;
+
+	public void intialize() throws Exception {
+		System.out.println("Initializing Grid Portal");
+		IntroduceHelp help = new IntroduceHelp();
+		JMenu helpMenu = PortalResourceManager.getInstance().getGridPortal().getJMenuBar().getMenu(HELP_MENU);
+		JMenuItem helpMenuItem = new JMenuItem("Introduce Help");
+		helpMenuItem.addActionListener(help.getFDisplayHelp());
+		helpMenu.add(helpMenuItem);
+	}
+
+}
---
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/help/IntroduceHelp.java
@@ -0,0 +1,83 @@
+package gov.nih.nci.cagrid.introduce.portal.help;
+
+import java.net.URL;
+
+import javax.help.CSH;
+import javax.help.HelpBroker;
+import javax.help.HelpSet;
+import javax.help.HelpSetException;
+import javax.swing.JFrame;
+
+
+public class IntroduceHelp extends JFrame {
+	private HelpBroker fHelp;
+	private CSH.DisplayHelpFromSource fDisplayHelp;
+	private HelpSet helpSet;
+
+
+	public IntroduceHelp() {
+		initHelpSystem();
+	}
+
+
+	public CSH.DisplayHelpFromSource getFDisplayHelp() {
+		return fDisplayHelp;
+	}
+
+
+	public void setFDisplayHelp(CSH.DisplayHelpFromSource displayHelp) {
+		fDisplayHelp = displayHelp;
+	}
+
+
+	public HelpBroker getFHelp() {
+		return fHelp;
+	}
+
+
+	public void setFHelp(HelpBroker help) {
+		fHelp = help;
+	}
+
+
+	public HelpSet getHelpSet() {
+		return helpSet;
+	}
+
+
+	public void setHelpSet(HelpSet helpSet) {
+		this.helpSet = helpSet;
+	}
+
+
+	/**
+	 * Initialize the JavaHelp system.
+	 */
+	public void initHelpSystem() {
+		// optimization to avoid repeated init
+		if (fHelp != null && fDisplayHelp != null)
+			return;
+
+		// (uses the classloader mechanism)
+		ClassLoader loader = this.getClass().getClassLoader();
+		URL helpSetURL = HelpSet.findHelpSet(loader, "IntroduceHelp.hs");
+		try {
+			helpSet = new HelpSet(null, helpSetURL);
+			fHelp = helpSet.createHelpBroker();
+			fHelp.enableHelpKey(this.getRootPane(), "index", helpSet);
+			fDisplayHelp = new CSH.DisplayHelpFromSource(fHelp);
+
+		} catch (HelpSetException ex) {
+			System.out.println("Cannot create help system with: " + helpSetURL + " " + ex.getMessage());
+		}
+	}
+
+
+	public static void main(String[] args) {
+		IntroduceHelp help = new IntroduceHelp();
+		help.setSize(200, 200);
+		help.setVisible(true);
+
+	}
+
+}
