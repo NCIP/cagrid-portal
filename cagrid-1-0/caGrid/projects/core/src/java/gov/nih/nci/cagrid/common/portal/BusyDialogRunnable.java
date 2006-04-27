eb4efc8b1bf6f0b6f7c198a253d449e101368098
dervin <dervin>
2006-04-27 17:06:27 +0000

    updates to status bar forced to run on the Swing EventDispatch thread
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@1976 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
@@ -6,19 +6,19 @@ import javax.swing.SwingUtilities;
 
 
 public abstract class BusyDialogRunnable implements Runnable {
-
+	
 	final private BusyDialog dialog;
 	private String errorMessage = "";
 	private boolean valid = true;
 	private JFrame owner;
-
-
+	
+	
 	public BusyDialogRunnable(JFrame owner, String title) {
 		this.owner = owner;
 		dialog = new BusyDialog(owner, "Progress (" + title + ")");
 	}
-
-
+	
+	
 	public void setProgressText(final String text) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -26,8 +26,8 @@ public abstract class BusyDialogRunnable implements Runnable {
 			}
 		});
 	}
-
-
+	
+	
 	public void run() {
 		Thread thread = new Thread(new Runnable() {
 			public void run() {
@@ -36,42 +36,42 @@ public abstract class BusyDialogRunnable implements Runnable {
 		});
 		thread.start();
 		try {
-			//SwingUtilities.invokeLater(new Runnable() {
-			//	public void run() {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
 					dialog.getProgress().setIndeterminate(true);
-			//	}
-			//});
+				}
+			});
 			process();
-			//SwingUtilities.invokeLater(new Runnable() {
-			//	public void run() {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
 					dialog.getProgress().setIndeterminate(false);
-			//	}
-			//});
+				}
+			});
 		} catch (Exception e) {
 			valid = false;
 			errorMessage = e.getMessage();
 		}
-
+		
 		setProgressText("");
 		dialog.setVisible(false);
-
+		
 		if (!valid) {
 			JOptionPane.showMessageDialog(owner, errorMessage);
 		}
 	}
-
-
+	
+	
 	public abstract void process();
-
-
+	
+	
 	public String getErrorMessage() {
 		return errorMessage;
 	}
-
-
+	
+	
 	public void setErrorMessage(String message) {
 		this.valid = false;
 		this.errorMessage = message;
 	}
-
+	
 }
