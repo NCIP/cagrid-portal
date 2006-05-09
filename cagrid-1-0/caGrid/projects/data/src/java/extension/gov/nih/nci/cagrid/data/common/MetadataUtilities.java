2acb26b37c0239206919d5013e7a94164d6c80c2
dervin <dervin>
2006-05-09 19:15:19 +0000

    metadata updates
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2200 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/common/MetadataUtilities.java
+++ cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/common/MetadataUtilities.java
@@ -8,11 +8,17 @@ import gov.nih.nci.cadsr.umlproject.domain.UMLPackageMetadata;
 import gov.nih.nci.cagrid.metadata.common.UMLAttribute;
 import gov.nih.nci.cagrid.metadata.common.UMLClass;
 import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModelExposedUMLAssociationCollection;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModelExposedUMLClassCollection;
 import gov.nih.nci.cagrid.metadata.dataservice.UMLAssociation;
 import gov.nih.nci.cagrid.metadata.dataservice.UMLAssociationEdge;
+import gov.nih.nci.cagrid.metadata.dataservice.UMLAssociationEdgeUmlClass;
+import gov.nih.nci.cagrid.metadata.dataservice.UMLAssociationSourceUMLAssociationEdge;
+import gov.nih.nci.cagrid.metadata.dataservice.UMLAssociationTargetUMLAssociationEdge;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -34,15 +40,20 @@ public class MetadataUtilities {
 		clazz.setDescription(meta.getDescription());
 		clazz.setProjectName(meta.getProject().getLongName());
 		clazz.setProjectVersion(meta.getProject().getVersion());
-		clazz.setSemanticMetadataCollection(meta.getSemanticMetadataCollection());
+		SemanticMetadata[] semanticMetadata = new SemanticMetadata[meta.getSemanticMetadataCollection().size()];
+		meta.getSemanticMetadataCollection().toArray(semanticMetadata);
+		clazz.setSemanticMetadataCollection(semanticMetadata);
 		
-		UMLAttributeMetadata[] attribMeta = meta.getUMLAttributeMetadataCollection();
-		UMLAttribute[] attribs = new UMLAttribute[attribMeta.length];
-		for (int i = 0; i < attribMeta.length; i++) {
+		Iterator attribMetaIter = meta.getUMLAttributeMetadataCollection().iterator();
+		UMLAttribute[] attribs = new UMLAttribute[meta.getUMLAttributeMetadataCollection().size()];
+		int i = 0;
+		while (attribMetaIter.hasNext()) {
+			UMLAttributeMetadata attribMeta = (UMLAttributeMetadata) attribMetaIter.next();
 			attribs[i] = new UMLAttribute();
-			attribs[i].setDescription(attribMeta[i].getDescription());
-			attribs[i].setName(attribMeta[i].getName());
-			SemanticMetadata[] sem = attribMeta[i].getSemanticMetadataCollection();
+			attribs[i].setDescription(attribMeta.getDescription());
+			attribs[i].setName(attribMeta.getName());
+			SemanticMetadata[] sem = new SemanticMetadata[attribMeta.getSemanticMetadataCollection().size()];
+			attribMeta.getSemanticMetadataCollection().toArray(sem);
 			attribs[i].setSemanticMetadataCollection(sem);
 		}
 		clazz.setUmlAttributeCollection(attribs);
@@ -53,23 +64,25 @@ public class MetadataUtilities {
 	
 	public static UMLAssociation createUmlAssociation(UMLAssociationMetadata meta) {
 		UMLAssociation assoc = new UMLAssociation();
-		assoc.setBidirectional(meta.isIsBidirectional());
+		assoc.setBidirectional(meta.getIsBidirectional().booleanValue());
 		
 		// create target association edge
 		UMLAssociationEdge targetEdge = new UMLAssociationEdge();
 		targetEdge.setMaxCardinality(meta.getTargetHighCardinality().intValue());
 		targetEdge.setMinCardinality(meta.getTargetLowCardinality().intValue());
 		targetEdge.setRoleName(meta.getTargetRoleName());
-		targetEdge.setUmlClass(createUmlClass(meta.getTargetUMLClassMetadata()));
-		assoc.setTargetUMLAssociationEdge(targetEdge);
+		targetEdge.setUmlClass(new UMLAssociationEdgeUmlClass(
+			createUmlClass(meta.getTargetUMLClassMetadata())));
+		assoc.setTargetUMLAssociationEdge(new UMLAssociationTargetUMLAssociationEdge(targetEdge));
 		
 		// create source association edge
 		UMLAssociationEdge sourceEdge = new UMLAssociationEdge();
 		sourceEdge.setMaxCardinality(meta.getSourceHighCardinality().intValue());
 		sourceEdge.setMinCardinality(meta.getSourceLowCardinality().intValue());
 		sourceEdge.setRoleName(meta.getSourceRoleName());
-		sourceEdge.setUmlClass(createUmlClass(meta.getSourceUMLClassMetadata()));
-		assoc.setSourceUMLAssociationEdge(sourceEdge);
+		sourceEdge.setUmlClass(new UMLAssociationEdgeUmlClass(
+			createUmlClass(meta.getSourceUMLClassMetadata())));
+		assoc.setSourceUMLAssociationEdge(new UMLAssociationSourceUMLAssociationEdge(sourceEdge));
 		
 		return assoc;
 	}
@@ -93,24 +106,28 @@ public class MetadataUtilities {
 			UMLClassMetadata umlClassMd = (UMLClassMetadata) umlClasses.get(classNames[i]);
 			UMLClass umlClass = createUmlClass(umlClassMd);
 			exposedClasses[i] = umlClass;
-			UMLAssociationMetadata[] assocMd = umlClassMd.getUMLAssociationMetadataCollection();
-			for (int j = 0; j < assocMd.length; j++) {
-				UMLAssociation association = createUmlAssociation(assocMd[j]);
+			Iterator assocMdIter = umlClassMd.getUMLAssociationMetadataCollection().iterator();
+			while (assocMdIter.hasNext()) {
+				UMLAssociationMetadata assocMd = (UMLAssociationMetadata) assocMdIter.next();
+				UMLAssociation association = createUmlAssociation(assocMd);
 				associationEdges.add(association);
 			}
 		}
-		model.setExposedUMLClassCollection(exposedClasses);
+		model.setExposedUMLClassCollection(
+			new DomainModelExposedUMLClassCollection(exposedClasses));
 		UMLAssociation[] associations = new UMLAssociation[associationEdges.size()];
 		associationEdges.toArray(associations);
-		model.setExposedUMLAssociationCollection(associations);
+		model.setExposedUMLAssociationCollection(
+			new DomainModelExposedUMLAssociationCollection(associations));
 	}
 	
 	
 	private static Map umlClassMetadataByName(UMLPackageMetadata pack) {
 		Map classMd = new HashMap();
-		UMLClassMetadata[] classMdArray = pack.getUMLClassMetadataCollection();
-		for (int i = 0; i < classMdArray.length; i++) {
-			classMd.put(classMdArray[i].getName(), classMdArray[i]);
+		Iterator classMdIter = pack.getUMLClassMetadataCollection().iterator();
+		while (classMdIter.hasNext()) {
+			UMLClassMetadata md = (UMLClassMetadata) classMdIter.next();
+			classMd.put(md.getName(), md);
 		}
 		return classMd;
 	}
