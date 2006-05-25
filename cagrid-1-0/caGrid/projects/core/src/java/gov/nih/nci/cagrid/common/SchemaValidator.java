b3c9e4d3634fa1c68caf19bdf36b65ce181fc04a
dervin <dervin>
2006-05-25 19:44:10 +0000

    XML document validation against schema with jaxp
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2500 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/SchemaValidator.java
@@ -0,0 +1,166 @@
+package gov.nih.nci.cagrid.common;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.StringReader;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+/** 
+ *  SchemaValidator
+ *  Validates XML documents against a schema.  For validating multiple documents against the
+ *  same schema, create an instance of this class with the schema filename and pass the
+ *  documents to the validator in succession to save on the overhead of creating new
+ *  parser factories.
+ * 
+ * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
+ * 
+ * @created May 25, 2006 
+ * @version $Id$ 
+ */
+public class SchemaValidator {
+	public static final String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+	public static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
+	public static final String W3C_NAMESPACE = "http://www.w3.org/2001/XMLSchema";
+	
+	private SAXParserFactory factory;
+	private SAXParser parser;
+	private XMLReader xmlReader;
+	
+	public SchemaValidator(String schemaFilename) throws SchemaValidationException {
+		try {
+			// initialize the sax parser factory
+			factory = SAXParserFactory.newInstance();
+			factory.setValidating(true);
+			factory.setNamespaceAware(true);
+			if (!factory.isValidating()) {
+				throw new SchemaValidationException("Unable to set validation on sax parser factory: " + factory.getClass().getName());
+			}
+			
+			// create a parser
+			parser = factory.newSAXParser();
+			
+			// enable scheams for the parser
+			parser.setProperty(JAXP_SCHEMA_LANGUAGE, W3C_NAMESPACE);
+			
+			// configure the schema
+			parser.setProperty(JAXP_SCHEMA_SOURCE, schemaFilename);
+			
+			// create an XML reader from the parser
+			xmlReader = parser.getXMLReader();
+			
+			// set content and error handlers on the reader
+			xmlReader.setContentHandler(new SimpleErrorHandler());
+			xmlReader.setErrorHandler(new SimpleErrorHandler());
+		}  catch (ParserConfigurationException ex) {
+			throw new SchemaValidationException("Error configuring SAX parser: " + ex.getMessage(), ex);
+		} catch (SAXException ex) {
+			throw new SchemaValidationException("Error in SAX: " + ex.getMessage(), ex);
+		}
+	}
+	
+	
+	public void validate(String xml) throws SchemaValidationException {
+		InputSource xmlInput = new InputSource(new BufferedReader(new StringReader(xml)));
+		// only one document can be handled by the xml reader at once
+		synchronized (xmlReader) {
+			try {
+				xmlReader.parse(xmlInput);
+			} catch (Exception ex) {
+				throw new SchemaValidationException("Invalid Document: " + ex.getMessage(), ex);
+			}
+		}
+	}
+	
+	
+	public void validate(File xmlFile) throws SchemaValidationException {
+		String xmlText = null;
+		try {
+			xmlText = Utils.fileToStringBuffer(xmlFile).toString();
+		} catch (Exception ex) {
+			throw new SchemaValidationException("Error reading file: " + ex.getMessage(), ex);
+		}		
+		validate(xmlText);
+	}
+	
+	
+	public static void validate(String xsdFilename, String xmlText) throws SchemaValidationException {
+		SchemaValidator validator = new SchemaValidator(xsdFilename);
+		validator.validate(xmlText);
+	}
+	
+	
+	public static void validate(String xsdFilename, File xmlFile) throws SchemaValidationException {
+		String xmlText = null;
+		try {
+			xmlText = Utils.fileToStringBuffer(xmlFile).toString();
+		} catch (Exception ex) {
+			throw new SchemaValidationException("Error reading file: " + ex.getMessage(), ex);
+		}		
+		validate(xsdFilename, xmlText);
+	}
+	
+	
+	private class SimpleErrorHandler extends DefaultHandler {
+		public void warning(SAXParseException e) {
+			System.out.println("Warning Line " + e.getLineNumber() + ": " + e.getMessage() + "\n");
+		}
+		
+		
+		public void error(SAXParseException e) throws SAXException {
+			throw new SAXException("Error Line " + e.getLineNumber() + ": " + e.getMessage(), e);
+		}
+		
+		
+		public void fatalError(SAXParseException e) throws SAXException {
+			throw new SAXException("Fatal Error Line " + e.getLineNumber() + ": " + e.getMessage(), e);
+		}
+	}
+	
+	
+	private static void usage() {
+		System.err.println("Usage: " + SchemaValidator.class.getName() + " <schemaFile> <xmlFile1> <xmlFile2> .. <xmlFileN>");
+		System.err.println("\t<schemaFile> -- The filename of the XSD schema to validate documents against");
+		System.err.println("\t<xmlFile1> .. <xmlFileN> -- A list of xml files to validate against the schema");
+	}
+
+
+	/**
+	 * This utility can be run from the command line as well
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			usage();
+			System.exit(1);
+		}
+		int errorCount = 0;
+		SchemaValidator validator = null;
+		try {
+			validator = new SchemaValidator(args[0]);
+		} catch (SchemaValidationException ex) {
+			System.err.println("Error initializing the schema validator, exiting...");
+			ex.printStackTrace();
+			System.exit(1);
+		}
+		
+		for (int i = 1; i < args.length; i++) {
+			try {
+				validator.validate(new File(args[i]));
+				System.out.println("Valid: " + args[i]);
+			} catch (Exception ex) {
+				System.err.println(ex.getMessage());
+				errorCount++;
+			}
+		}
+		System.exit(errorCount);
+	}
+}
