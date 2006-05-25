bebffc607f31e156b6a6d36869a838b6449b0f85
dervin <dervin>
2006-05-25 20:38:19 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2506 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/Utils.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/Utils.java
@@ -170,10 +170,8 @@ public class Utils {
 			return false;
 		} else if ((o1 == null) && (o2 != null)) {
 			return false;
-		} else if (o1.equals(o2)) {
-			return true;
 		} else {
-			return false;
+			return o1.equals(o2);
 		}
 	}
 
