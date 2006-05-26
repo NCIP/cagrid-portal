b396ea78c017258e00dbecb679ce9322a5b9648e
xffffffff <xffffffff>
2006-05-26 19:34:44 +0000

    - domain model application based on umldiagram component - visual studio theme that i plan to use for the domain model application
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2531 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelViewer.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.cagrid.graph.domainmodelapplication;
+
+import gov.nih.nci.cagrid.graph.uml.UMLDiagram;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
+
+import javax.swing.JTree;
+
+public class DomainModelViewer {
+	
+	public DomainModel activeModel;
+	
+	public DomainModelViewer()
+	{
+		
+	}
+
+}
