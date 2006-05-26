496b2f6a9f14a50597d96f4c7620650f2ef4415d
xffffffff <xffffffff>
2006-05-26 20:12:54 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2535 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelViewer.java
+++ cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelViewer.java
@@ -1,9 +1,6 @@
 package gov.nih.nci.cagrid.graph.domainmodelapplication;
 
-import gov.nih.nci.cagrid.graph.uml.UMLDiagram;
 import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
-
-import javax.swing.JTree;
 
 public class DomainModelViewer {
 	
