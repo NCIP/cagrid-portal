167eb0b41405dbc93d10ec8b7a2418785c92a148
dervin <dervin>
2006-05-31 20:23:57 +0000

    added domain validation test case
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2634 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/ValidDomainTestCase.java
@@ -0,0 +1,114 @@
+package gov.nih.nci.cagrid.data;
+
+import gov.nih.nci.cagrid.cqlquery.CQLQuery;
+import gov.nih.nci.cagrid.data.cql.validation.CQLValidator;
+import gov.nih.nci.cagrid.data.cql.validation.ObjectWalkingCQLValidator;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import org.globus.wsrf.encoding.ObjectDeserializer;
+import org.xml.sax.InputSource;
+
+/** 
+ *  ValidDomainTestCase
+ *  TODO:DOCUMENT ME
+ * 
+ * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
+ * 
+ * @created May 26, 2006 
+ * @version $Id$ 
+ */
+public class ValidDomainTestCase extends TestCase {
+	private CQLValidator validator = null;
+	private DomainModel domainModel = null;
+	private String cqlDocsDir = null;
+	
+	public ValidDomainTestCase(String name) {
+		super(name);
+	}
+	
+	
+	protected void setUp() {
+		validator = new ObjectWalkingCQLValidator();
+		cqlDocsDir = System.getProperty("cql.docs.dir");
+	}
+	
+	
+	protected void tearDown() {
+		validator = null;
+	}
+	
+	
+	private CQLQuery getQuery(String filename) {
+		try {
+			System.out.println("Validating CQL " + filename + " against domain model");
+			InputSource queryInput = new InputSource(new FileReader(filename));
+			CQLQuery query = (CQLQuery) ObjectDeserializer.deserialize(queryInput, CQLQuery.class);
+			return query;
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found: " + filename);
+			fail(ex.getMessage());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	private DomainModel getDomainModel() {
+		if (domainModel == null) {
+			try {
+				InputSource domainModelInput = new InputSource(new FileReader("test/resources/domainModel.xml"));
+				domainModel = (DomainModel) ObjectDeserializer.deserialize(domainModelInput, DomainModel.class);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				return null;
+			}
+		}
+		return domainModel;
+	}
+	
+	
+	private void checkQuery(String filename) {
+		CQLQuery query = getQuery(cqlDocsDir + File.separator + "domain" + File.separator + filename);
+		try {
+			validator.validateDomain(query, getDomainModel());
+			System.out.println("The query appears valid");
+			assertTrue("The query appears valid", true);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("Query is invalid: " + ex.getMessage());
+			fail("Query found to be invalid: " + ex.getMessage());
+		}
+	}
+	
+	
+	public void testValidObjectWithAttribute() {
+		checkQuery("objectWithAttribute.xml");
+	}
+	
+	
+	/*
+	 * Until the caDSR accuratly returns target and source association edges, 
+	 * this test is turned off.  Presently it returns the same class for both
+	 * edges, which obviously isn't right
+	public void testValidObjectWithAssociation() {
+		checkQuery("objectWithAssociation.xml");
+	}
+	*/
+	
+	
+	public static void main(String[] args) {
+		TestRunner runner = new TestRunner();
+		TestResult result = runner.doRun(new TestSuite(ValidDomainTestCase.class));
+		System.exit(result.errorCount() + result.failureCount());
+	}
+}
