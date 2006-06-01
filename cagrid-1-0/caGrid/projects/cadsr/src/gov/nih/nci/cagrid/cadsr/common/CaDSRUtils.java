d2122083b16b26f15ad9df8a0986f96b054b05e7
dervin <dervin>
2006-06-01 15:02:46 +0000

    support utilities for DomainModel building
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2654 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/cadsr/src/gov/nih/nci/cagrid/cadsr/common/CaDSRUtils.java
+++ cagrid-1-0/caGrid/projects/cadsr/src/gov/nih/nci/cagrid/cadsr/common/CaDSRUtils.java
@@ -1,13 +1,20 @@
-/**
- * 
- */
 package gov.nih.nci.cagrid.cadsr.common;
 
+import gov.nih.nci.cadsr.umlproject.domain.SemanticMetadata;
 import gov.nih.nci.cadsr.umlproject.domain.UMLAssociationMetadata;
+import gov.nih.nci.cadsr.umlproject.domain.UMLAttributeMetadata;
 import gov.nih.nci.cadsr.umlproject.domain.UMLClassMetadata;
 import gov.nih.nci.cagrid.cadsr.domain.UMLAssociation;
 import gov.nih.nci.cagrid.cadsr.domain.UMLAssociationSourceUMLClassMetadata;
 import gov.nih.nci.cagrid.cadsr.domain.UMLAssociationTargetUMLClassMetadata;
+import gov.nih.nci.cagrid.metadata.common.UMLAttribute;
+import gov.nih.nci.cagrid.metadata.common.UMLAttributeSemanticMetadataCollection;
+import gov.nih.nci.cagrid.metadata.common.UMLClass;
+import gov.nih.nci.cagrid.metadata.common.UMLClassSemanticMetadataCollection;
+import gov.nih.nci.cagrid.metadata.common.UMLClassUmlAttributeCollection;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 
 /**
@@ -43,6 +50,60 @@ public class CaDSRUtils {
 		}
 
 		return converted;
-
+	}
+	
+	
+	public static UMLClass convertClass(UMLClassMetadata classMetadata) {
+		UMLClass converted = null;
+		if (classMetadata != null) {
+			converted = new UMLClass();
+			converted.setClassName(classMetadata.getFullyQualifiedName());
+			converted.setDescription(classMetadata.getDescription());
+			converted.setPackageName(classMetadata.getUMLPackageMetadata().getName());
+			converted.setProjectName(classMetadata.getProject().getLongName());
+			converted.setUmlAttributeCollection(convertAttributeCollection(classMetadata));
+			SemanticMetadata[] smArray = convertSemanticMetadataCollection(
+				classMetadata.getSemanticMetadataCollection()); 
+			converted.setSemanticMetadataCollection(new UMLClassSemanticMetadataCollection(smArray));
+		}
+		return converted;
+	}
+	
+	
+	public static UMLClassUmlAttributeCollection convertAttributeCollection(UMLClassMetadata classMetadata) {
+		UMLAttribute[] attribArray = new UMLAttribute[classMetadata.getUMLAssociationMetadataCollection().size()];
+		int i = 0;
+		Iterator attribIter = classMetadata.getUMLAttributeMetadataCollection().iterator();
+		while (attribIter.hasNext()) {
+			UMLAttributeMetadata attribMetadata = (UMLAttributeMetadata) attribIter.next();
+			attribArray[i] = convertAttribute(attribMetadata);
+			i++;
+		}
+		return new UMLClassUmlAttributeCollection(attribArray);
+	}
+	
+	
+	public static UMLAttribute convertAttribute(UMLAttributeMetadata attribMetadata) {
+		UMLAttribute converted = null;
+		if (attribMetadata != null) {
+			converted = new UMLAttribute();
+			converted.setDescription(attribMetadata.getDescription());
+			converted.setName(attribMetadata.getName());
+			SemanticMetadata[] smArray = convertSemanticMetadataCollection(attribMetadata.getSemanticMetadataCollection());
+			converted.setSemanticMetadataCollection(new UMLAttributeSemanticMetadataCollection(smArray));
+		}
+		return converted;
+	}
+	
+	
+	public static SemanticMetadata[] convertSemanticMetadataCollection(Collection semanticMetadata) {
+		SemanticMetadata[] smArray = new SemanticMetadata[semanticMetadata.size()];
+		int i = 0;
+		Iterator semanticIter = semanticMetadata.iterator();
+		while (semanticIter.hasNext()) {
+			smArray[i] = (SemanticMetadata) semanticIter.next();
+			i++;
+		}
+		return smArray;
 	}
 }
