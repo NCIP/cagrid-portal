212dbd23115cb8fedb25735e2d86e735c575fc19
dervin <dervin>
2006-06-06 14:07:42 +0000

    fixed
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2800 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/creation/ExtensionsTable.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/creation/ExtensionsTable.java
@@ -2,8 +2,6 @@ package gov.nih.nci.cagrid.introduce.portal.creation;
 
 import gov.nih.nci.cagrid.common.portal.PortalBaseTable;
 
-import java.util.Vector;
-
 import javax.swing.table.DefaultTableModel;
 
 
@@ -15,12 +13,9 @@ import javax.swing.table.DefaultTableModel;
 public class ExtensionsTable extends PortalBaseTable {
 
 	public static String NAME = "Extension Name";
-	public static String DATA = "Data";
-
 
 	public ExtensionsTable() {
 		super(createTableModel());
-		initialize();
 	}
 
 
@@ -29,18 +24,15 @@ public class ExtensionsTable extends PortalBaseTable {
 	}
 
 
-	public void addRow(final String input) {
+	public void addRow(String input) {
 		for (int i = 0; i < getRowCount(); i++) {
 			String existing = (String) getValueAt(i, 0);
 			if (existing.equals(input)) {
 				return;
 			}
 		}
-		final Vector v = new Vector();
-		v.add(input);
-		v.add(v);
 
-		((DefaultTableModel) this.getModel()).addRow(v);
+		((DefaultTableModel) this.getModel()).addRow(new String[] {input});
 		this.setRowSelectionInterval(this.getModel().getRowCount() - 1, this.getModel().getRowCount() - 1);
 	}
 
@@ -49,9 +41,7 @@ public class ExtensionsTable extends PortalBaseTable {
 		if ((row < 0) || (row >= getRowCount())) {
 			throw new Exception("invalid row");
 		}
-		Vector v = (Vector) getValueAt(row, 1);
-		v.set(0, input);
-		v.set(1, v);
+		setValueAt(input, row, 0);
 	}
 
 
@@ -71,7 +61,7 @@ public class ExtensionsTable extends PortalBaseTable {
 			modifySelectedRow(input2);
 			modifyRow(input1, getSelectedRow() - 1);
 			setRowSelectionInterval(getSelectedRow() - 1, getSelectedRow() - 1);
-			this.paint(this.getGraphics());
+			repaint();
 		}
 	}
 
@@ -83,7 +73,7 @@ public class ExtensionsTable extends PortalBaseTable {
 			modifySelectedRow(input2);
 			modifyRow(input1, getSelectedRow() + 1);
 			setRowSelectionInterval(getSelectedRow() + 1, getSelectedRow() + 1);
-			this.paint(this.getGraphics());
+			repaint();
 		}
 	}
 
@@ -108,7 +98,7 @@ public class ExtensionsTable extends PortalBaseTable {
 		if ((row < 0) || (row >= getRowCount())) {
 			throw new Exception("invalid row");
 		}
-		String name = ((String) getValueAt(row, 0));
+		String name = (String) getValueAt(row, 0);
 
 		return name;
 	}
@@ -116,13 +106,6 @@ public class ExtensionsTable extends PortalBaseTable {
 
 	public String getSelectedRowData() throws Exception {
 		return getRowData(getSelectedRow());
-	}
-
-
-	private void initialize() {
-		this.getColumn(DATA).setMaxWidth(0);
-		this.getColumn(DATA).setMinWidth(0);
-		this.getColumn(DATA).setPreferredWidth(0);
 	}
 
 
@@ -149,7 +132,6 @@ public class ExtensionsTable extends PortalBaseTable {
 		public MyDefaultTableModel() {
 			super();
 			addColumn(NAME);
-			addColumn(DATA);
 		}
 
 
