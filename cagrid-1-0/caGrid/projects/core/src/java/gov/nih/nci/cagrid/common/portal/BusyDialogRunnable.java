6fa0b227a99f4c2b4ce269ea828f64a97b987aa0
hastings <hastings>
2006-06-07 18:19:56 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2861 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
@@ -4,21 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
-
 public abstract class BusyDialogRunnable implements Runnable {
-	
+
 	final private BusyDialog dialog;
+
 	private String errorMessage = "";
+
 	private boolean valid = true;
+
 	private JFrame owner;
-	
-	
+
 	public BusyDialogRunnable(JFrame owner, String title) {
 		this.owner = owner;
 		dialog = new BusyDialog(owner, "Progress (" + title + ")");
 	}
-	
-	
+
 	public void setProgressText(final String text) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -26,8 +26,7 @@ public abstract class BusyDialogRunnable implements Runnable {
 			}
 		});
 	}
-	
-	
+
 	public void run() {
 		Thread thread = new Thread(new Runnable() {
 			public void run() {
@@ -52,29 +51,27 @@ public abstract class BusyDialogRunnable implements Runnable {
 			valid = false;
 			errorMessage = e.getMessage();
 		}
-		
-		//setProgressText("");
-		//dialog.setVisible(false);
-		
-		
+
 		if (!valid) {
 			JOptionPane.showMessageDialog(owner, errorMessage);
 		}
-		dialog.dispose();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				dialog.dispose();
+			}
+		});
+
 	}
-	
-	
+
 	public abstract void process();
-	
-	
+
 	public String getErrorMessage() {
 		return errorMessage;
 	}
-	
-	
+
 	public void setErrorMessage(String message) {
 		this.valid = false;
 		this.errorMessage = message;
 	}
-	
+
 }
