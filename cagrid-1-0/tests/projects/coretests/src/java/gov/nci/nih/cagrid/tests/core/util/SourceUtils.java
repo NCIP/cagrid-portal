c683caba46ec4657177669bc0908730f359bbc48
mccon012 <mccon012>
2006-06-11 13:14:33 +0000

    added modifyImpl and support methods
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2930 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/SourceUtils.java
+++ cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/SourceUtils.java
@@ -3,10 +3,12 @@
  */
 package gov.nci.nih.cagrid.tests.core.util;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
 
 import org.apache.ws.jaxme.js.JavaMethod;
 import org.apache.ws.jaxme.js.JavaSource;
@@ -21,7 +23,7 @@ public class SourceUtils
 	/**
 	 * Don't use this method - it is based on JAXME and does not work.
 	 */
-	public static void replaceMethodBodyWithJAXME(File inFile, String methodName, File outFile) 
+	public static void replaceMethodBodyWithJAXME(File inFile, File outFile, String methodName) 
 		throws RecognitionException, TokenStreamException, IOException
 	{
 		JavaSourceFactory inFactory = new JavaSourceFactory();
@@ -47,6 +49,87 @@ public class SourceUtils
 		outSource.write(out);
 		out.flush();
 		out.close();
+	}
+	
+	public static void modifyImpl(File sourceJava, File targetJava, String methodName) 
+		throws IOException
+	{
+		StringBuffer sourceContent = new StringBuffer(FileUtils.readText(sourceJava));
+		StringBuffer targetContent = new StringBuffer(FileUtils.readText(targetJava));
+
+		//SyncSource ss = new SyncSource(new File("junk"), null, null);
+		
+		int sourceStartOfMethod = startOfSignature(sourceContent, methodName);
+		int sourceEndOfSignature = endOfSignature(sourceContent, sourceStartOfMethod);
+		int sourceEndOfImplementation = endOfMethod(sourceContent, sourceEndOfSignature);
+		
+		int targetStartOfMethod = startOfSignature(targetContent, methodName);
+		int targetEndOfSignature = endOfSignature(targetContent, targetStartOfMethod);
+		int targetEndOfImplementation = endOfMethod(targetContent, targetEndOfSignature);
+		
+		targetContent.delete(targetEndOfSignature, targetEndOfImplementation);
+		targetContent.insert(targetEndOfSignature, 
+			sourceContent.substring(sourceEndOfSignature, sourceEndOfImplementation)
+		);
+		
+		FileWriter out = new FileWriter(targetJava);
+		out.write(targetContent.toString());
+		out.flush();
+		out.close();
+	}
+	
+	public static int endOfMethod(StringBuffer sb, int startingIndex)
+	{
+		int index = startingIndex;
+		if (index < 0) return index;
+
+		boolean found = false;
+		int openCount = 0;
+		do { 
+			char ch = sb.charAt(index);
+			if (ch == '{') {
+				openCount++;
+				found = true;
+			} else if (ch == '}') {
+				openCount--;
+			}
+			index++;
+		} while (openCount >= 0);// || ! found);
+		return index-1;
+	}
+	
+	public static int endOfSignature(StringBuffer sb, int startingIndex) 
+	{
+		int index = startingIndex;
+		if (index < 0) {
+			return index;
+		}
+		boolean found = false;
+		while (!found) {
+			char ch = sb.charAt(index);
+			if (ch == '{') {
+				found = true;
+			}
+			index++;
+		}
+		return index;
+	}
+
+	public static int startOfSignature(StringBuffer sb, String methodName) 
+		throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new StringReader(sb.toString()));
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			int index = line.indexOf(methodName);
+			if (index == -1) continue;
+			index = line.indexOf("public");
+			if (index == -1) continue;
+			br.close();
+			return sb.indexOf(line);
+		}
+		br.close();
+		return -1;
 	}
 	
 	public static JavaMethod findMethod(JavaSource source, String methodName)
