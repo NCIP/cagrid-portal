2f16c65606adb180cb72e15c498a011d043f806f
mccon012 <mccon012>
2006-06-11 13:18:21 +0000

    organized imports
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2938 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/FileUtils.java
+++ cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/FileUtils.java
@@ -12,8 +12,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * @author MCCON012
