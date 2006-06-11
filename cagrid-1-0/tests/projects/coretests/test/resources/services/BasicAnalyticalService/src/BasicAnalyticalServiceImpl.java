b50992db669619aa8eb90651270c4a2b08540369
mccon012 <mccon012>
2006-06-11 18:31:43 +0000

    removed metadata and schemas, changed xml and impl with latest introduce
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@2964 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/resources/services/BasicAnalyticalService/src/BasicAnalyticalServiceImpl.java
+++ cagrid-1-0/tests/projects/coretests/test/resources/services/BasicAnalyticalService/src/BasicAnalyticalServiceImpl.java
@@ -1,7 +1,5 @@
 package gov.nih.nci.cagrid.tests.service;
 
-import gov.nih.nci.cagrid.tests.service.globus.ServiceConfiguration;
-
 import java.rmi.RemoteException;
 
 import javax.naming.InitialContext;
@@ -44,28 +42,28 @@ public class BasicAnalyticalServiceImpl {
 
 
 	     public java.lang.String reverseTranslate(java.lang.String dna) throws RemoteException {
-		// reverse
-		StringBuffer dnaBuf = new StringBuffer(dna);
-		dnaBuf.reverse();
-		char[] bases = dnaBuf.toString().toCharArray();
-		
-		// translate
-		for (int i = 0; i < bases.length; i++) {
-			char base = bases[i]; 			
-			switch (base) {
-				case 'a': base = 't'; break;
-				case 'A': base = 'T'; break;
-				case 't': base = 'a'; break;
-				case 'T': base = 'A'; break;
-				case 'c': base = 'g'; break;
-				case 'C': base = 'G'; break;
-				case 'g': base = 'c'; break;
-				case 'G': base = 'C'; break;
-			}
-			bases[i] = base;
-		}
-		
-		return new String(bases);
+	 		// reverse
+	 		StringBuffer dnaBuf = new StringBuffer(dna);
+	 		dnaBuf.reverse();
+	 		char[] bases = dnaBuf.toString().toCharArray();
+	 		
+	 		// translate
+	 		for (int i = 0; i < bases.length; i++) {
+	 			char base = bases[i]; 			
+	 			switch (base) {
+	 				case 'a': base = 't'; break;
+	 				case 'A': base = 'T'; break;
+	 				case 't': base = 'a'; break;
+	 				case 'T': base = 'A'; break;
+	 				case 'c': base = 'g'; break;
+	 				case 'C': base = 'G'; break;
+	 				case 'g': base = 'c'; break;
+	 				case 'G': base = 'C'; break;
+	 			}
+	 			bases[i] = base;
+	 		}
+	 		
+	 		return new String(bases);
 	}
 
 }
