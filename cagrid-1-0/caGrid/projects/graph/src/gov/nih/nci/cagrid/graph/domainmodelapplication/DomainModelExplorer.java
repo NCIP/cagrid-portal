86dd972ad497825cd64c9b90504f07ed38c6459e
oster <oster>
2006-06-12 23:13:27 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3007 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelExplorer.java
+++ cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelExplorer.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.cagrid.graph.domainmodelapplication;
 
+
+
 import gov.nih.nci.cagrid.graph.uml.UMLDiagram;
 import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
 
@@ -16,289 +18,346 @@ import java.util.Vector;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
-
-public class DomainModelExplorer extends JLayeredPane {
+public class DomainModelExplorer extends JLayeredPane
+{
 	public DomainModel model;
 	public Vector packages;
-
+	
 	public DomainModelOutline domainModelOutline;
-
+	
 	public DomainModelOutlines outlineMDI;
 	public DomainModelUMLViews umlMDI;
-	public JPanel splitter = new JPanel();
-
+	public JPanel              splitter = new JPanel();
+	
 	public Point splitterLastClicked;
 	public boolean splitterMoving = false;
-
+	
 	public static int preferredOutlinesWidth = 300;
-
+	
 	public boolean hideOutlines = false;
-
+	
 	public boolean resizedOnce = false;
-
+	
 	public HashMap cache = new HashMap();
+	
 
-
-	public DomainModelExplorer() {
-		domainModelOutline = new DomainModelOutline(this);
-
-		outlineMDI = new DomainModelOutlines(this);
-		umlMDI = new DomainModelUMLViews(this);
-
-		this.add(outlineMDI);
-		this.add(umlMDI);
-		this.add(splitter);
-
-		this.setLayer(splitter, JLayeredPane.MODAL_LAYER.intValue());
-
-		this.splitter.setCursor(Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR));
-
-		this.addComponentListener(new DomainModelExplorerComponentListener());
-		this.splitter.addMouseListener(new DomainModelSplitterMouseListener());
-		this.splitter.addMouseMotionListener(new DomainModelSplitterMouseMotionListener());
-
-		setDomainModel(null);
-
+	
+	
+	public DomainModelExplorer()
+	{
+			domainModelOutline = new DomainModelOutline(this);
+		
+			outlineMDI = new DomainModelOutlines(this);
+			umlMDI = new DomainModelUMLViews(this);
+					
+			this.add(outlineMDI);
+			this.add(umlMDI);
+			this.add(splitter);
+			
+			this.setLayer(splitter, JLayeredPane.MODAL_LAYER.intValue());
+			
+				
+			this.splitter.setCursor(Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR));
+	
+			
+			this.addComponentListener(new DomainModelExplorerComponentListener());
+			this.splitter.addMouseListener(new DomainModelSplitterMouseListener());
+			this.splitter.addMouseMotionListener(new DomainModelSplitterMouseMotionListener());
+		
+			setDomainModel(null);
+		
 	}
-
-
-	public void showPage(DomainModelTreeNode node, String pageId) {
-		if (this.umlMDI.pageIDs.contains(pageId)) {
-			if (node.type == DomainModelTreeNode.CLASS) {
+	
+	public void showPage(DomainModelTreeNode node, String pageId)
+	{
+		if(this.umlMDI.pageIDs.contains(pageId))
+		{
+			if(node.type == DomainModelTreeNode.CLASS)
+			{
 				UMLDiagram diagram = (UMLDiagram) this.umlMDI.getPageById(node.name);
-
-				if (diagram != null) {
+				
+				if(diagram != null)
+				{
 					diagram.scrollToShowClass(node.name);
 				}
 			}
-
+			
 			this.umlMDI.setActivePageByID(pageId);
-		} else {
-			if (node.type == DomainModelTreeNode.CLASS) {
-				// UMLDiagram d = new UMLDiagram();
-				// initializeUMLDiagram(d, model, node.name);
-				// d.scrollToShowClass(node.name);
-				// this.umlMDI.addPage(d, null, node.name, node.name);
-
-			} else if (node.type == DomainModelTreeNode.PACKAGE) {
+		}
+		else
+		{
+			if(node.type == DomainModelTreeNode.CLASS)
+			{
+				//UMLDiagram d = new UMLDiagram();
+				//initializeUMLDiagram(d, model, node.name);
+				//d.scrollToShowClass(node.name);
+				//this.umlMDI.addPage(d, null, node.name, node.name);
+				
+			
+			}
+			else if(node.type == DomainModelTreeNode.PACKAGE)
+			{
 				UMLDiagram d = new UMLDiagram();
 				initializeUMLDiagram(d, model, node.name);
 				this.umlMDI.addPage(d, null, node.name, node.name);
-
-			} else if (node.type == DomainModelTreeNode.DOMAIN) {
+				
+			}
+			else if(node.type == DomainModelTreeNode.DOMAIN)
+			{
 				DomainModelOverview o = new DomainModelOverview(model, packages);
-
-				this.umlMDI.addPage(o, null, node.name, node.name);
+				
+				this.umlMDI.addPage(o, null, node.name , node.name);
 			}
 		}
 	}
-
-
-	public void initializeUMLDiagram(UMLDiagram d, DomainModel m, String name) {
+	
+	
+	public void initializeUMLDiagram(UMLDiagram d, DomainModel m, String name)
+	{
 		d.clear();
-
-		// add classes
-		for (int k = 0; k < this.model.getExposedUMLClassCollection().length; k++) {
-			gov.nih.nci.cagrid.metadata.common.UMLClass c = model.getExposedUMLClassCollection()[k];
-
-			if (c.getPackageName().equals(name)) {
-				gov.nih.nci.cagrid.graph.uml.UMLClass C = new gov.nih.nci.cagrid.graph.uml.UMLClass(DomainModelOutline
-					.trimClassName(c.getClassName()));
-
-				d.addClass(C);
-
+		
+		// add classes 
+		for(int k = 0 ; k < this.model.getExposedUMLClassCollection().getUMLClass().length; k++)
+		{
+			gov.nih.nci.cagrid.metadata.common.UMLClass c =  model.getExposedUMLClassCollection().getUMLClass()[k];
+			
+			if(c.getPackageName().equals(name))
+			{
+				if(c != null)
+				{
+					gov.nih.nci.cagrid.graph.uml.UMLClass C = new gov.nih.nci.cagrid.graph.uml.UMLClass(DomainModelOutline.trimClassName(c.getClassName()));
+					
+					d.addClass(C);
+				}
 			}
 		}
-
+		
 		// add assocs
-
+		
 		d.refresh();
 	}
-
-
-	public void setDomainModel(DomainModel model) {
+	
+	
+	public void setDomainModel(DomainModel model)
+	{
 		clear();
 		this.domainModelOutline.setDomainModel(model, packages);
-
-		if (model != null) {
+		
+		if(model != null)
+		{
 			initModel(model);
 			this.domainModelOutline.setDomainModel(this.model, this.packages);
-
-			this.showPage((DomainModelTreeNode) this.domainModelOutline.tree.getModel().getRoot(), "");
+			
+			this.showPage((DomainModelTreeNode)this.domainModelOutline.tree.getModel().getRoot(), "");
 		}
-
+		
 		this.domainModelOutline.tree.expandAll();
 	}
-
-
-	public void initModel(DomainModel model) {
+	
+	
+	
+	public void initModel(DomainModel model)
+	{
 		this.model = model;
 		packages = new Vector();
-
-		// uses O(n^2) algorithm to make a list of classes in each package in
-		// the
+		
+		// uses O(n^2) algorithm to make a list of classes in each package in the
 		// domain model... could be left for improvement later.
-
-		for (int k = 0; k < model.getExposedUMLClassCollection().length; k++) {
-			gov.nih.nci.cagrid.metadata.common.UMLClass c = model.getExposedUMLClassCollection()[k];
-
-			if (c != null) {
+		
+		for(int k = 0; k < model.getExposedUMLClassCollection().getUMLClass().length; k++)
+		{
+			gov.nih.nci.cagrid.metadata.common.UMLClass c =  model.getExposedUMLClassCollection().getUMLClass()[k];
+			
+			if(c != null)
+			{
 				String packageName = c.getPackageName();
 				String className = c.getClassName();
-
+				
 				int found = -1;
-
-				for (int j = 0; j < packages.size(); j++) {
+				
+				for(int j = 0; j < packages.size() ; j++)
+				{
 					MultiMapElement e = (MultiMapElement) packages.get(j);
-
-					// System.out.println(packageName + " . " + e.head);
-
-					if (packageName.equals(e.head)) {
+					
+					//System.out.println(packageName + " . " + e.head);
+					
+					if(packageName.equals(e.head))
+					{
 						found = j;
 					}
 				}
-
-				if (found == -1) {
+				
+				if(found == -1)
+				{
 					MultiMapElement e = new MultiMapElement();
 					e.head = packageName;
 					e.list.add(className);
-
+					
 					packages.add(e);
-				} else {
+				}
+				else
+				{
 					MultiMapElement e = (MultiMapElement) packages.get(found);
 					e.list.add(className);
 					packages.remove(found);
 					packages.add(found, e);
-
+					
+				
 				}
 			}
 		}
-
-		// // 'packages' is now a "multimap" of package-class[] elements;
-		// for(int z = 0; z < packages.size(); z++)
-		// {
-		// MultiMapElement e = (MultiMapElement) packages.get(z);
-		// System.out.println(e.head);
-		//			
-		// for(int u = 0 ; u < e.list.size(); u++)
-		// {
-		// System.out.println(" " + (String)e.list.get(u));
-		// }
-		// }
-
+		
+		
+//		// 'packages' is now a "multimap" of package-class[] elements;
+//		for(int z = 0; z < packages.size(); z++)
+//		{
+//			MultiMapElement e = (MultiMapElement) packages.get(z);
+//			System.out.println(e.head);
+//			
+//			for(int u = 0 ; u < e.list.size(); u++)
+//			{
+//				System.out.println("        "  + (String)e.list.get(u));
+//			}
+//		}
+		
+		
 	}
-
-
-	public void clear() {
+	
+	
+	
+	public void clear()
+	{
 		this.model = null;
-		if (packages == null) {
+		if(packages == null)
+		{
 			this.packages = new Vector();
-		} else {
+		}
+		else
+		{
 			this.packages.clear();
 		}
-
+		
+		
 		this.outlineMDI.clear();
 		this.umlMDI.clear();
-
+		
+		
+		
+		
 	}
-
-
-	public void hideOutlines() {
+	
+	public void hideOutlines()
+	{
 		this.hideOutlines = true;
 		this.resizeComponents();
 	}
-
-
-	public void showOutlines() {
+	
+	
+	
+	public void showOutlines()
+	{
 		this.hideOutlines = false;
 		this.resizeComponents();
 	}
-
-
-	public void selectDomainModel() {
-
+	
+	public void selectDomainModel()
+	{
+		
 	}
-
-
-	protected void resizeComponents() {
-		if (this.hideOutlines) {
+	
+	protected void resizeComponents()
+	{
+		if(this.hideOutlines)
+		{
 			this.outlineMDI.setVisible(false);
 			this.splitter.setVisible(false);
 			this.outlineMDI.setBounds(0, 0, 0, 0);
 			this.splitter.setBounds(0, 0, 0, 0);
 			this.umlMDI.setBounds(0, 0, this.getWidth(), this.getHeight());
-		} else {
+		}
+		else
+		{
 			this.outlineMDI.setVisible(true);
 			this.splitter.setVisible(true);
-			if (!this.resizedOnce) {
+			if(!this.resizedOnce)
+			{
 				this.outlineMDI.setBounds(0, 0, DomainModelExplorer.preferredOutlinesWidth, this.getHeight());
 				this.splitter.setBounds(DomainModelExplorer.preferredOutlinesWidth, 0, 3, this.getHeight());
-				this.umlMDI.setBounds(DomainModelExplorer.preferredOutlinesWidth + this.splitter.getWidth(), 0, this
-					.getWidth()
-					- DomainModelExplorer.preferredOutlinesWidth - this.splitter.getWidth(), this.getHeight());
+				this.umlMDI.setBounds(DomainModelExplorer.preferredOutlinesWidth + this.splitter.getWidth(), 0, this.getWidth() - DomainModelExplorer.preferredOutlinesWidth - this.splitter.getWidth(), this.getHeight());
 				this.resizedOnce = true;
-			} else {
+			}
+			else
+			{
 				this.outlineMDI.setBounds(0, 0, outlineMDI.getWidth(), this.getHeight());
 				this.splitter.setBounds(outlineMDI.getWidth(), 0, 3, this.getHeight());
-				this.umlMDI.setBounds(outlineMDI.getWidth() + this.splitter.getWidth(), 0, this.getWidth()
-					- outlineMDI.getWidth() - this.splitter.getWidth(), this.getHeight());
+				this.umlMDI.setBounds(outlineMDI.getWidth() + this.splitter.getWidth(), 0, this.getWidth() - outlineMDI.getWidth() - this.splitter.getWidth(), this.getHeight());			
 			}
 		}
 	}
-
-
-	protected void requestSplitterMove(int x) {
-		// if *PROJECTED/anticipated* move violates bounds THEN disallow move,
-		// otherwise grant
-		// splitter.setBackground(Color.gray);
-		if (splitter.getLocation().getX() + x - splitterLastClicked.x >= 200) {
-			splitter.setLocation(splitter.getX() + x - splitterLastClicked.x + 1, splitter.getY());
-		} else {
+	
+	protected void requestSplitterMove(int x)
+	{
+		// if *PROJECTED/anticipated* move violates bounds THEN disallow move, otherwise grant
+		//splitter.setBackground(Color.gray);
+		if(splitter.getLocation().getX()+x-splitterLastClicked.x >= 200)
+		{
+			splitter.setLocation(splitter.getX() + x  - splitterLastClicked.x + 1, splitter.getY() );		
+		}
+		else
+		{
 			splitter.setLocation(200, splitter.getY());
 		}
 	}
-
-
-	protected void splitterMoved() {
+	
+	protected void splitterMoved()
+	{
 		outlineMDI.setSize(splitter.getX(), outlineMDI.getHeight());
-		umlMDI.setBounds(splitter.getX() + splitter.getWidth(), 0, getWidth() - outlineMDI.getWidth()
-			- splitter.getWidth(), getHeight());
-
+		umlMDI.setBounds(splitter.getX() + splitter.getWidth(), 0, getWidth() - outlineMDI.getWidth() - splitter.getWidth(), getHeight());		
+			
 	}
 }
 
-class DomainModelExplorerComponentListener extends ComponentAdapter {
-	public void componentResized(ComponentEvent e) {
+
+
+class DomainModelExplorerComponentListener extends ComponentAdapter
+{
+	public void componentResized(ComponentEvent e)
+	{
 		DomainModelExplorer s = (DomainModelExplorer) e.getSource();
 		s.resizeComponents();
 	}
 }
 
-class DomainModelSplitterMouseListener extends MouseAdapter {
-	public void mousePressed(MouseEvent e) {
-		JPanel splitter = (JPanel) e.getSource();
+class DomainModelSplitterMouseListener extends MouseAdapter
+{
+	public void mousePressed(MouseEvent e)
+	{
+		JPanel splitter = (JPanel)	e.getSource();
 		DomainModelExplorer parent = (DomainModelExplorer) splitter.getParent();
 		parent.splitterLastClicked = e.getPoint();
 		parent.splitterMoving = true;
-
+		
 	}
-
-
-	public void mouseReleased(MouseEvent e) {
-		JPanel splitter = (JPanel) e.getSource();
+	
+	public void mouseReleased(MouseEvent e)
+	{
+		JPanel splitter = (JPanel)	e.getSource();
 		DomainModelExplorer parent = (DomainModelExplorer) splitter.getParent();
-
+		
 		parent.splitterMoved();
-		// parent.splitter.setBackground(Color.lightGray);
+		//parent.splitter.setBackground(Color.lightGray);
 	}
 }
 
-class DomainModelSplitterMouseMotionListener extends MouseMotionAdapter {
-	public void mouseDragged(MouseEvent e) {
-		JPanel splitter = (JPanel) e.getSource();
+class DomainModelSplitterMouseMotionListener extends MouseMotionAdapter
+{
+	public void mouseDragged(MouseEvent e)
+	{
+		JPanel splitter = (JPanel)	e.getSource();
 		DomainModelExplorer parent = (DomainModelExplorer) splitter.getParent();
-
+		
 		parent.requestSplitterMove(e.getX());
-
+		
+	
 	}
 }
--- cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelOverview.java
+++ cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/domainmodelapplication/DomainModelOverview.java
@@ -13,69 +13,76 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-
-public class DomainModelOverview extends JPanel {
+public class DomainModelOverview extends JPanel
+{
 	public DomainModel model;
-
+	
 	public ImageIcon nci = new ImageIcon(System.getProperty("user.dir") + "\\resource\\nci.png");
 	public ImageIcon nih = new ImageIcon(System.getProperty("user.dir") + "\\resource\\nih.png");
 	public ImageIcon red = new ImageIcon(System.getProperty("user.dir") + "\\resource\\red.png");
-
+	
 	public Font bold = new Font("verdana", Font.BOLD, 12);
 	public Font plain = new Font("verdana", Font.PLAIN, 13);
-
+	
 	public JTextArea text = new JTextArea();
-
-
-	public DomainModelOverview(DomainModel m, Vector packages) {
+	
+	public DomainModelOverview(DomainModel m, Vector packages)
+	{
 		super();
-
+		
 		this.model = m;
-
+		
 		this.add(text);
-
+		
 		super.setLayout(null);
-
+		
 		this.setBackground(Color.white);
-
+		
 		this.text.setEditable(false);
 		this.text.setFont(plain);
-
+		
 		this.addComponentListener(new DomainModelOverviewComponentListener());
-
-		if (m != null) {
+		
+		if(m != null)
+		{
 			this.text.append("Project Name: " + m.getProjectLongName() + "\n");
 			this.text.append("Project Version: " + m.getProjectVersion() + "\n");
 			this.text.append("Total Packages: " + packages.size() + "\n");
-
-			this.text.append("Total Classes: " + m.getExposedUMLClassCollection().length + "\n");
+			
+			this.text.append("Total Classes: " + m.getExposedUMLClassCollection().getUMLClass().length + "\n");
 			this.text.append("______________________________________________________________\n\n");
 			this.text.append("Project Description: " + m.getProjectDescription() + "\n");
-
+			
 		}
 	}
-
-
-	public void paint(Graphics g) {
+	
+	public void paint(Graphics g)
+	{
 		super.paint(g);
 		g.drawImage(red.getImage(), 0, 0, this.getWidth(), nci.getIconHeight(), this);
-		// g.drawImage(nci.getImage(), 0, 0, nci.getIconWidth(),
-		// nci.getIconHeight(), this);
-		g.drawImage(nih.getImage(), this.getWidth() - this.nih.getIconWidth(), 0, nih.getIconWidth(), nih
-			.getIconHeight(), this);
-
+		//g.drawImage(nci.getImage(), 0, 0, nci.getIconWidth(), nci.getIconHeight(), this);
+		g.drawImage(nih.getImage(), this.getWidth()-this.nih.getIconWidth(), 0, nih.getIconWidth(), nih.getIconHeight(), this);
+		
+		
+	
 	}
-
+	
+	
 }
 
-class DomainModelOverviewComponentListener extends ComponentAdapter {
-	public void componentResized(ComponentEvent e) {
+class DomainModelOverviewComponentListener extends ComponentAdapter
+{
+	public void componentResized(ComponentEvent e)
+	{
 		DomainModelOverview s = (DomainModelOverview) e.getSource();
+		
+
 
 		s.text.setBounds(35, 60, s.getWidth() - 80, s.getHeight() - 80);
-
+		
+		
 		s.validate();
-
+		
 	}
-
+	
 }
