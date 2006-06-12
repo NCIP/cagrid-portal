a373eebc9264f637b88e07d38874ceb4d1f9a535
xffffffff <xffffffff>
2006-06-12 20:05:30 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3000 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/uml/Main.java
+++ cagrid-1-0/caGrid/projects/graph/src/gov/nih/nci/cagrid/graph/uml/Main.java
@@ -1,25 +1,16 @@
 package gov.nih.nci.cagrid.graph.uml;
 
 import gov.nih.nci.cagrid.graph.domainmodelapplication.DomainModelExplorer;
-import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import org.globus.wsrf.encoding.DeserializationException;
-import org.globus.wsrf.encoding.ObjectDeserializer;
-import org.xml.sax.InputSource;
-
 // Test Driver for the UMLDiagram class
 
 public class Main {
 
-	public static void main(String args[]) throws InterruptedException, FileNotFoundException, DeserializationException {
+	public static void main(String args[]) {
 
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
@@ -35,15 +26,10 @@ public class Main {
 	
 		
 		
-		FileInputStream fis = new FileInputStream(new File(System.getProperty("user.dir") + "\\domainmodel.xml"));
-		
-		InputSource is = new InputSource(fis);
-		
-		DomainModel model = (DomainModel) ObjectDeserializer.deserialize(is, DomainModel.class);
-		
+
 		DomainModelExplorer e = new DomainModelExplorer();
 		
-		e.setDomainModel(model);
+
 		
 		
 		
