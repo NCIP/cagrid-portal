4031c4383e88fa5d7d6ef721e437a2fda09cba78
hastings <hastings>
2006-06-13 01:56:11 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3016 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/preferences/extensions/ExtensionsPreferencesConfigurationPanel.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/preferences/extensions/ExtensionsPreferencesConfigurationPanel.java
@@ -1,23 +1,6 @@
 package gov.nih.nci.cagrid.introduce.portal.preferences.extensions;
 
-import IntroducePortalConf;
 import gov.nih.nci.cagrid.introduce.portal.preferences.BasePreferenceConfigurationPanel;
-
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-
-import org.jdom.Document;
-import org.jdom.Element;
-import org.omg.CORBA.INITIALIZE;
-import org.projectmobius.common.MobiusException;
-import org.projectmobius.common.XMLUtilities;
-import org.projectmobius.portal.PortalResourceManager;
 
 public abstract class ExtensionsPreferencesConfigurationPanel extends
 		BasePreferenceConfigurationPanel {
