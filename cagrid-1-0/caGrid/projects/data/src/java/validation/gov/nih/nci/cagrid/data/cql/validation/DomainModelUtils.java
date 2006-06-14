2702cd7a93af767e7525ea360c198ab045730203
dervin <dervin>
2006-06-14 18:57:52 +0000

    walk the model once, return classes quicker
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3079 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/validation/gov/nih/nci/cagrid/data/cql/validation/DomainModelUtils.java
+++ cagrid-1-0/caGrid/projects/data/src/java/validation/gov/nih/nci/cagrid/data/cql/validation/DomainModelUtils.java
@@ -4,13 +4,24 @@ import gov.nih.nci.cagrid.metadata.common.UMLClass;
 import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
 import gov.nih.nci.cagrid.metadata.dataservice.UMLClassReference;
 
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * @author oster
- * 
+
+/** 
+  *  DomainModelUtils
+  *  Utilities for walking and manipulating a DomainModel
+  * 
+  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
+  * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster</A>
+  * 
+  * @created Jun 14, 2006 
+  * @version $Id$
  */
 public class DomainModelUtils {
 
+	private static Map domainRefedClasses = new HashMap();
+	
 	/**
 	 * Return the UMLClass from the DomainModel's exposed UML Classes if the
 	 * UMLClass with an id exists which is equal to the refid of the
@@ -26,14 +37,17 @@ public class DomainModelUtils {
 		if (model == null) {
 			return null;
 		}
-		UMLClass[] classes = model.getExposedUMLClassCollection().getUMLClass();
-		for (int i = 0; i < classes.length; i++) {
-			UMLClass clazz = classes[i];
-			if (clazz.getId().equals(reference.getRefid())) {
-				return clazz;
+		Map refedClasses = (Map) domainRefedClasses.get(model);
+		if (refedClasses == null) {
+			// populate class references for this model
+			UMLClass[] classes = model.getExposedUMLClassCollection().getUMLClass();
+			refedClasses = new HashMap(classes.length);
+			for (int i = 0; i < classes.length; i++) {
+				UMLClass clazz = classes[i];
+				refedClasses.put(clazz.getId(), clazz);
 			}
 		}
-		return null;
-
+		UMLClass refedClass = (UMLClass) refedClasses.get(reference.getRefid());
+		return refedClass;
 	}
 }
