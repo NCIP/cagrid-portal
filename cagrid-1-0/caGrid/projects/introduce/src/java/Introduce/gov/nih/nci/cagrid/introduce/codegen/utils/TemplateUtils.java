1bd36c7132eace927ea6b380210dc72f07a56a8c
dervin <dervin>
2006-06-16 15:05:15 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3129 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/codegen/utils/TemplateUtils.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/codegen/utils/TemplateUtils.java
@@ -32,7 +32,7 @@ import org.projectmobius.common.XMLUtilities;
  *          Exp $
  */
 public class TemplateUtils {
-
+	
 	/**
 	 * Returns the input string with the first character converted to lowercase
 	 * 
@@ -42,9 +42,10 @@ public class TemplateUtils {
 	 */
 	public static String lowerCaseFirstCharacter(String variableName) {
 		return variableName.substring(0, 1).toLowerCase()
-				+ variableName.substring(1);
+		+ variableName.substring(1);
 	}
-
+	
+	
 	/**
 	 * Returns the input string with the first character converted to uppercase
 	 * 
@@ -54,129 +55,113 @@ public class TemplateUtils {
 	 */
 	public static String upperCaseFirstCharacter(String variableName) {
 		return variableName.substring(0, 1).toUpperCase()
-				+ variableName.substring(1);
+		+ variableName.substring(1);
 	}
-
+	
+	
 	public static Map buildMasterNamespaceInformationMap(ServiceDescription desc) {
 		Map map = new HashMap();
 		int namespaceCount = 0;
 		if (desc.getNamespaces() != null
-				&& desc.getNamespaces().getNamespace() != null) {
+			&& desc.getNamespaces().getNamespace() != null) {
 			for (int i = 0; i < desc.getNamespaces().getNamespace().length; i++) {
 				NamespaceType ntype = desc.getNamespaces().getNamespace(i);
 				// add the ns=>prefix entry
 				if (!map.containsKey(ntype.getNamespace())) {
 					if (ntype.getNamespace().equals(
-							IntroduceConstants.W3CNAMESPACE)) {
+						IntroduceConstants.W3CNAMESPACE)) {
 						map.put(ntype.getNamespace(), new NamespaceInformation(
-								ntype, IntroduceConstants.W3CNAMESPACE_PREFIX));
+							ntype, IntroduceConstants.W3CNAMESPACE_PREFIX));
 					} else {
 						map.put(ntype.getNamespace(), new NamespaceInformation(
-								ntype, "ns" + namespaceCount++));
+							ntype, "ns" + namespaceCount++));
 					}
 				}
 			}
 		}
-
+		
 		return map;
 	}
-
+	
+	
 	public static void addImportedOperationToService(MethodType method,
-			SpecificServiceInformation serviceInfo) throws Exception {
-
-		Map prefixMap = TemplateUtils.buildWSDLImportMap(serviceInfo
-				.getService());
-
+		SpecificServiceInformation serviceInfo) throws Exception {
+		
 		// parse the wsdl and get the operation text.....
 		Document fromDoc = null;
 		Document toDoc = null;
 		try {
 			fromDoc = XMLUtilities.fileNameToDocument(serviceInfo
-					.getBaseDirectory().getAbsolutePath()
-					+ File.separator
-					+ "schema"
-					+ File.separator
-					+ serviceInfo.getIntroduceServiceProperties().getProperty(
-							IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME)
+				.getBaseDirectory().getAbsolutePath()
+				+ File.separator
+				+ "schema"
+				+ File.separator
+				+ serviceInfo.getIntroduceServiceProperties().getProperty(
+					IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME)
 					+ File.separator
 					+ method.getImportInformation().getWsdlFile());
 			toDoc = XMLUtilities.fileNameToDocument(serviceInfo
-					.getBaseDirectory().getAbsolutePath()
-					+ File.separator
-					+ "schema"
-					+ File.separator
-					+ serviceInfo.getIntroduceServiceProperties().getProperty(
-							IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME)
+				.getBaseDirectory().getAbsolutePath()
+				+ File.separator
+				+ "schema"
+				+ File.separator
+				+ serviceInfo.getIntroduceServiceProperties().getProperty(
+					IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME)
 					+ File.separator
 					+ serviceInfo.getService().getName()
 					+ ".wsdl");
 			List portTypes = fromDoc.getRootElement().getChildren("portType",
-					fromDoc.getRootElement().getNamespace());
+				fromDoc.getRootElement().getNamespace());
 			for (int i = 0; i < portTypes.size(); i++) {
 				Element el = (Element) portTypes.get(i);
 				if (el.getAttributeValue("name").equals(
-						method.getImportInformation().getPortTypeName())) {
-					List operations = el.getChildren("operation", fromDoc
-							.getRootElement().getNamespace());
+					method.getImportInformation().getPortTypeName())) {
+					List operations = el.getChildren("operation", 
+						fromDoc.getRootElement().getNamespace());
 					for (int j = 0; j < operations.size(); j++) {
 						Element opEl = (Element) operations.get(j);
-						if (opEl.getAttributeValue("name").equals(
-								method.getName())) {
+						if (opEl.getAttributeValue("name").equals(method.getName())) {
 							// need to detach the el and add it to the service
 							// which will be using it...
-							List toportTypes = toDoc.getRootElement()
-									.getChildren(
-											"portType",
-											toDoc.getRootElement()
-													.getNamespace());
+							List toportTypes = toDoc.getRootElement().getChildren(
+								"portType", toDoc.getRootElement().getNamespace());
 							for (int i2 = 0; i2 < toportTypes.size(); i2++) {
 								Element el2 = (Element) toportTypes.get(i2);
 								if (el2.getAttributeValue("name").equals(
-										serviceInfo.getService().getName()
-												+ "PortType")) {
+									serviceInfo.getService().getName() + "PortType")) {
 									// found the right one... add to here
 									Element copEl = (Element) opEl.clone();
 									List copElChildren = copEl.getChildren();
-									for (int childi = 0; childi < copElChildren
-											.size(); childi++) {
-										Element copElChild = (Element) copElChildren
-												.get(childi);
-										String messageString = copElChild
-												.getAttributeValue("message");
+									for (int childi = 0; childi < copElChildren.size(); childi++) {
+										Element copElChild = (Element) copElChildren.get(childi);
+										String messageString = copElChild.getAttributeValue("message");
+										System.out.println(
+											"Looking for namespace prefix for message " + messageString);
 										Namespace ns = null;
 										String prefix = "";
 										String message = "";
 										if (messageString.indexOf(":") >= 0) {
-											prefix = messageString.substring(0,
-													messageString.indexOf(":"));
-											message = messageString
-													.substring(messageString
-															.indexOf(":") + 1);
-											ns = fromDoc.getRootElement()
-													.getNamespace(prefix);
-
+											prefix = messageString.substring(0, messageString.indexOf(":"));
+											message = messageString.substring(
+												messageString.indexOf(":") + 1);
+											ns = fromDoc.getRootElement().getNamespace(prefix);
 										} else {
 											message = messageString;
-											ns = fromDoc.getRootElement()
-													.getNamespace();
+											ns = fromDoc.getRootElement().getNamespace();
 										}
-										List nslist = toDoc.getRootElement()
-												.getAdditionalNamespaces();
+										List nslist = toDoc.getRootElement().getAdditionalNamespaces();
+										System.out.println("Looking for URI: " + ns.getURI());
 										for (int nsli = 0; nsli < nslist.size(); nsli++) {
-											Namespace tempns = (Namespace) nslist
-													.get(nsli);
-											System.out
-													.println("Looking at prefix: "
-															+ tempns.getURI());
-											if (tempns.getURI().equals(
-													ns.getURI())) {
-												copElChild
-														.setAttribute(
-																"message",
-																tempns
-																		.getPrefix()
-																		+ ":"
-																		+ message);
+											Namespace tempns = (Namespace) nslist.get(nsli);
+											System.out.println(
+												"Looking at namespace: " + tempns.getPrefix() + " : "
+												+ tempns.getURI());
+											if (tempns.getURI().equals(ns.getURI())) {
+												System.out.println(
+													"Setting message " + message + " nsPrefix: " + tempns.getPrefix());
+												copElChild.setAttribute(
+													"message", tempns.getPrefix() + ":" + message);
+												break;
 											}
 										}
 									}
@@ -191,47 +176,45 @@ public class TemplateUtils {
 				}
 			}
 			FileWriter fw = new FileWriter(serviceInfo.getBaseDirectory()
-					.getAbsolutePath()
-					+ File.separator
-					+ "schema"
-					+ File.separator
-					+ serviceInfo.getIntroduceServiceProperties().getProperty(
-							IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME)
+				.getAbsolutePath()
+				+ File.separator
+				+ "schema"
+				+ File.separator
+				+ serviceInfo.getIntroduceServiceProperties().getProperty(
+					IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME)
 					+ File.separator
 					+ serviceInfo.getService().getName()
 					+ ".wsdl");
 			fw.write(XMLUtilities.formatXML(XMLUtilities
-					.documentToString(toDoc)));
+				.documentToString(toDoc)));
 			fw.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-
+		}		
 	}
-
+	
+	
 	public static Map buildWSDLImportMap(ServiceType service) {
 		Map map = new HashMap();
 		int namespaceCount = 0;
 		if (service.getMethods() != null
-				&& service.getMethods().getMethod() != null) {
+			&& service.getMethods().getMethod() != null) {
 			for (int i = 0; i < service.getMethods().getMethod().length; i++) {
 				MethodType method = service.getMethods().getMethod(i);
 				if (method.isIsImported()) {
-					if (!map.containsKey(method.getImportInformation()
-							.getNamespace())) {
-						ImportInformation ii = new ImportInformation(method
-								.getImportInformation(), "wns"
-								+ namespaceCount++);
-						map.put(method.getImportInformation().getNamespace(),
-								ii);
+					if (!map.containsKey(method.getImportInformation().getNamespace())) {
+						ImportInformation ii = new ImportInformation(
+							method.getImportInformation(), "wns"+ namespaceCount++);
+						map.put(method.getImportInformation().getNamespace(), ii);
 					}
 				}
 			}
 		}
-
+		
 		return map;
 	}
-
+	
+	
 	/**
 	 * Walks a schema tree, following imports and placing namespaces in the
 	 * namespaces set
@@ -244,13 +227,11 @@ public class TemplateUtils {
 	 * @throws Exception
 	 */
 	public static void walkSchemasGetNamespaces(String schemaFile,
-			Set namespaces, Set excludedNamespaces) throws Exception {
+		Set namespaces, Set excludedNamespaces) throws Exception {
 		System.out.println("Looking at schema " + schemaFile);
 		Document schema = XMLUtilities.fileNameToDocument(schemaFile);
 		List importEls = schema.getRootElement().getChildren(
-				"import",
-				schema.getRootElement().getNamespace(
-						IntroduceConstants.W3CNAMESPACE));
+			"import",schema.getRootElement().getNamespace(IntroduceConstants.W3CNAMESPACE));
 		for (int i = 0; i < importEls.size(); i++) {
 			org.jdom.Element importEl = (org.jdom.Element) importEls.get(i);
 			String namespace = importEl.getAttributeValue("namespace");
@@ -259,17 +240,14 @@ public class TemplateUtils {
 					System.out.println("adding namepace " + namespace);
 				}
 				String location = importEl.getAttributeValue("schemaLocation");
-				File currentPath = new File(schemaFile).getCanonicalFile()
-						.getParentFile();
+				File currentPath = new File(schemaFile).getCanonicalFile().getParentFile();
 				if (!schemaFile.equals(currentPath.getCanonicalPath()
-						+ File.separator + location)) {
-					File importedSchema = new File(currentPath + File.separator
-							+ location);
+					+ File.separator + location)) {
+					File importedSchema = new File(currentPath + File.separator + location);
 					walkSchemasGetNamespaces(importedSchema.getCanonicalPath(),
-							namespaces, excludedNamespaces);
+						namespaces, excludedNamespaces);
 				} else {
-					System.err.println("WARNING: Schema is importing itself. "
-							+ schemaFile);
+					System.err.println("WARNING: Schema is importing itself. " + schemaFile);
 				}
 			}
 		}
