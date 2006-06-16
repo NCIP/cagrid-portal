5880a1b17d6e19c8c245c1fae4196480ebf56398
dervin <dervin>
2006-06-16 12:36:07 +0000

    moved handle to utilities
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3124 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/utilities/gov/nih/nci/cagrid/data/utilities/DataServiceHandle.java
+++ cagrid-1-0/caGrid/projects/data/src/java/utilities/gov/nih/nci/cagrid/data/utilities/DataServiceHandle.java
@@ -1,11 +1,11 @@
-package gov.nih.nci.cagrid.data.client;
+package gov.nih.nci.cagrid.data.utilities;
+
 
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
 import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
 import gov.nih.nci.cagrid.data.common.DataServiceI;
 import gov.nih.nci.cagrid.data.stubs.MalformedQueryException;
 import gov.nih.nci.cagrid.data.stubs.QueryProcessingException;
-import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
 
 import java.rmi.RemoteException;
 import java.util.Iterator;
