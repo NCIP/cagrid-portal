3e8cf1a2ef3454ec037e90b8ac88319a1c598c4a
dervin <dervin>
2006-06-19 13:24:48 +0000

    added exceptions
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3152 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/service/DataServiceImpl.java
+++ cagrid-1-0/caGrid/projects/data/src/java/service/gov/nih/nci/cagrid/data/service/DataServiceImpl.java
@@ -1,6 +1,8 @@
 package gov.nih.nci.cagrid.data.service;
 
+import gov.nih.nci.cagrid.common.FaultHelper;
 import gov.nih.nci.cagrid.data.DataServiceConstants;
+import gov.nih.nci.cagrid.data.stubs.QueryProcessingException;
 
 import java.rmi.RemoteException;
 import java.util.Map;
@@ -23,7 +25,8 @@ public class DataServiceImpl {
 	}
 	
 	
-	public gov.nih.nci.cagrid.cqlresultset.CQLQueryResults query(gov.nih.nci.cagrid.cqlquery.CQLQuery cqlQuery) throws RemoteException, gov.nih.nci.cagrid.data.stubs.QueryProcessingException, gov.nih.nci.cagrid.data.stubs.MalformedQueryException {
+	public gov.nih.nci.cagrid.cqlresultset.CQLQueryResults query(gov.nih.nci.cagrid.cqlquery.CQLQuery cqlQuery) 
+		throws RemoteException, gov.nih.nci.cagrid.data.stubs.QueryProcessingException, gov.nih.nci.cagrid.data.stubs.MalformedQueryException {
 		gov.nih.nci.cagrid.data.cql.CQLQueryProcessor processor = null;
 		try {
 			Class qpClass = Class.forName((String) getConfiguration().get(
@@ -31,12 +34,20 @@ public class DataServiceImpl {
 			processor = (gov.nih.nci.cagrid.data.cql.CQLQueryProcessor) qpClass.newInstance();
 			processor.initialize(getConfiguration());
 		} catch (Exception ex) {
-			return null;
+			QueryProcessingException qpe = new QueryProcessingException();
+			FaultHelper helper = new FaultHelper(qpe);
+			helper.addFaultCause(ex);
+			helper.setDescription(ex.getMessage());
+			throw (QueryProcessingException) helper.getFault();
 		}
 		try {
 			return processor.processQuery(cqlQuery);
 		} catch (Exception ex) {
-			return null;
+			QueryProcessingException qpe = new QueryProcessingException();
+			FaultHelper helper = new FaultHelper(qpe);
+			helper.addFaultCause(ex);
+			helper.setDescription(ex.getMessage());
+			throw (QueryProcessingException) helper.getFault();
 		}
 	}	
 }
