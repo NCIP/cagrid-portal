ff1a9fa2e36a5e879d3c0e4b7a7865f417f9bdc1
mccon012 <mccon012>
2006-07-14 14:39:17 +0000

    fixed name of service impl java file
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3644 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/resources/services/BasicAnalyticalServiceWithSecurity/src/BasicAnalyticalServiceWithSecurityImpl.java
+++ cagrid-1-0/tests/projects/coretests/test/resources/services/BasicAnalyticalServiceWithSecurity/src/BasicAnalyticalServiceWithSecurityImpl.java
@@ -14,10 +14,10 @@ import org.globus.wsrf.Constants;
  * @created by Introduce Toolkit version 1.0
  * 
  */
-public class BasicAnalyticalServiceImpl {
+public class BasicAnalyticalServiceWithSecurityImpl {
     private ServiceConfiguration configuration;
 	
-	public BasicAnalyticalServiceImpl() throws RemoteException {
+	public BasicAnalyticalServiceWithSecurityImpl() throws RemoteException {
 	
 	}
 	
@@ -41,29 +41,29 @@ public class BasicAnalyticalServiceImpl {
 	}
 
 
-	     public java.lang.String reverseTranslate(java.lang.String dna) throws RemoteException {
-	 		// reverse
-	 		StringBuffer dnaBuf = new StringBuffer(dna);
-	 		dnaBuf.reverse();
-	 		char[] bases = dnaBuf.toString().toCharArray();
-	 		
-	 		// translate
-	 		for (int i = 0; i < bases.length; i++) {
-	 			char base = bases[i]; 			
-	 			switch (base) {
-	 				case 'a': base = 't'; break;
-	 				case 'A': base = 'T'; break;
-	 				case 't': base = 'a'; break;
-	 				case 'T': base = 'A'; break;
-	 				case 'c': base = 'g'; break;
-	 				case 'C': base = 'G'; break;
-	 				case 'g': base = 'c'; break;
-	 				case 'G': base = 'C'; break;
-	 			}
-	 			bases[i] = base;
-	 		}
-	 		
-	 		return new String(bases);
+	public java.lang.String reverseTranslate(java.lang.String dna) throws RemoteException {
+ 		// reverse
+ 		StringBuffer dnaBuf = new StringBuffer(dna);
+ 		dnaBuf.reverse();
+ 		char[] bases = dnaBuf.toString().toCharArray();
+ 		
+ 		// translate
+ 		for (int i = 0; i < bases.length; i++) {
+ 			char base = bases[i]; 			
+ 			switch (base) {
+ 				case 'a': base = 't'; break;
+ 				case 'A': base = 'T'; break;
+ 				case 't': base = 'a'; break;
+ 				case 'T': base = 'A'; break;
+ 				case 'c': base = 'g'; break;
+ 				case 'C': base = 'G'; break;
+ 				case 'g': base = 'c'; break;
+ 				case 'G': base = 'C'; break;
+ 			}
+ 			bases[i] = base;
+ 		}
+ 		
+ 		return new String(bases);
 	}
 
 }
