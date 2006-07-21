db678eec3fabd60394cab1e0d29f9cf43fcf1d23
mccon012 <mccon012>
2006-07-21 20:30:47 +0000

    added implementation
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3741 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/AdvertiseServiceStep.java
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/AdvertiseServiceStep.java
@@ -3,7 +3,14 @@
  */
 package gov.nci.nih.cagrid.tests.core.steps;
 
+import gov.nci.nih.cagrid.tests.core.util.FileUtils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 import org.apache.axis.types.URI.MalformedURIException;
 
@@ -31,5 +38,29 @@ public class AdvertiseServiceStep
 	
 	public void runStep() throws Throwable
 	{
+		File tmpFile = File.createTempFile("AdvertiseServiceStep", ".xml");
+		tmpFile.deleteOnExit();
+		File registrationFile = new File(serviceDir, "etc" + File.separator + "registration.xml");
+		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(tmpFile)));
+		BufferedReader br = new BufferedReader(new FileReader(registrationFile));
+		try {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				if (line.trim().startsWith("<wsa:Address>")) {
+					int index1 = line.indexOf(">");
+					int index2 = line.indexOf("<", index1+1);
+					line = line.substring(0, index1+1) + indexServiceURL + line.substring(index2);
+				}
+				out.println(line);
+			}
+		} finally {
+			out.close();
+			out.flush();
+			br.close();
+		}
+		
+		FileUtils.copy(tmpFile, registrationFile);
+		tmpFile.delete();
 	}
 }
