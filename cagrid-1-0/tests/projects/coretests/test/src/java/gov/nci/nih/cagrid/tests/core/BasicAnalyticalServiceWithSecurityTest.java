3f5492939903baa083f8f5b959fecf3d630b0e15
mccon012 <mccon012>
2006-07-24 21:50:51 +0000

    added dorian steps
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3783 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/BasicAnalyticalServiceWithSecurityTest.java
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/BasicAnalyticalServiceWithSecurityTest.java
@@ -3,6 +3,10 @@
  */
 package gov.nci.nih.cagrid.tests.core;
 
+import gov.nci.nih.cagrid.tests.core.steps.DorianAddTrustedCAStep;
+import gov.nci.nih.cagrid.tests.core.steps.DorianAuthenticateStep;
+import gov.nci.nih.cagrid.tests.core.steps.DorianCleanupStep;
+import gov.nci.nih.cagrid.tests.core.steps.DorianDestroyDefaultProxyStep;
 import gov.nci.nih.cagrid.tests.core.steps.GlobusCleanupStep;
 import gov.nci.nih.cagrid.tests.core.steps.GlobusCreateStep;
 import gov.nci.nih.cagrid.tests.core.steps.GlobusDeployServiceStep;
@@ -20,34 +24,55 @@ import junit.textui.TestRunner;
 public class BasicAnalyticalServiceWithSecurityTest
 	extends AbstractServiceTest
 {
+	private File caFile;
+
 	public BasicAnalyticalServiceWithSecurityTest()
 	{
 		super();
+	}
+
+	protected void storyTearDown() 
+		throws Throwable
+	{
+		if (caFile != null) caFile.delete();
+		super.storyTearDown();
+		new DorianDestroyDefaultProxyStep().runStep();
+		new DorianCleanupStep().runStep();
 	}
 	
 	@SuppressWarnings("unchecked")
 	protected Vector steps() 
 	{
 		super.init("BasicAnalyticalServiceWithSecurity");
+		
+		File dorianDir = new File(System.getProperty("dorian.dir",
+			".." + File.separator + ".." + File.separator + ".." + File.separator + 
+			"caGrid" + File.separator + "projects" + File.separator + "dorian"
+		));
+		caFile = new File(
+			System.getProperty("user.home"), 
+			".globus" + File.separator + "certificates" + File.separator + "BasicAnalyticalServiceWithSecurityTest_ca.1"
+		);
 
 		Vector steps = new Vector();
+		steps.add(createServiceStep);
 		steps.add(new GlobusCreateStep(globus));
 		steps.add(new GTSSyncOnceStep(globus));
-		steps.add(createServiceStep);
-		if (super.serviceInfo.isTransportSecurity()) {
-			globus.setUseCounterCheck(false);
-			steps.add(new GlobusDeployServiceStep(globus, new File("..", "echo")));
-		}
+		steps.add(new GlobusDeployServiceStep(globus, dorianDir));
+		steps.add(new GlobusDeployServiceStep(globus, new File("..", "echo")));
 		steps.add(new GlobusDeployServiceStep(globus, createServiceStep.getServiceDir()));
 		steps.add(new GlobusStartStep(globus, port));
+		steps.add(new DorianAuthenticateStep("dorian", "password", port));
+		steps.add(new DorianAddTrustedCAStep(caFile, port));
 		try {
 			addInvokeSteps(steps);
 		} catch (Exception e) {
 			throw new IllegalArgumentException("could not add invoke steps", e);
 		}
-		//steps.add(new CheckServiceMetadataStep(endpoint, metadataFile));
 		steps.add(new GlobusStopStep(globus, port));
 		steps.add(new GlobusCleanupStep(globus));
+		steps.add(new DorianCleanupStep());
+		steps.add(new DorianDestroyDefaultProxyStep());
 		return steps;
 	}
 
