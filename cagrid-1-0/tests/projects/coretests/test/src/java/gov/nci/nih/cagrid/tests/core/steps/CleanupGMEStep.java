4c14d3ffb34ee516c633516b9c9b8a5250669c2f
mccon012 <mccon012>
2006-07-24 17:51:42 +0000

    abstracted implementation to AbstractDbCleanupStep
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3771 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/CleanupGMEStep.java
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/CleanupGMEStep.java
@@ -3,67 +3,27 @@
  */
 package gov.nci.nih.cagrid.tests.core.steps;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import com.atomicobject.haste.framework.Step;
-
 public class CleanupGMEStep
-	extends Step
+	extends AbstractDbCleanupStep
 {
-	private String dbUrl;
-	private String user;
-	private String password;
-	
 	public CleanupGMEStep() 
 	{
-		this("jdbc:mysql://localhost/test", "root", "");
+		super();
 	}
 	
 	public CleanupGMEStep(String dbUrl, String user, String password) 
 	{
-		super();
-		
-		this.dbUrl = dbUrl;
-		this.user = user;
-		this.password = password;
+		super(dbUrl, user, password);		
 	}
 	
 	public void runStep() 
 		throws Throwable
 	{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection(dbUrl, user, password);
-		
-		try {
-			Statement stmt = con.createStatement();
-	
-			// drop databases
-			String[] dbNames = new String[] { 
-				"globusgme_gme_registry", 
-				"globusgme_gme_schema_cache", 
-				"globusgme_gme_schema_store"
-			};
-			for (String dbName : dbNames) {
-				try {
-					stmt.executeUpdate("drop database " + dbName);
-				} catch (SQLException e) {
-					if (e.getMessage().indexOf("Database doesn't exist") == -1) throw e;
-				}
-			}
-			
-			// make sure no gme databases left
-			ResultSet rs = stmt.executeQuery("show databases");
-			while (rs.next()) {
-				String dbName = rs.getString(1);
-				assertTrue(dbName.indexOf("gme") == -1);
-			}
-		} finally {
-			try { con.close(); } catch (Exception e) { }
-		}
+		super.dropDatabases(new String[] { 
+			"globusgme_gme_registry", 
+			"globusgme_gme_schema_cache", 
+			"globusgme_gme_schema_store"
+		});
 	}
 	
 	public static void main(String[] args)
