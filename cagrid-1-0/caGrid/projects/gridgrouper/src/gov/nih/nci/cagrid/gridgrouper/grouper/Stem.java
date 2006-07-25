0c1a3794091526babad3938fede3865016c515ff
langella <langella>
2006-07-25 20:45:06 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3809 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/gridgrouper/src/gov/nih/nci/cagrid/gridgrouper/grouper/Stem.java
@@ -0,0 +1,107 @@
+package gov.nih.nci.cagrid.gridgrouper.grouper;
+
+import java.util.Date;
+import java.util.Set;
+
+import edu.internet2.middleware.grouper.GrantPrivilegeException;
+import edu.internet2.middleware.grouper.GroupAddException;
+import edu.internet2.middleware.grouper.GrouperRuntimeException;
+import edu.internet2.middleware.grouper.InsufficientPrivilegeException;
+import edu.internet2.middleware.grouper.RevokePrivilegeException;
+import edu.internet2.middleware.grouper.SchemaException;
+import edu.internet2.middleware.grouper.StemAddException;
+import edu.internet2.middleware.grouper.StemDeleteException;
+import edu.internet2.middleware.grouper.StemModifyException;
+import edu.internet2.middleware.grouper.StemNotFoundException;
+import edu.internet2.middleware.subject.Subject;
+import edu.internet2.middleware.subject.SubjectNotFoundException;
+
+/**
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public interface Stem {
+	public Group addChildGroup(String extension, String displayExtension) throws GroupAddException,
+		InsufficientPrivilegeException;
+
+
+	public Stem addChildStem(String extension, String displayExtension) throws InsufficientPrivilegeException,
+		StemAddException;
+
+
+	public void delete() throws InsufficientPrivilegeException, StemDeleteException;
+
+
+	public Set getChildGroups();
+
+
+	public Set getChildStems();
+
+
+	public String getCreateSource();
+
+
+	public Subject getCreateSubject() throws SubjectNotFoundException;
+
+
+	public Date getCreateTime();
+
+
+	public Set getCreators() throws GrouperRuntimeException;
+
+
+	public String getDescription();
+
+
+	public String getDisplayExtension();
+
+
+	public String getDisplayName();
+
+
+	public String getExtension();
+
+
+	public String getModifySource();
+
+
+	public Subject getModifySubject() throws SubjectNotFoundException;
+
+
+	public Date getModifyTime();
+
+
+	public String getName();
+
+
+	public Stem getParentStem() throws StemNotFoundException;
+
+
+	public Set getPrivs(Subject subj);
+
+
+	public Set getStemmers();
+
+
+	public void grantPriv(Subject subj, Privilege priv) throws GrantPrivilegeException, InsufficientPrivilegeException,
+		SchemaException;
+
+
+	public boolean hasCreate(Subject subj);
+
+
+	public boolean hasStem(Subject subj) throws SchemaException;
+
+
+	public void revokePriv(Subject subj, Privilege priv) throws InsufficientPrivilegeException,
+		RevokePrivilegeException, SchemaException;
+
+
+	public void setDescription(String value) throws InsufficientPrivilegeException, StemModifyException;
+
+
+	public void setDisplayExtension(String value) throws InsufficientPrivilegeException, StemModifyException;
+}
