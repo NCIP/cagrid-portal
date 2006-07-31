9db562d94ac33b3f3b8369071e9622e5680730f1
mccon012 <mccon012>
2006-07-31 20:47:28 +0000

    initial commit
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3877 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/GMEConfigureStep.java
@@ -0,0 +1,47 @@
+/*
+ * Created on Jul 31, 2006
+ */
+package gov.nci.nih.cagrid.tests.core.steps;
+
+import gov.nci.nih.cagrid.tests.core.GlobusHelper;
+import gov.nci.nih.cagrid.tests.core.util.SimpleXmlReplacer;
+
+import java.io.File;
+
+import com.atomicobject.haste.framework.Step;
+
+public class GMEConfigureStep
+	extends Step
+{
+	private GlobusHelper globus;
+	private String user;
+	private String password;
+	
+	public GMEConfigureStep(GlobusHelper globus)
+	{
+		this(globus,
+			System.getProperty("mysql.user", "root"),
+			System.getProperty("mysql.password", "")
+		);
+	}
+	
+	public GMEConfigureStep(GlobusHelper globus, String user, String password)
+	{
+		super();
+		
+		this.globus = globus;
+		this.user = user;
+		this.password = password;
+	}
+
+	public void runStep() throws Throwable
+	{
+		File configFile = new File(globus.getTempGlobusLocation(), 
+			"etc" + File.separator + "GlobalModelExchange" + File.separator + "gme-globus-config.xml"
+		);
+		SimpleXmlReplacer replacer = new SimpleXmlReplacer();
+		replacer.addReplacement("username", user);
+		replacer.addReplacement("password", password);
+		replacer.performReplacement(configFile);
+	}
+}
