0119b0c8c7edaa816238a8f3c96733ce16744a4c
mccon012 <mccon012>
2006-08-01 13:05:49 +0000

    initial commit
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@3889 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/DorianConfigureStep.java
@@ -0,0 +1,51 @@
+/*
+ * Created on Jul 31, 2006
+ */
+package gov.nci.nih.cagrid.tests.core.steps;
+
+import gov.nci.nih.cagrid.tests.core.GlobusHelper;
+import gov.nci.nih.cagrid.tests.core.util.SimpleXmlReplacer;
+
+import java.io.File;
+
+import com.atomicobject.haste.framework.Step;
+
+/**
+ * This step sets the mysql username and password fields in dorian-conf.xml of a deployed Dorian service
+ * @author Patrick McConnell
+ */
+public class DorianConfigureStep
+	extends Step
+{
+	private GlobusHelper globus;
+	private String user;
+	private String password;
+	
+	public DorianConfigureStep(GlobusHelper globus)
+	{
+		this(globus,
+			System.getProperty("mysql.user", "root"),
+			System.getProperty("mysql.password", "")
+		);
+	}
+	
+	public DorianConfigureStep(GlobusHelper globus, String user, String password)
+	{
+		super();
+		
+		this.globus = globus;
+		this.user = user;
+		this.password = password;
+	}
+
+	public void runStep() throws Throwable
+	{
+		File configFile = new File(globus.getTempGlobusLocation(), 
+			"etc" + File.separator + "GlobalModelExchange" + File.separator + "dorian-conf.xml"
+		);
+		SimpleXmlReplacer replacer = new SimpleXmlReplacer();
+		replacer.addReplacement("username", user);
+		replacer.addReplacement("password", password);
+		replacer.performReplacement(configFile);
+	}
+}
