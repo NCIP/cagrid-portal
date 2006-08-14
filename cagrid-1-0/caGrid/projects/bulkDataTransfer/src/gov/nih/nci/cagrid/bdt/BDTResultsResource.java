27dbbd54727ff2623e383ca4decbf2b707e2c3e4
hastings <hastings>
2006-08-14 17:35:59 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4114 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/BDTResultsResource.java
@@ -0,0 +1,15 @@
+package gov.nih.nci.cagrid.bdt;
+
+import org.globus.wsrf.impl.ResourceHomeImpl;
+
+public class BDTResultsResource extends ResourceHomeImpl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
