17c9b332f1b9ed0bbaae22f110f88c949e22458d
hastings <hastings>
2006-08-15 19:03:41 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4157 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/service/globus/resource/ResourceConstants.java
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/service/globus/resource/ResourceConstants.java
@@ -4,7 +4,7 @@ import javax.xml.namespace.QName;
 
 
 public interface ResourceConstants {
-	public static final String SERVICE_NS = "http://cagrid.nci.nih.gov/BDTHandler";
+	public static final String SERVICE_NS = "http://cagrid.nci.nih.gov/BDTHandlerService";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "BDTHandlerServiceKey");
 	public static final QName RESOURCE_PROPERY_SET = new QName(SERVICE_NS, "BDTHandlerServiceResourceProperties");
 
