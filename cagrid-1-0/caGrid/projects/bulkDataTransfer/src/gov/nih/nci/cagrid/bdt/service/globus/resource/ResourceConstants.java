af876fd7bd5e3265d2002b60937894c436744ef1
hastings <hastings>
2006-08-17 14:19:29 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4210 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/service/globus/resource/ResourceConstants.java
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/service/globus/resource/ResourceConstants.java
@@ -4,9 +4,9 @@ import javax.xml.namespace.QName;
 
 
 public interface ResourceConstants {
-	public static final String SERVICE_NS = "http://cagrid.nci.nih.gov/BDTHandlerService";
-	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "BDTHandlerServiceKey");
-	public static final QName RESOURCE_PROPERY_SET = new QName(SERVICE_NS, "BDTHandlerServiceResourceProperties");
+	public static final String SERVICE_NS = "http://cagrid.nci.nih.gov/BulkDataHandler";
+	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "BulkDataHandlerKey");
+	public static final QName RESOURCE_PROPERY_SET = new QName(SERVICE_NS, "BulkDataHandlerResourceProperties");
 
 	//Service level metadata (exposed as resouce properties)
 	
