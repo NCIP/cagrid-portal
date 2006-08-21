cbabbbf787de67e49989517a858c3d7711b62305
oster <oster>
2006-08-21 14:28:57 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4291 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/cabigextensions/src/java/gov/nih/nci/cagrid/introduce/extensions/metadata/codegen/MetadataCodegenPreProcessor.java
@@ -0,0 +1,62 @@
+package gov.nih.nci.cagrid.introduce.extensions.metadata.codegen;
+
+import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
+import gov.nih.nci.cagrid.introduce.beans.resource.ResourcePropertyType;
+import gov.nih.nci.cagrid.introduce.beans.service.ServiceType;
+import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionException;
+import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionPreProcessor;
+import gov.nih.nci.cagrid.introduce.extensions.metadata.constants.MetadataConstants;
+import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * Just sets the metadata filelocation if its not set.
+ * 
+ * @author oster
+ * 
+ */
+public class MetadataCodegenPreProcessor implements CodegenExtensionPreProcessor {
+	private static final String DEFAULT_FILENAME = "serviceMetadata.xml";
+
+	protected static Log LOG = LogFactory.getLog(MetadataCodegenPreProcessor.class.getName());
+
+
+	public void preCodegen(ServiceExtensionDescriptionType desc, ServiceInformation info)
+		throws CodegenExtensionException {
+
+		String filename = setFilename(info);
+		if (filename == null) {
+			LOG.error("Unable to locate Service Metadata resource property.");
+			return;
+		} else {
+			LOG.debug("Set service metadata file location to:" + filename);
+		}
+	}
+
+
+	/**
+	 * @return
+	 */
+	private String setFilename(ServiceInformation info) {
+		ServiceType mainServ = info.getServiceDescriptor().getServices().getService()[0];
+		ResourcePropertyType[] resourceProperty = mainServ.getResourcePropertiesList().getResourceProperty();
+		for (int i = 0; i < resourceProperty.length; i++) {
+			ResourcePropertyType rp = resourceProperty[i];
+			if (rp.getQName().equals(MetadataConstants.SERVICE_METADATA_QNAME)) {
+				String fileLocation = rp.getFileLocation();
+				if (fileLocation == null || fileLocation.trim().equals("")) {
+					rp.setFileLocation(DEFAULT_FILENAME);
+				}
+
+				return rp.getFileLocation();
+
+			}
+		}
+
+		return null;
+	}
+
+}
