0659a92a7c81927b5b1608ec59cce87999e34cd6
hastings <hastings>
2006-08-21 15:00:24 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4295 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/extension/BDTCreationExtensionPostProcessor.java
@@ -0,0 +1,25 @@
+package gov.nih.nci.cagrid.bdt.extension;
+
+import java.util.Properties;
+
+import gov.nih.nci.cagrid.introduce.beans.ServiceDescription;
+import gov.nih.nci.cagrid.introduce.extension.CreationExtensionException;
+import gov.nih.nci.cagrid.introduce.extension.CreationExtensionPostProcessor;
+
+public class BDTCreationExtensionPostProcessor implements CreationExtensionPostProcessor {
+
+	public void postCreate(ServiceDescription descriptor, Properties properties) throws CreationExtensionException {
+		
+
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
