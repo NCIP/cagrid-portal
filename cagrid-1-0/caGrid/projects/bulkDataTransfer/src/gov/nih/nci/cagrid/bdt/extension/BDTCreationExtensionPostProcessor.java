468b28d33d24584721ef6f6695fcf2864a5944a1
hastings <hastings>
2006-08-24 15:20:45 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4434 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/extension/BDTCreationExtensionPostProcessor.java
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/extension/BDTCreationExtensionPostProcessor.java
@@ -2,6 +2,7 @@ package gov.nih.nci.cagrid.bdt.extension;
 
 import gov.nih.nci.cagrid.bdt.service.BDTServiceConstants;
 import gov.nih.nci.cagrid.bdt.templates.BDTResourceTemplate;
+import gov.nih.nci.cagrid.bdt.templates.JNDIConfigResourceTemplate;
 import gov.nih.nci.cagrid.common.Utils;
 import gov.nih.nci.cagrid.introduce.IntroduceConstants;
 import gov.nih.nci.cagrid.introduce.beans.ServiceDescription;
@@ -29,11 +30,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.common.XMLUtilities;
+
 
 public class BDTCreationExtensionPostProcessor implements CreationExtensionPostProcessor {
 
+	private ServiceInformation info;
+
+
 	public void postCreate(ServiceDescription serviceDescription, Properties serviceProperties)
 		throws CreationExtensionException {
+
+		info = new ServiceInformation(serviceDescription, serviceProperties, new File(serviceProperties
+			.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR)));
+
 		// apply data service requirements to it
 		try {
 			System.out.println("Adding data service components to template");
@@ -147,14 +160,48 @@ public class BDTCreationExtensionPostProcessor implements CreationExtensionPostP
 		ServiceType[] servicesArr = new ServiceType[services.size()];
 		services.toArray(servicesArr);
 		description.getServices().setService(servicesArr);
+
+		// change the resource in the jndi file
+		File jndiConfigF = new File(info.getBaseDirectory().getAbsolutePath() + File.separator + "jndi-config.xml");
+
+		Document serverConfigJNDIDoc = null;
+		try {
+			serverConfigJNDIDoc = XMLUtilities.fileNameToDocument(jndiConfigF.getAbsolutePath());
+		} catch (MobiusException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		List serviceEls = serverConfigJNDIDoc.getRootElement().getChildren();
+		for (int i = 0; i < serviceEls.size(); i++) {
+			Element serviceEl = (Element) serviceEls.get(i);
+			if (serviceEl.getName().equals("service") && serviceEl.getAttributeValue("name").equals(
+				"SERVICE-INSTANCE-PREFIX/" + mainService.getName() + bdtService.getName())) {
+				List resourceEls = serviceEl.getChildren();
+				for (int j = 0; j < resourceEls.size(); j++) {
+					Element resourceEl = (Element) resourceEls.get(j);
+					if (resourceEl.getName().equals("resource") && resourceEl.getAttributeValue("name").equals("home")) {
+						serviceEl.removeContent(resourceEl);
+						JNDIConfigResourceTemplate resourceT = new JNDIConfigResourceTemplate();
+						String resourceS = resourceT.generate(new SpecificServiceInformation(info, info.getServices()
+							.getService(0)));
+						Element newResourceEl = XMLUtilities.stringToDocument(resourceS).getRootElement();
+						serviceEl.addContent(newResourceEl);
+						FileWriter resourceFW = new FileWriter(jndiConfigF);
+						resourceFW.write(resourceS);
+						resourceFW.close();
+						break;
+					}
+				}
+				break;
+			}
+
+		}
 	}
-	
 
 
 	private void addResourceImplStub(ServiceDescription desc, Properties serviceProperties) throws Exception {
 		BDTResourceTemplate resourceT = new BDTResourceTemplate();
-		ServiceInformation info = new ServiceInformation(desc, serviceProperties, new File(serviceProperties
-			.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR)));
 		String resourceS = resourceT.generate(new SpecificServiceInformation(info, info.getServices().getService(0)));
 		File resourceF = new File(serviceProperties.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR)
 			+ File.separator + "src" + File.separator + CommonTools.getPackageDir(desc.getServices().getService(0))
