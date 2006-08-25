1ed4889ca094e05dbacca02c806132709697465e
mccon012 <mccon012>
2006-08-25 17:48:26 +0000

    added constructors and logic to determine whether to use a non-secure or secure globus
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4457 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/GlobusDeployServiceStep.java
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/GlobusDeployServiceStep.java
@@ -8,6 +8,7 @@ import java.io.File;
 import com.atomicobject.haste.framework.Step;
 
 import gov.nci.nih.cagrid.tests.core.GlobusHelper;
+import gov.nci.nih.cagrid.tests.core.util.IntroduceServiceInfo;
 
 /**
  * This step deploys a service to a temporary globus container by running the deployGlobus ant task
@@ -28,11 +29,38 @@ public class GlobusDeployServiceStep
 	
 	public GlobusDeployServiceStep(GlobusHelper globus, File serviceDir, String target)
 	{
-		super();
+		this(globus, null, serviceDir, target);
 		
 		this.globus = globus;
 		this.serviceDir = serviceDir;
 		this.target = target;
+	}
+	
+	public GlobusDeployServiceStep(GlobusHelper globus, GlobusHelper secureGlobus, File serviceDir)
+	{
+		this(globus, secureGlobus, serviceDir,  null);
+	}
+	
+	public GlobusDeployServiceStep(GlobusHelper globus, GlobusHelper secureGlobus, File serviceDir, String target)
+	{
+		super();
+		
+		this.serviceDir = serviceDir;
+		this.target = target;
+		
+		if (globus == null) {
+			this.globus = globus;
+		} else if (secureGlobus == null) {
+			this.globus = secureGlobus;
+		} else {
+			try {
+				IntroduceServiceInfo introduce = new IntroduceServiceInfo(new File(serviceDir, "introduce.xml"));
+				if (introduce.isTransportSecurity()) this.globus = secureGlobus;
+				else this.globus = globus;
+			} catch (Exception e) {
+				throw new RuntimeException("unable to get service info from introduce.xml", e);
+			}
+		}
 	}
 	
 	public void runStep() throws Throwable
