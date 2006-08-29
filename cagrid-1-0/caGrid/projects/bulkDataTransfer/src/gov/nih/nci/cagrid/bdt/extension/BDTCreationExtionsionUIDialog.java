bb670ce7eaa0e7db9586c01a67a20df413f81753
hastings <hastings>
2006-08-29 17:02:56 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4484 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/extension/BDTCreationExtionsionUIDialog.java
@@ -0,0 +1,31 @@
+package gov.nih.nci.cagrid.bdt.extension;
+
+import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
+import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
+import gov.nih.nci.cagrid.introduce.portal.extension.CreationExtensionUIDialog;
+
+/** 
+ *  BDTCreationExtionsionUIDialog
+ *  TODO:DOCUMENT ME
+ * 
+ * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Hastings</A>
+ * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster</A>
+ * @author <A HREF="MAILTO:langella@bmi.osu.edu">Stephen Langella</A>
+ * 
+ * @created Aug 29, 2006 
+ */
+public class BDTCreationExtionsionUIDialog extends CreationExtensionUIDialog {
+
+	public BDTCreationExtionsionUIDialog(ServiceExtensionDescriptionType arg0, ServiceInformation arg1) {
+		super(arg0, arg1);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
