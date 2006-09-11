2f01ab03745a56029b5c46291e8d58c315fe4a58
oster <oster>
2006-09-11 15:24:24 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4678 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/extension/BDTCreationExtionsionUIDialog.java
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/extension/BDTCreationExtionsionUIDialog.java
@@ -4,28 +4,22 @@ import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionT
 import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
 import gov.nih.nci.cagrid.introduce.portal.extension.CreationExtensionUIDialog;
 
-/** 
- *  BDTCreationExtionsionUIDialog
- *  TODO:DOCUMENT ME
+import java.awt.Frame;
+
+
+/**
+ * BDTCreationExtionsionUIDialog TODO:DOCUMENT ME
  * 
  * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Hastings</A>
  * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster</A>
  * @author <A HREF="MAILTO:langella@bmi.osu.edu">Stephen Langella</A>
  * 
- * @created Aug 29, 2006 
+ * @created Aug 29, 2006
  */
 public class BDTCreationExtionsionUIDialog extends CreationExtensionUIDialog {
 
-	public BDTCreationExtionsionUIDialog(ServiceExtensionDescriptionType arg0, ServiceInformation arg1) {
-		super(arg0, arg1);
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public BDTCreationExtionsionUIDialog(Frame f, ServiceExtensionDescriptionType arg0, ServiceInformation arg1) {
+		super(f, arg0, arg1);
 	}
 
 }
