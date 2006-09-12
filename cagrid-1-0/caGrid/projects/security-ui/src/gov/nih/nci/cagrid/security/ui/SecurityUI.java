1e87ab4ef34990b66a4de2e6639efbbb703f4ea2
langella <langella>
2006-09-12 14:56:50 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4700 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/security-ui/src/gov/nih/nci/cagrid/security/ui/SecurityUI.java
@@ -0,0 +1,51 @@
+package gov.nih.nci.cagrid.security.ui;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import org.projectmobius.common.MobiusException;
+import org.projectmobius.portal.GridPortal;
+import org.projectmobius.portal.PortalResourceManager;
+
+/**
+ * @author <A HREF="MAILTO:langella@bmi.osu.edu">Stephen Langella</A>
+ * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster</A>
+ * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Hastings</A>
+ * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
+ * 
+ * @version $Id: GridGrouperBaseTreeNode.java,v 1.1 2006/08/04 03:49:26 langella
+ *          Exp $
+ */
+public final class SecurityUI {
+
+	public static final String DEFAULT_CONFIG_FILE = "etc/security-ui-conf.xml";
+
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			showGridPortal(args[0]);
+		} else {
+			showGridPortal(null);
+		}
+	}
+
+	private static void showGridPortal(String confFile) {
+		try {
+			GridPortal portal = null;
+			if (confFile == null) {
+				confFile = DEFAULT_CONFIG_FILE;
+			}
+			portal = new GridPortal(confFile);
+			Dimension dim = PortalResourceManager.getInstance()
+					.getGridPortalConfig().getApplicationDimensions();
+			portal.pack();
+			portal.setSize(dim);
+			portal.setVisible(true);
+			portal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		} catch (MobiusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
