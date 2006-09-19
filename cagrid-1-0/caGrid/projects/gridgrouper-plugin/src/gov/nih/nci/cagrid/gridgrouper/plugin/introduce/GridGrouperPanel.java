a395bba8b24025e9370a99ec7cb0b8a0fa7be891
langella <langella>
2006-09-19 14:20:34 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4772 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/gridgrouper-plugin/src/gov/nih/nci/cagrid/gridgrouper/plugin/introduce/GridGrouperPanel.java
+++ cagrid-1-0/caGrid/projects/gridgrouper-plugin/src/gov/nih/nci/cagrid/gridgrouper/plugin/introduce/GridGrouperPanel.java
@@ -5,10 +5,13 @@ import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
 import gov.nih.nci.cagrid.introduce.portal.extension.AbstractAuthorizationPanel;
 
 import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
 
 public class GridGrouperPanel extends AbstractAuthorizationPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JLabel gridGrouper = null;
 
 	public GridGrouperPanel(AuthorizationExtensionDescriptionType authDesc,
 			ServiceInformation serviceInfo) {
@@ -32,8 +35,11 @@ public class GridGrouperPanel extends AbstractAuthorizationPanel {
 	 * @return void
 	 */
 	private void initialize() {
+		gridGrouper = new JLabel();
+		gridGrouper.setText("Grid Grouper");
 		this.setSize(300, 200);
 		this.setLayout(new GridBagLayout());
+		this.add(gridGrouper, new GridBagConstraints());
 	}
 
 }
