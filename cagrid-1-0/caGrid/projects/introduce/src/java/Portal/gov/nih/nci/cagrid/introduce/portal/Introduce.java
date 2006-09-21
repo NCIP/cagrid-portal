13524709d6bd063bd937a9633f98f792c9f35df4
hastings <hastings>
2006-09-21 17:24:55 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4848 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/Introduce.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/Introduce.java
@@ -33,7 +33,7 @@ public final class Introduce {
 		} else {
 			showGridPortal(null);
 		}
-		//EventQueue.invokeLater(new IntroduceSplashCloser());
+		EventQueue.invokeLater(new IntroduceSplashCloser());
 	}
 
 
@@ -55,7 +55,6 @@ public final class Introduce {
 			checkGlobusLocation(confFile);
 			portal = new GridPortal(confFile);
 			Dimension dim = PortalResourceManager.getInstance().getGridPortalConfig().getApplicationDimensions();
-			new IntroduceSplashCloser();
 			portal.setSize(dim);
 			portal.pack();
 			portal.setVisible(true);
