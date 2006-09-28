6223afd08cdf9948c66e9a152426afe1a4bf8c7d
hastings <hastings>
2006-09-28 23:26:22 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@4980 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/client/DataServiceClient.java
+++ cagrid-1-0/caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/client/DataServiceClient.java
@@ -103,8 +103,8 @@ public class DataServiceClient extends ServiceSecurityClient implements DataServ
     public gov.nih.nci.cagrid.metadata.security.ServiceSecurityMetadata getServiceSecurityMetadata() throws RemoteException {
       synchronized(portTypeMutex){
         configureStubSecurity((Stub)portType,"getServiceSecurityMetadata");
-        gov.nih.nci.cagrid.introduce.security.GetServiceSecurityMetadataRequest params = new gov.nih.nci.cagrid.introduce.security.GetServiceSecurityMetadataRequest();
-        gov.nih.nci.cagrid.introduce.security.GetServiceSecurityMetadataResponse boxedResult = portType.getServiceSecurityMetadata(params);
+        gov.nih.nci.cagrid.introduce.security.stubs.GetServiceSecurityMetadataRequest params = new gov.nih.nci.cagrid.introduce.security.stubs.GetServiceSecurityMetadataRequest();
+        gov.nih.nci.cagrid.introduce.security.stubs.GetServiceSecurityMetadataResponse boxedResult = portType.getServiceSecurityMetadata(params);
         return boxedResult.getServiceSecurityMetadata();
       }
     }
