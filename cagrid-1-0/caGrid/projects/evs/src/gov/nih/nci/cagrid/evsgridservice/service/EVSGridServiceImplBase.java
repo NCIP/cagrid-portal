2b1cb775d63d415651dadd275d7b225e7d8f8578
oster <oster>
2006-10-12 18:21:45 +0000

    added service metadata
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@5314 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/evs/src/gov/nih/nci/cagrid/evsgridservice/service/EVSGridServiceImplBase.java
+++ cagrid-1-0/caGrid/projects/evs/src/gov/nih/nci/cagrid/evsgridservice/service/EVSGridServiceImplBase.java
@@ -76,6 +76,21 @@ public abstract class EVSGridServiceImplBase {
 		return resourceHome;
 	}
 	
+	
+	
+	
+	protected gov.nih.nci.cagrid.metadata.ServiceMetadata getServiceMetadataMD(){
+		BaseResource serviceBaseResource;
+		try {
+			serviceBaseResource = (BaseResource)ResourceContext.getResourceContext().getResource();
+		} catch (ResourceContextException e) {
+			return null;
+		} catch (ResourceException e) {
+			return null;
+		}
+		return serviceBaseResource.getServiceMetadataMD();
+	}
+
 		
 	
 	
