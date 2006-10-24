a9e09751b31a636ec69b5601860ff32e7fdc2df5
hastings <hastings>
2006-10-24 19:13:19 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@5538 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/service/BDTServiceConstants.java
+++ cagrid-1-0/caGrid/projects/bulkDataTransfer/src/gov/nih/nci/cagrid/bdt/service/BDTServiceConstants.java
@@ -5,7 +5,7 @@ import javax.xml.namespace.QName;
 
 public interface BDTServiceConstants {
 	public static final QName METADATA_QNAME = new QName("http://cagrid.nci.nih.gov/1/BulkDataTransferMetadata", "BulkDataTransferMetadata");
-	public static final String METADATA_SCHEMA = "BulkDataTransferMetadata.xsd";
+	public static final String METADATA_SCHEMA = "BulkDataTransferServiceMetadata.xsd";
 	public static final String ENUMERATION_SCHEMA = "enumeration.xsd";
 	public static final String ADDRESSING_SCHEMA = "addressing.xsd";
 	public static final String TRANSFER_SCHEMA = "transfer.xsd";
