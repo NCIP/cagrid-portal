3f73015c8585892f8a0450dc5fad789fbbd76259
kherm <kherm>
2006-11-07 15:08:50 +0000

    no message
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@5744 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/portal/src/java/gov/nih/nci/cagrid/portal/utils/EPRPingService.java
+++ cagrid-1-0/caGrid/projects/portal/src/java/gov/nih/nci/cagrid/portal/utils/EPRPingService.java
@@ -5,6 +5,8 @@ import org.apache.axis.message.addressing.EndpointReferenceType;
 import org.oasis.wsrf.properties.WSResourcePropertiesServiceAddressingLocator;
 
 /**
+ * Utility that will ping an EPR to see if the endpoint is "Active" or "Inactive"
+ * <p/>
  * Created by IntelliJ IDEA.
  * User: kherm
  * Date: Oct 25, 2006
@@ -14,6 +16,12 @@ import org.oasis.wsrf.properties.WSResourcePropertiesServiceAddressingLocator;
 public class EPRPingService {
 
 
+    /**
+     * Utility that will ping an EPR to see if the endpoint is "Active" or "Inactive"
+     *
+     * @param epr
+     * @return boolean True means EPR is "Alive" or "Active"
+     */
     public static boolean ping(EndpointReferenceType epr) {
         WSResourcePropertiesServiceAddressingLocator locator = new WSResourcePropertiesServiceAddressingLocator();
 
