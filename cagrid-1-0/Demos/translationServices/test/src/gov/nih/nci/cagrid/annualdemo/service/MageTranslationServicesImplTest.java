84e913e01b858ec34e4ee03f617b37a16f7b41de
mccon012 <mccon012>
2007-01-25 15:14:12 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6371 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/Demos/translationServices/test/src/gov/nih/nci/cagrid/annualdemo/service/MageTranslationServicesImplTest.java
@@ -0,0 +1,60 @@
+/*
+ * Created on Jan 25, 2007
+ */
+package gov.nih.nci.cagrid.annualdemo.service;
+
+import java.io.File;
+
+import org.apache.axis.message.MessageElement;
+import org.apache.xerces.parsers.DOMParser;
+
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.cqlresultset.CQLObjectResult;
+import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
+import gridextensions.Data;
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class MageTranslationServicesImplTest
+	extends TestCase
+{
+	public MageTranslationServicesImplTest(String name)
+	{
+		super(name);
+	}
+	
+	public void testMageToStatML()
+		throws Exception
+	{
+		/*
+		DOMParser parser = new DOMParser();
+		parser.parse("test" + File.separator + "resources" + File.separator + "mage.xml");
+		
+		CQLQueryResults results = new CQLQueryResults();
+		
+		CQLObjectResult result = new CQLObjectResult();
+		
+		results.setObjectResult(new CQLObjectResult[] { result });
+
+		MessageElement message = new MessageElement(parser.getDocument().getDocumentElement());
+		result.set_any(new MessageElement[] { message });
+		*/
+
+		CQLQueryResults results = (CQLQueryResults) Utils.deserializeDocument(
+			"test" + File.separator + "resources" + File.separator + "mage.xml",
+			CQLQueryResults.class
+		);
+		
+		MageTranslationServicesImpl service = new MageTranslationServicesImpl();
+		Data staml = service.mageToStatML(results);
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		TestRunner runner = new TestRunner();
+		TestResult result = runner.doRun(new TestSuite(MageTranslationServicesImplTest.class));
+		System.exit(result.errorCount() + result.failureCount());
+	}
+}
