98652ed7ab0ec399b2893f1994ea04bd3bb96f16
dervin <dervin>
2007-02-20 14:50:59 +0000

    sp
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6544 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/upgrade/ExtensionUpgraderBase.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/upgrade/ExtensionUpgraderBase.java
@@ -6,9 +6,9 @@ import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionType;
 /**
  * Class must be extended to provide an extension upgrader. An extension
  * upgrader should be used to provide upgrades to the service to support a newer
- * version of extension. The extension upgrader should only touch parts of hte
- * service that the extension etself controls, i.e. the extension data in the
- * xml entity in the introduce.xml document in the services top level directory.
+ * version of an extension. The extension upgrader should only touch parts of the
+ * service that the extension itself controls, i.e. the extension data in the
+ * xml entity in the introduce.xml document in the service's top level directory.
  * The version attribute in the extensionType will automatically be updated.
  * 
  * @author hastings
@@ -25,18 +25,20 @@ public abstract class ExtensionUpgraderBase extends UpgraderBase {
 		this.extensionType = extensionType;
 	}
 
+	
 	public void execute() throws Exception {
 		// TODO Auto-generated method stub
 		super.execute();
 		getServiceDescription().setIntroduceVersion(getToVersion());
 	}
 
+	
 	public ExtensionType getExtensionType() {
 		return extensionType;
 	}
 
+	
 	public void setExtensionType(ExtensionType extensionType) {
 		this.extensionType = extensionType;
 	}
-
 }
