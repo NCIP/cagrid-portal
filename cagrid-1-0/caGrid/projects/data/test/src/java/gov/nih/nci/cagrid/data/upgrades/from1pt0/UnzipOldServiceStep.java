062fab0c659924c17c27a55c723771c75b8b5d29
dervin <dervin>
2007-02-21 16:48:56 +0000

    make use of zip utils
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6567 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/upgrades/from1pt0/UnzipOldServiceStep.java
+++ cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/upgrades/from1pt0/UnzipOldServiceStep.java
@@ -1,12 +1,8 @@
 package gov.nih.nci.cagrid.data.upgrades.from1pt0;
 
-import java.io.BufferedOutputStream;
+import gov.nih.nci.cagrid.common.ZipUtilities;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import com.atomicobject.haste.framework.Step;
 
@@ -16,7 +12,7 @@ import com.atomicobject.haste.framework.Step;
  * 
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>  * 
  * @created Feb 20, 2007 
- * @version $Id: UnzipOldServiceStep.java,v 1.1 2007-02-20 21:03:17 dervin Exp $ 
+ * @version $Id: UnzipOldServiceStep.java,v 1.2 2007-02-21 16:48:56 dervin Exp $ 
  */
 public class UnzipOldServiceStep extends Step {
 	private String testDir;
@@ -28,32 +24,7 @@ public class UnzipOldServiceStep extends Step {
 	
 
 	public void runStep() throws Throwable {
-		FileInputStream zipFileInput = new FileInputStream(testDir + File.separator + "resources" + File.separator + "CaGridTutorialService1.0.zip");
-		ZipInputStream zipInput = new ZipInputStream(zipFileInput);
-		ZipEntry entry = null;
-		while ((entry = zipInput.getNextEntry()) != null) {
-			String name = entry.getName();
-			File outFile = new File(testDir + File.separator + name);
-			if (entry.isDirectory()) {
-				outFile.mkdirs();
-			} else {
-				outFile.createNewFile();
-				BufferedOutputStream fileOut = new BufferedOutputStream(
-					new FileOutputStream(testDir + File.separator + name));
-				copyStreams(zipInput, fileOut);
-				fileOut.flush();
-				fileOut.close();
-			}
-		}
-		zipInput.close();
-	}
-	
-	
-	private void copyStreams(ZipInputStream input, BufferedOutputStream output) throws IOException {
-		byte[] temp = new byte[8192];
-		int read = -1;
-		while ((read = input.read(temp)) != -1) {
-			output.write(temp, 0, read);
-		}
+		File zippedService = new File(testDir + File.separator + "resources" + File.separator + "CaGridTutorialService1.0.zip");
+		ZipUtilities.unzip(zippedService, new File(testDir));
 	}
 }
