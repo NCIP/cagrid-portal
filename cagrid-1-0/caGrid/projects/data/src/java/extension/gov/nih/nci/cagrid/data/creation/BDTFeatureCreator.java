7f66df41a682f5eb94bd9a998b820986938352d8
dervin <dervin>
2007-03-12 17:00:39 +0000

    properly sets up Introduce model to add BDT query method
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6711 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/BDTFeatureCreator.java
+++ cagrid-1-0/caGrid/projects/data/src/java/extension/gov/nih/nci/cagrid/data/creation/BDTFeatureCreator.java
@@ -1,13 +1,21 @@
 package gov.nih.nci.cagrid.data.creation;
 
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.data.DataServiceConstants;
 import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionDescription;
 import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionType;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodType;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputs;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputsInput;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeOutput;
 import gov.nih.nci.cagrid.introduce.beans.service.ServiceType;
 import gov.nih.nci.cagrid.introduce.extension.CreationExtensionException;
 import gov.nih.nci.cagrid.introduce.extension.ExtensionsLoader;
 import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
 
 import java.util.Properties;
+
+import javax.xml.namespace.QName;
 
 /** 
  *  BDTFeatureCreator
@@ -16,9 +24,12 @@ import java.util.Properties;
  * @author David Ervin
  * 
  * @created Mar 9, 2007 3:45:48 PM
- * @version $Id: BDTFeatureCreator.java,v 1.1 2007-03-10 16:31:36 dervin Exp $ 
+ * @version $Id: BDTFeatureCreator.java,v 1.2 2007-03-12 17:00:39 dervin Exp $ 
  */
 public class BDTFeatureCreator extends FeatureCreator {
+	
+	public static final String BDT_RESOURCE_CREATION_LINE = "BDTResource thisResource = (BDTResource)bdtHome.find(bdtResourceKey);";
+	
 
 	public BDTFeatureCreator(ServiceInformation info, ServiceType mainService, Properties serviceProps) {
 		super(info, mainService, serviceProps);
@@ -26,13 +37,55 @@ public class BDTFeatureCreator extends FeatureCreator {
 
 
 	public void addFeature() throws CreationExtensionException {
+		System.out.println("ADDING BDT SERVICE");
 		ensureBdtExtensionAdded();
 		addBdtQueryMethod();
 	}
 	
 	
 	private void addBdtQueryMethod() throws CreationExtensionException {
+		// create the BDT query method
+		MethodType bdtQueryMethod = new MethodType();
+		bdtQueryMethod.setName(DataServiceConstants.BDT_QUERY_METHOD_NAME);
+		bdtQueryMethod.setDescription(DataServiceConstants.BDT_QUERY_METHOD_DESCRIPTION);
+		// input of CQL query
+		MethodTypeInputs inputs = new MethodTypeInputs();
+		MethodTypeInputsInput cqlInput = new MethodTypeInputsInput();
+		cqlInput.setQName(DataServiceConstants.CQL_QUERY_QNAME);
+		cqlInput.setName(DataServiceConstants.QUERY_METHOD_PARAMETER_NAME);
+		cqlInput.setDescription(DataServiceConstants.QUERY_METHOD_PARAMETER_DESCRIPTION);
+		cqlInput.setIsArray(false);
+		inputs.setInput(new MethodTypeInputsInput[] {cqlInput});
+		bdtQueryMethod.setInputs(inputs);
+		// output of BDT client handle
+		MethodTypeOutput bdtHandleOutput = new MethodTypeOutput();
+		// find the BDT handler service
+		ServiceType[] services = getServiceInformation().getServices().getService();
+		ServiceType bdtService = null;
+		for (int i = 0; i < services.length; i++) {
+			String serviceName = services[i].getName();
+			String packageName = services[i].getPackageName();
+			if (serviceName.endsWith("BulkDataHandler") && packageName.endsWith(".bdt")) {
+				bdtService = services[i];
+				break;
+			}
+		}
+		if (bdtService == null) {
+			throw new CreationExtensionException("BDT Sub-Service Not Found!");
+		}
+		QName bdtClientHandleQName = new QName(bdtService.getNamespace() + "/types",
+			bdtService.getName() + "Reference");
+		bdtHandleOutput.setQName(bdtClientHandleQName);
+		bdtHandleOutput.setIsArray(false);
+		bdtHandleOutput.setIsClientHandle(Boolean.TRUE);
+		bdtHandleOutput.setClientHandleClass(bdtService.getPackageName() + ".client." 
+			+ bdtService.getName() + "Client");
+		bdtQueryMethod.setOutput(bdtHandleOutput);
 		
+		// add the method to the service
+		getMainService().getMethods().setMethod(
+			(MethodType[]) Utils.appendToArray(
+				getMainService().getMethods().getMethod(), bdtQueryMethod));
 	}
 	
 	
@@ -66,6 +119,8 @@ public class BDTFeatureCreator extends FeatureCreator {
 			moreExtensions[0] = bdtExtension;
 			System.arraycopy(usedExtensions, 0, moreExtensions, 1, usedExtensions.length);
 			getServiceInformation().getExtensions().setExtension(moreExtensions);
+		} else {
+			System.out.println("BDT Extension was already present in the service");
 		}
 	}
 }
