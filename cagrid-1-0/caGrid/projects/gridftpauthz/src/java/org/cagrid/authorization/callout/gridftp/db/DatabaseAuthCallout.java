ce8292a98494305d04d4cd6b68fdb563b94dbc46
jpermar <jpermar>
2007-03-19 21:25:05 +0000

    Using properties from the classpath to get the db connection parameters
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6814 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/gridftpauthz/src/java/org/cagrid/authorization/callout/gridftp/db/DatabaseAuthCallout.java
+++ cagrid-1-0/caGrid/projects/gridftpauthz/src/java/org/cagrid/authorization/callout/gridftp/db/DatabaseAuthCallout.java
@@ -1,5 +1,9 @@
 package org.cagrid.authorization.callout.gridftp.db;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.util.Properties;
 import java.util.logging.Level;
 
 import org.cagrid.authorization.callout.gridftp.AbstractAuthCallout;
@@ -10,6 +14,12 @@ import org.cagrid.authorization.callout.gridftp.GridFTPOperation.Operation;
 
 public class DatabaseAuthCallout extends AbstractAuthCallout {
 
+	public static final String DB_CONNECT_STRING="db.connecturi";
+	public static final String DB_USER_PROP_NAME="db.user";
+	public static final String DB_PASSWORD_PROP_NAME="db.password";
+	
+	public static final String RESOURCE_LOCATION="/org/cagrid.authorization/callout/gridftp/db/db.props";
+	
 	@Override
 	public boolean authorizeOperation(String identity, Operation operation, String target) {
 
@@ -17,11 +27,31 @@ public class DatabaseAuthCallout extends AbstractAuthCallout {
 		
 		DBUtil util;
 		try {
-			String DB_NAME = "gridftp_authorization";
-			String connectionString = "jdbc:hsqldb:hsql://localhost/" + DB_NAME;
-			String dbuser = "sa";
-			String password = "ch3ck1t@uth";
+			/* read in properties file containing the db connection parameters */
+			InputStream stream = this.getClass().getClassLoader().getResourceAsStream(RESOURCE_LOCATION);
+			Properties props = new Properties();
+			props.load(stream);
+			String connectionString = props.getProperty(DB_CONNECT_STRING);
+			String dbuser = props.getProperty(DB_USER_PROP_NAME);
+			String password = props.getProperty(DB_PASSWORD_PROP_NAME);
 
+			//check that properties aren't null
+			if (connectionString == null) {
+				String msg = "Could not load property named " + DB_CONNECT_STRING + " from " + RESOURCE_LOCATION + " on the classpath.";
+				msg += " Check that your properties file is correct.";
+				throw new PropertyNotFoundException(msg);
+			}
+			if (dbuser == null) {
+				String msg = "Could not load property named " + DB_USER_PROP_NAME+ " from " + RESOURCE_LOCATION + " on the classpath.";
+				msg += " Check that your properties file is correct.";
+				throw new DatabaseException(msg);
+			}
+			if (password == null) {
+				String msg = "Could not load property named " + DB_PASSWORD_PROP_NAME + " from " + RESOURCE_LOCATION + " on the classpath.";
+				msg += " Check that your properties file is correct.";
+				throw new DatabaseException(msg);
+			}
+			
 			util = new DBUtil(connectionString, dbuser, password);
 			
 			GridFTPTuple tuple = new GridFTPTuple(identity, operation, target);
@@ -29,9 +59,19 @@ public class DatabaseAuthCallout extends AbstractAuthCallout {
 		} catch (DatabaseException e) {
 			String msg = "Could not check if tuple exists";
 			_logger.log(Level.SEVERE, msg, e);
+			System.out.println(msg);
 		} catch (MalformedURLException e) {
 			String msg = "Bad URL: " + target;
 			_logger.log(Level.SEVERE, msg, e);
+			System.out.println(msg);
+		} catch (IOException e) {
+			String msg = "Could not load db connection properties. Expected to find the following resource on the classpath: " + RESOURCE_LOCATION + ".";
+			msg += " Reason: " + e.getMessage();
+			_logger.log(Level.SEVERE, msg, e);
+			System.out.println(msg);
+		} catch (PropertyNotFoundException e) {
+			_logger.severe(e.getMessage());
+			System.out.println(e.getMessage());
 		}
 
 		return authorized;
