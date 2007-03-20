2a7987a5a4b83c99ac4e0c1474f07fcabcedacdd
dervin <dervin>
2007-03-20 17:32:43 +0000

    bdt data system test code
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6827 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/system/bdt/BDTDataServiceSystemTests.java
@@ -0,0 +1,111 @@
+package gov.nih.nci.cagrid.data.system.bdt;
+
+import gov.nih.nci.cagrid.data.creation.bdt.BDTDataServiceCreationTests;
+import gov.nih.nci.cagrid.data.system.AddBookstoreStep;
+import gov.nih.nci.cagrid.data.system.BaseSystemTest;
+import gov.nih.nci.cagrid.data.system.CreateCleanGlobusStep;
+import gov.nih.nci.cagrid.data.system.DeployDataServiceStep;
+import gov.nih.nci.cagrid.data.system.DestroyTempGlobusStep;
+import gov.nih.nci.cagrid.data.system.EnableValidationStep;
+import gov.nih.nci.cagrid.data.system.RebuildServiceStep;
+import gov.nih.nci.cagrid.data.system.SetQueryProcessorStep;
+import gov.nih.nci.cagrid.data.system.StartGlobusStep;
+import gov.nih.nci.cagrid.data.system.StopGlobusStep;
+import gov.nih.nci.cagrid.introduce.IntroduceConstants;
+import gov.nih.nci.cagrid.introduce.test.IntroduceTestConstants;
+import gov.nih.nci.cagrid.introduce.test.util.GlobusHelper;
+
+import java.io.File;
+import java.util.Vector;
+
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import com.atomicobject.haste.framework.Step;
+
+/** 
+ *  BDTDataServiceSystemTests
+ *  System tests for BDT Data Service
+ * 
+ * @author David Ervin
+ * 
+ * @created Mar 14, 2007 2:19:42 PM
+ * @version $Id: BDTDataServiceSystemTests.java,v 1.1 2007-03-20 17:32:43 dervin Exp $ 
+ */
+public class BDTDataServiceSystemTests extends BaseSystemTest {
+	private static GlobusHelper globusHelper = new GlobusHelper(false, new File(IntroduceTestConstants.TEST_TEMP),
+		IntroduceTestConstants.TEST_PORT + 10);
+	
+	public String getDescription() {
+		return "System tests for BDT Data Service";
+	}
+	
+	
+	protected boolean storySetUp() {
+		// make sure globus isn't running
+		assertFalse("Globus should NOT be running yet", globusHelper.isGlobusRunning());
+		// verify the BDT service has been built
+		File serviceDir = new File(BDTDataServiceCreationTests.SERVICE_DIR);
+		assertTrue("BDT Data Service directory NOT FOUND", serviceDir.exists());
+		File serviceModel = new File(serviceDir.getAbsolutePath() 
+			+ File.separator + IntroduceConstants.INTRODUCE_XML_FILE);
+		assertTrue("BDT Data Service directory does not appear to be an Introduce service", 
+			serviceModel.exists());
+		return true;
+	}
+
+
+	protected Vector steps() {
+		Vector steps = new Vector();
+		// assumes the BDT service has been created already
+		// 1) Add the bookstore schema to the data service
+		steps.add(new AddBookstoreStep(BDTDataServiceCreationTests.SERVICE_DIR, 
+			BDTDataServiceCreationTests.SERVICE_NAME));
+		// 2) change out query processor
+		steps.add(new SetQueryProcessorStep(BDTDataServiceCreationTests.SERVICE_DIR));
+		// 3) Turn on query validation
+		steps.add(new EnableValidationStep(BDTDataServiceCreationTests.SERVICE_DIR));
+		// 4) Rebuild the service to pick up the bookstore beans
+		steps.add(new RebuildServiceStep(getIntroduceBaseDir(), BDTDataServiceCreationTests.SERVICE_DIR,
+			BDTDataServiceCreationTests.SERVICE_NAME, BDTDataServiceCreationTests.PACKAGE_NAME,
+			BDTDataServiceCreationTests.SERVICE_NAMESPACE));
+		// 5) set up a clean, temporary Globus
+		steps.add(new CreateCleanGlobusStep(globusHelper));
+		// 6) deploy data service
+		steps.add(new DeployDataServiceStep(globusHelper, BDTDataServiceCreationTests.SERVICE_DIR));
+		// 7) start globus
+		steps.add(new StartGlobusStep(globusHelper));
+		// 8) test bdt data service
+		steps.add(new InvokeBDTDataServiceStep("localhost", IntroduceTestConstants.TEST_PORT + 10,
+			BDTDataServiceCreationTests.SERVICE_NAME));
+		return steps;
+	}
+	
+	
+	protected void storyTearDown() throws Throwable {
+		super.storyTearDown();
+		// 9) stop globus
+		Step stopStep = new StopGlobusStep(globusHelper);
+		try {
+			stopStep.runStep();
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+		}
+		// 10) throw away globus
+		Step destroyStep = new DestroyTempGlobusStep(globusHelper);
+		try {
+			destroyStep.runStep();
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		TestRunner runner = new TestRunner();
+		TestResult result = runner.doRun(new TestSuite(
+				BDTDataServiceSystemTests.class));
+		System.exit(result.errorCount() + result.failureCount());
+	}
+}
---
+++ cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/system/bdt/InvokeBDTDataServiceStep.java
@@ -0,0 +1,119 @@
+package gov.nih.nci.cagrid.data.system.bdt;
+
+import gov.nih.nci.cagrid.bdt.client.BulkDataHandlerClient;
+import gov.nih.nci.cagrid.cqlquery.CQLQuery;
+import gov.nih.nci.cagrid.data.bdt.client.BDTDataServiceClient;
+
+import java.util.NoSuchElementException;
+
+import javax.xml.soap.SOAPElement;
+
+import org.apache.axis.message.MessageElement;
+import org.globus.transfer.AnyXmlType;
+import org.globus.transfer.EmptyType;
+import org.globus.ws.enumeration.ClientEnumIterator;
+import org.projectmobius.bookstore.Book;
+import org.xmlsoap.schemas.ws._2004._09.enumeration.EnumerateResponse;
+
+import com.atomicobject.haste.framework.Step;
+
+/** 
+ *  InvokeBDTDataServiceStep
+ *  Step to invoke a BDT data service and exercise its methods
+ * 
+ * @author David Ervin
+ * 
+ * @created Mar 14, 2007 2:37:02 PM
+ * @version $Id: InvokeBDTDataServiceStep.java,v 1.1 2007-03-20 17:32:43 dervin Exp $ 
+ */
+public class InvokeBDTDataServiceStep extends Step {
+	public static final String URL_PART = "/wsrf/services/cagrid/";
+	
+	private String hostName;
+	private int port;
+	private String serviceName;
+	
+	
+	public InvokeBDTDataServiceStep(String hostName, int port, String serviceName) {
+		this.hostName = hostName;
+		this.port = port;
+		this.serviceName = serviceName;
+	}
+	
+
+	public void runStep() throws Throwable {
+		System.out.println("Running step " + getClass().getName());
+		String serviceUrl = getServiceUrl();
+		System.out.println("Invoking service at URL " + serviceUrl);
+		
+        // create the BDT service client handle
+        BDTDataServiceClient bdtClient = new BDTDataServiceClient(serviceUrl);
+        
+        BulkDataHandlerClient bdtHandlerClient = startBdt(bdtClient);
+        
+        iterateEnumeration(bdtHandlerClient);
+        
+        invokeTransfer(bdtHandlerClient);
+	}
+    
+    
+    private BulkDataHandlerClient startBdt(BDTDataServiceClient client) throws Exception {
+        CQLQuery bookQuery = new CQLQuery();
+        gov.nih.nci.cagrid.cqlquery.Object target = new gov.nih.nci.cagrid.cqlquery.Object();
+        target.setName(Book.class.getName());
+        bookQuery.setTarget(target);
+        BulkDataHandlerClient bdtHandlerClient = client.bdtQuery(bookQuery);
+        return bdtHandlerClient;
+    }
+    
+    
+    private EnumerateResponse beginEnumeration(BulkDataHandlerClient client) throws Exception {
+        EnumerateResponse response = client.createEnumeration();
+        assertNotNull("Enumeration response does not contain an enumeration context", response.getEnumerationContext());
+        return response;
+    }
+    
+    
+    private void iterateEnumeration(BulkDataHandlerClient client) throws Exception {
+        EnumerateResponse response = beginEnumeration(client);
+        /*
+         * This is the preferred way to access an enumeration, but the client enum iterator hides
+         * remote exceptions from the user and throws an empty NoSuchElement exception.
+         */
+        ClientEnumIterator iter = new ClientEnumIterator(client, response.getEnumerationContext());
+        int resultCount = 0;
+        try {
+            while (iter.hasNext()) {
+                SOAPElement elem = (SOAPElement) iter.next();
+                String elemText = elem.toString();
+                // make sure it's a book element
+                int bookIndex = elemText.indexOf("Book");
+                if (bookIndex == -1) {
+                    throw new NoSuchElementException("Element returned was not of the type Book!");
+                }
+                resultCount++;
+            }
+        } catch (NoSuchElementException ex) {
+            if (resultCount == 0) {
+                throw ex;
+            }
+        } finally {
+            iter.release();
+        }
+        assertTrue("No results were returned from the enumeration", resultCount != 0);
+    }
+    
+    
+    private void invokeTransfer(BulkDataHandlerClient client) throws Exception {
+        AnyXmlType any = client.get(new EmptyType());
+        MessageElement[] anyElements = any.get_any();
+        assertNotNull("Content from transfer was null", anyElements);
+        // TODO: get content from any type, deserialize as CQLQueryResults, iterate objects
+        
+    }
+	
+	
+	private String getServiceUrl() {
+		return "http://" + hostName + ":" + port + URL_PART + serviceName;
+	}
+}
