cfb9bfb659fc1f7e65d2b39fd74d3582e8fc031c
oster <oster>
2007-03-21 19:13:00 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6866 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/metadatautils/src/java/gov/nih/nci/cagrid/metadata/MetadataUtils.java
+++ cagrid-1-0/caGrid/projects/metadatautils/src/java/gov/nih/nci/cagrid/metadata/MetadataUtils.java
@@ -16,119 +16,119 @@ import org.w3c.dom.Element;
 
 
 public class MetadataUtils {
-	/**
-	 * Obtain the service metadata from the specified service.
-	 * 
-	 * @param serviceEPR
-	 * @return
-	 * @throws InvalidResourcePropertyException
-	 * @throws RemoteResourcePropertyRetrievalException
-	 * @throws ResourcePropertyRetrievalException
-	 */
-	public static ServiceMetadata getServiceMetadata(EndpointReferenceType serviceEPR)
-		throws InvalidResourcePropertyException, RemoteResourcePropertyRetrievalException,
-		ResourcePropertyRetrievalException {
-		Element resourceProperty = ResourcePropertyHelper.getResourceProperty(serviceEPR,
-			MetadataConstants.CAGRID_MD_QNAME);
-		ServiceMetadata result;
-		try {
-			result = result = deserializeServiceMetadata(new StringReader(XmlUtils.toString(resourceProperty)));
-		} catch (Exception e) {
-			throw new ResourcePropertyRetrievalException("Unable to deserailize ServiceMetadata: " + e.getMessage(), e);
-		}
-		return result;
-	}
+    /**
+     * Obtain the service metadata from the specified service.
+     * 
+     * @param serviceEPR
+     * @return
+     * @throws InvalidResourcePropertyException
+     * @throws RemoteResourcePropertyRetrievalException
+     * @throws ResourcePropertyRetrievalException
+     */
+    public static ServiceMetadata getServiceMetadata(EndpointReferenceType serviceEPR)
+        throws InvalidResourcePropertyException, RemoteResourcePropertyRetrievalException,
+        ResourcePropertyRetrievalException {
+        Element resourceProperty = ResourcePropertyHelper.getResourceProperty(serviceEPR,
+            MetadataConstants.CAGRID_MD_QNAME);
+        ServiceMetadata result;
+        try {
+            result = deserializeServiceMetadata(new StringReader(XmlUtils.toString(resourceProperty)));
+        } catch (Exception e) {
+            throw new ResourcePropertyRetrievalException("Unable to deserailize ServiceMetadata: " + e.getMessage(), e);
+        }
+        return result;
+    }
 
 
-	/**
-	 * Obtain the data service metadata from the specified service.
-	 * 
-	 * @param serviceEPR
-	 * @return
-	 * @throws InvalidResourcePropertyException
-	 * @throws RemoteResourcePropertyRetrievalException
-	 * @throws ResourcePropertyRetrievalException
-	 */
-	public static DomainModel getDomainModel(EndpointReferenceType serviceEPR) throws InvalidResourcePropertyException,
-		RemoteResourcePropertyRetrievalException, ResourcePropertyRetrievalException {
-		Element resourceProperty = ResourcePropertyHelper.getResourceProperty(serviceEPR,
-			MetadataConstants.CAGRID_DATA_MD_QNAME);
+    /**
+     * Obtain the data service metadata from the specified service.
+     * 
+     * @param serviceEPR
+     * @return
+     * @throws InvalidResourcePropertyException
+     * @throws RemoteResourcePropertyRetrievalException
+     * @throws ResourcePropertyRetrievalException
+     */
+    public static DomainModel getDomainModel(EndpointReferenceType serviceEPR) throws InvalidResourcePropertyException,
+        RemoteResourcePropertyRetrievalException, ResourcePropertyRetrievalException {
+        Element resourceProperty = ResourcePropertyHelper.getResourceProperty(serviceEPR,
+            MetadataConstants.CAGRID_DATA_MD_QNAME);
 
-		DomainModel result;
-		try {
-			System.out.println(XmlUtils.toString(resourceProperty));
-			result = deserializeDomainModel(new StringReader(XmlUtils.toString(resourceProperty)));
-		} catch (Exception e) {
-			throw new ResourcePropertyRetrievalException("Unable to deserailize DomainModel: " + e.getMessage(), e);
-		}
-		return result;
-	}
-
-
-	/**
-	 * Write the XML representation of the specified metadata to the specified
-	 * writer. If either are null, an IllegalArgumentException will be thown.
-	 * 
-	 * @param metadata
-	 * @param writer
-	 * @throws Exception
-	 */
-	public static void serializeServiceMetadata(ServiceMetadata metadata, Writer writer) throws Exception {
-		if (metadata == null || writer == null) {
-			throw new IllegalArgumentException("Null is not a valid argument");
-		}
-		Utils.serializeObject(metadata, MetadataConstants.CAGRID_MD_QNAME, writer);
-	}
+        DomainModel result;
+        try {
+            System.out.println(XmlUtils.toString(resourceProperty));
+            result = deserializeDomainModel(new StringReader(XmlUtils.toString(resourceProperty)));
+        } catch (Exception e) {
+            throw new ResourcePropertyRetrievalException("Unable to deserailize DomainModel: " + e.getMessage(), e);
+        }
+        return result;
+    }
 
 
-	/**
-	 * Create an instance of the service metadata from the specified reader. The
-	 * reader must point to a stream that contains an XML representation of the
-	 * metadata. If the reader is null, an IllegalArgumentException will be
-	 * thown.
-	 * 
-	 * @param xmlReader
-	 * @return
-	 * @throws Exception
-	 */
-	public static ServiceMetadata deserializeServiceMetadata(Reader xmlReader) throws Exception {
-		if (xmlReader == null) {
-			throw new IllegalArgumentException("Null is not a valid argument");
-		}
-		return (ServiceMetadata) Utils.deserializeObject(xmlReader, ServiceMetadata.class);
-	}
+    /**
+     * Write the XML representation of the specified metadata to the specified
+     * writer. If either are null, an IllegalArgumentException will be thown.
+     * 
+     * @param metadata
+     * @param writer
+     * @throws Exception
+     */
+    public static void serializeServiceMetadata(ServiceMetadata metadata, Writer writer) throws Exception {
+        if (metadata == null || writer == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        Utils.serializeObject(metadata, MetadataConstants.CAGRID_MD_QNAME, writer);
+    }
 
 
-	/**
-	 * Write the XML representation of the specified metadata to the specified
-	 * writer. If either are null, an IllegalArgumentException will be thown.
-	 * 
-	 * @param domainModel
-	 * @param writer
-	 * @throws Exception
-	 */
-	public static void serializeDomainModel(DomainModel domainModel, Writer writer) throws Exception {
-		if (domainModel == null || writer == null) {
-			throw new IllegalArgumentException("Null is not a valid argument");
-		}
-		Utils.serializeObject(domainModel, MetadataConstants.CAGRID_DATA_MD_QNAME, writer);
-	}
+    /**
+     * Create an instance of the service metadata from the specified reader. The
+     * reader must point to a stream that contains an XML representation of the
+     * metadata. If the reader is null, an IllegalArgumentException will be
+     * thown.
+     * 
+     * @param xmlReader
+     * @return
+     * @throws Exception
+     */
+    public static ServiceMetadata deserializeServiceMetadata(Reader xmlReader) throws Exception {
+        if (xmlReader == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        return (ServiceMetadata) Utils.deserializeObject(xmlReader, ServiceMetadata.class);
+    }
 
 
-	/**
-	 * Create an instance of the data service metadata from the specified
-	 * reader. The reader must point to a stream that contains an XML
-	 * representation of the metadata. If the reader is null, an
-	 * IllegalArgumentException will be thown.
-	 * 
-	 * @param xmlReader
-	 * @return
-	 * @throws Exception
-	 */
-	public static DomainModel deserializeDomainModel(Reader xmlReader) throws Exception {
-		if (xmlReader == null) {
-			throw new IllegalArgumentException("Null is not a valid argument");
-		}
-		return (DomainModel) Utils.deserializeObject(xmlReader, DomainModel.class);
-	}
+    /**
+     * Write the XML representation of the specified metadata to the specified
+     * writer. If either are null, an IllegalArgumentException will be thown.
+     * 
+     * @param domainModel
+     * @param writer
+     * @throws Exception
+     */
+    public static void serializeDomainModel(DomainModel domainModel, Writer writer) throws Exception {
+        if (domainModel == null || writer == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        Utils.serializeObject(domainModel, MetadataConstants.CAGRID_DATA_MD_QNAME, writer);
+    }
+
+
+    /**
+     * Create an instance of the data service metadata from the specified
+     * reader. The reader must point to a stream that contains an XML
+     * representation of the metadata. If the reader is null, an
+     * IllegalArgumentException will be thown.
+     * 
+     * @param xmlReader
+     * @return
+     * @throws Exception
+     */
+    public static DomainModel deserializeDomainModel(Reader xmlReader) throws Exception {
+        if (xmlReader == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        return (DomainModel) Utils.deserializeObject(xmlReader, DomainModel.class);
+    }
 }
