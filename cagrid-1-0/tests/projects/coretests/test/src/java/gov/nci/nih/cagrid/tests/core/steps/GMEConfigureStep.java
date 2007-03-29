9d24d5bab79d9b5b4e24c48920332786a17a032d
oster <oster>
2007-03-29 05:06:59 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@6930 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/GMEConfigureStep.java
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/tests/core/steps/GMEConfigureStep.java
@@ -3,6 +3,7 @@
  */
 package gov.nci.nih.cagrid.tests.core.steps;
 
+import gov.nci.nih.cagrid.tests.core.util.FileUtils;
 import gov.nci.nih.cagrid.tests.core.util.GlobusHelper;
 import gov.nci.nih.cagrid.tests.core.util.SimpleXmlReplacer;
 
@@ -10,42 +11,42 @@ import java.io.File;
 
 import com.atomicobject.haste.framework.Step;
 
+
 /**
- * This step sets the mysql username and password fields in gme-globus-config.xml of a deployed GME service
+ * This step sets the mysql username and password fields in
+ * gme-globus-config.xml of a deployed GME service
+ * 
  * @author Patrick McConnell
  */
-public class GMEConfigureStep
-	extends Step
-{
-	private GlobusHelper globus;
-	private String user;
-	private String password;
-	
-	public GMEConfigureStep(GlobusHelper globus)
-	{
-		this(globus,
-			System.getProperty("mysql.user", "root"),
-			System.getProperty("mysql.password", "")
-		);
-	}
-	
-	public GMEConfigureStep(GlobusHelper globus, String user, String password)
-	{
-		super();
-		
-		this.globus = globus;
-		this.user = user;
-		this.password = password;
-	}
+public class GMEConfigureStep extends Step {
+    private GlobusHelper globus;
+    private String user;
+    private String password;
 
-	public void runStep() throws Throwable
-	{
-		File configFile = new File(globus.getTempGlobusLocation(), 
-			"etc" + File.separator + "cagrid_GlobalModelExchange" + File.separator + "gme-globus-config.xml"
-		);
-		SimpleXmlReplacer replacer = new SimpleXmlReplacer();
-		replacer.addReplacement("username", user);
-		replacer.addReplacement("password", password);
-		replacer.performReplacement(configFile);
-	}
+
+    public GMEConfigureStep(GlobusHelper globus) {
+        this(globus, System.getProperty("mysql.user", "root"), System.getProperty("mysql.password", ""));
+    }
+
+
+    public GMEConfigureStep(GlobusHelper globus, String user, String password) {
+        super();
+
+        this.globus = globus;
+        this.user = user;
+        this.password = password;
+    }
+
+
+    @Override
+    public void runStep() throws Throwable {
+        File configFile = new File(this.globus.getTempGlobusLocation(), "etc" + File.separator
+            + "cagrid_GlobalModelExchange" + File.separator + "gme-globus-config.xml");
+        SimpleXmlReplacer replacer = new SimpleXmlReplacer();
+        replacer.addReplacement("username", this.user);
+        replacer.addReplacement("password", this.password);
+        replacer.performReplacement(configFile);
+
+        FileUtils.replace(configFile, "localhost:8080", "localhost:" + this.globus.getPort());
+    }
 }
