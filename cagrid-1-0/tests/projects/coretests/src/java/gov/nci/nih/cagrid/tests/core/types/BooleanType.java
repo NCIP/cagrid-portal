9166b370a9fb83dad6fec1ada955f0ef1a62fa63
oster <oster>
2007-04-05 20:35:48 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7029 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/types/BooleanType.java
+++ cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/types/BooleanType.java
@@ -1,9 +1,3 @@
-/*
- * Created on Feb 17, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package gov.nci.nih.cagrid.tests.core.types;
 
 /**
@@ -11,29 +5,31 @@ package gov.nci.nih.cagrid.tests.core.types;
  * 
  * @author MCCON012
  */
-public class BooleanType 
-{
-	/**
-	 * Actual boolean value
-	 */
-	private boolean value;
-	
-	/**
-	 * Construct a boolean from the string
-	 * @param str true, false, 1, or 0
-	 */
-	public BooleanType(String str)
-	{
-		super();
-		
-		value = str.equals("true") || str.equals("1");
-	}
-	
-	/**
-	 * Test the equality of two BooleanType boolean values
-	 */
-	public boolean equals(Object obj)
-	{
-		return value == ((BooleanType) obj).value;
-	}
+public class BooleanType {
+    /**
+     * Actual boolean value
+     */
+    private boolean value;
+
+
+    /**
+     * Construct a boolean from the string
+     * 
+     * @param str
+     *            true, false, 1, or 0
+     */
+    public BooleanType(String str) {
+        super();
+
+        this.value = str.equals("true") || str.equals("1");
+    }
+
+
+    /**
+     * Test the equality of two BooleanType boolean values
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return this.value == ((BooleanType) obj).value;
+    }
 }
