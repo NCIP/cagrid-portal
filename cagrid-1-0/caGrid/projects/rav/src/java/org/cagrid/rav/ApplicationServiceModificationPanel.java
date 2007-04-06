311fd6e024deb8239607ed4abf158b9c100addd0
maddurir <maddurir>
2007-04-06 18:45:05 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7037 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/rav/src/java/org/cagrid/rav/ApplicationServiceModificationPanel.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.cagrid.rav;
+
+import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
+import gov.nih.nci.cagrid.introduce.info.ServiceInformation;
+import gov.nih.nci.cagrid.introduce.portal.extension.ServiceModificationUIPanel;
+
+/**
+ * @author madduri
+ *
+ */
+public class ApplicationServiceModificationPanel extends
+		ServiceModificationUIPanel {
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public ApplicationServiceModificationPanel(ServiceExtensionDescriptionType desc, ServiceInformation info) {
+		super(desc, info);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.cagrid.introduce.portal.extension.ServiceModificationUIPanel#resetGUI()
+	 */
+	protected void resetGUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
