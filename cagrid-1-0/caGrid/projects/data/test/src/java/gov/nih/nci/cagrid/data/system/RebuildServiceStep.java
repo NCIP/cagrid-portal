b70bfec7974cc42a03c171b7e4971745dff512f9
dervin <dervin>
2007-04-12 14:24:55 +0000

    added stream output so we can see what the problem is
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7085 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/system/RebuildServiceStep.java
+++ cagrid-1-0/caGrid/projects/data/test/src/java/gov/nih/nci/cagrid/data/system/RebuildServiceStep.java
@@ -3,6 +3,10 @@ package gov.nih.nci.cagrid.data.system;
 import gov.nih.nci.cagrid.data.creation.TestServiceInfo;
 import gov.nih.nci.cagrid.introduce.common.CommonTools;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import com.atomicobject.haste.framework.Step;
 
 /** 
@@ -12,7 +16,7 @@ import com.atomicobject.haste.framework.Step;
  * 
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>  * 
  * @created Nov 7, 2006 
- * @version $Id: RebuildServiceStep.java,v 1.4 2007-03-22 14:21:25 dervin Exp $ 
+ * @version $Id: RebuildServiceStep.java,v 1.5 2007-04-12 14:24:55 dervin Exp $ 
  */
 public class RebuildServiceStep extends Step {
 	
@@ -34,7 +38,10 @@ public class RebuildServiceStep extends Step {
             serviceInfo.getName(), serviceInfo.getDir(), serviceInfo.getPackage(), 
             serviceInfo.getNamespace(), serviceInfo.getExtensions());
 		Process p = CommonTools.createAndOutputProcess(cmd);
+        new StreamDumpster(p.getInputStream(), System.out).start();
+        new StreamDumpster(p.getErrorStream(), System.err).start();
 		p.waitFor();
+        
 		assertTrue("Service post creation process failed", p.exitValue() == 0);
 
 		System.out.println("Building created service...");
@@ -43,4 +50,29 @@ public class RebuildServiceStep extends Step {
 		p.waitFor();
 		assertTrue("Build process failed", p.exitValue() == 0);
 	}
+    
+    
+    private static class StreamDumpster extends Thread {
+        private InputStream in;
+        private OutputStream out;
+        
+        public StreamDumpster(InputStream in, OutputStream out) {
+            this.in = in;
+            this.out = out;
+        }
+        
+        
+        public void run() {
+            byte[] buff = new byte[1024];
+            int len = -1;
+            try {
+                while ((len = in.read(buff)) != -1) {
+                    out.write(buff, 0, len);
+                    out.flush();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
