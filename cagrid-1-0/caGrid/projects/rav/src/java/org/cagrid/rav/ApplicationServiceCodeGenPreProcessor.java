5f8d465e1592732411ecebd7793bdaea5fad6d2a
maddurir <maddurir>
2007-04-24 20:53:10 +0000

    added code to add service method automatically
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7158 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/rav/src/java/org/cagrid/rav/ApplicationServiceCodeGenPreProcessor.java
+++ cagrid-1-0/caGrid/projects/rav/src/java/org/cagrid/rav/ApplicationServiceCodeGenPreProcessor.java
@@ -3,63 +3,109 @@
  */
 package org.cagrid.rav;
 
+import java.io.File;
+
+import javax.xml.namespace.QName;
+
 import org.apache.log4j.Logger;
 import org.ggf.schemas.jsdl._2005._11.jsdl.Application_Type;
 
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.introduce.IntroduceConstants;
+import gov.nih.nci.cagrid.introduce.beans.ServiceDescription;
 import gov.nih.nci.cagrid.introduce.beans.extension.ExtensionTypeExtensionData;
 import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
 import gov.nih.nci.cagrid.introduce.beans.method.MethodType;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputs;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeInputsInput;
+import gov.nih.nci.cagrid.introduce.beans.method.MethodTypeOutput;
 import gov.nih.nci.cagrid.introduce.beans.method.MethodsType;
 import gov.nih.nci.cagrid.introduce.common.CommonTools;
 import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
 import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionException;
 import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionPreProcessor;
 import gov.nih.nci.cagrid.introduce.extension.ExtensionTools;
+
 /**
  * @author madduri
- *
+ * 
  */
-public class ApplicationServiceCodeGenPreProcessor implements CodegenExtensionPreProcessor  {
-	private static final Logger logger = Logger.getLogger(ApplicationServiceCodeGenPreProcessor.class);
+public class ApplicationServiceCodeGenPreProcessor implements
+		CodegenExtensionPreProcessor {
 	
-	Application_Type appTypeData = null;
-	
-	String methodName = null;
-	String appDescritption = null;
-	String appVersion = null;
-	
+	private static final Logger logger = Logger
+			.getLogger(ApplicationServiceCodeGenPreProcessor.class);
 
-	/* (non-Javadoc)
-	 * @see gov.nih.nci.cagrid.introduce.extension.CodegenExtensionPreProcessor#preCodegen(gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType, gov.nih.nci.cagrid.introduce.common.ServiceInformation)
+	Application_Type appTypeData = null;
+
+	String methodName = null;
+
+	String appDescritption = null;
+
+	String appVersion = null;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gov.nih.nci.cagrid.introduce.extension.CodegenExtensionPreProcessor#preCodegen(gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType,
+	 *      gov.nih.nci.cagrid.introduce.common.ServiceInformation)
 	 */
-	public void preCodegen(ServiceExtensionDescriptionType desc, ServiceInformation info) throws CodegenExtensionException {
+	public void preCodegen(ServiceExtensionDescriptionType desc,
+			ServiceInformation info) throws CodegenExtensionException {
 		logger.info("in precodegen");
 		MethodsType methods = info.getServices().getService(0).getMethods();
-		for (int i=0;i<methods.getMethod().length;i++){
-			logger.info("Method name:" +methods.getMethod()[i].getName());
-		}
-		MethodsType newMethods = new MethodsType();
-		MethodType[] newMethodArray = new MethodType[1];
-		
-		//info.getServices().getService(0).setMethods(newMethods );
-		CommonTools.addMethod(info.getServices().getService()[0], newMethodArray[0]);
-		ExtensionTypeExtensionData data = ExtensionTools.getExtensionData(desc, info);
+		ExtensionTypeExtensionData data = ExtensionTools.getExtensionData(desc,
+				info);
+
 		try {
 			this.appTypeData = ExtensionDataUtils.getExtensionData(data);
 			this.methodName = this.appTypeData.getApplicationName();
 			this.appDescritption = this.appTypeData.getDescription();
 			this.appVersion = this.appTypeData.getApplicationVersion();
-			newMethodArray[0] = new MethodType("String", "String",this.appDescritption, 
-					null, null, "java.lang.String", null, false, false, null, appDescritption, null, appDescritption, null);
-			newMethodArray[0].setName(this.methodName);
-			newMethods.setMethod(newMethodArray);
-			logger.info(appTypeData.getApplicationName() + appTypeData.getApplicationVersion());
+			for (int i = 0; i < methods.getMethod().length; i++) {
+				logger.info("Method name:" + methods.getMethod()[i].getName());
+				if (methods.getMethod()[i].getName().equals(this.methodName)) {
+					logger.info("Method already added");
+					return;
+				}
+			}
+
+			MethodType method = new MethodType();
+			method.setName(this.methodName);
+			MethodTypeOutput output = new MethodTypeOutput();
+			output.setQName(new QName("http://www.w3.org/2001/XMLSchema",
+					"boolean"));
+			method.setOutput(output);
+
+			// create a new input param
+			MethodTypeInputsInput input1 = new MethodTypeInputsInput();
+			input1.setQName(new QName("http://www.w3.org/2001/XMLSchema",
+					"string"));
+			input1.setName("foo");
+			input1.setIsArray(false);
+			// create a new input param
+			MethodTypeInputsInput input2 = new MethodTypeInputsInput();
+			input2.setQName(new QName("http://www.w3.org/2001/XMLSchema",
+					"integer"));
+			input2.setName("bar");
+			input2.setIsArray(false);
+			MethodTypeInputsInput[] newInputs = new MethodTypeInputsInput[2];
+			newInputs[0] = input1;
+			newInputs[1] = input2;
+			MethodTypeInputs inputs = new MethodTypeInputs();
+			inputs.setInput(newInputs);
+			method.setInputs(inputs);
+			CommonTools.addMethod(info.getServices().getService()[0], method);
+			logger.info(appTypeData.getApplicationName()
+					+ appTypeData.getApplicationVersion());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	
+	private void editServiceImpl() {
+		
+	}
 
 }
