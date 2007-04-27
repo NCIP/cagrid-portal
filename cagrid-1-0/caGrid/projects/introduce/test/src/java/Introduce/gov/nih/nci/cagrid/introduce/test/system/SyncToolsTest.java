5b5b889dd2747588f48b2d7bf816eb2ddafb2653
hastings <hastings>
2007-04-27 17:19:23 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7188 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/test/src/java/Introduce/gov/nih/nci/cagrid/introduce/test/system/SyncToolsTest.java
+++ cagrid-1-0/caGrid/projects/introduce/test/src/java/Introduce/gov/nih/nci/cagrid/introduce/test/system/SyncToolsTest.java
@@ -9,6 +9,7 @@ import gov.nih.nci.cagrid.introduce.test.steps.AddComplexMethodWithFaultStep;
 import gov.nih.nci.cagrid.introduce.test.steps.AddImportedMethodStep;
 import gov.nih.nci.cagrid.introduce.test.steps.AddMetadataStep;
 import gov.nih.nci.cagrid.introduce.test.steps.AddMetadatatWithLoadFromFileStep;
+import gov.nih.nci.cagrid.introduce.test.steps.AddMethodReturningClientHandleMethodStep;
 import gov.nih.nci.cagrid.introduce.test.steps.AddServicePropertiesStep;
 import gov.nih.nci.cagrid.introduce.test.steps.AddServiceStep;
 import gov.nih.nci.cagrid.introduce.test.steps.AddSimpleMethodImplStep;
@@ -41,129 +42,133 @@ import junit.textui.TestRunner;
 
 import com.atomicobject.haste.framework.Story;
 
+
 public class SyncToolsTest extends Story {
-	private TestCaseInfo tci1;
+    private TestCaseInfo tci1;
 
-	private TestCaseInfo tci2;
+    private TestCaseInfo tci2;
 
-	private GlobusHelper helper;
+    private GlobusHelper helper;
 
-	public SyncToolsTest() {
-		this.setName("Introduce Codegen System Test");
-	}
 
-	protected Vector steps() {
-		tci1 = new TestCaseInfo1();
-		tci2 = new TestCaseInfo2();
-		helper = new GlobusHelper(false, new File(
-				IntroduceTestConstants.TEST_TEMP),
-				IntroduceTestConstants.TEST_PORT);
-		Vector steps = new Vector();
+    public SyncToolsTest() {
+        this.setName("Introduce Codegen System Test");
+    }
 
-		try {
-			steps.add(new CreateGlobusStep(helper));
-			steps.add(new CreateSkeletonStep(tci1, true));
-			steps.add(new AddServiceStep(tci2, true));
-			steps.add(new AddMetadataStep(tci1, true));
-			steps.add(new AddServicePropertiesStep(tci1, true));
-			steps.add(new AddSimpleMethodStep(tci1, "newMethod", false));
-			steps.add(new AddSimpleMethodImplStep(tci1, "newMethod", true));
-			steps.add(new AddSimpleMethodStep(tci2, "newMethod2", true));
-			steps.add(new DeployGlobusServiceStep(helper, tci1));
-			steps.add(new StartGlobusStep(helper));
-			steps.add(new InvokeSimpleMethodImplStep(tci1, "newMethod", false));
-			steps.add(new RemoveSimpleMethodImplStep(tci1, "newMethod", true));
-			steps.add(new ModifySimpleMethodStep(tci1, "newMethod", false));
-			steps.add(new ModifySimpleMethodStep(tci2, "newMethod2", true));
-			steps.add(new RemoveMethodStep(tci1, "newMethod", false));
-			steps.add(new RemoveMethodStep(tci2, "newMethod2", false));
-			steps.add(new AddSimpleMethodStep(tci2, "newMethod1", true));
-			steps.add(new AddImportedMethodStep(tci1, tci2, "newMethod1", true,
-					false));
-			steps.add(new AddSimpleMethodWithFaultStep(tci1,
-					"newMethodWithFault", false));
-			steps.add(new AddSimpleMethodWithReturnStep(tci1,
-					"newMethodWithReturn", false));
-			steps.add(new AddSimpleMethodWithArraysStep(tci1,
-					"newMethodWithArrays", true));
-			steps.add(new RollBackStep(tci1));
-			steps.add(new AddComplexMethodWithFaultStep(tci1,
-					"newComplexMethodWithFault", false));
-			steps.add(new AddComplexMethodWithFaulsAndArraysStep(tci1,
-					"newComplexMethodWithFaultStepsAndArrays", true));
-			steps.add(new AddMetadatatWithLoadFromFileStep(tci1, true));
-			steps.add(new RemoveAllMetadataStep(tci1, true));
-			steps.add(new RemoveAllServicePropertiesStep(tci1, true));
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-		return steps;
-	}
 
-	public String getDescription() {
-		return "Testing the Introduce code generation tools";
-	}
+    protected Vector steps() {
+        tci1 = new TestCaseInfo1();
+        tci2 = new TestCaseInfo2();
+        helper = new GlobusHelper(false, new File(IntroduceTestConstants.TEST_TEMP), IntroduceTestConstants.TEST_PORT);
+        Vector steps = new Vector();
 
-	protected boolean storySetUp() throws Throwable {
-		// TODO Auto-generated method stub
-		super.storySetUp();
+        try {
+            steps.add(new CreateGlobusStep(helper));
+            steps.add(new CreateSkeletonStep(tci1, true));
+            steps.add(new AddServiceStep(tci2, true));
+            steps.add(new AddMethodReturningClientHandleMethodStep(tci1, tci2, "testClientReturn", false, true));
+            steps
+                .add(new AddMethodReturningClientHandleMethodStep(tci1, tci2, "testClientReturnWithArray", true, true));
+            steps.add(new AddMetadataStep(tci1, true));
+            steps.add(new AddServicePropertiesStep(tci1, true));
+            steps.add(new AddSimpleMethodStep(tci1, "newMethod", false));
+            steps.add(new AddSimpleMethodImplStep(tci1, "newMethod", true));
+            steps.add(new AddSimpleMethodStep(tci2, "newMethod2", true));
+            steps.add(new DeployGlobusServiceStep(helper, tci1));
+            steps.add(new StartGlobusStep(helper));
+            steps.add(new InvokeSimpleMethodImplStep(tci1, "newMethod", false));
+            steps.add(new RemoveSimpleMethodImplStep(tci1, "newMethod", true));
+            steps.add(new ModifySimpleMethodStep(tci1, "newMethod", false));
+            steps.add(new ModifySimpleMethodStep(tci2, "newMethod2", true));
+            steps.add(new RemoveMethodStep(tci1, "newMethod", false));
+            steps.add(new RemoveMethodStep(tci2, "newMethod2", false));
+            steps.add(new AddSimpleMethodStep(tci2, "newMethod1", true));
+            steps.add(new AddImportedMethodStep(tci1, tci2, "newMethod1", true, false));
+            steps.add(new AddSimpleMethodWithFaultStep(tci1, "newMethodWithFault", false));
+            steps.add(new AddSimpleMethodWithReturnStep(tci1, "newMethodWithReturn", false));
+            steps.add(new AddSimpleMethodWithArraysStep(tci1, "newMethodWithArrays", true));
+            steps.add(new RollBackStep(tci1));
+            steps.add(new AddComplexMethodWithFaultStep(tci1, "newComplexMethodWithFault", false));
+            steps
+                .add(new AddComplexMethodWithFaulsAndArraysStep(tci1, "newComplexMethodWithFaultStepsAndArrays", true));
+            steps.add(new AddMetadatatWithLoadFromFileStep(tci1, true));
+            steps.add(new RemoveAllMetadataStep(tci1, true));
+            steps.add(new RemoveAllServicePropertiesStep(tci1, true));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+        return steps;
+    }
 
-		StopGlobusStep step2 = new StopGlobusStep(helper);
-		try {
-			step2.runStep();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
 
-		RemoveSkeletonStep step1 = new RemoveSkeletonStep(tci1);
-		try {
-			step1.runStep();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return true;
-	}
+    public String getDescription() {
+        return "Testing the Introduce code generation tools";
+    }
 
-	protected void storyTearDown() throws Throwable {
-		super.storyTearDown();
-		RemoveSkeletonStep step1 = new RemoveSkeletonStep(tci1);
-		try {
-			step1.runStep();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		StopGlobusStep step2 = new StopGlobusStep(helper);
-		try {
-			step2.runStep();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		CleanupGlobusStep step3 = new CleanupGlobusStep(helper);
-		try {
-			step3.runStep();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
-	// used to make sure that if we are going to use a junit testsuite to test
-	// this
-	// that the test suite will not error out looking for a single test......
-	public void testDummy() throws Throwable {
-	}
+    protected boolean storySetUp() throws Throwable {
+        // TODO Auto-generated method stub
+        super.storySetUp();
 
-	/**
-	 * Convenience method for running all the Steps in this Story.
-	 */
-	public static void main(String args[]) {
-		TestRunner runner = new TestRunner();
-		TestResult result = runner.doRun(new TestSuite(SyncToolsTest.class));
-		System.exit(result.errorCount() + result.failureCount());
-	}
+        StopGlobusStep step2 = new StopGlobusStep(helper);
+        try {
+            step2.runStep();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        RemoveSkeletonStep step1 = new RemoveSkeletonStep(tci1);
+        try {
+            step1.runStep();
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+
+    protected void storyTearDown() throws Throwable {
+        super.storyTearDown();
+        RemoveSkeletonStep step1 = new RemoveSkeletonStep(tci1);
+        try {
+            step1.runStep();
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        StopGlobusStep step2 = new StopGlobusStep(helper);
+        try {
+            step2.runStep();
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        CleanupGlobusStep step3 = new CleanupGlobusStep(helper);
+        try {
+            step3.runStep();
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    // used to make sure that if we are going to use a junit testsuite to test
+    // this
+    // that the test suite will not error out looking for a single test......
+    public void testDummy() throws Throwable {
+    }
+
+
+    /**
+     * Convenience method for running all the Steps in this Story.
+     */
+    public static void main(String args[]) {
+        TestRunner runner = new TestRunner();
+        TestResult result = runner.doRun(new TestSuite(SyncToolsTest.class));
+        System.exit(result.errorCount() + result.failureCount());
+    }
 
 }
