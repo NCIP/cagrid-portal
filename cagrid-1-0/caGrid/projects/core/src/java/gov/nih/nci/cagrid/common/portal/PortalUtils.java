eab38d4acd129c26d8833751f5271d506ba9debc
hastings <hastings>
2007-04-30 20:06:21 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7210 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/PortalUtils.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/PortalUtils.java
@@ -5,6 +5,7 @@ import gov.nih.nci.cagrid.common.Utils;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.Window;
 
 import javax.swing.JComponent;
@@ -31,25 +32,24 @@ public class PortalUtils {
 	}
 
 	public static void showConfigurationErrorMessage(String msg) {
-		showErrorMessage("Portal Configuration Error", new String[] {msg});
+		showErrorMessage("Portal Configuration Error", new String[] { msg });
 	}
 
 	public static void showMessage(String msg) {
-		showMessage(new String[] {msg});
+		showMessage(new String[] { msg });
 	}
-	
-	
+
 	public static void showMessage(String[] msg) {
 		showMessage("Information", msg);
 	}
 
 	public static void showMessage(String title, String msg) {
-		showMessage(title, new String[] {msg});
-	}	
-	
+		showMessage(title, new String[] { msg });
+	}
+
 	public static void showMessage(String title, String[] msg) {
 		JOptionPane.showMessageDialog(PortalResourceManager.getInstance()
-			.getGridPortal(), msg, title, JOptionPane.INFORMATION_MESSAGE);
+				.getGridPortal(), msg, title, JOptionPane.INFORMATION_MESSAGE);
 	}
 
 	public static void showErrorMessage(String title, Exception e) {
@@ -59,35 +59,37 @@ public class PortalUtils {
 	}
 
 	public static void showErrorMessage(String title, String msg) {
-		showErrorMessage(title, new String[] {msg});
-	}	
-	
+		showErrorMessage(title, new String[] { msg });
+	}
+
 	public static void showErrorMessage(String title, String[] msg) {
 		JOptionPane.showMessageDialog(PortalResourceManager.getInstance()
-			.getGridPortal(), msg, title, JOptionPane.ERROR_MESSAGE);		
+				.getGridPortal(), msg, title, JOptionPane.ERROR_MESSAGE);
 	}
 
 	public final static void centerComponent(JComponent comp) {
 		// Determine the new location of the window
 		int w = PortalResourceManager.getInstance().getGridPortal().getSize().width;
 		int h = PortalResourceManager.getInstance().getGridPortal().getSize().height;
-		int x = PortalResourceManager.getInstance().getGridPortal().getLocationOnScreen().x;
-		int y = PortalResourceManager.getInstance().getGridPortal().getLocationOnScreen().y;
+		int x = PortalResourceManager.getInstance().getGridPortal()
+				.getLocationOnScreen().x;
+		int y = PortalResourceManager.getInstance().getGridPortal()
+				.getLocationOnScreen().y;
 		Dimension dim = comp.getSize();
 		comp.setLocation(w / 2 + x - dim.width / 2, h / 2 + y - dim.height / 2);
 	}
 
 	public final static void centerWindow(Window comp) {
 		// Determine the new location of the window
-		int w = PortalResourceManager.getInstance().getGridPortal().getSize().width;
-		int h = PortalResourceManager.getInstance().getGridPortal().getSize().height;
-		int x = PortalResourceManager.getInstance().getGridPortal().getLocationOnScreen().x;
-		int y = PortalResourceManager.getInstance().getGridPortal().getLocationOnScreen().y;
-		Dimension dim = comp.getSize();
-		comp.setLocation(w / 2 + x - dim.width / 2, h / 2 + y - dim.height / 2);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		int w = comp.getSize().width;
+		int h = comp.getSize().height;
+		int x = (dim.width - w) / 2;
+		int y = (dim.height - h) / 2;
+
+		comp.setLocation(x, y);
 	}
-	
-	
+
 	public static void setContainerEnabled(Container con, boolean enable) {
 		for (int i = 0; i < con.getComponentCount(); i++) {
 			Component comp = con.getComponent(i);
