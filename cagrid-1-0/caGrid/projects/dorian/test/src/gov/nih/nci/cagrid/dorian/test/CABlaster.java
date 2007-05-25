bda99ff381e3c505c1f5caeb079f386ede8cf041
langella <langella>
2007-05-25 14:05:09 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7466 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/dorian/test/src/gov/nih/nci/cagrid/dorian/test/CABlaster.java
@@ -0,0 +1,45 @@
+package gov.nih.nci.cagrid.dorian.test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import gov.nih.nci.cagrid.dorian.service.ca.CertificateAuthority;
+
+
+public class CABlaster {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		try {
+			String userPrefix = "user";
+			String file = "count.txt";
+			CertificateAuthority ca = Utils.getCA();
+			Calendar c = new GregorianCalendar();
+			Date start = c.getTime();
+			c.add(Calendar.YEAR, 1);
+			Date expiration = c.getTime();
+			int count = 1;
+			while (true) {
+				try {
+					String user = userPrefix + count;
+					ca.createCredentials(user, user, null, start, expiration);
+					System.out.println("Created credentials for the user " + user);
+					PrintWriter out = new PrintWriter(new FileOutputStream(new File(file), false));
+					out.println(user);
+					count++;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
