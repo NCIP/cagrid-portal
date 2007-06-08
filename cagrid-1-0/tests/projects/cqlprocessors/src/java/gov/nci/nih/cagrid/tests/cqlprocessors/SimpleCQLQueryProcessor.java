a783a8310931ee97c0e5cb7d5d2134f828b81289
dervin <dervin>
2007-06-08 14:50:38 +0000

    passes along the real exception
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7611 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/cqlprocessors/src/java/gov/nci/nih/cagrid/tests/cqlprocessors/SimpleCQLQueryProcessor.java
+++ cagrid-1-0/tests/projects/cqlprocessors/src/java/gov/nci/nih/cagrid/tests/cqlprocessors/SimpleCQLQueryProcessor.java
@@ -5,46 +5,37 @@ package gov.nci.nih.cagrid.tests.cqlprocessors;
 
 import gov.nci.nih.cagrid.tests.cqlprocessors.util.RandomObject;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
-import gov.nih.nci.cagrid.cqlresultset.CQLObjectResult;
 import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
-import gov.nih.nci.cagrid.data.InitializationException;
 import gov.nih.nci.cagrid.data.MalformedQueryException;
 import gov.nih.nci.cagrid.data.QueryProcessingException;
 import gov.nih.nci.cagrid.data.cql.CQLQueryProcessor;
 import gov.nih.nci.cagrid.data.utilities.CQLResultsCreationUtil;
 
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Random;
-
-import javax.xml.namespace.QName;
-
-import org.apache.axis.MessageContext;
-import org.apache.axis.message.MessageElement;
 
 
 public class SimpleCQLQueryProcessor extends CQLQueryProcessor {
-	private byte[] axisConfig;
+    
+    public SimpleCQLQueryProcessor() {
+        super();
+    }
+    
 
-
-	public CQLQueryResults processQuery(CQLQuery cqlQuery) throws MalformedQueryException, QueryProcessingException
-	{
-		
+	public CQLQueryResults processQuery(CQLQuery cqlQuery) 
+        throws MalformedQueryException, QueryProcessingException {
 		try {
 			ArrayList<Object> resultList = new ArrayList<Object>();
-			for (int i = new Random().nextInt(10); i >= 0; i--) {
+			for (int i = 10; i >= 0; i--) {
 				Object obj = new RandomObject().next(
-					Class.forName(cqlQuery.getTarget().getName()),3);
+					Class.forName(cqlQuery.getTarget().getName()), 3);
 				resultList.add(obj);
 			}
 			
 			return CQLResultsCreationUtil.createObjectResults(resultList, cqlQuery.getTarget().getName(),  null);
-		} catch (Throwable t) {
+		} catch (Exception t) {
 			System.out.println(t.getMessage());
 			t.printStackTrace();
-			throw new QueryProcessingException(t.getMessage());
+			throw new QueryProcessingException(t.getMessage(), t);
 		}
 	}
 }
