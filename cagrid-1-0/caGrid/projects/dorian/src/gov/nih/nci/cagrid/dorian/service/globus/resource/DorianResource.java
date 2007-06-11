98ab1ed37748b03d4a07dd9d0937897058219f54
hastings <hastings>
2007-06-11 19:59:40 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7647 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/service/globus/resource/DorianResource.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.cagrid.dorian.service.globus.resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** 
+ * The implementation of this DorianResource type.
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class DorianResource extends BaseResourceBase {
+
+
+
+}
