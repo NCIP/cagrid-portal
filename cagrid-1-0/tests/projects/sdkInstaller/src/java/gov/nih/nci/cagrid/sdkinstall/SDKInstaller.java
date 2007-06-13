fba06c7dde7bec13293dffbc3b4550d1a0d7eddd
dervin <dervin>
2007-06-13 16:36:39 +0000

    starting sdk installer utility project
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7698 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/tests/projects/sdkInstaller/src/java/gov/nih/nci/cagrid/sdkinstall/SDKInstaller.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.cagrid.sdkinstall;
+
+/** 
+ *  SDKInstaller
+ *  Base "driver" class for installing the caCORE sdk
+ * 
+ * @author David Ervin
+ * 
+ * @created Jun 13, 2007 10:45:43 AM
+ * @version $Id: SDKInstaller.java,v 1.1 2007-06-13 16:36:39 dervin Exp $ 
+ */
+public class SDKInstaller {
+
+}
