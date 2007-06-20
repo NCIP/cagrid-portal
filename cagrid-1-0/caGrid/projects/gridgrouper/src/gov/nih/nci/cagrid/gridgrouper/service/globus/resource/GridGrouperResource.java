97f912b03d05f8426ce66f40f7837643b502fd40
hastings <hastings>
2007-06-20 14:45:22 +0000

    roll to introduce 1.1
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7772 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/gridgrouper/src/gov/nih/nci/cagrid/gridgrouper/service/globus/resource/GridGrouperResource.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.cagrid.gridgrouper.service.globus.resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** 
+ * The implementation of this GridGrouperResource type.
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class GridGrouperResource extends BaseResourceBase {
+
+
+
+}
--- cagrid-1-0/caGrid/projects/gridgrouper/src/gov/nih/nci/cagrid/gridgrouper/service/globus/resource/ResourceConstants.java
+++ cagrid-1-0/caGrid/projects/gridgrouper/src/gov/nih/nci/cagrid/gridgrouper/service/globus/resource/ResourceConstants.java
@@ -6,9 +6,9 @@ import javax.xml.namespace.QName;
 public interface ResourceConstants {
 	public static final String SERVICE_NS = "http://cagrid.nci.nih.gov/GridGrouper";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "GridGrouperKey");
-	public static final QName RESOURCE_PROPERY_SET = new QName(SERVICE_NS, "GridGrouperResourceProperties");
+	public static final QName RESOURCE_PROPERTY_SET = new QName(SERVICE_NS, "GridGrouperResourceProperties");
 
 	//Service level metadata (exposed as resouce properties)
-	public static final QName SERVICEMETADATA_MD_RP = new QName("gme://caGrid.caBIG/1.0/gov.nih.nci.cagrid.metadata", "ServiceMetadata");
+	public static final QName SERVICEMETADATA_Value_RP = new QName("gme://caGrid.caBIG/1.0/gov.nih.nci.cagrid.metadata", "ServiceMetadata");
 	
 }
