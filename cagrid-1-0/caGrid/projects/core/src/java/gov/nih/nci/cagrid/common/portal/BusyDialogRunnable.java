3be5e0e041c8d9f7f01c9e8201cb418ceaaa4874
oster <oster>
2007-06-20 16:51:11 +0000

    fixed so you can't close a busy dialog
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@7781 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/portal/BusyDialogRunnable.java
@@ -3,75 +3,84 @@ package gov.nih.nci.cagrid.common.portal;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 
 public abstract class BusyDialogRunnable implements Runnable {
 
-	final private BusyDialog dialog;
+    final private BusyDialog dialog;
 
-	private String errorMessage = "";
+    private String errorMessage = "";
 
-	private boolean valid = true;
+    private boolean valid = true;
 
-	private JFrame owner;
+    private JFrame owner;
 
-	public BusyDialogRunnable(JFrame owner, String title) {
-		this.owner = owner;
-		dialog = new BusyDialog(owner, "Progress (" + title + ")");
-	}
 
-	public void setProgressText(final String text) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				dialog.getProgress().setString(text);
-			}
-		});
-	}
+    public BusyDialogRunnable(JFrame owner, String title) {
+        this.owner = owner;
+        this.dialog = new BusyDialog(owner, "Progress (" + title + ")");
+        this.dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    }
 
-	public void run() {
-		Thread thread = new Thread(new Runnable() {
-			public void run() {
-				dialog.setVisible(true);
-			}
-		});
-		thread.start();
-		try {
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					dialog.getProgress().setIndeterminate(true);
-				}
-			});
-			process();
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					dialog.getProgress().setIndeterminate(false);
-				}
-			});
-		} catch (Throwable e) {
-			e.printStackTrace();
-			valid = false;
-			errorMessage = e.getMessage();
-		}
 
-		if (!valid) {
-			JOptionPane.showMessageDialog(owner, errorMessage);
-		}
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				dialog.dispose();
-			}
-		});
+    public void setProgressText(final String text) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                BusyDialogRunnable.this.dialog.getProgress().setString(text);
+            }
+        });
+    }
 
-	}
 
-	public abstract void process();
+    public void run() {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                BusyDialogRunnable.this.dialog.setVisible(true);
+            }
+        });
+        thread.start();
+        try {
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    BusyDialogRunnable.this.dialog.getProgress().setIndeterminate(true);
+                }
+            });
+            process();
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    BusyDialogRunnable.this.dialog.getProgress().setIndeterminate(false);
+                }
+            });
+        } catch (Throwable e) {
+            e.printStackTrace();
+            this.valid = false;
+            this.errorMessage = e.getMessage();
+        }
 
-	public String getErrorMessage() {
-		return errorMessage;
-	}
+        if (!this.valid) {
+            JOptionPane.showMessageDialog(this.owner, this.errorMessage);
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                BusyDialogRunnable.this.dialog.dispose();
+            }
+        });
 
-	public void setErrorMessage(String message) {
-		this.valid = false;
-		this.errorMessage = message;
-	}
+    }
+
+
+    public abstract void process();
+
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+
+    public void setErrorMessage(String message) {
+        this.valid = false;
+        this.errorMessage = message;
+    }
 
 }
