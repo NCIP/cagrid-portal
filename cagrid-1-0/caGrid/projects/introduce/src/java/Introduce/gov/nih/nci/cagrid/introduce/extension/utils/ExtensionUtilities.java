25bcd2b7d456aae3253f735af371dac68b7813e5
dervin <dervin>
2007-08-08 12:30:23 +0000

    added remove capability to classpath management
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@8245 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/extension/utils/ExtensionUtilities.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/extension/utils/ExtensionUtilities.java
@@ -4,8 +4,11 @@ import gov.nih.nci.cagrid.common.Utils;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import org.jdom.Element;
@@ -20,7 +23,7 @@ import org.projectmobius.common.XMLUtilities;
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
  * 
  * @created Jun 7, 2006 
- * @version $Id: ExtensionUtilities.java,v 1.5 2006-09-05 20:26:27 dervin Exp $ 
+ * @version $Id: ExtensionUtilities.java,v 1.6 2007-08-08 12:30:23 dervin Exp $ 
  */
 public class ExtensionUtilities {
 	public static final String CLASSPATHENTRY_ELEMENT = "classpathentry";
@@ -39,7 +42,7 @@ public class ExtensionUtilities {
 	public static void syncEclipseClasspath(File classpathFile, File[] additionalLibs) throws Exception {
 		Element classpathElement = XMLUtilities.fileNameToDocument(classpathFile.getAbsolutePath()).getRootElement();
 		
-		// get the list of additional libraries for the query processor
+		// get the list of additional libraries to add to the classpath
 		Set libNames = new HashSet();
 		for (int i = 0; i < additionalLibs.length; i++) {
 			String relativeLibName = Utils.getRelativePath(classpathFile, additionalLibs[i]);
@@ -75,6 +78,89 @@ public class ExtensionUtilities {
 		writer.close();
 	}
 	
+    
+    /**
+     * Gets the libraries from an eclipse .classpath file
+     * 
+     * @param classpathFile
+     * @return
+     *      An array of Files for each library in the classpath
+     * @throws Exception
+     */
+    public static File[] getLibrariesFromEclipseClasspath(File classpathFile) throws Exception {
+        Element classpathElement = XMLUtilities.fileNameToDocument(classpathFile.getAbsolutePath()).getRootElement();
+        
+        List<File> libs = new ArrayList();
+        // find out which libs are in the classpath
+        Iterator classpathEntryIter = classpathElement.getChildren(
+            CLASSPATHENTRY_ELEMENT, classpathElement.getNamespace()).iterator();
+        while (classpathEntryIter.hasNext()) {
+            Element entry = (Element) classpathEntryIter.next();
+            if (entry.getAttributeValue("kind").equals("lib")) {
+                String relPath = entry.getAttributeValue("path");
+                File fullPath = new File(classpathFile.getParentFile().getAbsolutePath() 
+                    + File.separator + relPath);
+                libs.add(fullPath.getCanonicalFile());
+            }
+        }
+        File[] libArray = new File[libs.size()];
+        libs.toArray(libArray);
+        return libArray;
+    }
+    
+    
+    /**
+     * Removes library entries from an Eclipse .classpath file
+     * Libraries to be removed may be fully qualified paths, and will first be
+     * made relative to the .classpath file before removal is attempted.
+     * 
+     * @param classpathFile
+     * @param removeLibs
+     * @throws Exception
+     */
+    public static void removeLibrariesFromClasspath(File classpathFile, File[] removeLibs) throws Exception {
+        Element classpathElement = XMLUtilities.fileNameToDocument(classpathFile.getAbsolutePath()).getRootElement();
+        
+        // get the list of libraries to be removed
+        Set<String> libNames = new HashSet();
+        for (File remove : removeLibs) {
+            String relativeLibName = Utils.getRelativePath(classpathFile, remove);
+            relativeLibName = convertToUnixStylePath(relativeLibName);
+            libNames.add(relativeLibName);
+        }
+        
+        // start removing entries
+        List<Element> keptEntries = new LinkedList();
+        Iterator classpathEntryIter = classpathElement.getChildren(
+            CLASSPATHENTRY_ELEMENT, classpathElement.getNamespace()).iterator();
+        while (classpathEntryIter.hasNext()) {
+            Element entry = (Element) classpathEntryIter.next();
+            boolean keep = true;
+            if (entry.getAttributeValue("kind").equals("lib")) {
+                String libPath = entry.getAttributeValue("path");
+                if (libNames.contains(libPath)) {
+                    keep = false;
+                }
+            }
+            if (keep) {
+                keptEntries.add(entry);
+            }
+        }
+        // remove ALL classpath entries
+        classpathElement.removeChildren(CLASSPATHENTRY_ELEMENT, classpathElement.getNamespace());
+        // restore the ones we're not removing
+        for (Element e : keptEntries) {
+            classpathElement.addContent(e);
+        }
+        
+        // write the .classpath file back out to disk
+        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
+        FileWriter writer = new FileWriter(classpathFile);
+        outputter.output(classpathElement, writer);
+        writer.flush();
+        writer.close();
+    }
+    
 	
 	private static String convertToUnixStylePath(String pathname) {
 		return pathname.replace('\\', '/');
