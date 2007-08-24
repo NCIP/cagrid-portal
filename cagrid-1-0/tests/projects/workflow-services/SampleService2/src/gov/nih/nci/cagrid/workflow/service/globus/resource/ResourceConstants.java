2eb1a428e4fd79cb89e020f17b0115e5f321e777
maddurir <maddurir>
2007-08-24 17:43:29 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@8575 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/workflow-services/SampleService2/src/gov/nih/nci/cagrid/workflow/service/globus/resource/ResourceConstants.java
+++ cagrid-1-0/tests/projects/workflow-services/SampleService2/src/gov/nih/nci/cagrid/workflow/service/globus/resource/ResourceConstants.java
@@ -6,7 +6,7 @@ import javax.xml.namespace.QName;
 public interface ResourceConstants {
 	public static final String SERVICE_NS = "http://workflow.cagrid.nci.nih.gov/SampleService2";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "SampleService2Key");
-	public static final QName RESOURCE_PROPERY_SET = new QName(SERVICE_NS, "SampleService2ResourceProperties");
+	public static final QName RESOURCE_PROPERTY_SET = new QName(SERVICE_NS, "SampleService2ResourceProperties");
 
 	//Service level metadata (exposed as resouce properties)
 	
