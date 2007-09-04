363c00146d7d416fae0b51a49f3c675005c8bad9
dervin <dervin>
2007-09-04 16:23:47 +0000

    properly edits wsdds and mapping files
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@8701 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/dataExtensions/src/java/style/cacore32/gov/nih/nci/cagrid/data/style/cacore32/helpers/SDK32PostCodegenHelper.java
+++ cagrid-1-0/caGrid/projects/dataExtensions/src/java/style/cacore32/gov/nih/nci/cagrid/data/style/cacore32/helpers/SDK32PostCodegenHelper.java
@@ -1,7 +1,6 @@
 package gov.nih.nci.cagrid.data.style.cacore32.helpers;
 
-import java.io.File;
-
+import gov.nih.nci.cagrid.common.Utils;
 import gov.nih.nci.cagrid.data.common.CastorMappingUtil;
 import gov.nih.nci.cagrid.data.style.cacore32.encoding.SDK32EncodingUtils;
 import gov.nih.nci.cagrid.data.style.sdkstyle.helpers.PostCodegenHelper;
@@ -13,6 +12,8 @@ import gov.nih.nci.cagrid.introduce.common.CommonTools;
 import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
 import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionException;
 
+import java.io.File;
+
 /** 
  *  SDK32PostCodegenHelper
  *  Fixes stuff in the castor mapping
@@ -20,7 +21,7 @@ import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionException;
  * @author David Ervin
  * 
  * @created Aug 30, 2007 3:56:50 PM
- * @version $Id: SDK32PostCodegenHelper.java,v 1.1 2007-08-31 15:50:47 dervin Exp $ 
+ * @version $Id: SDK32PostCodegenHelper.java,v 1.2 2007-09-04 16:23:47 dervin Exp $ 
  */
 public class SDK32PostCodegenHelper extends PostCodegenHelper {
 
@@ -51,22 +52,38 @@ public class SDK32PostCodegenHelper extends PostCodegenHelper {
                 + " not found!");
         }
         
-        // set properties in the client
+        // edit the marshalling castor mapping to avoid serializing associations
+        String marshallingXmlText = Utils.fileToStringBuffer(
+            new File(CastorMappingUtil.getMarshallingCastorMappingFileName(info))).toString();
+        String editedMarshallingText = CastorMappingUtil.removeAssociationMappings(marshallingXmlText);
+        String editedMarshallingFileName = CastorMappingUtil.getEditedMarshallingCastorMappingFileName(info);
+        Utils.stringBufferToFile(new StringBuffer(editedMarshallingText), editedMarshallingFileName);
+        
+        // edit the UNmarshalling castor mapping to avoid DEserializing associations
+        String unmarshallingXmlText = Utils.fileToStringBuffer(
+            new File(CastorMappingUtil.getUnmarshallingCastorMappingFileName(info))).toString();
+        String editedUnmarshallingText = CastorMappingUtil.removeAssociationMappings(unmarshallingXmlText);
+        String editedUnmarshallingFileName = CastorMappingUtil.getEditedUnmarshallingCastorMappingFileName(info);
+        Utils.stringBufferToFile(new StringBuffer(editedUnmarshallingText), editedUnmarshallingFileName);
+        
+        // set properties in the client to use the edited marshaller
         WsddUtil.setGlobalClientParameter(clientConfigFile.getAbsolutePath(),
             SDK32EncodingUtils.CASTOR_MARSHALLER_PROPERTY, 
-            CastorMappingUtil.getMarshallingCastorMappingName(info));
+            CastorMappingUtil.getEditedMarshallingCastorMappingName(info));
+        // and the edited unmarshaller
         WsddUtil.setGlobalClientParameter(clientConfigFile.getAbsolutePath(),
             SDK32EncodingUtils.CASTOR_UNMARSHALLER_PROPERTY, 
-            CastorMappingUtil.getUnmarshallingCastorMappingName(info));
+            CastorMappingUtil.getEditedUnmarshallingCastorMappingName(info));
         
-        // set properties in the server
+        // set properties in the server to use the edited marshaller
         WsddUtil.setServiceParameter(serverConfigFile.getAbsolutePath(),
             info.getServices().getService(0).getName(),
             SDK32EncodingUtils.CASTOR_MARSHALLER_PROPERTY,
-            CastorMappingUtil.getMarshallingCastorMappingName(info));
+            CastorMappingUtil.getEditedMarshallingCastorMappingName(info));
+        // and the edited unmarshaller
         WsddUtil.setServiceParameter(serverConfigFile.getAbsolutePath(),
             info.getServices().getService(0).getName(),
             SDK32EncodingUtils.CASTOR_UNMARSHALLER_PROPERTY,
-            CastorMappingUtil.getUnmarshallingCastorMappingName(info));
+            CastorMappingUtil.getEditedUnmarshallingCastorMappingName(info));
     }
 }
