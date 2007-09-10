74784225b834901d0cbbf7bf8e9b96b32e8357c0
dervin <dervin>
2007-09-10 14:32:49 +0000

    extends the base step
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@8748 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/validator/steps/dorian/TestDorianStep.java
+++ cagrid-1-0/tests/projects/coretests/test/src/java/gov/nci/nih/cagrid/validator/steps/dorian/TestDorianStep.java
@@ -3,7 +3,9 @@
  */
 package gov.nci.nih.cagrid.validator.steps.dorian;
 
-import com.atomicobject.haste.framework.Step;
+import gov.nci.nih.cagrid.validator.steps.AbstractBaseServiceTestStep;
+
+import java.io.File;
 
 
 /**
@@ -13,20 +15,16 @@ import com.atomicobject.haste.framework.Step;
  * @author David Ervin
  * 
  * @created Aug 27, 2007 4:04:29 PM
- * @version $Id: TestDorianStep.java,v 1.1 2007-08-27 20:06:02 dervin Exp $
+ * @version $Id: TestDorianStep.java,v 1.2 2007-09-10 14:32:49 dervin Exp $
  */
-public class TestDorianStep extends Step {
+public class TestDorianStep extends AbstractBaseServiceTestStep {
 
-    private String serviceURL;
-
-
-    public TestDorianStep(String serviceURL) {
-        System.out.println("Checking Globus Service");
-        this.serviceURL = serviceURL;
+    
+    public TestDorianStep(String serviceURL, File tempDir) {
+        super(serviceURL, tempDir);
     }
 
 
-    @Override
     public void runStep() throws Throwable {
         // TODO: connect to the url and validate a return
     }
