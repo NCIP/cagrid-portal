d20d02b20977bd74afcd7e889237220f16169fa6
langella <langella>
2007-09-18 15:26:18 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@8794 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/cds/src/org/cagrid/gaards/cds/service/CredentialDelegationServiceImpl.java
@@ -0,0 +1,30 @@
+package org.cagrid.gaards.cds.service;
+
+import java.rmi.RemoteException;
+
+
+/**
+ * TODO:I am the service side implementation class. IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.1
+ */
+public class CredentialDelegationServiceImpl extends CredentialDelegationServiceImplBase {
+
+	public CredentialDelegationServiceImpl() throws RemoteException {
+		super();
+	}
+
+
+	public void delegateCredential(org.cagrid.gaards.cds.common.DelegationPolicy policy) throws RemoteException,
+		org.cagrid.gaards.cds.stubs.types.CDSInternalFault, org.cagrid.gaards.cds.stubs.types.InvalidPolicyFault,
+		org.cagrid.gaards.cds.stubs.types.PermissionDeniedFault {
+		System.out.println("*****************************************");
+		System.out.println("*****************************************");
+		System.out.println("*****************************************");
+		System.out.println(policy.getClass().getName());
+		System.out.println("*****************************************");
+		System.out.println("*****************************************");
+		System.out.println("*****************************************");
+	}
+
+}
---
+++ cagrid-1-0/caGrid/projects/cds/src/org/cagrid/gaards/cds/service/globus/resource/CredentialDelegationServiceResource.java
@@ -0,0 +1,16 @@
+package org.cagrid.gaards.cds.service.globus.resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** 
+ * The implementation of this CredentialDelegationServiceResource type.
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class CredentialDelegationServiceResource extends BaseResourceBase {
+
+
+
+}
