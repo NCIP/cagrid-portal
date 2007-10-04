e3e7feca770c200f2b33c8cb10f9137fd0e8ef32
langella <langella>
2007-10-04 02:24:42 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@8921 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/cds/test/src/org/cagrid/gaards/cds/testutils/Constants.java
+++ cagrid-1-0/caGrid/projects/cds/test/src/org/cagrid/gaards/cds/testutils/Constants.java
@@ -1,6 +1,6 @@
 package org.cagrid.gaards.cds.testutils;
 
 public class Constants {
-	public static final String CDS_CONFIGURATION = "cds-conf.xml";
+	public static final String CDS_CONFIGURATION = "cds-configuration.xml";
 	public static final String CDS_PROPERTIES = "cds.properties";
 }
