7b0568eb7a5396b9dbcc68c9de2495398349e2e8
dervin <dervin>
2007-11-05 15:27:26 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9183 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/testing/src/java/system/gov/nih/nci/cagrid/testing/system/deployment/ServiceContainerType.java
+++ cagrid-1-0/caGrid/projects/testing/src/java/system/gov/nih/nci/cagrid/testing/system/deployment/ServiceContainerType.java
@@ -1,5 +1,8 @@
 package gov.nih.nci.cagrid.testing.system.deployment;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /** 
  *  ServiceContainerType
  *  Identifies the type of service container
@@ -7,7 +10,7 @@ package gov.nih.nci.cagrid.testing.system.deployment;
  * @author David Ervin
  * 
  * @created Oct 16, 2007 12:01:50 PM
- * @version $Id: ServiceContainerType.java,v 1.2 2007-11-02 14:49:00 dervin Exp $ 
+ * @version $Id: ServiceContainerType.java,v 1.3 2007-11-05 15:27:26 dervin Exp $ 
  */
 public enum ServiceContainerType {
 
@@ -20,8 +23,7 @@ public enum ServiceContainerType {
             case TOMCAT_CONTAINER:
                 return "../testing/resources/containers/minimal-tomcat-5.0.28-with-globus-4.0.3.zip";
             case JBOSS_CONTAINER:
-                
-                break;
+                throw new AssertionError("Container type " + this + " is not yet supported");
         }
         throw new AssertionError("Unknown service container type: " + this);
     }
@@ -37,5 +39,26 @@ public enum ServiceContainerType {
                 return "JBoss";
         }
         throw new AssertionError("Unknown service container type: " + this);
+    }
+    
+    
+    /**
+     * Utility method for testing to get a service container
+     * based on which day of the year it is.
+     * @return
+     *      A service container type
+     */
+    public static ServiceContainerType getTypeOfTheDay() {
+        Calendar today = new GregorianCalendar();
+        int dayOfYear = today.get(Calendar.DAY_OF_YEAR);
+        int type = dayOfYear % 2; // TODO: when JBoss works, do % 3
+        ServiceContainerType container = null;
+        switch (type) {
+            case 0:
+                container = GLOBUS_CONTAINER;
+            case 1:
+                container = TOMCAT_CONTAINER;
+        }
+        return container;
     }
 }
