608ce52d89f546c98495738ea4f0e7749014908a
dervin <dervin>
2007-11-06 21:27:30 +0000

    made this work
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9208 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/AntUtils.java
+++ cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/AntUtils.java
@@ -3,8 +3,6 @@ package gov.nci.nih.cagrid.tests.core.util;
 import gov.nih.nci.cagrid.introduce.common.AntTools;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -16,7 +14,7 @@ import java.util.Properties;
   * @author David Ervin
   * 
   * @created Nov 6, 2007 12:52:56 PM
-  * @version $Id: AntUtils.java,v 1.10 2007-11-06 21:06:24 dervin Exp $
+  * @version $Id: AntUtils.java,v 1.11 2007-11-06 21:27:30 dervin Exp $
  */
 public class AntUtils {
     public static String getAntCommand() {
@@ -43,65 +41,6 @@ public class AntUtils {
             throw new IllegalArgumentException(ant + " does not exist");
         }
         return ant;
-    }
-
-
-    /**
-     * @deprecated This code is a MESS! use getAntCommand();
-     * @param dir
-     * @param buildFile
-     * @param target
-     * @param sysProps
-     * @param envp
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public static void runAnt(File dir, String buildFile, String target, Properties sysProps, String[] envp)
-        throws IOException, InterruptedException {
-        // build command
-        ArrayList<String> cmd = new ArrayList<String>();
-        cmd.add(getAntCommand());
-
-        // add system properties
-        if (sysProps != null) {
-            Enumeration keys = sysProps.keys();
-            while (keys.hasMoreElements()) {
-                String name = (String) keys.nextElement();
-                String value = sysProps.getProperty(name);
-                if (!OSUtils.isWindows()) {
-                    value = value.replaceAll(" ", "\\\\ ");
-                }
-                cmd.add("\"-D" + name + "=" + value + "\"");
-            }
-        }
-
-        // add build file
-        if (buildFile != null) {
-            cmd.add("-f");
-            cmd.add(buildFile);
-        }
-
-        // add target
-        if (target != null) {
-            cmd.add(target);
-        }
-
-        // run ant
-        Process p = Runtime.getRuntime().exec(cmd.toArray(new String[0]), envp, dir);
-        // track stdout and stderr
-        StringBuffer stdout = new StringBuffer();
-        StringBuffer stderr = new StringBuffer();
-        new IOThread(p.getInputStream(), System.out, stdout).start();
-        new IOThread(p.getErrorStream(), System.err, stderr).start();
-
-        // wait and return
-        int result = p.waitFor();
-        if (stdout.indexOf("BUILD FAILED") != -1 || stderr.indexOf("BUILD FAILED") != -1
-            || stdout.indexOf("Build failed") != -1 || stderr.indexOf("Build failed") != -1) {
-            System.err.println(stderr);
-            System.out.println(stdout);
-            throw new IOException("ant command '" + target + "' failed");
-        }
     }
     
     
