6d98740c603b85bfe70e9b9fe2c717cd5b1d9566
dervin <dervin>
2007-11-06 21:06:03 +0000

    format
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9204 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/OSUtils.java
+++ cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/OSUtils.java
@@ -1,12 +1,7 @@
-/*
- * Created on Jun 10, 2006
- */
 package gov.nci.nih.cagrid.tests.core.util;
 
-public class OSUtils
-{
-	public static boolean isWindows()
-	{
-		return System.getProperty("os.name").toLowerCase().indexOf("win") != -1;
-	}
+public class OSUtils {
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().indexOf("win") != -1;
+    }
 }
