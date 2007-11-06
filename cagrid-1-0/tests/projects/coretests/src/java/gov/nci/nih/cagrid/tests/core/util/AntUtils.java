2496470b7c7da18875705274a3a3bf0e73f1f848
dervin <dervin>
2007-11-06 21:06:24 +0000

    made this work
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9205 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/AntUtils.java
+++ cagrid-1-0/tests/projects/coretests/src/java/gov/nci/nih/cagrid/tests/core/util/AntUtils.java
@@ -1,7 +1,6 @@
-/*
- * Created on Jun 10, 2006
- */
 package gov.nci.nih.cagrid.tests.core.util;
+
+import gov.nih.nci.cagrid.introduce.common.AntTools;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,79 +8,124 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Properties;
 
-public class AntUtils
-{
-	public static String getAntCommand()
-	{
-		// ant home
-		String ant = System.getenv("ANT_HOME");
-		if (ant == null) throw new IllegalArgumentException("ANT_HOME not set");
-		
-		// ant home/bin
-		if (! ant.endsWith(File.separator)) ant += File.separator;
-		ant += "bin" + File.separator;
-		
-		// ant home/bin/ant
-		if (OSUtils.isWindows()) {
-			ant += "ant.bat";
-		} else {
-			ant += "ant";
-		}
-		
-		if (! new File(ant).exists()) throw new IllegalArgumentException(ant + " does not exist");
-		return ant;
-	}
-	
-	public static void runAnt(
-		File dir, String buildFile, String target, Properties sysProps, String[] envp
-	)
-		throws IOException, InterruptedException 
-	{
-		// build command
-		ArrayList<String> cmd = new ArrayList<String>();
-		cmd.add(getAntCommand());
-		
-		// add system properties
-		if (sysProps != null) {
-			Enumeration keys = sysProps.keys();
-			while (keys.hasMoreElements()) {
-				String name = (String) keys.nextElement();
-				String value = (String) sysProps.getProperty(name);
-				if (! OSUtils.isWindows()) value = value.replaceAll(" ", "\\\\ ");
-				cmd.add("\"-D" + name + "=" + value + "\"");
-			}
-		}
-		
-		// add build file
-		if (buildFile != null) {
-			cmd.add("-f");
-			cmd.add(buildFile);
-		}
-		
-		// add target
-		if (target != null) cmd.add(target);
-		
-		// run ant
-		Process p = Runtime.getRuntime().exec(
-			cmd.toArray(new String[0]), envp, dir
-		);
-		// track stdout and stderr
-		StringBuffer stdout = new StringBuffer();
-		StringBuffer stderr = new StringBuffer();
-		new IOThread(p.getInputStream(), System.out, stdout).start();
-		new IOThread(p.getErrorStream(), System.err, stderr).start();
-		
-		// wait and return
-		int result = p.waitFor();
-		if (
-			stdout.indexOf("BUILD FAILED") != -1 || 
-			stderr.indexOf("BUILD FAILED") != -1 ||
-			stdout.indexOf("Build failed") != -1 || 
-			stderr.indexOf("Build failed") != -1
-		) {
-			System.err.println(stderr);
-			System.out.println(stdout);
-			throw new IOException("ant command '" + target + "' failed");
-		}
-	}
+
+/**
+  *  AntUtils
+  *  Utilities to fire off ant commands
+  * 
+  * @author David Ervin
+  * 
+  * @created Nov 6, 2007 12:52:56 PM
+  * @version $Id: AntUtils.java,v 1.10 2007-11-06 21:06:24 dervin Exp $
+ */
+public class AntUtils {
+    public static String getAntCommand() {
+        // ant home
+        String ant = System.getenv("ANT_HOME");
+        if (ant == null) {
+            throw new IllegalArgumentException("ANT_HOME not set");
+        }
+
+        // ant home/bin
+        if (!ant.endsWith(File.separator)) {
+            ant += File.separator;
+        }
+        ant += "bin" + File.separator;
+
+        // ant home/bin/ant
+        if (OSUtils.isWindows()) {
+            ant += "ant.bat";
+        } else {
+            ant += "ant";
+        }
+
+        if (!new File(ant).exists()) {
+            throw new IllegalArgumentException(ant + " does not exist");
+        }
+        return ant;
+    }
+
+
+    /**
+     * @deprecated This code is a MESS! use getAntCommand();
+     * @param dir
+     * @param buildFile
+     * @param target
+     * @param sysProps
+     * @param envp
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void runAnt(File dir, String buildFile, String target, Properties sysProps, String[] envp)
+        throws IOException, InterruptedException {
+        // build command
+        ArrayList<String> cmd = new ArrayList<String>();
+        cmd.add(getAntCommand());
+
+        // add system properties
+        if (sysProps != null) {
+            Enumeration keys = sysProps.keys();
+            while (keys.hasMoreElements()) {
+                String name = (String) keys.nextElement();
+                String value = sysProps.getProperty(name);
+                if (!OSUtils.isWindows()) {
+                    value = value.replaceAll(" ", "\\\\ ");
+                }
+                cmd.add("\"-D" + name + "=" + value + "\"");
+            }
+        }
+
+        // add build file
+        if (buildFile != null) {
+            cmd.add("-f");
+            cmd.add(buildFile);
+        }
+
+        // add target
+        if (target != null) {
+            cmd.add(target);
+        }
+
+        // run ant
+        Process p = Runtime.getRuntime().exec(cmd.toArray(new String[0]), envp, dir);
+        // track stdout and stderr
+        StringBuffer stdout = new StringBuffer();
+        StringBuffer stderr = new StringBuffer();
+        new IOThread(p.getInputStream(), System.out, stdout).start();
+        new IOThread(p.getErrorStream(), System.err, stderr).start();
+
+        // wait and return
+        int result = p.waitFor();
+        if (stdout.indexOf("BUILD FAILED") != -1 || stderr.indexOf("BUILD FAILED") != -1
+            || stdout.indexOf("Build failed") != -1 || stderr.indexOf("Build failed") != -1) {
+            System.err.println(stderr);
+            System.out.println(stdout);
+            throw new IOException("ant command '" + target + "' failed");
+        }
+    }
+    
+    
+    public static String getAntCommand(File baseDir, String target) throws Exception {
+        return getAntCommand(baseDir, target, null);
+    }
+    
+    
+    public static String getAntCommand(File baseDir, String target, Properties systemProps) throws Exception {
+        String command = AntTools.getAntCommand(target, baseDir.getAbsolutePath());
+        // add system properties
+        if (systemProps != null) {
+            Enumeration keys = systemProps.keys();
+            while (keys.hasMoreElements()) {
+                String name = (String) keys.nextElement();
+                String value = systemProps.getProperty(name);
+                if (!OSUtils.isWindows()) {
+                    value = value.replaceAll(" ", "\\\\ ");
+                }
+                String propPart = " \"-D" + name + "=" + value + "\"";
+                command += propPart;
+            }
+        }
+        
+        return command;
+    }
 }
