68caf0b7d5e47ae6ccf4ff8207145cefc2c46cc4
joshua <joshua>
2007-12-13 23:58:06 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9467 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/query/cql/CQLQueryBean.java
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/query/cql/CQLQueryBean.java
@@ -22,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class CQLQueryBean extends CriteriaBean {
 	
-	private QueryModifierType modifierType;
+	private QueryModifierType modifierType = QueryModifierType.COUNT_ONLY;
 	private List<String> selectedAttributes = new ArrayList<String>();
 
 	/**
