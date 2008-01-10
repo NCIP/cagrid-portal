2655dba8e13108d86ed0dcd87a90905129070ce6
dervin <dervin>
2008-01-10 20:27:46 +0000

    made every test use its own zip file
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9668 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/test/src/java/gov/nih/nci/cagrid/common/ZipUtilitiesTestCase.java
+++ cagrid-1-0/caGrid/projects/core/test/src/java/gov/nih/nci/cagrid/common/ZipUtilitiesTestCase.java
@@ -18,35 +18,59 @@ import junit.framework.TestCase;
  * 
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>  * 
  * @created Feb 21, 2007 
- * @version $Id: ZipUtilitiesTestCase.java,v 1.8 2008-01-09 21:46:38 dervin Exp $ 
+ * @version $Id: ZipUtilitiesTestCase.java,v 1.9 2008-01-10 20:27:46 dervin Exp $ 
  */
 public class ZipUtilitiesTestCase extends TestCase {
 	public static final String SOURCE_DIR = "src" + File.separator + "java";
-	public static final String ZIP_FILE_NAME = "ZIP_UTILS_TEST_FILE.zip";	
+	public static final String TESTING_ZIP_PREFIX = "ZIP_UTILS_TEST_FILE";
+    
+    private File zipFile = null;
+    
+    public ZipUtilitiesTestCase() {
+        System.out.println("Construction");
+    }
+    
+    
+    public void setUp() {
+        System.out.println("SET UP");
+        zipFile = null;
+        try {
+            zipFile = File.createTempFile(TESTING_ZIP_PREFIX, ".zip");
+            System.out.println("Creating zip " + zipFile.getAbsolutePath());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Error creating temporary zip file");
+        }
+        if (zipFile.exists()) {
+            boolean deleted = zipFile.delete();
+            assertTrue("Old zip file existed, and was deleted", deleted);
+        }
+    }
+    
+    
+    public void tearDown() {
+        System.out.println("TEAR DOWN");
+        zipFile.deleteOnExit();
+    }
+    
 
 	public void testZip() {
-		File zipFile = new File(ZIP_FILE_NAME);
-		if (zipFile.exists()) {
-			boolean deleted = zipFile.delete();
-			assertTrue("Old zip file existed, and was deleted", deleted);
-		}
-		try {
-			ZipUtilities.zipDirectory(new File(SOURCE_DIR), new File(ZIP_FILE_NAME));
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			fail("Failed to create zip file: " + ex.getMessage());
-		}
+        System.out.println("testZip()");
+        // zip the source directory
+		zipSource();
 	}
 	
 	
 	public void testUnzip() {
+        System.out.println("testUnzip()");
+        zipSource();
 		File outDir = new File("test" + File.separator + "ziptest");
 		if (!outDir.exists()) {
 			boolean created = outDir.mkdirs();
 			assertTrue("Directory " + outDir.getName() + " did not exist and was created", created);
 		}
 		try {
-			ZipUtilities.unzip(new File(ZIP_FILE_NAME), outDir);
+			ZipUtilities.unzip(zipFile, outDir);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 			fail("Failed to unzip: " + ex.getMessage());
@@ -75,6 +99,8 @@ public class ZipUtilitiesTestCase extends TestCase {
 	
 	
 	public void testExtractFromZip() {
+        System.out.println("testExtractFromZip()");
+        zipSource();
 		// convert the name of a class to a file name
 		String filePart = ZipUtilities.class.getName().replace('.', File.separatorChar) + ".java";
 		// get the original file contents
@@ -98,7 +124,7 @@ public class ZipUtilitiesTestCase extends TestCase {
 		// get the contents of the same file from the zip
 		byte[] extractedContentBytes = null;
 		try {
-			extractedContentBytes = ZipUtilities.extractEntryContents(new File(ZIP_FILE_NAME), filePart);
+			extractedContentBytes = ZipUtilities.extractEntryContents(zipFile, filePart);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 			fail("Error loading zipped file's contents: " + ex.getMessage());
@@ -112,25 +138,15 @@ public class ZipUtilitiesTestCase extends TestCase {
 	}
     
     
-    public void testInsertZipEntry() {
-        // ensure the testing zip exists
-        if (new File(ZIP_FILE_NAME).exists()) {
-            boolean deleted = new File(ZIP_FILE_NAME).delete();
-            assertTrue("Old zip file existed, and was deleted", deleted);
-        }
-        try {
-            ZipUtilities.zipDirectory(new File(SOURCE_DIR), new File(ZIP_FILE_NAME));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            fail("Error creating test zip file: " + ex.getMessage());
-        }
-        
+    public void testInsertZipEntry() {   
+        System.out.println("testInsertZipEntry()");
+        zipSource();
         String insertText = "I am the very model of a modern major general";
         byte[] insertData = insertText.getBytes();
         String entryPath = "test" + File.separator + "insert" + File.separator + "entry" + File.separator + "majorGeneral.txt";
         
         try {
-            ZipUtilities.insertEntry(new File(ZIP_FILE_NAME), entryPath, insertData);
+            ZipUtilities.insertEntry(zipFile, entryPath, insertData);
         } catch (IOException ex) {
             ex.printStackTrace();
             fail("Failed to insert: " + ex.getMessage());
@@ -138,7 +154,7 @@ public class ZipUtilitiesTestCase extends TestCase {
         
         byte[] extractedData = null;
         try {
-            extractedData = ZipUtilities.extractEntryContents(new File(ZIP_FILE_NAME), entryPath);
+            extractedData = ZipUtilities.extractEntryContents(zipFile, entryPath);
         } catch (Exception ex) {
             ex.printStackTrace();
             fail("Error extracting entry: " + ex.getMessage());
@@ -146,8 +162,15 @@ public class ZipUtilitiesTestCase extends TestCase {
         
         String extractText = new String(extractedData, 0, extractedData.length);
         assertEquals("Extracted bytes did not match input", insertText, extractText);
-        
-        // now delete the zip file
-        new File(ZIP_FILE_NAME).delete();
+    }
+    
+    
+    private void zipSource() {
+        try {
+            ZipUtilities.zipDirectory(new File(SOURCE_DIR), zipFile);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            fail("Failed to create zip file: " + ex.getMessage());
+        }
     }
 }
