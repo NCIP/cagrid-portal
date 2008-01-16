580c47e8e5d9707b6388cdd0b94966487be9124b
dervin <dervin>
2008-01-16 18:18:10 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9735 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/sdkQuery4/src/java/style/gov/nih/nci/cagrid/sdkquery4/style/beanmap/DomainTypesInformationUtil.java
+++ cagrid-1-0/caGrid/projects/sdkQuery4/src/java/style/gov/nih/nci/cagrid/sdkquery4/style/beanmap/DomainTypesInformationUtil.java
@@ -1,10 +1,14 @@
 package gov.nih.nci.cagrid.sdkquery4.style.beanmap;
 
 import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.sdkquery4.beans.domaininfo.DomainType;
 import gov.nih.nci.cagrid.sdkquery4.beans.domaininfo.DomainTypesInformation;
+import gov.nih.nci.cagrid.sdkquery4.beans.domaininfo.TypeAttribute;
 
 import java.io.Reader;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.namespace.QName;
 
@@ -15,23 +19,81 @@ import javax.xml.namespace.QName;
  * @author David Ervin
  * 
  * @created Jan 16, 2008 11:30:36 AM
- * @version $Id: DomainTypesInformationUtil.java,v 1.1 2008-01-16 17:05:31 dervin Exp $ 
+ * @version $Id: DomainTypesInformationUtil.java,v 1.2 2008-01-16 18:18:10 dervin Exp $ 
  */
 public class DomainTypesInformationUtil {
     
     public static final QName DOMAIN_TYPES_INFORMATION_QNAME = 
         new QName("http://SDKQuery4.caBIG/1/gov.nih.nci.cagrid.sdkquery4.beans.domaininfo", "DomainTypesInformation");
     
+    private DomainTypesInformation info = null;
+    private Map<String, String> attributeJavaTypes = null;
+    
+    public DomainTypesInformationUtil(DomainTypesInformation info) {
+        this.info = info;
+        attributeJavaTypes = new HashMap<String, String>();
+    }
+    
+    
+    /**
+     * Gets the java type of an attribute
+     * 
+     * @param classname
+     *      The fully qualified name of the class to which the attribute belongs
+     * @param attributeName
+     *      The name of the attribute
+     * @return
+     *      The java type name, or <code>null</code> if no class / attribute were found
+     */
+    public String getAttributeJavaType(String classname, String attributeName) {
+        String qualifiedName = classname + "." + attributeName;
+        if (!attributeJavaTypes.containsKey(qualifiedName)) {
+            String javaType = null;
+            for (DomainType domainType : info.getDomainType()) {
+                if (domainType.getJavaClassName().equals(classname)) {
+                    TypeAttribute[] attribs = domainType.getTypeAttribute();
+                    if (attribs != null) {
+                        for (TypeAttribute attrib : attribs) {
+                            if (attrib.getAttributeName().equals(attributeName)) {
+                                javaType = attrib.getJavaTypeName();
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            // MAY put null in the map, indicating no attribute was found
+            attributeJavaTypes.put(qualifiedName, javaType);
+        }
+        return attributeJavaTypes.get(qualifiedName);
+    }
+    
+    
+    /**
+     * Deserializes a domain types information document from XML to the Java object model
+     * 
+     * @param reader
+     *      A reader to the XML content of the document
+     * @return
+     *      The object representation of the information document
+     * @throws Exception
+     */
     public static DomainTypesInformation deserializeDomainTypesInformation(Reader reader) throws Exception {
         return (DomainTypesInformation) Utils.deserializeObject(reader, DomainTypesInformation.class);
     }
     
     
+    /**
+     * Serializes a domain types information object model instance to XML
+     * 
+     * @param info
+     *      The domain types information instance
+     * @param writer
+     *      A writer to which the XML of the domain types document will be sent
+     * @throws Exception
+     */
     public static void serializeDomainTypesInformation(DomainTypesInformation info, Writer writer) throws Exception {
         Utils.serializeObject(info, DOMAIN_TYPES_INFORMATION_QNAME, writer);
-    }
-    
-
-    public static void main(String[] args) {
     }
 }
--- cagrid-1-0/caGrid/projects/sdkQuery4/test/src/java/gov/nih/nci/cagrid/sdkquery4/test/BeanTypeDiscoveryTestCase.java
+++ cagrid-1-0/caGrid/projects/sdkQuery4/test/src/java/gov/nih/nci/cagrid/sdkquery4/test/BeanTypeDiscoveryTestCase.java
@@ -10,16 +10,17 @@ import gov.nih.nci.cagrid.sdkquery4.style.beanmap.DomainTypesInformationUtil;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-
-import org.apache.log4j.Logger;
-
-import sun.net.www.content.audio.x_aiff;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
+
+import org.apache.log4j.Logger;
 
 /** 
  *  BeanTypeDiscoveryTest
@@ -28,7 +29,7 @@ import junit.textui.TestRunner;
  * @author David Ervin
  * 
  * @created Jan 15, 2008 1:33:01 PM
- * @version $Id: BeanTypeDiscoveryTestCase.java,v 1.1 2008-01-16 17:05:54 dervin Exp $ 
+ * @version $Id: BeanTypeDiscoveryTestCase.java,v 1.2 2008-01-16 18:18:10 dervin Exp $ 
  */
 public class BeanTypeDiscoveryTestCase extends TestCase {
 
@@ -93,6 +94,57 @@ public class BeanTypeDiscoveryTestCase extends TestCase {
         }
         
         assertEquals("Processed types information did not match expected", goldTypes, typesInformation);
+    }
+    
+    
+    public void testGetAttributeTypeNames() {
+        // create a URL classloader for the beans jar
+        URLClassLoader beansLoader = null;
+        try {
+            beansLoader = new URLClassLoader(new URL[] {new File(BEANS_JAR_FILENAME).toURL()});
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Error creating beans classloader: " + ex.getMessage());
+        }
+        // load the gold domain types document
+        DomainTypesInformation goldInfo = null;
+        try {
+            File goldTypesFile = new File(GOLD_DOMAIN_TYPES_FILENAME);
+            LOG.debug("Using gold domain types from " + goldTypesFile.getAbsolutePath());
+            FileReader reader = new FileReader(goldTypesFile);
+            goldInfo = DomainTypesInformationUtil.deserializeDomainTypesInformation(reader);
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Error deserializing gold domain types information document: " + ex.getMessage());
+        }
+        
+        DomainTypesInformationUtil infoUtil = new DomainTypesInformationUtil(goldInfo);
+        String testClassname = "gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Book";
+        
+        // load a class from the beans class loader
+        Class bookClass = null;
+        try {
+            bookClass = beansLoader.loadClass(testClassname);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Error loading class " + testClassname + ": " + ex.getMessage());
+        }
+        
+        // walk the fields, get names and types
+        Field[] bookFields = bookClass.getDeclaredFields();
+        for (Field field : bookFields) {
+            int mods = field.getModifiers();
+            if (!Modifier.isStatic(mods) && Modifier.isPublic(mods)) {
+                String name = field.getName();
+                String fieldJavaType = field.getType().getName();
+                // ask the domain types info util about the field
+                String infoJavaType = infoUtil.getAttributeJavaType(testClassname, name);
+                assertNotNull("Unable to find field " + name + " of class " + testClassname + " in domain types information", infoJavaType);
+                assertEquals("Java type of field " + name + " of class " + testClassname + " differs in info from actual class",
+                    fieldJavaType, infoJavaType);
+            }
+        }
     }
     
     
