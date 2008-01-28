b25f0d3b42228ed014e14378bb0780d224d13a8b
langella <langella>
2008-01-28 17:47:14 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9861 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/metrics/src/org/cagrid/metrics/service/MetricsImpl.java
@@ -0,0 +1,19 @@
+package org.cagrid.metrics.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class MetricsImpl extends MetricsImplBase {
+
+	
+	public MetricsImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
---
+++ cagrid-1-0/caGrid/projects/metrics/src/org/cagrid/metrics/service/globus/resource/MetricsResource.java
@@ -0,0 +1,16 @@
+package org.cagrid.metrics.service.globus.resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** 
+ * The implementation of this MetricsResource type.
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class MetricsResource extends BaseResourceBase {
+
+
+
+}
