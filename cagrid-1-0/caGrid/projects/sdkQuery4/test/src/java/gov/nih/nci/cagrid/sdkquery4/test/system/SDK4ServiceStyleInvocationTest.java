eca64fbebebca07d9bdcba30c0b58a07b665f512
dervin <dervin>
2008-02-01 16:34:43 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9926 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/sdkQuery4/test/src/java/gov/nih/nci/cagrid/sdkquery4/test/system/SDK4ServiceStyleInvocationTest.java
+++ cagrid-1-0/caGrid/projects/sdkQuery4/test/src/java/gov/nih/nci/cagrid/sdkquery4/test/system/SDK4ServiceStyleInvocationTest.java
@@ -1,11 +1,16 @@
 package gov.nih.nci.cagrid.sdkquery4.test.system;
 
+import gov.nih.nci.cagrid.common.Utils;
 import gov.nih.nci.cagrid.data.creation.DataTestCaseInfo;
+import gov.nih.nci.cagrid.sdkquery4.test.system.steps.InvokeSDK4DataServiceStep;
 import gov.nih.nci.cagrid.sdkquery4.test.system.steps.SDK4StyleCreationStep;
 import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainer;
 import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainerFactory;
 import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainerType;
+import gov.nih.nci.cagrid.testing.system.deployment.steps.DeployServiceStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.DestroyContainerStep;
+import gov.nih.nci.cagrid.testing.system.deployment.steps.StartContainerStep;
+import gov.nih.nci.cagrid.testing.system.deployment.steps.StopContainerStep;
 import gov.nih.nci.cagrid.testing.system.deployment.steps.UnpackContainerStep;
 import gov.nih.nci.cagrid.testing.system.haste.Step;
 import gov.nih.nci.cagrid.testing.system.haste.Story;
@@ -20,7 +25,7 @@ import java.util.Vector;
  * @author David Ervin
  * 
  * @created Feb 1, 2008 7:49:44 AM
- * @version $Id: SDK4ServiceStyleInvocationTest.java,v 1.1 2008-02-01 15:52:13 dervin Exp $ 
+ * @version $Id: SDK4ServiceStyleInvocationTest.java,v 1.2 2008-02-01 16:34:43 dervin Exp $ 
  */
 public class SDK4ServiceStyleInvocationTest extends Story {
     
@@ -59,11 +64,16 @@ public class SDK4ServiceStyleInvocationTest extends Story {
         Vector<Step> steps = new Vector<Step>();
         steps.add(new SDK4StyleCreationStep(serviceTestInfo, getIntroduceBaseDir()));
         steps.add(new UnpackContainerStep(container));
+        steps.add(new DeployServiceStep(container, serviceTestInfo.getDir()));
+        steps.add(new StartContainerStep(container));
+        steps.add(new InvokeSDK4DataServiceStep(container, serviceTestInfo));
+        steps.add(new StopContainerStep(container));
         return steps;
     }
     
     
     public void storyTearDown() throws Throwable {
+        Utils.deleteDir(new File(serviceTestInfo.getDir()));
         new DestroyContainerStep(container).runStep();
     }
     
