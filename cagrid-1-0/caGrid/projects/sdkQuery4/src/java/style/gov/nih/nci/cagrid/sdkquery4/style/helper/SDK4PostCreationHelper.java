83dbc11897ad164a5c047284505e1e52a15cace4
dervin <dervin>
2008-02-05 15:39:44 +0000

    added castor mapping properties to client and server-config.wsdd
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@9961 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/sdkQuery4/src/java/style/gov/nih/nci/cagrid/sdkquery4/style/helper/SDK4PostCreationHelper.java
+++ cagrid-1-0/caGrid/projects/sdkQuery4/src/java/style/gov/nih/nci/cagrid/sdkquery4/style/helper/SDK4PostCreationHelper.java
@@ -1,12 +1,20 @@
 package gov.nih.nci.cagrid.sdkquery4.style.helper;
 
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.data.common.CastorMappingUtil;
+import gov.nih.nci.cagrid.data.style.StyleCreationPostProcessor;
+import gov.nih.nci.cagrid.data.utilities.WsddUtil;
+import gov.nih.nci.cagrid.introduce.IntroduceConstants;
+import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
+import gov.nih.nci.cagrid.introduce.beans.service.ServiceType;
+import gov.nih.nci.cagrid.introduce.common.CommonTools;
+import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
+import gov.nih.nci.cagrid.introduce.extension.CodegenExtensionException;
+import gov.nih.nci.cagrid.sdkquery4.encoding.SDK40EncodingUtils;
+
 import java.io.File;
 
 import org.apache.log4j.Logger;
-
-import gov.nih.nci.cagrid.data.style.StyleCreationPostProcessor;
-import gov.nih.nci.cagrid.introduce.beans.extension.ServiceExtensionDescriptionType;
-import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
 
 /** 
  *  SDK4PostCreationHelper
@@ -15,7 +23,7 @@ import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
  * @author David Ervin
  * 
  * @created Jan 18, 2008 12:38:01 PM
- * @version $Id: SDK4PostCreationHelper.java,v 1.1 2008-01-18 18:16:27 dervin Exp $ 
+ * @version $Id: SDK4PostCreationHelper.java,v 1.2 2008-02-05 15:39:44 dervin Exp $ 
  */
 public class SDK4PostCreationHelper implements StyleCreationPostProcessor {
     
@@ -26,6 +34,7 @@ public class SDK4PostCreationHelper implements StyleCreationPostProcessor {
     public void creationPostProcessStyle(ServiceExtensionDescriptionType desc, 
         ServiceInformation serviceInfo) throws Exception {
         deleteOldCastorJar(serviceInfo);
+        editWsddForCastorMappings(serviceInfo);
     }
     
     
@@ -34,5 +43,62 @@ public class SDK4PostCreationHelper implements StyleCreationPostProcessor {
             + File.separator + "lib" + File.separator + OLD_CASTOR_JAR_NAME);
         LOG.debug("Deleting old castor jar (" + castorLib.getAbsolutePath() + ")");
         castorLib.delete();
+    }
+    
+    
+    private void editWsddForCastorMappings(ServiceInformation info) throws Exception {
+        String mainServiceName = info.getIntroduceServiceProperties().getProperty(
+            IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME);
+        ServiceType mainService = CommonTools.getService(info.getServices(), mainServiceName);
+        String servicePackageName = mainService.getPackageName();
+        String packageDir = servicePackageName.replace('.', File.separatorChar);
+        // find the client source directory, where the client-config will be located
+        File clientConfigFile = new File(info.getBaseDirectory().getAbsolutePath() + File.separator + "src"
+            + File.separator + packageDir + File.separator + "client" + File.separator + "client-config.wsdd");
+        if (!clientConfigFile.exists()) {
+            throw new CodegenExtensionException("Client config file " + clientConfigFile.getAbsolutePath()
+                + " not found!");
+        }
+        // fine the server-config.wsdd, located in the service's root directory
+        File serverConfigFile = new File(info.getBaseDirectory().getAbsolutePath() + File.separator
+            + "server-config.wsdd");
+        if (!serverConfigFile.exists()) {
+            throw new CodegenExtensionException("Server config file " 
+                + serverConfigFile.getAbsolutePath() + " not found!");
+        }
+        
+        // edit the marshalling castor mapping to avoid serializing associations
+        String marshallingXmlText = Utils.fileToStringBuffer(
+            new File(CastorMappingUtil.getMarshallingCastorMappingFileName(info))).toString();
+        String editedMarshallingText = CastorMappingUtil.removeAssociationMappings(marshallingXmlText);
+        String editedMarshallingFileName = CastorMappingUtil.getEditedMarshallingCastorMappingFileName(info);
+        Utils.stringBufferToFile(new StringBuffer(editedMarshallingText), editedMarshallingFileName);
+        
+        // edit the UNmarshalling castor mapping to avoid DEserializing associations
+        String unmarshallingXmlText = Utils.fileToStringBuffer(
+            new File(CastorMappingUtil.getUnmarshallingCastorMappingFileName(info))).toString();
+        String editedUnmarshallingText = CastorMappingUtil.removeAssociationMappings(unmarshallingXmlText);
+        String editedUnmarshallingFileName = CastorMappingUtil.getEditedUnmarshallingCastorMappingFileName(info);
+        Utils.stringBufferToFile(new StringBuffer(editedUnmarshallingText), editedUnmarshallingFileName);
+        
+        // set properties in the client to use the edited marshaller
+        WsddUtil.setGlobalClientParameter(clientConfigFile.getAbsolutePath(),
+            SDK40EncodingUtils.CASTOR_MARSHALLER_PROPERTY, 
+            CastorMappingUtil.getEditedMarshallingCastorMappingName(info));
+        // and the edited unmarshaller
+        WsddUtil.setGlobalClientParameter(clientConfigFile.getAbsolutePath(),
+            SDK40EncodingUtils.CASTOR_UNMARSHALLER_PROPERTY, 
+            CastorMappingUtil.getEditedUnmarshallingCastorMappingName(info));
+        
+        // set properties in the server to use the edited marshaller
+        WsddUtil.setServiceParameter(serverConfigFile.getAbsolutePath(),
+            info.getServices().getService(0).getName(),
+            SDK40EncodingUtils.CASTOR_MARSHALLER_PROPERTY,
+            CastorMappingUtil.getEditedMarshallingCastorMappingName(info));
+        // and the edited unmarshaller
+        WsddUtil.setServiceParameter(serverConfigFile.getAbsolutePath(),
+            info.getServices().getService(0).getName(),
+            SDK40EncodingUtils.CASTOR_UNMARSHALLER_PROPERTY,
+            CastorMappingUtil.getEditedUnmarshallingCastorMappingName(info));
     }
 }
