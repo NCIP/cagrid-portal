dae620aaeb8e1fbe39754f01f291eb57614ff43a
kherm <kherm>
2008-02-11 16:06:00 +0000

    utility class
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-2-0_release@10037 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/Applications/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/util/StringUtils.java
@@ -0,0 +1,23 @@
+package gov.nih.nci.cagrid.portal.util;
+
+/**
+ * User: kherm
+ *
+ * @author kherm manav.kher@semanticbits.com
+ */
+public class StringUtils {
+
+    /**
+     * Will take out special characters from postal code.
+     * @param postalCode
+     * @return
+     */
+    public static String parsePostalCode(String postalCode){
+        while (postalCode!=null && postalCode.indexOf(".")>-1){
+            postalCode = postalCode.substring(0,postalCode.indexOf("."));
+        }
+        return postalCode;
+    }
+}
+
+
