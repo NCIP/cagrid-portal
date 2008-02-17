9ef0e58b779d4582e11fd70466924f47560d78cb
oster <oster>
2008-02-17 03:17:41 +0000

    moved sdk to repo added sdk dep to metadatautils
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10134 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/metadatautils/test/src/java/gov/nih/nci/cagrid/metadata/xmi/FixXmiTestStory.java
+++ cagrid-1-0/caGrid/projects/metadatautils/test/src/java/gov/nih/nci/cagrid/metadata/xmi/FixXmiTestStory.java
@@ -12,35 +12,35 @@ import junit.framework.TestResult;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
-/** 
- *  FixXmiTestStory
- *  Haste system-testing story to exercise fix-xmi functionality
+
+/**
+ * FixXmiTestStory Haste system-testing story to exercise fix-xmi functionality
  * 
  * @author David Ervin
- * 
  * @created Nov 8, 2007 10:50:25 AM
- * @version $Id: FixXmiTestStory.java,v 1.2 2007-12-03 16:27:18 hastings Exp $ 
+ * @version $Id: FixXmiTestStory.java,v 1.3 2008-02-17 03:17:41 oster Exp $
  */
-public class FixXmiTestStory extends Story {    
-    public static final String SDK_ZIP_LOCATION = "ext/resources/caCORE_SDK_321.zip";
+public class FixXmiTestStory extends Story {
+    public static final String SDK_ZIP_LOCATION = "ext/dependencies/test/zips/caCORE_SDK_321.zip";
     public static final String FIX_MODELS_ZIP_LOCATION = "test/resources/fixModels.zip";
-    
+
     private File sdkZipFile;
     private File sdkTempDir;
-    
+
     private File modelsZipFile;
     private File modelsTempDir;
-    
+
+
     public FixXmiTestStory() {
         super();
         setName("Fix Xmi Test Story");
     }
-    
-    
+
+
     public String getName() {
         return "Fix Xmi Test Story";
     }
-    
+
 
     public String getDescription() {
         return "Tests the fix-xmi functionality";
@@ -65,7 +65,7 @@ public class FixXmiTestStory extends Story {
             ex.printStackTrace();
             fail("Error creating XMI Models temp dir: " + ex.getMessage());
         }
-        
+
         // sequence steps
         Vector<Step> steps = new Vector<Step>();
         // unzip the SDK
@@ -73,11 +73,12 @@ public class FixXmiTestStory extends Story {
         // unzip the models
         steps.add(new UnzipStep(modelsZipFile.getAbsolutePath(), modelsTempDir.getAbsolutePath()));
         // fix the models
-        steps.add(new FixXmiStep(sdkTempDir.getAbsolutePath() + File.separator + "cacoresdk", modelsTempDir.getAbsolutePath()));
+        steps.add(new FixXmiStep(sdkTempDir.getAbsolutePath() + File.separator + "cacoresdk", modelsTempDir
+            .getAbsolutePath()));
         return steps;
     }
-    
-    
+
+
     protected void storyTearDown() {
         if (sdkTempDir.exists()) {
             Utils.deleteDir(sdkTempDir);
