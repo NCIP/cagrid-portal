123c74508bc09bb31db88486830fc8d77a0db14a
joshua <joshua>
2008-02-18 14:57:13 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-2-0_release@10140 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/war/WEB-INF/classes/migration_2.0.1.sql
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/war/WEB-INF/classes/migration_2.0.1.sql
@@ -1,5 +1,7 @@
 
-
 alter table uml_attr add publicID bigint;
-
 update grid_services set metadataHash='dummy' where char_length(metadataHash) > 1;
+delete from models_schemas;
+update params set schema_id = null;
+update ctx_prop set schema_id = null;
+delete from xmlschemas;
