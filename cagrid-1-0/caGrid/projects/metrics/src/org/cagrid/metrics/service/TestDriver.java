dd869d64c6538b746eb4bd2e55da437e9804c807
langella <langella>
2008-02-21 15:16:54 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10192 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/metrics/src/org/cagrid/metrics/service/TestDriver.java
+++ cagrid-1-0/caGrid/projects/metrics/src/org/cagrid/metrics/service/TestDriver.java
@@ -2,12 +2,14 @@ package org.cagrid.metrics.service;
 
 import java.util.GregorianCalendar;
 
+import javax.xml.namespace.QName;
+
 import org.cagrid.metrics.common.Detail;
 import org.cagrid.metrics.common.EventDescription;
 import org.cagrid.metrics.common.EventRecord;
 import org.cagrid.metrics.common.EventSource;
+import org.cagrid.metrics.common.InvocationEvent;
 import org.cagrid.metrics.common.Service;
-import org.cagrid.metrics.common.UsageEvent;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -24,10 +26,10 @@ public class TestDriver {
 
 			s.save(getEvent());
 			s.getTransaction().commit();
-			
-			//TODO: Test Get
-			
-			//TODO: Test Delete
+
+			// TODO: Test Get
+
+			// TODO: Test Delete
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -35,6 +37,8 @@ public class TestDriver {
 
 	public static EventRecord getEvent() {
 		EventRecord e = new EventRecord();
+		e.setStartedAt(new GregorianCalendar());
+		e.setEndedAt(new GregorianCalendar());
 		e.setReportedAt(new GregorianCalendar());
 		EventSource source = new EventSource();
 
@@ -42,18 +46,29 @@ public class TestDriver {
 		s.setName("Dorian");
 		s.setVersion("1.2");
 		s.setNamespace("http://cagrid.nci.nih.gov/Dorian");
-		s.setAddress("https://dorian.cagrid.org:6443/wsrf/services/cagrid/Dorian");
+		s
+				.setAddress("https://dorian.cagrid.org:6443/wsrf/services/cagrid/Dorian");
 		s.setAdditionalDetails(getDetails(s.getName(), 3));
 
 		source.setComponent(s);
 		e.setAdditionalDetails(getDetails("Event", 3));
 		e.setEventSource(source);
-		
+
 		EventDescription des = new EventDescription();
-		//des.setCustomEvent("MyEvent");
-		des.setUsageEvent(UsageEvent.LAUNCH);
-		//des.setServiceLifeCycleEvent(ServiceLifeCycleEvent.MODIFICATION);
+		// des.setCustomEvent("MyEvent");
+		// des.setUsageEvent(UsageEvent.LAUNCH);
+		// des.setServiceLifeCycleEvent(ServiceLifeCycleEvent.MODIFICATION);
+		InvocationEvent ie = new InvocationEvent();
+		ie.setOperationName("createProxy");
+		QName qname = new QName("http://cagrid.nci.nih.gov/Dorian",
+				"createProxy");
+		ie.setOperationQName(qname);
+		ie
+				.setClientIdentity("/O=caBIG/OU=caGrid/OU=Training/OU=Dorian/CN=langella");
+		ie.setClientAddress("dwight");
+		des.setInvocationEvent(ie);
 		e.setEventDescription(des);
+		System.out.println(qname.toString());
 		return e;
 	}
 
