1fdbd16d89642d1f182301157681f309d40d76d8
hastings <hastings>
2008-02-27 17:42:56 +0000

    only add prefs menu if there was a config element in the app config
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10271 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/grape/src/org/cagrid/grape/WindowMenu.java
+++ cagrid-1-0/caGrid/projects/grape/src/org/cagrid/grape/WindowMenu.java
@@ -25,117 +25,126 @@ import org.cagrid.grape.utils.ErrorDialog;
  *          Exp $
  */
 public class WindowMenu extends JMenu {
-	private MDIDesktopPane desktop;
-	private JMenuItem cascade = new JMenuItem("Cascade");
-	private JMenuItem tile = new JMenuItem("Tile");
-	private JMenuItem prefs;
+    private MDIDesktopPane desktop;
+
+    private JMenuItem cascade = new JMenuItem("Cascade");
+
+    private JMenuItem tile = new JMenuItem("Tile");
+
+    private JMenuItem prefs;
+
+    private GridApplication app;
 
 
-	public WindowMenu(MDIDesktopPane desktop, final GridApplication app) {
-		this.desktop = desktop;
-		setText("Window");
-		setMnemonic(java.awt.event.KeyEvent.VK_W);
-		cascade.setMnemonic(java.awt.event.KeyEvent.VK_C);
-		cascade.setIcon(LookAndFeel.getCascadeIcon());
-		cascade.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				WindowMenu.this.desktop.cascadeFrames();
-			}
-		});
+    public WindowMenu(MDIDesktopPane desktop, final GridApplication app) {
+        this.desktop = desktop;
+        this.app = app;
+        setText("Window");
+        setMnemonic(java.awt.event.KeyEvent.VK_W);
+        cascade.setMnemonic(java.awt.event.KeyEvent.VK_C);
+        cascade.setIcon(LookAndFeel.getCascadeIcon());
+        cascade.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                WindowMenu.this.desktop.cascadeFrames();
+            }
+        });
 
-		tile.setMnemonic(java.awt.event.KeyEvent.VK_T);
-		tile.setIcon(LookAndFeel.getTileIcon());
-		tile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				WindowMenu.this.desktop.tileFrames();
-			}
-		});
+        tile.setMnemonic(java.awt.event.KeyEvent.VK_T);
+        tile.setIcon(LookAndFeel.getTileIcon());
+        tile.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                WindowMenu.this.desktop.tileFrames();
+            }
+        });
 
-		prefs = new javax.swing.JMenuItem();
-		prefs.setText("Preferences");
-		prefs.setIcon(LookAndFeel.getPreferencesIcon());
-		prefs.setMnemonic(java.awt.event.KeyEvent.VK_Q);
-		prefs.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				try {
-					ConfigurationWindow window = new ConfigurationWindow(app);
-					window.setModal(false);
-					window.setSize(700, 400);
-					window.setVisible(true);
-					GridApplication.getContext().centerWindow(window);
-				} catch (Exception ex) {
-					ErrorDialog.showError(Utils.getExceptionMessage(ex));
-					ex.printStackTrace();
-				}
-			}
-		});
-		addMenuListener(new MenuListener() {
-			public void menuCanceled(MenuEvent e) {
-			}
+        prefs = new javax.swing.JMenuItem();
+        prefs.setText("Preferences");
+        prefs.setIcon(LookAndFeel.getPreferencesIcon());
+        prefs.setMnemonic(java.awt.event.KeyEvent.VK_Q);
+        prefs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                try {
+                    ConfigurationWindow window = new ConfigurationWindow(app);
+                    window.setModal(false);
+                    window.setSize(700, 400);
+                    window.setVisible(true);
+                    GridApplication.getContext().centerWindow(window);
+                } catch (Exception ex) {
+                    ErrorDialog.showError(Utils.getExceptionMessage(ex));
+                    ex.printStackTrace();
+                }
+            }
+        });
 
-
-			public void menuDeselected(MenuEvent e) {
-				removeAll();
-			}
+        addMenuListener(new MenuListener() {
+            public void menuCanceled(MenuEvent e) {
+            }
 
 
-			public void menuSelected(MenuEvent e) {
-				buildChildMenus();
-			}
-		});
-	}
+            public void menuDeselected(MenuEvent e) {
+                removeAll();
+            }
 
 
-	/* Sets up the children menus depending on the current desktop state */
-	private void buildChildMenus() {
-		int i;
-		ChildMenuItem menu;
-		JInternalFrame[] array = desktop.getAllFrames();
-
-		add(cascade);
-		add(tile);
-		add(prefs);
-		if (array.length > 0)
-			addSeparator();
-		cascade.setEnabled(array.length > 0);
-		tile.setEnabled(array.length > 0);
-
-		for (i = 0; i < array.length; i++) {
-			menu = new ChildMenuItem(array[i]);
-			menu.setState(i == 0);
-			menu.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent ae) {
-					JInternalFrame frame = ((ChildMenuItem) ae.getSource()).getFrame();
-					frame.moveToFront();
-					try {
-						frame.setSelected(true);
-					} catch (PropertyVetoException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-			menu.setIcon(array[i].getFrameIcon());
-			add(menu);
-		}
-	}
+            public void menuSelected(MenuEvent e) {
+                buildChildMenus();
+            }
+        });
+    }
 
 
-	/*
-	 * This JCheckBoxMenuItem descendant is used to track the child frame that
-	 * corresponds to a give menu.
-	 */
-	static class ChildMenuItem extends JCheckBoxMenuItem {
-		private JInternalFrame frame;
+    /* Sets up the children menus depending on the current desktop state */
+    private void buildChildMenus() {
+        int i;
+        ChildMenuItem menu;
+        JInternalFrame[] array = desktop.getAllFrames();
+
+        add(cascade);
+        add(tile);
+        if (app.getConfigurationManager().getConfiguration() != null) {
+            add(prefs);
+        }
+        if (array.length > 0)
+            addSeparator();
+        cascade.setEnabled(array.length > 0);
+        tile.setEnabled(array.length > 0);
+
+        for (i = 0; i < array.length; i++) {
+            menu = new ChildMenuItem(array[i]);
+            menu.setState(i == 0);
+            menu.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent ae) {
+                    JInternalFrame frame = ((ChildMenuItem) ae.getSource()).getFrame();
+                    frame.moveToFront();
+                    try {
+                        frame.setSelected(true);
+                    } catch (PropertyVetoException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            menu.setIcon(array[i].getFrameIcon());
+            add(menu);
+        }
+    }
 
 
-		public ChildMenuItem(JInternalFrame frame) {
-			super(frame.getTitle());
-			this.frame = frame;
-		}
+    /*
+     * This JCheckBoxMenuItem descendant is used to track the child frame that
+     * corresponds to a give menu.
+     */
+    static class ChildMenuItem extends JCheckBoxMenuItem {
+        private JInternalFrame frame;
 
 
-		public JInternalFrame getFrame() {
-			return frame;
-		}
-	}
+        public ChildMenuItem(JInternalFrame frame) {
+            super(frame.getTitle());
+            this.frame = frame;
+        }
+
+
+        public JInternalFrame getFrame() {
+            return frame;
+        }
+    }
 }
