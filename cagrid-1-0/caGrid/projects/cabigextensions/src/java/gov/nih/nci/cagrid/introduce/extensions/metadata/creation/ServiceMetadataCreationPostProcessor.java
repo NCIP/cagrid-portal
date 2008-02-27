23145e5f02b38656339e5875b634ae075232fa4c
oster <oster>
2008-02-27 15:37:27 +0000

    fixed metadata schemas getting copied to inconsistent location (both cagrid/ and xsd/cagrid)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10265 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/cabigextensions/src/java/gov/nih/nci/cagrid/introduce/extensions/metadata/creation/ServiceMetadataCreationPostProcessor.java
+++ cagrid-1-0/caGrid/projects/cabigextensions/src/java/gov/nih/nci/cagrid/introduce/extensions/metadata/creation/ServiceMetadataCreationPostProcessor.java
@@ -34,149 +34,149 @@ import java.util.Properties;
  */
 public class ServiceMetadataCreationPostProcessor implements CreationExtensionPostProcessor {
 
-	public void postCreate(ServiceExtensionDescriptionType desc, ServiceInformation info)
-		throws CreationExtensionException {
-		try {
-			Properties serviceProperties = info.getIntroduceServiceProperties();
-			ServiceDescription serviceDescription = info.getServiceDescriptor();
+    public void postCreate(ServiceExtensionDescriptionType desc, ServiceInformation info)
+        throws CreationExtensionException {
+        try {
+            Properties serviceProperties = info.getIntroduceServiceProperties();
+            ServiceDescription serviceDescription = info.getServiceDescriptor();
 
-			// grab schemas and copy them into the service's directory
-			copyMetadataSchemas(getServiceSchemaDir(serviceProperties));
+            // grab schemas and copy them into the service's directory
+            copyMetadataSchemas(getServiceSchemaDir(serviceProperties) + File.separator + "xsd");
 
-			// copy libraries into the new lib directory
-			copyLibraries(serviceProperties);
+            // copy libraries into the new lib directory
+            copyLibraries(serviceProperties);
 
-			// add the namespace types
-			addNamespaces(serviceDescription, getServiceSchemaDir(serviceProperties));
+            // add the namespace types
+            addNamespaces(serviceDescription, getServiceSchemaDir(serviceProperties));
 
-			// add the service metadata
-			addServiceMetadata(serviceDescription);
+            // add the service metadata
+            addServiceMetadata(serviceDescription);
 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			throw new CreationExtensionException("Error adding service metadata components to template!", ex);
-		}
-	}
-
-
-	private void copyMetadataSchemas(String schemaDir) throws Exception {
-		System.out.println("Copying schemas to " + schemaDir);
-		File extensionSchemaDir = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator
-			+ MetadataConstants.EXTENSION_NAME + File.separator + "schema");
-		List schemaFiles = Utils.recursiveListFiles(extensionSchemaDir, new FileFilters.XSDFileFilter());
-		for (int i = 0; i < schemaFiles.size(); i++) {
-			File schemaFile = (File) schemaFiles.get(i);
-			String subname = schemaFile.getCanonicalPath().substring(
-				extensionSchemaDir.getCanonicalPath().length() + File.separator.length());
-			copySchema(subname, schemaDir);
-		}
-	}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new CreationExtensionException("Error adding service metadata components to template!", ex);
+        }
+    }
 
 
-	private void addNamespaces(ServiceDescription description, String schemaDir) throws Exception {
-		// namespaces
-		System.out.println("Modifying namespace definitions");
-		NamespacesType namespaces = description.getNamespaces();
-		if (namespaces == null) {
-			namespaces = new NamespacesType();
-		}
-
-		// add some namespaces to the service
-		List<NamespaceType> newNamespaces = new ArrayList<NamespaceType>(Arrays.asList(namespaces.getNamespace()));
-		// caGrid metadata namespace
-		NamespaceType cagridMdNamespace = CommonTools.createNamespaceType(schemaDir + File.separator
-			+ MetadataConstants.CAGRID_METADATA_SCHEMA, new File(schemaDir));
-		cagridMdNamespace.setGenerateStubs(Boolean.FALSE);
-		newNamespaces.add(cagridMdNamespace);
-
-		// add those new namespaces to the list of namespace types
-		NamespaceType[] nsArray = new NamespaceType[newNamespaces.size()];
-		newNamespaces.toArray(nsArray);
-		namespaces.setNamespace(nsArray);
-		description.setNamespaces(namespaces);
-	}
+    private void copyMetadataSchemas(String schemaDir) throws Exception {
+        System.out.println("Copying schemas to " + schemaDir);
+        File extensionSchemaDir = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator
+            + MetadataConstants.EXTENSION_NAME + File.separator + "schema");
+        List schemaFiles = Utils.recursiveListFiles(extensionSchemaDir, new FileFilters.XSDFileFilter());
+        for (int i = 0; i < schemaFiles.size(); i++) {
+            File schemaFile = (File) schemaFiles.get(i);
+            String subname = schemaFile.getCanonicalPath().substring(
+                extensionSchemaDir.getCanonicalPath().length() + File.separator.length());
+            copySchema(subname, schemaDir);
+        }
+    }
 
 
-	private void addServiceMetadata(ServiceDescription desc) {
-		ResourcePropertyType serviceMetadata = new ResourcePropertyType();
-		serviceMetadata.setPopulateFromFile(true);
-		serviceMetadata.setRegister(true);
-		serviceMetadata.setQName(MetadataConstants.SERVICE_METADATA_QNAME);
-		ServiceMetadata smd = new ServiceMetadata();
-		ServiceMetadataServiceDescription des = new ServiceMetadataServiceDescription();
-		Service service = new Service();
-		des.setService(service);
-		smd.setServiceDescription(des);
-		ResourcePropertiesListType propsList = desc.getServices().getService()[0].getResourcePropertiesList();
-		if (propsList == null) {
-			propsList = new ResourcePropertiesListType();
-			desc.getServices().getService()[0].setResourcePropertiesList(propsList);
-		}
-		ResourcePropertyType[] metadataArray = propsList.getResourceProperty();
-		if (metadataArray == null || metadataArray.length == 0) {
-			metadataArray = new ResourcePropertyType[]{serviceMetadata};
-		} else {
-			ResourcePropertyType[] tmpArray = new ResourcePropertyType[metadataArray.length + 1];
-			System.arraycopy(metadataArray, 0, tmpArray, 0, metadataArray.length);
-			tmpArray[metadataArray.length] = serviceMetadata;
-			metadataArray = tmpArray;
-		}
-		propsList.setResourceProperty(metadataArray);
-	}
+    private void addNamespaces(ServiceDescription description, String schemaDir) throws Exception {
+        // namespaces
+        System.out.println("Modifying namespace definitions");
+        NamespacesType namespaces = description.getNamespaces();
+        if (namespaces == null) {
+            namespaces = new NamespacesType();
+        }
+
+        // add some namespaces to the service
+        List<NamespaceType> newNamespaces = new ArrayList<NamespaceType>(Arrays.asList(namespaces.getNamespace()));
+        // caGrid metadata namespace
+        NamespaceType cagridMdNamespace = CommonTools.createNamespaceType(schemaDir + File.separator + "xsd"
+            + File.separator + MetadataConstants.CAGRID_METADATA_SCHEMA, new File(schemaDir));
+        cagridMdNamespace.setGenerateStubs(Boolean.FALSE);
+        newNamespaces.add(cagridMdNamespace);
+
+        // add those new namespaces to the list of namespace types
+        NamespaceType[] nsArray = new NamespaceType[newNamespaces.size()];
+        newNamespaces.toArray(nsArray);
+        namespaces.setNamespace(nsArray);
+        description.setNamespaces(namespaces);
+    }
 
 
-	private String getServiceSchemaDir(Properties props) {
-		return props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR) + File.separator + "schema"
-			+ File.separator + props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME);
-	}
+    private void addServiceMetadata(ServiceDescription desc) {
+        ResourcePropertyType serviceMetadata = new ResourcePropertyType();
+        serviceMetadata.setPopulateFromFile(true);
+        serviceMetadata.setRegister(true);
+        serviceMetadata.setQName(MetadataConstants.SERVICE_METADATA_QNAME);
+        ServiceMetadata smd = new ServiceMetadata();
+        ServiceMetadataServiceDescription des = new ServiceMetadataServiceDescription();
+        Service service = new Service();
+        des.setService(service);
+        smd.setServiceDescription(des);
+        ResourcePropertiesListType propsList = desc.getServices().getService()[0].getResourcePropertiesList();
+        if (propsList == null) {
+            propsList = new ResourcePropertiesListType();
+            desc.getServices().getService()[0].setResourcePropertiesList(propsList);
+        }
+        ResourcePropertyType[] metadataArray = propsList.getResourceProperty();
+        if (metadataArray == null || metadataArray.length == 0) {
+            metadataArray = new ResourcePropertyType[]{serviceMetadata};
+        } else {
+            ResourcePropertyType[] tmpArray = new ResourcePropertyType[metadataArray.length + 1];
+            System.arraycopy(metadataArray, 0, tmpArray, 0, metadataArray.length);
+            tmpArray[metadataArray.length] = serviceMetadata;
+            metadataArray = tmpArray;
+        }
+        propsList.setResourceProperty(metadataArray);
+    }
 
 
-	private String getServiceLibDir(Properties props) {
-		return props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR) + File.separator + "lib";
-	}
+    private String getServiceSchemaDir(Properties props) {
+        return props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR) + File.separator + "schema"
+            + File.separator + props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_SERVICE_NAME);
+    }
 
 
-	private void copySchema(String schemaName, String outputDir) throws Exception {
-		File schemaFile = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator
-			+ MetadataConstants.EXTENSION_NAME + File.separator + "schema" + File.separator + schemaName);
-		System.out.println("Copying schema from " + schemaFile.getAbsolutePath());
-		File outputFile = new File(outputDir + File.separator + schemaName);
-		System.out.println("Saving schema to " + outputFile.getAbsolutePath());
-		Utils.copyFile(schemaFile, outputFile);
-	}
+    private String getServiceLibDir(Properties props) {
+        return props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR) + File.separator + "lib";
+    }
 
 
-	private void copyLibraries(Properties props) throws Exception {
-		String toDir = getServiceLibDir(props);
-		File directory = new File(toDir);
-		if (!directory.exists()) {
-			directory.mkdirs();
-		}
-		// from the lib directory
-		File libDir = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator + "lib");
-		File[] libs = libDir.listFiles(new FileFilter() {
-			public boolean accept(File pathname) {
-				String name = pathname.getName();
-				return (name.endsWith(".jar") && (name.startsWith(MetadataConstants.METADATA_JAR_PREFIX)));
-			}
-		});
-
-		if (libs != null) {
-			File[] copiedLibs = new File[libs.length];
-			for (int i = 0; i < libs.length; i++) {
-				File outFile = new File(toDir + File.separator + libs[i].getName());
-				copiedLibs[i] = outFile;
-				Utils.copyFile(libs[i], outFile);
-			}
-			modifyClasspathFile(copiedLibs, props);
-		}
-
-	}
+    private void copySchema(String schemaName, String outputDir) throws Exception {
+        File schemaFile = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator
+            + MetadataConstants.EXTENSION_NAME + File.separator + "schema" + File.separator + schemaName);
+        System.out.println("Copying schema from " + schemaFile.getAbsolutePath());
+        File outputFile = new File(outputDir + File.separator + schemaName);
+        System.out.println("Saving schema to " + outputFile.getAbsolutePath());
+        Utils.copyFile(schemaFile, outputFile);
+    }
 
 
-	private void modifyClasspathFile(File[] libs, Properties props) throws Exception {
-		File classpathFile = new File(props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR)
-			+ File.separator + ".classpath");
-		ExtensionUtilities.syncEclipseClasspath(classpathFile, libs);
-	}
+    private void copyLibraries(Properties props) throws Exception {
+        String toDir = getServiceLibDir(props);
+        File directory = new File(toDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        // from the lib directory
+        File libDir = new File(ExtensionsLoader.EXTENSIONS_DIRECTORY + File.separator + "lib");
+        File[] libs = libDir.listFiles(new FileFilter() {
+            public boolean accept(File pathname) {
+                String name = pathname.getName();
+                return (name.endsWith(".jar") && (name.startsWith(MetadataConstants.METADATA_JAR_PREFIX)));
+            }
+        });
+
+        if (libs != null) {
+            File[] copiedLibs = new File[libs.length];
+            for (int i = 0; i < libs.length; i++) {
+                File outFile = new File(toDir + File.separator + libs[i].getName());
+                copiedLibs[i] = outFile;
+                Utils.copyFile(libs[i], outFile);
+            }
+            modifyClasspathFile(copiedLibs, props);
+        }
+
+    }
+
+
+    private void modifyClasspathFile(File[] libs, Properties props) throws Exception {
+        File classpathFile = new File(props.getProperty(IntroduceConstants.INTRODUCE_SKELETON_DESTINATION_DIR)
+            + File.separator + ".classpath");
+        ExtensionUtilities.syncEclipseClasspath(classpathFile, libs);
+    }
 }
