1ddc5324ab8fa57ad43cccf290e7ebd1280268b3
oster <oster>
2008-03-05 21:09:00 +0000

    fixed paths in syncgts install
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10359 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/installer/src/org/cagrid/installer/syncgts/ReplaceDefaultGTSCAStep.java
+++ cagrid-1-0/caGrid/projects/installer/src/org/cagrid/installer/syncgts/ReplaceDefaultGTSCAStep.java
@@ -14,66 +14,64 @@ import org.cagrid.installer.steps.PropertyConfigurationStep;
 import org.cagrid.installer.util.InstallerUtils;
 import org.pietschy.wizard.InvalidStateException;
 
+
 /**
  * @author <a href="joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
  */
 public class ReplaceDefaultGTSCAStep extends PropertyConfigurationStep {
 
-	private static final Log logger = LogFactory
-			.getLog(ReplaceDefaultGTSCAStep.class);
+    private static final Log logger = LogFactory.getLog(ReplaceDefaultGTSCAStep.class);
 
-	/**
-	 * 
-	 */
-	public ReplaceDefaultGTSCAStep() {
-	}
 
-	/**
-	 * @param name
-	 * @param description
-	 */
-	public ReplaceDefaultGTSCAStep(String name, String description) {
-		super(name, description);
-	}
+    /**
+     * 
+     */
+    public ReplaceDefaultGTSCAStep() {
+    }
 
-	/**
-	 * @param name
-	 * @param description
-	 * @param icon
-	 */
-	public ReplaceDefaultGTSCAStep(String name, String description, Icon icon) {
-		super(name, description, icon);
-	}
 
-	public void applyState() throws InvalidStateException {
+    /**
+     * @param name
+     * @param description
+     */
+    public ReplaceDefaultGTSCAStep(String name, String description) {
+        super(name, description);
+    }
 
-		super.applyState();
-		try {
-			File certsDir = new File(this.model.getServiceDestDir()
-					+ "/syncgts/ext/resources/certificates");
-			File[] files = certsDir.listFiles();
-			for (File file : files) {
-				logger.info("Deleting '" + file.getAbsolutePath() + "'");
-				file.delete();
-			}
 
-			File f = new File(this.model
-					.getProperty(Constants.REPLACEMENT_GTS_CA_CERT_PATH));
-			String fileName = f.getName();
-			if (!fileName.matches(".+\\.[0-9]$")) {
-				fileName += ".0";
-			}
+    /**
+     * @param name
+     * @param description
+     * @param icon
+     */
+    public ReplaceDefaultGTSCAStep(String name, String description, Icon icon) {
+        super(name, description, icon);
+    }
 
-			InstallerUtils.copyFile(f.getAbsolutePath(), certsDir
-					.getAbsolutePath()
-					+ "/" + fileName);
-		} catch (Exception ex) {
-			String msg = "Error replacing certificate: "
-					+ ex.getMessage();
-			logger.error(msg, ex);
-			throw new InvalidStateException(msg, ex);
-		}
-	}
+
+    public void applyState() throws InvalidStateException {
+
+        super.applyState();
+        try {
+            File certsDir = new File(this.model.getServiceDestDir() + "/syncgts/ext/target_grid/certificates");
+            File[] files = certsDir.listFiles();
+            for (File file : files) {
+                logger.info("Deleting '" + file.getAbsolutePath() + "'");
+                file.delete();
+            }
+
+            File f = new File(this.model.getProperty(Constants.REPLACEMENT_GTS_CA_CERT_PATH));
+            String fileName = f.getName();
+            if (!fileName.matches(".+\\.[0-9]$")) {
+                fileName += ".0";
+            }
+
+            InstallerUtils.copyFile(f.getAbsolutePath(), certsDir.getAbsolutePath() + "/" + fileName);
+        } catch (Exception ex) {
+            String msg = "Error replacing certificate: " + ex.getMessage();
+            logger.error(msg, ex);
+            throw new InvalidStateException(msg, ex);
+        }
+    }
 
 }
