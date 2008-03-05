d7c30b38dc50c2a5e1204cd0cf3cf996f854a182
oster <oster>
2008-03-05 20:34:20 +0000

    fixed bug #12884
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10357 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/installer/src/org/cagrid/installer/tasks/CaGridAntTask.java
+++ cagrid-1-0/caGrid/projects/installer/src/org/cagrid/installer/tasks/CaGridAntTask.java
@@ -3,56 +3,59 @@
  */
 package org.cagrid.installer.tasks;
 
-import org.cagrid.installer.model.CaGridInstallerModel;
-import org.cagrid.installer.steps.Constants;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.cagrid.installer.model.CaGridInstallerModel;
+import org.cagrid.installer.steps.Constants;
+
+
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
  */
 public abstract class CaGridAntTask extends BasicTask {
 
-	private String targetName;
+    private String targetName;
 
-	/**
-	 * @param name
-	 * @param description
-	 */
-	public CaGridAntTask(String name, String description, String targetName) {
-		super(name, description);
-		this.targetName = targetName;
-	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.cagrid.installer.tasks.BasicTask#internalExecute(java.util.Map)
-	 */
-	@Override
-	protected Object internalExecute(CaGridInstallerModel model) throws Exception {
-		Map<String,String> env = new HashMap<String,String>();
-		env.put("GLOBUS_LOCATION", model.getProperty(Constants.GLOBUS_HOME));
-		env.put("CATALINA_HOME", model.getProperty(Constants.TOMCAT_HOME));
-		Properties sysProps = new Properties();
-		sysProps.setProperty(Constants.SERVICE_DEST_DIR, model.getServiceDestDir());
-		sysProps.setProperty(Constants.GRIDCA_BUILD_FILE_PATH, model.getProperty(Constants.CAGRID_HOME)
-				+ "/projects/gridca/build.xml");
-		sysProps.setProperty("env.GLOBUS_LOCATION", model.getProperty(Constants.GLOBUS_HOME));
-        sysProps.setProperty(Constants.PORTAL_HOME, model.getProperty(Constants.PORTAL_INSTALL_DIR_PATH));
+    /**
+     * @param name
+     * @param description
+     */
+    public CaGridAntTask(String name, String description, String targetName) {
+        super(name, description);
+        this.targetName = targetName;
+    }
 
-		model.setProperty(Constants.BUILD_FILE_PATH, getBuildFilePath(model));
-		
-		return runAntTask(model, this.targetName, env, sysProps);
 
-	}
-	
-	protected abstract Object runAntTask(CaGridInstallerModel model, String target, Map<String,String> env,
-			Properties sysProps) throws Exception;
-	
-	protected abstract String getBuildFilePath(CaGridInstallerModel model);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.cagrid.installer.tasks.BasicTask#internalExecute(java.util.Map)
+     */
+    @Override
+    protected Object internalExecute(CaGridInstallerModel model) throws Exception {
+        Map<String, String> env = new HashMap<String, String>();
+        env.put("GLOBUS_LOCATION", model.getProperty(Constants.GLOBUS_HOME));
+        env.put("CATALINA_HOME", model.getProperty(Constants.TOMCAT_HOME));
+        Properties sysProps = new Properties();
+        sysProps.setProperty(Constants.SERVICE_DEST_DIR, model.getServiceDestDir());
+        sysProps.setProperty(Constants.GRIDCA_BUILD_FILE_PATH, model.getProperty(Constants.CAGRID_HOME)
+            + "/projects/gridca/build.xml");
+        sysProps.setProperty("env.GLOBUS_LOCATION", model.getProperty(Constants.GLOBUS_HOME));
+
+        model.setProperty(Constants.BUILD_FILE_PATH, getBuildFilePath(model));
+
+        return runAntTask(model, this.targetName, env, sysProps);
+
+    }
+
+
+    protected abstract Object runAntTask(CaGridInstallerModel model, String target, Map<String, String> env,
+        Properties sysProps) throws Exception;
+
+
+    protected abstract String getBuildFilePath(CaGridInstallerModel model);
 
 }
