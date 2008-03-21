42325a26f33b6c8aa14bfeb6d1e473b59d16d7c4
dervin <dervin>
2008-03-21 16:30:30 +0000

    Allows user to specify a Globus credential to the client enum iterator; feature request 13107
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10498 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/wsEnum/src/java/utils/gov/nih/nci/cagrid/wsenum/utils/EnumerationResponseHelper.java
+++ cagrid-1-0/caGrid/projects/wsEnum/src/java/utils/gov/nih/nci/cagrid/wsenum/utils/EnumerationResponseHelper.java
@@ -10,7 +10,9 @@ import org.apache.axis.client.AxisClient;
 import org.apache.axis.client.Stub;
 import org.apache.axis.configuration.FileProvider;
 import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.globus.gsi.GlobusCredential;
 import org.globus.ws.enumeration.ClientEnumIterator;
+import org.ietf.jgss.GSSException;
 import org.xmlsoap.schemas.ws._2004._09.enumeration.DataSource;
 import org.xmlsoap.schemas.ws._2004._09.enumeration.service.EnumerationServiceAddressingLocator;
 
@@ -23,13 +25,13 @@ import org.xmlsoap.schemas.ws._2004._09.enumeration.service.EnumerationServiceAd
  * @author David Ervin
  * 
  * @created May 16, 2007 2:09:06 PM
- * @version $Id: EnumerationResponseHelper.java,v 1.2 2007-12-17 18:51:43 dervin Exp $ 
+ * @version $Id: EnumerationResponseHelper.java,v 1.3 2008-03-21 16:30:30 dervin Exp $ 
  */
 public class EnumerationResponseHelper {
     
     /**
      * Creates a client enumeration iterator to iterate over
-     * a remove enumeration resource
+     * a remote enumeration resource
      * 
      * @param container
      *      The enumeration response container
@@ -44,7 +46,7 @@ public class EnumerationResponseHelper {
     
     /**
      * Creates a client enumeration iterator to iterate over
-     * a remove enumeration resource
+     * a remote enumeration resource
      * 
      * @param container
      *      The enumeration response container
@@ -62,7 +64,7 @@ public class EnumerationResponseHelper {
     
     /**
      * Creates a client enumeration iterator to iterate over
-     * a remove enumeration resource
+     * a remote enumeration resource
      * 
      * @param container
      *      The enumeration response container
@@ -90,7 +92,29 @@ public class EnumerationResponseHelper {
         ClientEnumIterator iter = new ClientEnumIterator(dataSource, container.getContext());
         return iter;
     }
-
+    
+    
+    /**
+     * Creates a client enumeration iterator to iterate over
+     * a remote enumeration resource
+     * 
+     * @param container
+     *      The enumeration response container
+     * @param wsddInputStream
+     *      The WSDD configuration stream for the service.  May be <code>null</code>
+     * @param credential
+     *      The globus credential to use for authenticating the connection
+     * @return
+     *      The client side iterator
+     * @throws RemoteException
+     */
+    public static ClientEnumIterator createClientIterator(EnumerationResponseContainer container,
+        InputStream wsddInputStream, GlobusCredential credential) throws RemoteException {
+        DataSource dataSource = createDataSource(container.getEPR(), wsddInputStream, credential);
+        ClientEnumIterator iter = new ClientEnumIterator(dataSource, container.getContext());
+        return iter;
+    }
+    
 
     /**
      * Creates a DataSource implementation which connects to a remote
@@ -140,6 +164,46 @@ public class EnumerationResponseHelper {
      * @throws RemoteException
      */
     public static DataSource createDataSource(EndpointReferenceType epr, InputStream wsddInputStream, boolean disableClientAuth) throws RemoteException {
+        return createDataSource(epr, wsddInputStream, disableClientAuth, null);
+    }
+    
+    
+    /**
+     * Creates a DataSource implementation which connects to a remote 
+     * WS-Enumeration implementation, using a globus credential
+     * 
+     * @param epr
+     *      The endpoint of the remote enumeration      
+     * @param wsddInputStream
+     *      The WSDD configuration stream for the service.  May be <code>null</code>
+     * @param credential
+     *      The globus credential to be used for authenticating to the service.
+     * @return
+     *      The data source
+     * @throws RemoteException
+     */
+    public static DataSource createDataSource(EndpointReferenceType epr, InputStream wsddInputStream, GlobusCredential credential) throws RemoteException {
+        return createDataSource(epr, wsddInputStream, false, credential);
+    }
+    
+    
+    /**
+     * Internal method for creating a DataSource implementation
+     * 
+     * @param epr
+     *      The EPR of the service
+     * @param wsddInputStream
+     *      The WSDD configuration; can be <code>null</code>
+     * @param disableClientAuth
+     *      Flag to disable client authorization
+     * @param credential
+     *      The globus credential; can be <code>null</code>
+     * @return
+     *      The data source impl
+     * @throws RemoteException
+     */
+    private static DataSource createDataSource(EndpointReferenceType epr, InputStream wsddInputStream,
+        boolean disableClientAuth, GlobusCredential credential) throws RemoteException {
         EnumerationServiceAddressingLocator locator = new EnumerationServiceAddressingLocator();
         
         // attempt to load our context sensitive wsdd file
@@ -152,12 +216,22 @@ public class EnumerationResponseHelper {
         DataSource port = null;
         try {
             port = locator.getDataSourcePort(epr);
-            if (disableClientAuth) {
-                ((Stub) port)._setProperty(org.globus.wsrf.security.Constants.AUTHORIZATION,
-                    org.globus.wsrf.impl.security.authorization.NoAuthorization.getInstance());
-            }
         } catch (Exception e) {
             throw new RemoteException("Unable to locate portType:" + e.getMessage(), e);
+        }
+        
+        if (disableClientAuth) {
+            ((Stub) port)._setProperty(org.globus.wsrf.security.Constants.AUTHORIZATION,
+                org.globus.wsrf.impl.security.authorization.NoAuthorization.getInstance());
+        }
+        if (credential != null) {
+            try {
+                org.ietf.jgss.GSSCredential gss = new org.globus.gsi.gssapi.GlobusGSSCredentialImpl(
+                    credential, org.ietf.jgss.GSSCredential.INITIATE_AND_ACCEPT);
+                ((Stub)port)._setProperty(org.globus.axis.gsi.GSIConstants.GSI_CREDENTIALS, gss);
+            } catch (GSSException ex) {
+                throw new RemoteException("Error setting credential: " + ex.getMessage(), ex);
+            }
         }
 
         return port;
