bb10bb676f934538a205cc8b8679bcad6f7c9791
dervin <dervin>
2008-03-25 15:57:49 +0000

    XMLDataModelServiceClient is printing stack traces and not rethrowing exceptions.  Added helper class to implement the re-throw
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10536 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/tools/src/java/validator/gov/nih/nci/cagrid/validator/steps/gme/SchemaDownloadStep.java
+++ cagrid-1-0/caGrid/projects/tools/src/java/validator/gov/nih/nci/cagrid/validator/steps/gme/SchemaDownloadStep.java
@@ -1,16 +1,27 @@
 package gov.nih.nci.cagrid.validator.steps.gme;
 
+import gov.nih.nci.cagrid.common.XMLUtilities;
 import gov.nih.nci.cagrid.introduce.common.CommonTools;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.projectmobius.castor.xml.schema.Schema;
+import org.projectmobius.castor.xml.schema.reader.SchemaReader;
 import org.projectmobius.client.gme.ImportInfo;
-import org.projectmobius.common.MobiusArgumentException;
 import org.projectmobius.common.MobiusException;
 import org.projectmobius.common.Namespace;
-import org.projectmobius.common.gme.SchemaReferenceException;
+import org.projectmobius.common.gme.SchemaParsingException;
+import org.projectmobius.gme.XMLDataModelService;
+import org.projectmobius.protocol.gme.SchemaNode;
+import org.xml.sax.InputSource;
 
 /** 
  *  SchemaDownloadStep
@@ -20,7 +31,7 @@ import org.projectmobius.common.gme.SchemaReferenceException;
  * @author David Ervin
  * 
  * @created Aug 27, 2007 4:37:36 PM
- * @version $Id: SchemaDownloadStep.java,v 1.1 2008-03-25 14:20:30 dervin Exp $ 
+ * @version $Id: SchemaDownloadStep.java,v 1.2 2008-03-25 15:57:49 dervin Exp $ 
  */
 public class SchemaDownloadStep extends BaseGmeTestStep {
     
@@ -80,28 +91,108 @@ public class SchemaDownloadStep extends BaseGmeTestStep {
     private List<Namespace> cacheSchemasToDirectory(Namespace baseSchema, File tempDir) {
         List<Namespace> cached = null;
         try {
-            cached = getGmeHandle().cacheSchema(baseSchema, tempDir);
-        } catch (SchemaReferenceException ex) {
-            System.out.println("CAUGHT SCHEMA REFERENCE EXCEPTION");
-            System.out.println("CAUGHT SCHEMA REFERENCE EXCEPTION");
-            System.out.println("CAUGHT SCHEMA REFERENCE EXCEPTION");
-            System.out.println("CAUGHT SCHEMA REFERENCE EXCEPTION");
-            System.out.println("CAUGHT SCHEMA REFERENCE EXCEPTION");
-        } catch (MobiusException ex) {
-            System.out.println("CAUGHT MOBIUS EXCEPTION");
-            System.out.println("CAUGHT MOBIUS EXCEPTION");
-            System.out.println("CAUGHT MOBIUS EXCEPTION");
-            System.out.println("CAUGHT MOBIUS EXCEPTION");
-            System.out.println("CAUGHT MOBIUS EXCEPTION");
+            GmeClientHelper helper = new GmeClientHelper(getGmeHandle());
+            cached = helper.cacheSchema(baseSchema, tempDir);
         } catch (Exception ex) {
-            System.out.println("CAUGHT EXCEPTION");
-            System.out.println("CAUGHT EXCEPTION");
-            System.out.println("CAUGHT EXCEPTION");
-            System.out.println("CAUGHT EXCEPTION");
-            System.out.println("CAUGHT EXCEPTION");
-            // ex.printStackTrace();
+            ex.printStackTrace();
             fail("Error caching schemas: " + ex.getMessage());
         } 
         return cached;
+    }
+    
+    
+    /**
+     * This inner class duplicates some functionality of the Mobius GME client class
+     * so as not to simply print exceptions and move on without rethrowing them
+     */
+    private class GmeClientHelper {
+        
+        private XMLDataModelService gmeClient = null;
+        
+        public GmeClientHelper(XMLDataModelService client) {
+            this.gmeClient = client;
+        }
+        
+        
+        /* CODE FROM org.projectmobius.client.gme.XMLDataModelServiceClient */
+        public List cacheSchema(Namespace namespace, File directory) throws MobiusException {
+            directory.mkdirs();
+            
+            // list holds the namespaces cached
+            List namespaces = new ArrayList();
+            
+            // get the base namespace and its imports
+            SchemaNode node = gmeClient.getSchema(namespace, true);
+            
+            // recursively parse the schema and its imports
+            this.processSchemaNode(node, directory.getAbsolutePath(), namespaces);
+
+            return namespaces;
+        }
+        
+        
+        /* CODE FROM org.projectmobius.client.gme.XMLDataModelServiceClient */
+        private ImportInfo processSchemaNode(SchemaNode node, String directory, List writtenNamespaces) throws MobiusException {
+            List fileImports = new ArrayList();
+            List referencedSchemas = node.getReferenced();
+            // recursively parse imports
+            for (int i = 0; i < referencedSchemas.size(); i++) {
+                fileImports.add(processSchemaNode(
+                    (SchemaNode) referencedSchemas.get(i), directory, writtenNamespaces));
+            }
+
+            // read the schema itself
+            String schemaString = node.getSchemaContents();
+            Schema schema = null;
+            ByteArrayInputStream bis = new ByteArrayInputStream(schemaString.getBytes());
+            InputSource is = new InputSource(bis);
+            try {
+                SchemaReader sr = new SchemaReader(is);
+                sr.setValidation(false);
+                schema = sr.read();
+            } catch (IOException ex) {
+                throw new SchemaParsingException("Error parsing schema: " + ex.getMessage(), ex);
+            }
+            
+            // create the namespace and local file name for the schema
+            Namespace thisnamespace = new Namespace(schema.getTargetNamespace());
+            ImportInfo ii = new ImportInfo(thisnamespace);
+            String fileName = directory + File.separator + ii.getFileName();            
+
+            // convert the schema to a JDom element for further processing
+            Document schemaDoc = null;
+            try {
+                schemaDoc = XMLUtilities.stringToDocument(schemaString);
+            } catch (Exception ex) {
+                throw new SchemaParsingException("Error converting schema to JDOM object: " + ex.getMessage(), ex);
+            }
+            Element root = schemaDoc.getRootElement();
+            //remove the imports if the schema text was submitted with some.....
+            root.removeChildren("import", root.getNamespace());
+            //add the new imports based on fetching them from GME's
+            for (int i = 0; i < fileImports.size(); i++) {
+                Element im = new Element("import", root.getNamespace());
+                ImportInfo fim = (ImportInfo) fileImports.get(i);
+                im.setAttribute("namespace", fim.getNamespace().getRaw());
+                im.setAttribute("schemaLocation", fim.getFileName());
+                root.addContent(0, im);
+            }
+
+            // write the edited schema back out
+            try {
+                FileWriter fw = new FileWriter(new File(fileName));
+                fw.write(XMLUtilities.formatXML(XMLUtilities.documentToString(schemaDoc)));
+                fw.flush();
+                fw.close();
+            } catch (Exception ex) {
+                throw new SchemaParsingException("Error writing schema to disk: " + ex.getMessage(), ex);
+            }
+            
+            // include the base schema's target namespace
+            Namespace ns = new Namespace(schema.getTargetNamespace());
+            writtenNamespaces.add(ns);
+            
+            return new ImportInfo(ns);
+        }
     }
 }
