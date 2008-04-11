09af48c65dcac46f29e05e7933ae5b9dc1ce6c80
kherm <kherm>
2008-04-11 18:33:10 +0000

    bug fix
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10640 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/util/PortletUtils.java
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/util/PortletUtils.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.util;
 
@@ -28,173 +28,198 @@ import org.w3c.dom.NodeList;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
+ *
  */
 public class PortletUtils {
 
-	private static final Log logger = LogFactory.getLog(PortletUtils.class);
-	
-	public static final String EMPTY_RESULT_PATTERN = "count(/CQLQueryResults/child::*) = 0";
-	public static final String OBJECT_RESULT_PATTERN = "count(/CQLQueryResults/ObjectResult) > 0";
-	public static final String ATTRIBUTE_RESULT_PATTERN = "count(/CQLQueryResults/AttributeResult) > 0";
-	public static final String COUNT_RESULT_PATTERN = "count(/CQLQueryResults/CountResult) = 1";
+    private static final Log logger = LogFactory.getLog(PortletUtils.class);
 
-	public static Table buildTableFromCQLResults(InputStream in) throws Exception {
-		Table table = new Table();
-		Document doc = DocumentBuilderFactory.newInstance()
-				.newDocumentBuilder().parse(in);
-		XPathFactory xpFact = XPathFactory.newInstance();
+    public static final String EMPTY_RESULT_PATTERN = "count(/CQLQueryResults/child::*) = 0";
+    public static final String OBJECT_RESULT_PATTERN = "count(/CQLQueryResults/ObjectResult) > 0";
+    public static final String ATTRIBUTE_RESULT_PATTERN = "count(/CQLQueryResults/AttributeResult) > 0";
+    public static final String COUNT_RESULT_PATTERN = "count(/CQLQueryResults/CountResult) = 1";
 
-		Boolean isEmpty = (Boolean) xpFact.newXPath().compile(
-				EMPTY_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
-		if (isEmpty) {
-			return null;
-		}
+    public static Table buildTableFromCQLResults(InputStream in) throws Exception {
+        Table table = new Table();
+        Document doc = DocumentBuilderFactory.newInstance()
+                .newDocumentBuilder().parse(in);
+        XPathFactory xpFact = XPathFactory.newInstance();
 
-		if (isObjectResult(doc)) {
-			populateObjectResults(doc, table);
-		} else if (isAttributeResult(doc)) {
-			populateAttributeResults(doc, table);
-		} else if (isCountResult(doc)) {
-			populateCountResult(doc, table);
-		}
+        Boolean isEmpty = (Boolean) xpFact.newXPath().compile(
+                EMPTY_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
+        if (isEmpty) {
+            return null;
+        }
 
-		return table;
-	}
+        if (isObjectResult(doc)) {
+            populateObjectResults(doc, table);
+        } else if (isAttributeResult(doc)) {
+            populateAttributeResults(doc, table);
+        } else if (isCountResult(doc)) {
+            populateCountResult(doc, table);
+        }
 
-	private static void populateCountResult(Document doc, Table table) throws Exception {
-		table.getHeaders().add("count");
-		XPathFactory xpFact = XPathFactory.newInstance();
-		Element countEl = (Element) xpFact.newXPath().compile(
-				"/CQLQueryResults/CountResult").evaluate(doc,
-				XPathConstants.NODE);
-		Map<String,Object> row = new HashMap<String,Object>();
-		row.put("count", countEl.getAttribute("count"));
-		table.getRows().add(row);
-	}
+        return table;
+    }
 
-	private static boolean isCountResult(Document doc)
-			throws XPathExpressionException {
-		return (Boolean) XPathFactory.newInstance().newXPath().compile(
-				COUNT_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
+    private static void populateCountResult(Document doc, Table table) throws Exception {
+        table.getHeaders().add("count");
+        XPathFactory xpFact = XPathFactory.newInstance();
+        Element countEl = (Element) xpFact.newXPath().compile(
+                "/CQLQueryResults/CountResult").evaluate(doc,
+                XPathConstants.NODE);
+        Map<String,Object> row = new HashMap<String,Object>();
+        row.put("count", countEl.getAttribute("count"));
+        table.getRows().add(row);
+    }
 
-	}
+    private static boolean isCountResult(Document doc)
+            throws XPathExpressionException {
+        return (Boolean) XPathFactory.newInstance().newXPath().compile(
+                COUNT_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
 
-	private static void populateAttributeResults(Document doc, Table table) throws Exception {
-		XPathFactory xpFact = XPathFactory.newInstance();
-		NodeList attResults = (NodeList) xpFact.newXPath().compile(
-				"/CQLQueryResults/AttributeResult").evaluate(doc,
-				XPathConstants.NODESET);
-		for (int i = 0; i < attResults.getLength(); i++) {
-			Element attResult = (Element) attResults.item(i);
-			NodeList attEls = (NodeList) xpFact.newXPath().compile(
-					"./Attribute").evaluate(attResult,
-					XPathConstants.NODESET);
-			Map<String, Object> row = new HashMap<String, Object>();
-			for (int j = 0; j < attEls.getLength(); j++) {
-				Element attEl = (Element)attEls.item(j);
-				String name = attEl.getAttribute("name");
-				String value = attEl.getAttribute("value");
-				if (!table.getHeaders().contains(name)) {
-					table.getHeaders().add(name);
-				}
-				
-				row.put(name, value);
-			}
-			table.getRows().add(row);
-		}
+    }
 
-	}
+    private static void populateAttributeResults(Document doc, Table table) throws Exception {
+        XPathFactory xpFact = XPathFactory.newInstance();
+        NodeList attResults = (NodeList) xpFact.newXPath().compile(
+                "/CQLQueryResults/AttributeResult").evaluate(doc,
+                XPathConstants.NODESET);
+        for (int i = 0; i < attResults.getLength(); i++) {
+            Element attResult = (Element) attResults.item(i);
+            NodeList attEls = (NodeList) xpFact.newXPath().compile(
+                    "./Attribute").evaluate(attResult,
+                    XPathConstants.NODESET);
+            Map<String, Object> row = new HashMap<String, Object>();
+            for (int j = 0; j < attEls.getLength(); j++) {
+                Element attEl = (Element)attEls.item(j);
+                String name = attEl.getAttribute("name");
+                String value = attEl.getAttribute("value");
+                if (!table.getHeaders().contains(name)) {
+                    table.getHeaders().add(name);
+                }
 
-	private static boolean isAttributeResult(Document doc)
-			throws XPathExpressionException {
-		return (Boolean) XPathFactory.newInstance().newXPath().compile(
-				ATTRIBUTE_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
-	}
+                row.put(name, value);
+            }
+            table.getRows().add(row);
+        }
 
-	private static void populateObjectResults(Document doc, Table table) throws Exception {
-		
-		XPathFactory xpFact = XPathFactory.newInstance();
-		NodeList objResults = (NodeList) xpFact.newXPath().compile(
-				"/CQLQueryResults/ObjectResult/child::*").evaluate(doc,
-				XPathConstants.NODESET);
-		for (int i = 0; i < objResults.getLength(); i++) {
-			Element objResult = (Element) objResults.item(i);
-			NodeList atts = (NodeList) xpFact.newXPath().compile(
-					"./attribute::*").evaluate(objResult,
-					XPathConstants.NODESET);
-			Map<String, Object> row = new HashMap<String, Object>();
-			for (int j = 0; j < atts.getLength(); j++) {
-				Attr att = (Attr) atts.item(j);
-				if (!table.getHeaders().contains(att.getName())) {
-					table.getHeaders().add(att.getName());
-				}
-				
-				row.put(att.getName(), att.getValue());
-			}
-			table.getRows().add(row);
-		}
-	}
+    }
 
-	private static boolean isObjectResult(Document doc)
-			throws XPathExpressionException {
-		return (Boolean) XPathFactory.newInstance().newXPath().compile(
-				OBJECT_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
-	}
-	
-	public static void doScrollOp(PortletRequest request, Scroller scroller){
-		String scrollOp = request.getParameter("scrollOp");
-		logger.debug("scrollOp = '" + scrollOp + "'");
-		if (!PortalUtils.isEmpty(scrollOp)) {
-			if ("first".equals(scrollOp)) {
-				scroller.first();
-			} else if ("previous".equals(scrollOp)) {
-				scroller.previous();
-			} else if ("next".equals(scrollOp)) {
-				scroller.next();
-			} else if ("last".equals(scrollOp)) {
-				scroller.last();
-			} else {
-				logger.warn("Invalid scroll operation: '" + scrollOp + "'");
-			}
-		}
-	}
-	
-	public static String[] parsePath(String path) {
-		String[] parts = null;
-		int idx = path.indexOf("/");
-		if (idx == -1) {
-			parts = new String[] { path };
-		} else {
-			parts = new String[] { path.substring(0, idx),
-					path.substring(idx + 1) };
-		}
-		return parts;
-	}
-	
-	public static List<GridService> filterBannedServices(List<GridService> in){
-		return filterServicesByStatus(in, ServiceStatus.BANNED);
-	}
-	
-	public static List<GridService> filterDormantServices(List<GridService> in){
-		return filterServicesByStatus(in, ServiceStatus.DORMANT);
-	}
-	
-	public static List<GridService> filterServicesByStatus(List<GridService> in, ServiceStatus... statuses){
-		List<GridService> out = new ArrayList<GridService>();
-		for(GridService svc : in){
-			boolean filter = false;
-			for(ServiceStatus status : statuses){
-				if(status.equals(svc.getCurrentStatus())){
-					filter = true;
-					break;
-				}
-			}
-			if(!filter){
-				out.add(svc);
-			}
-		}
-		return out;
-	}
+    private static boolean isAttributeResult(Document doc)
+            throws XPathExpressionException {
+        return (Boolean) XPathFactory.newInstance().newXPath().compile(
+                ATTRIBUTE_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
+    }
+
+    private static void populateObjectResults(Document doc, Table table) throws Exception {
+
+        XPathFactory xpFact = XPathFactory.newInstance();
+        NodeList objResults = (NodeList) xpFact.newXPath().compile(
+                "/CQLQueryResults/ObjectResult/child::*").evaluate(doc,
+                XPathConstants.NODESET);
+        for (int i = 0; i < objResults.getLength(); i++) {
+            Element objResult = (Element) objResults.item(i);
+            NodeList atts = (NodeList) xpFact.newXPath().compile(
+                    "./attribute::*").evaluate(objResult,
+                    XPathConstants.NODESET);
+            Map<String, Object> row = new HashMap<String, Object>();
+            for (int j = 0; j < atts.getLength(); j++) {
+                Attr att = (Attr) atts.item(j);
+                if (!table.getHeaders().contains(att.getName())) {
+                    table.getHeaders().add(att.getName());
+                }
+
+                row.put(att.getName(), att.getValue());
+            }
+            table.getRows().add(row);
+        }
+    }
+
+    private static boolean isObjectResult(Document doc)
+            throws XPathExpressionException {
+        return (Boolean) XPathFactory.newInstance().newXPath().compile(
+                OBJECT_RESULT_PATTERN).evaluate(doc, XPathConstants.BOOLEAN);
+    }
+
+    public static void doScrollOp(PortletRequest request, Scroller scroller){
+        String scrollOp = request.getParameter("scrollOp");
+        logger.debug("scrollOp = '" + scrollOp + "'");
+        if (!PortalUtils.isEmpty(scrollOp)) {
+            if ("first".equals(scrollOp)) {
+                scroller.first();
+            } else if ("previous".equals(scrollOp)) {
+                scroller.previous();
+            } else if ("next".equals(scrollOp)) {
+                scroller.next();
+            } else if ("last".equals(scrollOp)) {
+                scroller.last();
+            } else {
+                logger.warn("Invalid scroll operation: '" + scrollOp + "'");
+            }
+        }
+    }
+
+    public static String[] parsePath(String path) {
+        String[] parts = null;
+        int idx = path.indexOf("/");
+        if (idx == -1) {
+            parts = new String[] { path };
+        } else {
+            parts = new String[] { path.substring(0, idx),
+                    path.substring(idx + 1) };
+        }
+        return parts;
+    }
+
+    public static List<GridService> filterBannedServices(List<GridService> in){
+        return filterServicesByStatus(in, ServiceStatus.BANNED);
+    }
+
+    public static List<GridService> filterDormantServices(List<GridService> in){
+        return filterServicesByStatus(in, ServiceStatus.DORMANT);
+
+    }
+
+    public static List<GridService> filterServicesByInvalidMetadata(List<GridService> in){
+        List<GridService> out = new ArrayList<GridService>();
+        for(GridService svc : in){
+            boolean filter = false;
+            try {
+                if(svc.getServiceMetadata() == null){
+                    filter = true;
+                }
+                else if(svc.getServiceMetadata().getServiceDescription()==null){
+                    filter = true;
+                }
+                else if(svc.getServiceMetadata().getServiceDescription().getName()==null){
+                    filter = true;
+                }
+            } catch (Exception e) {
+                filter = true;
+            }
+            if(!filter){
+                out.add(svc);
+            }
+        }
+        return out;
+    }
+
+    public static List<GridService> filterServicesByStatus(List<GridService> in, ServiceStatus... statuses){
+        List<GridService> out = new ArrayList<GridService>();
+        for(GridService svc : in){
+            boolean filter = false;
+            for(ServiceStatus status : statuses){
+                if(status.equals(svc.getCurrentStatus())){
+                    filter = true;
+                    break;
+                }
+            }
+            if(!filter){
+                out.add(svc);
+            }
+        }
+        return out;
+    }
 
 }
