ab8f8a19199f78a0c146d5e3b48d22387bfce265
oster <oster>
2008-04-16 17:39:07 +0000

    hooked in database creation to testing hooked in loading of test app context
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10652 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/globalModelExchange/test/src/org/cagrid/gme/test/GMEIntegrationTestCase.java
+++ cagrid-1-0/caGrid/projects/globalModelExchange/test/src/org/cagrid/gme/test/GMEIntegrationTestCase.java
@@ -1,34 +1,35 @@
 package org.cagrid.gme.test;
 
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
 
 public abstract class GMEIntegrationTestCase extends AbstractDependencyInjectionSpringContextTests {
 
     private static final String APPLICATION_CONTEXT_GME_XML = "etc/applicationContext-gme.xml";
-    private static final String TEST_APPLICATION_CONTEXT_GME_XML = "test/resources/test-applicationContext-gme.xml";
+    private static final String TEST_APPLICATION_CONTEXT_GME_XML = "test-applicationContext-gme.xml";
 
 
     @Override
     protected String[] getConfigLocations() {
-        // return new String[]{"file:" + APPLICATION_CONTEXT_GME_XML, "file:" +
-        // TEST_APPLICATION_CONTEXT_GME_XML};
-        return new String[]{"file:" + APPLICATION_CONTEXT_GME_XML};
+        return new String[]{"file:" + APPLICATION_CONTEXT_GME_XML, "classpath*:" + TEST_APPLICATION_CONTEXT_GME_XML};
     }
 
-
-    @Override
-    protected void customizeBeanFactory(DefaultListableBeanFactory beanFactory) {
-        super.customizeBeanFactory(beanFactory);
-        PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
-        FileSystemResource fileSystemResource = new FileSystemResource("etc/gme.properties");
-        assertNotNull(fileSystemResource);
-        assertTrue(fileSystemResource.exists());
-        cfg.setLocation(fileSystemResource);
-        // now actually do the replacement
-        cfg.postProcessBeanFactory(beanFactory);
-    }
+    // Really I'd like to do something like this to add the properties, but this
+    // is called before the beans are loaded, so has no effect (I can't find a
+    // place to override which works)
+    // @Override
+    // protected void customizeBeanFactory(DefaultListableBeanFactory
+    // beanFactory) {
+    // super.customizeBeanFactory(beanFactory);
+    // PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
+    // FileSystemResource fileSystemResource = new
+    // FileSystemResource("etc/gme.properties");
+    // assertNotNull(fileSystemResource);
+    // assertTrue(fileSystemResource.exists());
+    // cfg.setLocation(fileSystemResource);
+    // // now actually do the replacement
+    // cfg.postProcessBeanFactory(beanFactory);
+    //        
+    // beanFactory.addBeanPostProcessor(beanPostProcessor)
+    // }
 }
