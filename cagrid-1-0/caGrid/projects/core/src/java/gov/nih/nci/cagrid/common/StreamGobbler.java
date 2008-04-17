69a9bffa01a0634306c0ffcc67ce0f102fee4f4b
dervin <dervin>
2008-04-17 14:55:06 +0000

    deprecated Log4j stuff in favor of commons logging; added wrappers for use until all projects are moved forward
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10661 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/StreamGobbler.java
+++ cagrid-1-0/caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/StreamGobbler.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import org.apache.commons.logging.Log;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Priority;
 
@@ -18,18 +19,18 @@ import org.apache.log4j.Priority;
  * @author David Ervin
  * @author Shannon Hastings
  * @created Jun 21, 2007 11:03:06 AM
- * @version $Id: StreamGobbler.java,v 1.4 2007-10-31 18:35:24 dervin Exp $
+ * @version $Id: StreamGobbler.java,v 1.5 2008-04-17 14:55:06 dervin Exp $
  */
 public class StreamGobbler extends Thread {
 
     public static final String TYPE_OUT = "OUT";
     public static final String TYPE_ERR = "ERR";
 
-    private Logger logger = null;
-    private Priority priority = null;
     private InputStream gobble;
     private String type;
     private PrintStream redirect;
+    private Log log;
+    private LogPriority priority;
     
     /**
      * Creates a stream gobbler which will just read the input stream until it's gone
@@ -57,11 +58,10 @@ public class StreamGobbler extends Thread {
             this.redirect = new PrintStream(redrirect);
         }
     }
-
-
+    
+    
     /**
-     * Creates a stream gobbler to consume an input stream and redirect its
-     * contents to a Log4J logger
+     * @deprecated use a commons logger, StreamGobbler(InputStream is, String type, Log log, LogPriority priority)
      * 
      * @param is
      * @param type
@@ -69,9 +69,23 @@ public class StreamGobbler extends Thread {
      * @param priority
      */
     public StreamGobbler(InputStream is, String type, Logger logger, Priority priority) {
+        this(is, type, wrapLog4J(logger), convertLog4JPriority(priority));
+    }
+    
+    
+    /**
+     * Creates a stream gobbler to consume an input stream and redirect
+     * its contents to a Log with the specified priority level
+     * 
+     * @param is
+     * @param type
+     * @param log
+     * @param priority
+     */
+    public StreamGobbler(InputStream is, String type, Log log, LogPriority priority) {
         this.gobble = is;
         this.type = type;
-        this.logger = logger;
+        this.log = log;
         this.priority = priority;
     }
 
@@ -85,12 +99,25 @@ public class StreamGobbler extends Thread {
             BufferedReader br = new BufferedReader(isr);
             String line = null;
             while ((line = br.readLine()) != null) {
-                if (redirect != null || (logger != null && priority != null)) {
+                if (redirect != null || (log != null && priority != null)) {
                     line = type + "> " + line;
                     if (redirect != null) {
                         redirect.println(line);
-                    } else { // if (logger != null && priority !=null) {
-                        logger.log(priority, line);
+                    } else {
+                        switch (priority) {
+                            case INFO:
+                                log.info(line);
+                                break;
+                            case DEBUG:
+                                log.debug(line);
+                                break;
+                            case WARN:
+                                log.warn(line);
+                                break;
+                            case ERROR:
+                                log.error(line);
+                                break;
+                        }
                     }
                 }
             }
@@ -98,5 +125,134 @@ public class StreamGobbler extends Thread {
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+    }
+    
+    
+    public static Log wrapLog4J(final Logger logger) {
+        Log log = new Log() {
+            public void debug(Object arg0, Throwable arg1) {
+                logger.debug(arg0, arg1);
+            }
+
+
+            public void debug(Object arg0) {
+                logger.debug(arg0);
+            }
+
+
+            public void error(Object arg0, Throwable arg1) {
+                logger.error(arg0, arg1);
+            }
+
+
+            public void error(Object arg0) {
+                logger.error(arg0);
+            }
+
+
+            public void fatal(Object arg0, Throwable arg1) {
+                logger.fatal(arg0, arg1);
+            }
+
+
+            public void fatal(Object arg0) {
+                logger.fatal(arg0);
+            }
+
+
+            public void info(Object arg0, Throwable arg1) {
+                logger.info(arg0, arg1);
+            }
+
+
+            public void info(Object arg0) {
+                logger.info(arg0);
+            }
+
+
+            public boolean isDebugEnabled() {
+                return logger.isDebugEnabled();
+            }
+
+
+            public boolean isErrorEnabled() {
+                return logger.isEnabledFor(Priority.ERROR);
+            }
+
+
+            public boolean isFatalEnabled() {
+                return logger.isEnabledFor(Priority.FATAL);
+            }
+
+
+            public boolean isInfoEnabled() {
+                return logger.isInfoEnabled();
+            }
+
+
+            public boolean isTraceEnabled() {
+                // TODO: solve what this should be
+                return false;
+            }
+
+
+            public boolean isWarnEnabled() {
+                return logger.isEnabledFor(Priority.WARN);
+            }
+
+
+            public void trace(Object arg0, Throwable arg1) {
+                // TODO: trace == info?
+                logger.info(arg0, arg1);
+            }
+
+
+            public void trace(Object arg0) {
+                // TODO: trace == info?
+                logger.info(arg0);
+            }
+
+
+            public void warn(Object arg0, Throwable arg1) {
+                logger.warn(arg0, arg1);
+            }
+
+
+            public void warn(Object arg0) {
+                logger.warn(arg0);
+            }
+        };
+        return log;
+    }
+    
+    
+    public static LogPriority convertLog4JPriority(Priority priority) {
+        LogPriority pri = LogPriority.INFO; // a reasonable default
+        if (priority == Priority.DEBUG) {
+            pri = LogPriority.DEBUG;
+        } else if (priority == Priority.ERROR) {
+            pri = LogPriority.ERROR;
+        } else if (priority == Priority.FATAL) {
+            pri = LogPriority.FATAL;
+        } else if (priority == Priority.INFO) {
+            pri = LogPriority.INFO;
+        } else if (priority == Priority.WARN) {
+            pri = LogPriority.WARN;
+        }
+        return pri;
+    }
+    
+    
+    /**
+      *  LogPriority
+      *  Priority with which to log output from the stream gobbler
+      * 
+      * @author David Ervin
+      * 
+      * @created Apr 17, 2008 9:29:33 AM
+      * @version $Id: StreamGobbler.java,v 1.5 2008-04-17 14:55:06 dervin Exp $
+     */
+    public static enum LogPriority {
+        INFO, DEBUG, WARN, ERROR, FATAL
     }
 }
