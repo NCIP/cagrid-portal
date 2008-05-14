4f75d71ddee4982997238dc2f4f78809a00f8065
langella <langella>
2008-05-14 13:53:32 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10774 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/authentication-service/src/org/cagrid/gaards/authentication/client/AuthenticationClient.java
@@ -0,0 +1,46 @@
+package org.cagrid.gaards.authentication.client;
+
+
+import gov.nih.nci.cagrid.opensaml.SAMLAssertion;
+
+import java.rmi.RemoteException;
+
+import org.apache.axis.types.URI.MalformedURIException;
+import org.cagrid.gaards.authentication.Credential;
+import org.cagrid.gaards.authentication.faults.AuthenticationProviderFault;
+import org.cagrid.gaards.authentication.faults.InsufficientAttributeFault;
+import org.cagrid.gaards.authentication.faults.InvalidCredentialFault;
+
+
+/**
+ * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
+ * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
+ * @author <A href="mailto:hastings@bmi.osu.edu">Shannon Hastings </A>
+ * @version $Id: ArgumentManagerTable.java,v 1.2 2004/10/15 16:35:16 langella
+ *          Exp $
+ */
+public class AuthenticationClient {
+
+	private AuthenticationServiceClient client;
+
+	public AuthenticationClient(String serviceURI)
+			throws MalformedURIException, RemoteException {
+		client = new AuthenticationServiceClient(serviceURI);
+	}
+
+	public SAMLAssertion authenticate(Credential cred) throws RemoteException,
+			InvalidCredentialFault, InsufficientAttributeFault,
+			AuthenticationProviderFault {
+		try {
+			return client.authenticateWithIdentityProvider(cred);
+		} catch (InvalidCredentialFault gie) {
+			throw gie;
+		} catch (InsufficientAttributeFault ilf) {
+			throw ilf;
+		} catch (AuthenticationProviderFault ilf) {
+			throw ilf;
+		} catch (Exception e) {
+			throw new RemoteException(e.getMessage());
+		}
+	}
+}
