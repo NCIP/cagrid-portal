18066d337d2b40b55205a557b90be8871ed876a9
langella <langella>
2008-05-15 17:46:10 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10802 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/gaards-core/src/org/cagrid/gaards/saml/encoding/SAMLUtils.java
+++ cagrid-1-0/caGrid/projects/gaards-core/src/org/cagrid/gaards/saml/encoding/SAMLUtils.java
@@ -1,8 +1,11 @@
 package org.cagrid.gaards.saml.encoding;
 
 import gov.nih.nci.cagrid.opensaml.SAMLAssertion;
+import gov.nih.nci.cagrid.opensaml.SAMLAttribute;
+import gov.nih.nci.cagrid.opensaml.SAMLAttributeStatement;
 
 import java.io.ByteArrayInputStream;
+import java.util.Iterator;
 
 import org.w3c.dom.Element;
 
@@ -36,6 +39,33 @@ public class SAMLUtils {
 
 	public static Element samlAssertionToDOM(SAMLAssertion saml)
 			throws Exception {
-		return (Element)saml.toDOM();
+		return (Element) saml.toDOM();
+	}
+
+	public static String getAttributeValue(SAMLAssertion saml,
+			String namespace, String name) {
+		Iterator itr = saml.getStatements();
+		while (itr.hasNext()) {
+			Object o = itr.next();
+			if (o instanceof SAMLAttributeStatement) {
+				SAMLAttributeStatement att = (SAMLAttributeStatement) o;
+				Iterator attItr = att.getAttributes();
+				while (attItr.hasNext()) {
+					SAMLAttribute a = (SAMLAttribute) attItr.next();
+					if ((a.getNamespace().equals(namespace))
+							&& (a.getName().equals(name))) {
+						Iterator vals = a.getValues();
+						while (vals.hasNext()) {
+							String val = gov.nih.nci.cagrid.common.Utils
+									.clean((String) vals.next());
+							if (val != null) {
+								return val;
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
 	}
 }
