317a8e636d9d560f9df3869f8fb68bff7b3e8eaf
langella <langella>
2008-05-19 03:23:05 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10832 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/authentication-service/src/org/cagrid/gaards/authentication/client/AuthenticationClient.java
+++ cagrid-1-0/caGrid/projects/authentication-service/src/org/cagrid/gaards/authentication/client/AuthenticationClient.java
@@ -1,14 +1,31 @@
 package org.cagrid.gaards.authentication.client;
 
+import gov.nih.nci.cagrid.authentication.bean.BasicAuthenticationCredential;
+import gov.nih.nci.cagrid.common.FaultHelper;
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.metadata.ResourcePropertyHelper;
+import gov.nih.nci.cagrid.metadata.exceptions.InvalidResourcePropertyException;
+import gov.nih.nci.cagrid.metadata.exceptions.RemoteResourcePropertyRetrievalException;
+import gov.nih.nci.cagrid.metadata.exceptions.ResourcePropertyRetrievalException;
 import gov.nih.nci.cagrid.opensaml.SAMLAssertion;
 
+import java.io.InputStream;
+import java.io.StringReader;
 import java.rmi.RemoteException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
 
 import org.apache.axis.types.URI.MalformedURIException;
+import org.cagrid.gaards.authentication.AuthenticationProfiles;
+import org.cagrid.gaards.authentication.BasicAuthentication;
 import org.cagrid.gaards.authentication.Credential;
 import org.cagrid.gaards.authentication.faults.AuthenticationProviderFault;
 import org.cagrid.gaards.authentication.faults.InsufficientAttributeFault;
 import org.cagrid.gaards.authentication.faults.InvalidCredentialFault;
+import org.globus.wsrf.utils.XmlUtils;
+import org.w3c.dom.Element;
 
 /**
  * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
@@ -19,44 +36,144 @@ import org.cagrid.gaards.authentication.faults.InvalidCredentialFault;
  */
 public class AuthenticationClient {
 
+	public static final QName AUTHENTICATION_PROFILES_METADATA = new QName(
+			"http://gaards.cagrid.org/authentication", "AuthenticationProfiles");
+
 	private AuthenticationServiceClient client;
+	private String serviceURI;
 
 	public AuthenticationClient(String serviceURI)
 			throws MalformedURIException, RemoteException {
+		this.serviceURI = serviceURI;
 		client = new AuthenticationServiceClient(serviceURI);
 	}
+	
+	/**
+	 * This method authenticates with the authentication service using the supplied credential.
+	 * 
+	 * @param cred The credential to use to authenticate with the credential service
+	 * @return A SAMLAssertion asserting successful authentication.
+	 * @throws RemoteException
+	 * @throws InvalidCredentialFault
+	 * @throws InsufficientAttributeFault
+	 * @throws AuthenticationProviderFault
+	 */
 
 	public SAMLAssertion authenticate(Credential cred) throws RemoteException,
 			InvalidCredentialFault, InsufficientAttributeFault,
 			AuthenticationProviderFault {
+
+		Set<QName> profiles = null;
 		try {
-			return client.authenticateWithIdentityProvider(cred);
-		} catch (InvalidCredentialFault gie) {
-			throw gie;
-		} catch (InsufficientAttributeFault ilf) {
-			throw ilf;
-		} catch (AuthenticationProviderFault ilf) {
-			throw ilf;
+			profiles = getSupportedAuthenticationProfiles();
 		} catch (Exception e) {
-			throw new RemoteException(e.getMessage());
+			throw new RemoteException(
+					"An unexpected error was encountered in trying to determine the supported authentication profiles: "
+							+ Utils.getExceptionMessage(e), e);
 		}
+		if (profiles == null) {
+			if (cred.getClass().equals(BasicAuthentication.class)) {
+				BasicAuthentication ba = (BasicAuthentication) cred;
+				BasicAuthenticationCredential bac = new BasicAuthenticationCredential();
+				bac.setUserId(ba.getUserId());
+				bac.setPassword(ba.getPassword());
+				gov.nih.nci.cagrid.authentication.bean.Credential c = new gov.nih.nci.cagrid.authentication.bean.Credential();
+				c.setBasicAuthenticationCredential(bac);
+				try {
+					gov.nih.nci.cagrid.authentication.client.AuthenticationClient ac = new gov.nih.nci.cagrid.authentication.client.AuthenticationClient(
+							this.serviceURI, c);
+					return ac.authenticate();
+				} catch (gov.nih.nci.cagrid.authentication.stubs.types.InvalidCredentialFault e) {
+					InvalidCredentialFault fault = new InvalidCredentialFault();
+					fault.setFaultString(e.getFaultString());
+					FaultHelper fh = new FaultHelper(fault);
+					fh.addFaultCause(e);
+					fault = (InvalidCredentialFault) fh.getFault();
+					throw fault;
+				} catch (gov.nih.nci.cagrid.authentication.stubs.types.InsufficientAttributeFault e) {
+					InsufficientAttributeFault fault = new InsufficientAttributeFault();
+					fault.setFaultString(e.getFaultString());
+					FaultHelper fh = new FaultHelper(fault);
+					fh.addFaultCause(e);
+					fault = (InsufficientAttributeFault) fh.getFault();
+					throw fault;
+				} catch (gov.nih.nci.cagrid.authentication.stubs.types.AuthenticationProviderFault e) {
+					AuthenticationProviderFault fault = new AuthenticationProviderFault();
+					fault.setFaultString(e.getFaultString());
+					FaultHelper fh = new FaultHelper(fault);
+					fh.addFaultCause(e);
+					fault = (AuthenticationProviderFault) fh.getFault();
+					throw fault;
+				} catch (Exception e) {
+					throw new RemoteException(e.getMessage());
+				}
+			} else {
+				InvalidCredentialFault f = new InvalidCredentialFault();
+				f
+						.setFaultString("The service you are authenticated to, is an older version of the authentication service and does not support the credential you provided.");
+				throw f;
+			}
+		} else {
+			try {
+				return client.authenticateWithIdentityProvider(cred);
+			} catch (InvalidCredentialFault gie) {
+				throw gie;
+			} catch (InsufficientAttributeFault ilf) {
+				throw ilf;
+			} catch (AuthenticationProviderFault ilf) {
+				throw ilf;
+			} catch (Exception e) {
+				throw new RemoteException(e.getMessage());
+			}
+		}
+
 	}
 
-	public static SAMLAssertion authenticate(String serviceURI, Credential cred)
-			throws RemoteException, InvalidCredentialFault,
-			InsufficientAttributeFault, AuthenticationProviderFault {
+	/**
+	 * This method obtains the authentication profiles supported by the
+	 * authentication service that the client is connecting to. The
+	 * authentication profiles are represented by the resource property:
+	 * (http://gaards.cagrid.org/authentication,AuthenticationProfiles).
+	 * 
+	 * @return If the resource property exists a set is returned containing the
+	 *         QName(s) of the authentication profiles supported. If the
+	 *         resource property does not exist null is returned.
+	 * 
+	 * @throws InvalidResourcePropertyException
+	 * @throws RemoteResourcePropertyRetrievalException
+	 * @throws ResourcePropertyRetrievalException
+	 */
+	public Set<QName> getSupportedAuthenticationProfiles()
+			throws ResourcePropertyRetrievalException {
+		Element resourceProperty = null;
 		try {
-			AuthenticationServiceClient client = new AuthenticationServiceClient(
-					serviceURI);
-			return client.authenticateWithIdentityProvider(cred);
-		} catch (InvalidCredentialFault gie) {
-			throw gie;
-		} catch (InsufficientAttributeFault ilf) {
-			throw ilf;
-		} catch (AuthenticationProviderFault ilf) {
-			throw ilf;
+			InputStream wsdd = getClass().getResourceAsStream(
+					"client-config.wsdd");
+			resourceProperty = ResourcePropertyHelper.getResourceProperty(
+					client.getEndpointReference(),
+					AUTHENTICATION_PROFILES_METADATA, wsdd);
+		} catch (InvalidResourcePropertyException e) {
+			return null;
+		}
+		try {
+			AuthenticationProfiles result = (AuthenticationProfiles) Utils
+					.deserializeObject(new StringReader(XmlUtils
+							.toString(resourceProperty)),
+							AuthenticationProfiles.class);
+			Set<QName> profiles = new HashSet<QName>();
+			if (result != null) {
+				QName[] list = result.getProfile();
+				if (list != null) {
+					for (int i = 0; i < list.length; i++) {
+						profiles.add(list[i]);
+					}
+				}
+			}
+			return profiles;
+
 		} catch (Exception e) {
-			throw new RemoteException(e.getMessage());
+			throw new ResourcePropertyRetrievalException(
+					"Unable to deserailize: " + e.getMessage(), e);
 		}
 	}
 }
