5180e9342b6c3d65ff9fba2a4661d2426686a21e
dervin <dervin>
2008-05-28 16:32:35 +0000

    fixes previously broken tests
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@10915 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/sdkQuery4/src/java/processor/gov/nih/nci/cagrid/sdkquery4/processor/SDK4QueryProcessor.java
+++ cagrid-1-0/caGrid/projects/sdkQuery4/src/java/processor/gov/nih/nci/cagrid/sdkquery4/processor/SDK4QueryProcessor.java
@@ -40,7 +40,7 @@ import org.globus.wsrf.security.SecurityManager;
  * @author David Ervin
  * 
  * @created Oct 3, 2007 10:34:55 AM
- * @version $Id: SDK4QueryProcessor.java,v 1.15 2008-04-17 15:26:12 dervin Exp $ 
+ * @version $Id: SDK4QueryProcessor.java,v 1.16 2008-05-28 16:32:35 dervin Exp $ 
  */
 public class SDK4QueryProcessor extends CQLQueryProcessor {
     // configuration property keys
@@ -115,7 +115,13 @@ public class SDK4QueryProcessor extends CQLQueryProcessor {
                     attributeNames = mods.getAttributeNames();
                     resultsAsArrays = new LinkedList<Object[]>();
                     for (Object o : rawResults) {
-                        resultsAsArrays.add((Object[]) o);
+                        Object[] array = null;
+                        if (o.getClass().isArray()) {
+                            array = (Object[]) o;
+                        } else {
+                            array = new Object[] {o};
+                        }
+                        resultsAsArrays.add(array);
                     }
                 }
                 cqlResults = CQLResultsCreationUtil.createAttributeResults(
