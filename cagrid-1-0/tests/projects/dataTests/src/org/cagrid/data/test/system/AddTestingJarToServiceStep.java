ba611f3bda581df2a9e35348de862f5e9b2023c0
dervin <dervin>
2008-06-04 14:35:30 +0000

    fixed the filename being searched for
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11025 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/dataTests/src/org/cagrid/data/test/system/AddTestingJarToServiceStep.java
+++ cagrid-1-0/tests/projects/dataTests/src/org/cagrid/data/test/system/AddTestingJarToServiceStep.java
@@ -17,11 +17,11 @@ import org.cagrid.data.test.creation.DataTestCaseInfo;
  * @author David Ervin
  * 
  * @created May 21, 2008 4:07:21 PM
- * @version $Id: AddTestingJarToServiceStep.java,v 1.1 2008-05-22 16:30:02 dervin Exp $ 
+ * @version $Id: AddTestingJarToServiceStep.java,v 1.2 2008-06-04 14:35:30 dervin Exp $ 
  */
 public class AddTestingJarToServiceStep extends Step {
     
-    public static final String DATA_TESTS_JAR_PREFIX = "DataTests-";
+    public static final String DATA_TESTS_JAR_PREFIX = "caGrid-dataTests";
 
     private DataTestCaseInfo info;
     
@@ -33,26 +33,26 @@ public class AddTestingJarToServiceStep extends Step {
     public void runStep() throws Throwable {
         File libDir = new File(info.getDir(), "lib");
         // find the testing jar
-        File dataTestsJar = null;
+        boolean jarsCopied = false;
         assertTrue("Class loader was not a URLClassLoader", getClass().getClassLoader() instanceof URLClassLoader);
         URLClassLoader currentClassLoader = (URLClassLoader) getClass().getClassLoader();
         URL[] urls = currentClassLoader.getURLs();
         for (URL u : urls) {
             URI uri = u.toURI();
+            System.out.println("CLASSPATH URI: " + uri.toString());
             if (uri.getScheme().equalsIgnoreCase("file")) {
                 // is a file
                 File classpathFile = new File(uri);
                 String name = classpathFile.getName();
                 if (name.endsWith(".jar") && name.startsWith(DATA_TESTS_JAR_PREFIX)) {
                     // found the data tests jar file
-                    dataTestsJar = classpathFile;
-                    break;
+                    // copy the jar to the service's lib dir
+                    File libOut = new File(libDir, classpathFile.getName());
+                    Utils.copyFile(classpathFile, libOut);
+                    jarsCopied = true;
                 }
             }
         }
-        assertNotNull("Could not locate DataTests jar on classpath", dataTestsJar);
-        // copy the jar to the service's lib dir
-        File libOut = new File(libDir, dataTestsJar.getName());
-        Utils.copyFile(dataTestsJar, libOut);
+        assertTrue("Could not locate caGrid-dataTests jar on classpath", jarsCopied);
     }
 }
