c691707d4bda12fa379723a9013e3e56a75b875b
hastings <hastings>
2008-06-10 16:08:11 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11073 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/introduce/src/java/Portal/gov/nih/nci/cagrid/introduce/portal/preferences/base/BasePreferenceConfigurationPanel.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.cagrid.introduce.portal.preferences.base;
-
-import javax.swing.JPanel;
-
-public abstract class BasePreferenceConfigurationPanel extends JPanel {
-	
-	public abstract void apply();
-
-}
