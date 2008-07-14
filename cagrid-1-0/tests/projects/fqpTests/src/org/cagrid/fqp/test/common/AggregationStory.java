a5f667628f3964a0ae38ae90e8e449168110edfe
dervin <dervin>
2008-07-14 21:10:04 +0000

    system tests!
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11287 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/tests/projects/fqpTests/src/org/cagrid/fqp/test/common/AggregationStory.java
@@ -0,0 +1,63 @@
+package org.cagrid.fqp.test.common;
+
+import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainer;
+import gov.nih.nci.cagrid.testing.system.haste.Step;
+import gov.nih.nci.cagrid.testing.system.haste.Story;
+
+import java.io.File;
+import java.util.Vector;
+
+import org.cagrid.fqp.test.common.steps.AggregationStep;
+
+/** 
+ *  AggregationStory
+ *  Tests the aggregation capabilities of the Federated Query Engine
+ * 
+ * @author David Ervin
+ * 
+ * @created Jun 30, 2008 12:48:50 PM
+ * @version $Id: AggregationStory.java,v 1.1 2008-07-14 21:10:04 dervin Exp $ 
+ */
+public class AggregationStory extends Story {
+    
+    public static final String QUERIES_LOCATION = "resources" + File.separator + "queries";
+    public static final String GOLD_LOCATION = "resources" + File.separator + "gold" + File.separator + "aggregation";
+    
+    public static final String SERVICE_NAME_BASE = "cagrid/ExampleSdkService";
+    
+    private DataServiceContainerSource[] dataContainers = null;
+    private FederatedQueryProcessorHelper queryHelper = null;
+    
+    public AggregationStory(DataServiceContainerSource[] dataServiceContainers,
+        FederatedQueryProcessorHelper queryHelper) {
+        this.dataContainers = dataServiceContainers;
+        this.queryHelper = queryHelper;
+    }
+    
+
+    public String getDescription() {
+        return "Tests the aggregation capabilities of the Federated Query Engine";
+    }
+
+
+    protected Vector steps() {
+        Vector<Step> steps = new Vector<Step>();
+        // figure out the URLs of the test services
+        String[] serviceUrls = new String[dataContainers.length];
+        for (int i = 0; i < dataContainers.length; i++) {
+            ServiceContainer container = dataContainers[i].getDataServiceContainer();
+            try {
+                String base = container.getContainerBaseURI().toString();
+                serviceUrls[i] = base + SERVICE_NAME_BASE + String.valueOf(i + 1);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                fail("Error creating data service URL: " + ex.getMessage());
+            }
+        }
+        
+        steps.add(new AggregationStep(QUERIES_LOCATION + File.separator + "exampleAggregation1.xml",
+            GOLD_LOCATION + File.separator + "exampleAggregation1_gold.xml",
+            queryHelper, serviceUrls));
+        return steps;
+    }
+}
---
+++ cagrid-1-0/tests/projects/fqpTests/src/org/cagrid/fqp/test/local/LocalFqpSystemTests.java
@@ -0,0 +1,81 @@
+package org.cagrid.fqp.test.local;
+
+import gov.nih.nci.cagrid.fqp.processor.FederatedQueryEngine;
+import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainer;
+import gov.nih.nci.cagrid.testing.system.deployment.steps.DestroyContainerStep;
+import gov.nih.nci.cagrid.testing.system.deployment.steps.StopContainerStep;
+import gov.nih.nci.cagrid.testing.system.haste.StoryBook;
+
+import java.io.File;
+
+import junit.framework.TestResult;
+
+import org.cagrid.fqp.test.common.AggregationStory;
+import org.cagrid.fqp.test.common.DataServiceContainerSource;
+import org.cagrid.fqp.test.common.DataServiceDeploymentStory;
+import org.cagrid.fqp.test.common.FederatedQueryProcessorHelper;
+
+/** 
+ *  LocalFqpSystemTests
+ *  System tests for FQP using the local Federated Query API
+ * 
+ * @author David Ervin
+ * 
+ * @created Jul 10, 2008 10:57:40 AM
+ * @version $Id: LocalFqpSystemTests.java,v 1.1 2008-07-14 21:10:04 dervin Exp $ 
+ */
+public class LocalFqpSystemTests extends StoryBook {
+    
+    private DataServiceDeploymentStory[] deployments;
+    
+    public LocalFqpSystemTests() {
+        super();
+    }
+    
+
+    protected void stories() {
+        // deploy two example SDK data services which pull from slightly different data
+        DataServiceDeploymentStory exampleService1Deployment = 
+            new DataServiceDeploymentStory(new File("resources/services/ExampleSdkService1.zip"));
+        DataServiceDeploymentStory exampleService2Deployment =
+            new DataServiceDeploymentStory(new File("resources/services/ExampleSdkService2.zip"));
+        deployments = new DataServiceDeploymentStory[] {
+            exampleService1Deployment, exampleService2Deployment
+        };
+        addStory(exampleService1Deployment);
+        addStory(exampleService2Deployment);
+        
+        // run the local aggregation queries
+        DataServiceContainerSource[] containerSources = new DataServiceContainerSource[] {
+            exampleService1Deployment, exampleService2Deployment
+        };
+        FederatedQueryProcessorHelper queryHelper = new FederatedQueryProcessorHelper(new FederatedQueryEngine());
+        AggregationStory aggregationTests = new AggregationStory(containerSources, queryHelper);
+        addStory(aggregationTests);
+    }
+    
+    
+    public void run(TestResult result) {
+        System.out.println("RUNNING TESTS");
+        System.out.println("RUNNING TESTS");
+        System.out.println("RUNNING TESTS");
+        super.run(result);
+        cleanUp();
+    }
+    
+    
+    private void cleanUp() {
+        System.out.println("CLEANING UP TESTING RESOURCES");
+        System.out.println("CLEANING UP TESTING RESOURCES");
+        System.out.println("CLEANING UP TESTING RESOURCES");
+        for (DataServiceDeploymentStory deployment : deployments) {
+            ServiceContainer container = deployment.getDataServiceContainer();
+            try {
+                new StopContainerStep(container).runStep();
+                new DestroyContainerStep(container).runStep();
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
