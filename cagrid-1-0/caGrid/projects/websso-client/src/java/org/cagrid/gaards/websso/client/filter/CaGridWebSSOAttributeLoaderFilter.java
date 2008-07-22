8022dee3b527a4238b1a6ab689b9bb6e14ae2590
garmillas <garmillas>
2008-07-22 14:33:52 +0000

    fixed bug [#15196] CaGridWebSSOAttributeLoaderFilter will not work as designed
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11332 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/websso-client/src/java/org/cagrid/gaards/websso/client/filter/CaGridWebSSOAttributeLoaderFilter.java
+++ cagrid-1-0/caGrid/projects/websso-client/src/java/org/cagrid/gaards/websso/client/filter/CaGridWebSSOAttributeLoaderFilter.java
@@ -1,8 +1,7 @@
 package org.cagrid.gaards.websso.client.filter;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
+import java.util.StringTokenizer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,6 +12,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.jasig.cas.authentication.principal.Principal;
 import org.jasig.cas.client.validation.Assertion;
 import org.jasig.cas.client.web.filter.AbstractCasFilter;
 
@@ -20,6 +20,9 @@ public class CaGridWebSSOAttributeLoaderFilter implements Filter
 {
 
 	public static final String IS_SESSION_ATTRIBUTES_LOADED = "IS_SESSION_ATTRIBUTES_LOADED";
+	public static final String ATTRIBUTE_DELIMITER = "$";
+	public static final String KEY_VALUE_PAIR_DELIMITER = "^";
+	
 	public void destroy()
 	{
 		// do nothing
@@ -32,20 +35,22 @@ public class CaGridWebSSOAttributeLoaderFilter implements Filter
 		if (null == isSessionLoaded || isSessionLoaded == Boolean.FALSE)
 		{
 			Assertion assertion = (Assertion)session.getAttribute(AbstractCasFilter.CONST_ASSERTION);
-			Map<String, String> attributeMap = assertion.getAttributes();
-			Set<String> keySet = attributeMap.keySet();
-			for (String key : keySet)
+			Principal principal = assertion.getPrincipal();
+			String attributesString = principal.getId();
+			
+			StringTokenizer stringTokenizer = new StringTokenizer(attributesString, ATTRIBUTE_DELIMITER);
+			while (stringTokenizer.hasMoreTokens())
 			{
-				String value = attributeMap.get(key);
-				session.setAttribute(key, value);
+				String attributeKeyValuePair = stringTokenizer.nextToken();
+				session.setAttribute(attributeKeyValuePair.substring(0, attributeKeyValuePair.indexOf(KEY_VALUE_PAIR_DELIMITER)), attributeKeyValuePair.substring(attributeKeyValuePair.indexOf(KEY_VALUE_PAIR_DELIMITER) + 1, attributeKeyValuePair.length()));
 			}
-			session.setAttribute(IS_SESSION_ATTRIBUTES_LOADED, Boolean.TRUE);
-		}	
+			session.setAttribute(IS_SESSION_ATTRIBUTES_LOADED, Boolean.TRUE);			
+		}
+		filterChain.doFilter(request, response);
 	}
 
 	public void init(FilterConfig arg0) throws ServletException
 	{
 		// do nothing
 	}
-
 }
