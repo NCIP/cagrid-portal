9ec330c3f4351de117e4c14707f0b6cd8283cf58
dervin <dervin>
2008-07-30 13:38:06 +0000

    now using globus' work manager implementation
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11371 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/fqp/src/gov/nih/nci/cagrid/fqp/processor/CQLAggregator.java
+++ cagrid-1-0/caGrid/projects/fqp/src/gov/nih/nci/cagrid/fqp/processor/CQLAggregator.java
@@ -11,31 +11,35 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.globus.gsi.GlobusCredential;
+import org.globus.wsrf.impl.work.WorkManagerImpl;
+
+import commonj.work.Work;
+import commonj.work.WorkException;
+import commonj.work.WorkItem;
+import commonj.work.WorkManager;
 
 /** 
  *  CQLAggregator
- *  Runs CQL against multiple data services in parallel and aggregates the results
+ *  Performs DCQL Aggregation queries in paralell using the Globus 
+ *  implementation of the commonj work interfaces
  * 
  * @author David Ervin
  * 
- * @created Jun 26, 2008 2:10:45 PM
- * @version $Id: CQLAggregator.java,v 1.2 2008-07-16 20:06:40 dervin Exp $ 
+ * @created Jul 28, 2008 1:16:46 PM
+ * @version $Id: CQLAggregator.java,v 1.3 2008-07-30 13:38:06 dervin Exp $ 
  */
 public class CQLAggregator {
     
-    private static final Log logger = LogFactory.getLog(CQLAggregator.class);
-    
-    public static final int DEFAULT_MAX_WORKER_THREADS = 5;
+    // default number of worker threads
+    public static final int DEFAULT_POOL_SIZE = 5;
 
+    private static Log logger = LogFactory.getLog(CQLAggregator.class);
+    private static WorkManager workManager = null;
+    
     private CQLAggregator() {
         // prevents instantiation
     }
@@ -58,68 +62,55 @@ public class CQLAggregator {
     public static CQLQueryResults aggregateQueryResults(
         CQLQuery query, String[] dataServiceUrls, GlobusCredential credential) 
         throws RemoteDataServiceException, FederatedQueryProcessingException {
-        return aggregateQueryResults(query, dataServiceUrls, credential, DEFAULT_MAX_WORKER_THREADS);
-    }
-    
-    
-    /**
-     * Performs a CQL query aggregation
-     * 
-     * @param query
-     *      The CQL query to aggregate across multiple data services
-     * @param dataServiceUrls
-     *      The URLs of the data services
-     * @param credential
-     *      <b>Can be null.</b> The globus credential to connect with
-     * @param numThreads
-     *      The number of threads to use in conncurent query processing
-     * @return
-     *      The aggregated object results of the CQL query
-     * @throws RemoteDataServiceException
-     * @throws FederatedQueryProcessingException
-     */
-    public static CQLQueryResults aggregateQueryResults(
-        CQLQuery query, String[] dataServiceUrls, GlobusCredential credential, int numThreads) 
-        throws RemoteDataServiceException, FederatedQueryProcessingException {
+        logger.debug("Creating object results list");
         List<CQLObjectResult> objectResults = new LinkedList<CQLObjectResult>();
         
-        // create thread pool executor with max of MAX_WORKER_THREADS
-        logger.debug("Creating CQL Aggregation execution thread pool of size " + numThreads);
-        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-        Map<String, FutureTask<CQLQueryResults>> workers = 
-            new HashMap<String, FutureTask<CQLQueryResults>>();
-        
-        // set up worker tasks for each URL
+        logger.debug("Creating work items for each data service");
+        Map<WorkItem, QueryExecutionContext> workExecution = 
+            new HashMap<WorkItem, QueryExecutionContext>();
         for (String url : dataServiceUrls) {
-            QueryExecutor exec = new QueryExecutor(query, url, credential);
-            FutureTask worker = new FutureTask<CQLQueryResults>(exec);
-            executor.execute(worker);
-            workers.put(url, worker);
+            QueryExecutionContext executionContext = new QueryExecutionContext(query, url, credential);
+            Work executor = new QueryExecutor(executionContext);
+            try {
+                WorkItem workItem = getWorkManager().schedule(executor);
+                workExecution.put(workItem, executionContext);
+            } catch (WorkException ex) {
+                throw new FederatedQueryProcessingException(
+                    "Error scheduling query aggregation work: " + ex.getMessage(), ex);
+            }
         }
         
-        try {
-            // get results from each task
-            for (String service : workers.keySet()) {
-                FutureTask<CQLQueryResults> task = workers.get(service);
-                CQLQueryResults results = task.get();
+        logger.debug("Work scheduled, waiting for completion");
+        getWorkManager().waitForAll(workExecution.keySet(), WorkManager.INDEFINITE);
+        logger.debug("Work completed");
+        
+        // aggregate results from all workers
+        for (WorkItem worker : workExecution.keySet()) {
+            QueryExecutionContext context = workExecution.get(worker);
+            logger.debug("Analyizing results from " + context.getServiceURL());
+            CQLQueryResults results = context.getResults();
+            Exception processingException = context.getProcessingException();
+            if (results != null) {
+                logger.debug("Query returned non-null results");
                 // ensure the query results are of the correct data type
                 if (!results.getTargetClassname().equals(query.getTarget().getName())) {
-                    throw new RemoteDataServiceException("Data service (" + service
+                    throw new RemoteDataServiceException("Data service (" + context.getServiceURL()
                         + ") returned results of type (" + results.getTargetClassname() 
                         + ") when type (" + query.getTarget().getName() + ") was requested!");
                 }
                 if (results.getObjectResult() != null) {
                     // add the results together
                     Collections.addAll(objectResults, results.getObjectResult());
+                } else {
+                    throw new FederatedQueryProcessingException(
+                        "Data service " + context.getServiceURL() + " returned non-object results for the query!");
                 }
+            } else if (processingException != null) {
+                throw new FederatedQueryProcessingException(
+                    "Data service " + context.getServiceURL() + 
+                    " threw a query processing exception: " + processingException.getMessage(), 
+                    processingException);
             }
-        } catch (InterruptedException ex) {
-            throw new FederatedQueryProcessingException("Error executing aggregation in parallel: " + ex.getMessage(), ex);
-        } catch (ExecutionException ex) {
-            throw new FederatedQueryProcessingException("Error executing aggregation in parallel: " + ex.getMessage(), ex);
-        } finally {
-            logger.debug("Shutting down CQL Aggregation execution thread pool");
-            executor.shutdownNow();
         }
         
         // generate the aggregate query result
@@ -133,23 +124,102 @@ public class CQLAggregator {
     }
     
     
-    private static class QueryExecutor implements Callable<CQLQueryResults> {
+    private static WorkManager getWorkManager() {
+        if (workManager == null) {
+            int poolSize = DEFAULT_POOL_SIZE;
+            // TODO: make this configurable, i.e. with a service property
+            workManager = new WorkManagerImpl(poolSize);
+        }
+        return workManager;
+    }
+    
+    
+    public void setWorkManager(WorkManager manager) {
+        workManager = manager;
+    }
+    
+    
+    private static class QueryExecutionContext {
         private CQLQuery query = null;
-        private String serviceURL = null;
+        private CQLQueryResults results = null;
         private GlobusCredential credential = null;
+        private String serviceURL = null;
+        private Exception processingException = null;
         
-        public QueryExecutor(CQLQuery query, String serviceURL, GlobusCredential credential) {
+        public QueryExecutionContext(CQLQuery query, String serviceURL, GlobusCredential credential) {
             this.query = query;
             this.serviceURL = serviceURL;
             this.credential = credential;
         }
+
+
+        public GlobusCredential getCredential() {
+            return credential;
+        }
         
-        
-        public CQLQueryResults call() throws RemoteDataServiceException {
-            logger.debug("CQL Aggregator querying data service " + serviceURL);
-            CQLQueryResults results = DataServiceQueryExecutor.queryDataService(query, serviceURL, credential);
-            logger.debug("CQL Aggregator done with " + serviceURL);
+
+        public Exception getProcessingException() {
+            return processingException;
+        }
+
+
+        public void setProcessingException(Exception processingException) {
+            this.processingException = processingException;
+        }
+
+
+        public CQLQuery getQuery() {
+            return query;
+        }
+
+
+        public CQLQueryResults getResults() {
             return results;
+        }
+
+
+        public void setResults(CQLQueryResults results) {
+            this.results = results;
+        }
+
+
+        public String getServiceURL() {
+            return serviceURL;
+        }
+    }
+    
+    
+    private static class QueryExecutor implements Work {
+        private QueryExecutionContext queryContext = null;
+        
+        public QueryExecutor(QueryExecutionContext queryContext) {
+            this.queryContext = queryContext;
+        }
+        
+        
+        public boolean isDaemon() {
+            return false;
+        }
+        
+        
+        public void release() {
+            // nothing to release
+        }
+        
+        
+        public void run() {
+            logger.debug("CQL Aggregator querying data service " + queryContext.getServiceURL());
+            CQLQueryResults results = null;
+            try {
+                results = DataServiceQueryExecutor.queryDataService(
+                    queryContext.getQuery(), queryContext.getServiceURL(), queryContext.getCredential());
+            } catch (Exception ex) {
+                logger.error("Query processing failed with exception: " + ex.getMessage());
+                queryContext.setProcessingException(ex);
+                ex.printStackTrace();
+            }
+            logger.debug("CQL Aggregator done with " + queryContext.getServiceURL());
+            queryContext.setResults(results);
         }
     }
 }
