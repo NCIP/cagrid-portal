5a27bb7e5aa005af9df52b6076b5243db672bcfb
kherm <kherm>
2008-08-19 17:56:48 +0000

    Fix for Bug 16187
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11461 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/discovery/details/ViewServiceDetailsController.java
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/discovery/details/ViewServiceDetailsController.java
@@ -1,11 +1,12 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.discovery.details;
 
 import gov.nih.nci.cagrid.portal.dao.GridServiceDao;
 import gov.nih.nci.cagrid.portal.domain.GridService;
 import gov.nih.nci.cagrid.portal.portlet.AbstractViewObjectController;
+import gov.nih.nci.cagrid.portal.portlet.discovery.filter.ServiceFilter;
 import gov.nih.nci.cagrid.portal.portlet.tree.TreeFacade;
 import gov.nih.nci.cagrid.portal.portlet.tree.TreeNode;
 
@@ -22,74 +23,84 @@ import org.springframework.web.portlet.ModelAndView;
  *
  */
 public class ViewServiceDetailsController extends AbstractDiscoveryViewObjectController {
-	
-	
-	private TreeFacade serviceDetailsTreeFacade;
-	
-	private ServiceMetadataTreeNodeListener serviceMetadataTreeNodeListener;
-	
-	private GridServiceDao gridServiceDao;
-
-	/**
-	 * 
-	 */
-	public ViewServiceDetailsController() {
-
-	}
-	
-	protected Object getObject(RenderRequest request){
-
-		TreeNode rootNode = null;
-		GridService gridService = getDiscoveryModel().getSelectedService();
-		if (gridService != null) {
-			//Associate with current session
-			gridService = getGridServiceDao().getById(gridService.getId());
-			rootNode = getServiceDetailsTreeFacade().getRootNode();
-			if(rootNode == null || !((GridService)rootNode.getContent()).getId().equals(gridService.getId())){
-				logger.debug("Creating new tree for gridService:" + gridService.getId());
-				rootNode = createRootNode(gridService);
-			}
-			getServiceDetailsTreeFacade().setRootNode(rootNode);
-		} else {
-			logger.debug("No grid service selected.");
-		}
-		return rootNode;
-	}
-	
-	private TreeNode createRootNode(GridService gridService) {
-		TreeNode rootNode = new TreeNode(null, "gridService");
-		rootNode.setContent(gridService);
-		getServiceMetadataTreeNodeListener().handleGridServiceNode(rootNode, new HashMap(), gridService);
-		return rootNode;
-	}
-
-	@Required
-	public TreeFacade getServiceDetailsTreeFacade() {
-		return serviceDetailsTreeFacade;
-	}
-
-	public void setServiceDetailsTreeFacade(TreeFacade treeFacade) {
-		this.serviceDetailsTreeFacade = treeFacade;
-	}
-
-	@Required
-	public ServiceMetadataTreeNodeListener getServiceMetadataTreeNodeListener() {
-		return serviceMetadataTreeNodeListener;
-	}
-
-	public void setServiceMetadataTreeNodeListener(
-			ServiceMetadataTreeNodeListener serviceMetadataTreeNodeListener) {
-		this.serviceMetadataTreeNodeListener = serviceMetadataTreeNodeListener;
-	}
-	
 
 
-	public GridServiceDao getGridServiceDao() {
-		return gridServiceDao;
-	}
+    private TreeFacade serviceDetailsTreeFacade;
 
-	public void setGridServiceDao(GridServiceDao gridServiceDao) {
-		this.gridServiceDao = gridServiceDao;
-	}
+    private ServiceMetadataTreeNodeListener serviceMetadataTreeNodeListener;
 
+    private GridServiceDao gridServiceDao;
+    private ServiceFilter servicefilter;
+
+    /**
+     *
+     */
+    public ViewServiceDetailsController() {
+
+    }
+
+    protected Object getObject(RenderRequest request){
+
+        TreeNode rootNode = null;
+        GridService gridService = getDiscoveryModel().getSelectedService();
+        if (gridService != null) {
+            //Associate with current session
+            gridService = getGridServiceDao().getById(gridService.getId());
+            if(servicefilter.willBeFiltered(gridService)){
+                logger.debug("Service should be filtered. Will not return Grid Service details");
+                return null;
+            }
+            rootNode = getServiceDetailsTreeFacade().getRootNode();
+            logger.debug("Creating new tree for gridService:" + gridService.getId());
+            rootNode = createRootNode(gridService);
+            getServiceDetailsTreeFacade().setRootNode(rootNode);
+        } else {
+            logger.debug("No grid service selected.");
+        }
+        return rootNode;
+    }
+
+    private TreeNode createRootNode(GridService gridService) {
+        TreeNode rootNode = new TreeNode(null, "gridService");
+        rootNode.setContent(gridService);
+        getServiceMetadataTreeNodeListener().handleGridServiceNode(rootNode, new HashMap(), gridService);
+        return rootNode;
+    }
+
+    @Required
+    public TreeFacade getServiceDetailsTreeFacade() {
+        return serviceDetailsTreeFacade;
+    }
+
+    public void setServiceDetailsTreeFacade(TreeFacade treeFacade) {
+        this.serviceDetailsTreeFacade = treeFacade;
+    }
+
+    @Required
+    public ServiceMetadataTreeNodeListener getServiceMetadataTreeNodeListener() {
+        return serviceMetadataTreeNodeListener;
+    }
+
+    public void setServiceMetadataTreeNodeListener(
+            ServiceMetadataTreeNodeListener serviceMetadataTreeNodeListener) {
+        this.serviceMetadataTreeNodeListener = serviceMetadataTreeNodeListener;
+    }
+
+
+
+    public GridServiceDao getGridServiceDao() {
+        return gridServiceDao;
+    }
+
+    public void setGridServiceDao(GridServiceDao gridServiceDao) {
+        this.gridServiceDao = gridServiceDao;
+    }
+
+    public ServiceFilter getServicefilter() {
+        return servicefilter;
+    }
+
+    public void setServicefilter(ServiceFilter servicefilter) {
+        this.servicefilter = servicefilter;
+    }
 }
