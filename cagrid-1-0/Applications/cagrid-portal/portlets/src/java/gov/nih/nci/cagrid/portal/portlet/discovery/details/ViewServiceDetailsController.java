a78802f3a6c5c6bb2a06e113b76005d0e278e42e
kherm <kherm>
2008-08-21 16:24:45 +0000

    only update root content not entire tree
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11485 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/discovery/details/ViewServiceDetailsController.java
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/discovery/details/ViewServiceDetailsController.java
@@ -5,22 +5,16 @@ package gov.nih.nci.cagrid.portal.portlet.discovery.details;
 
 import gov.nih.nci.cagrid.portal.dao.GridServiceDao;
 import gov.nih.nci.cagrid.portal.domain.GridService;
-import gov.nih.nci.cagrid.portal.portlet.AbstractViewObjectController;
 import gov.nih.nci.cagrid.portal.portlet.discovery.filter.ServiceFilter;
 import gov.nih.nci.cagrid.portal.portlet.tree.TreeFacade;
 import gov.nih.nci.cagrid.portal.portlet.tree.TreeNode;
-
-import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Required;
 
 import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.web.portlet.ModelAndView;
+import java.util.HashMap;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- *
  */
 public class ViewServiceDetailsController extends AbstractDiscoveryViewObjectController {
 
@@ -39,20 +33,26 @@ public class ViewServiceDetailsController extends AbstractDiscoveryViewObjectCon
 
     }
 
-    protected Object getObject(RenderRequest request){
+    protected Object getObject(RenderRequest request) {
 
         TreeNode rootNode = null;
         GridService gridService = getDiscoveryModel().getSelectedService();
         if (gridService != null) {
             //Associate with current session
             gridService = getGridServiceDao().getById(gridService.getId());
-            if(servicefilter.willBeFiltered(gridService)){
+            if (servicefilter.willBeFiltered(gridService)) {
                 logger.debug("Service should be filtered. Will not return Grid Service details");
                 return null;
             }
             rootNode = getServiceDetailsTreeFacade().getRootNode();
-            logger.debug("Creating new tree for gridService:" + gridService.getId());
-            rootNode = createRootNode(gridService);
+            if (rootNode == null || !((GridService) rootNode.getContent()).getId().equals(gridService.getId())) {
+                logger.debug("Creating new tree for gridService:" + gridService.getId());
+                rootNode = createRootNode(gridService);
+            }
+            //this will refresh basic details like service
+            // status which can change more frequently 
+            rootNode.setContent(gridService);
+
             getServiceDetailsTreeFacade().setRootNode(rootNode);
         } else {
             logger.debug("No grid service selected.");
@@ -62,7 +62,6 @@ public class ViewServiceDetailsController extends AbstractDiscoveryViewObjectCon
 
     private TreeNode createRootNode(GridService gridService) {
         TreeNode rootNode = new TreeNode(null, "gridService");
-        rootNode.setContent(gridService);
         getServiceMetadataTreeNodeListener().handleGridServiceNode(rootNode, new HashMap(), gridService);
         return rootNode;
     }
@@ -85,7 +84,6 @@ public class ViewServiceDetailsController extends AbstractDiscoveryViewObjectCon
             ServiceMetadataTreeNodeListener serviceMetadataTreeNodeListener) {
         this.serviceMetadataTreeNodeListener = serviceMetadataTreeNodeListener;
     }
-
 
 
     public GridServiceDao getGridServiceDao() {
