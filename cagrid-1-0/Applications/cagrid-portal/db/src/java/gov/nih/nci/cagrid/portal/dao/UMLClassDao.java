5ae66d3704172fbddf103dd543448b0e40f0f150
kherm <kherm>
2008-09-05 17:04:34 +0000

    bug fix 16415
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-2-0-2_release@11567 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/dao/UMLClassDao.java
+++ cagrid-1-0/Applications/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/dao/UMLClassDao.java
@@ -3,6 +3,7 @@
  */
 package gov.nih.nci.cagrid.portal.dao;
 
+import gov.nih.nci.cagrid.portal.domain.ServiceStatus;
 import gov.nih.nci.cagrid.portal.domain.metadata.common.SemanticMetadata;
 import gov.nih.nci.cagrid.portal.domain.metadata.common.UMLAttribute;
 import gov.nih.nci.cagrid.portal.domain.metadata.dataservice.*;
@@ -20,207 +21,221 @@ import java.util.*;
  * @author <a href="joshua.phillips@semanticbits.com">Joshua Phillips</a>
  */
 public class UMLClassDao extends AbstractDao<UMLClass> {
-	public int maxResultSize = 10;
+    public int maxResultSize = 10;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see gov.nih.nci.cagrid.portal.dao.AbstractDao#domainClass()
-	 */
-	@Override
-	public Class domainClass() {
-		return UMLClass.class;
-	}
+    /*
+      * (non-Javadoc)
+      *
+      * @see gov.nih.nci.cagrid.portal.dao.AbstractDao#domainClass()
+      */
+    @Override
+    public Class domainClass() {
+        return UMLClass.class;
+    }
 
-	/**
-	 * Will get a list of same UMLClasses from other models
-	 * 
-	 * @param example
-	 * @return ToDo refactor to SBE?
-	 */
-	public List<UMLClass> getSameClassesInDifferentModel(final UMLClass example) {
-		List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
-				.execute(new HibernateCallback() {
-					public Object doInHibernate(Session session)
-							throws HibernateException, SQLException {
-						return session.createCriteria(UMLClass.class).add(
-								Restrictions.eq("className", example
-										.getClassName())).add(
-								Restrictions.eq("packageName", example
-										.getPackageName())).add(
-								Restrictions.eq("projectName", example
-										.getProjectName())).add(
-								Restrictions.ne("model.id", example.getModel()
-										.getId())).setResultTransformer(
-								Criteria.DISTINCT_ROOT_ENTITY).setMaxResults(
-								maxResultSize).list();
-					}
-				});
-		return resultSet;
-	}
+    /**
+     * Will get a list of same UMLClasses from other models
+     *
+     * @param example
+     * @return ToDo refactor to SBE?
+     */
+    public List<UMLClass> getSameClassesInDifferentModel(final UMLClass example) {
+        List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
+                .execute(new HibernateCallback() {
+                    public Object doInHibernate(Session session)
+                            throws HibernateException, SQLException {
+                        return session.createCriteria(UMLClass.class).add(
+                                Restrictions.eq("className", example
+                                        .getClassName())).add(
+                                Restrictions.eq("packageName", example
+                                        .getPackageName())).add(
+                                Restrictions.eq("projectName", example
+                                        .getProjectName())).add(
+                                Restrictions.ne("model.id", example.getModel()
+                                        .getId())).setResultTransformer(
+                                Criteria.DISTINCT_ROOT_ENTITY).setMaxResults(
+                                maxResultSize).list();
+                    }
+                });
+        return resultSet;
+    }
 
-	public List<UMLClass> getClassesWithSameConceptCode(final UMLClass example) {
-		List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
-				.execute(new HibernateCallback() {
-					public Object doInHibernate(Session session)
-							throws HibernateException, SQLException {
-						Set<String> codes = new HashSet<String>();
-						for (SemanticMetadata sm : example
-								.getSemanticMetadata()) {
-							codes.add(sm.getConceptCode());
-						}
-						List<UMLClass> l = new ArrayList<UMLClass>();
-						if (codes.size() > 0) {
-							l = session
-									.createCriteria(UMLClass.class)
-									.add(Restrictions.ne("id", example.getId()))
-									// not from the same model
-									.add(
-											Restrictions.ne("model.id", example
-													.getModel().getId()))
-									.addOrder(Order.asc("className"))
-									.createCriteria("semanticMetadata").add(
-											Restrictions.in("conceptCode",
-													codes))
-									.setResultTransformer(
-											Criteria.DISTINCT_ROOT_ENTITY)
-									.setMaxResults(maxResultSize).list();
-						}
-						return l;
-					}
-				});
-		return resultSet;
-	}
+    public List<UMLClass> getClassesWithSameConceptCode(final UMLClass example) {
+        List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
+                .execute(new HibernateCallback() {
+                    public Object doInHibernate(Session session)
+                            throws HibernateException, SQLException {
+                        Set<String> codes = new HashSet<String>();
+                        for (SemanticMetadata sm : example
+                                .getSemanticMetadata()) {
+                            codes.add(sm.getConceptCode());
+                        }
+                        List<UMLClass> l = new ArrayList<UMLClass>();
+                        if (codes.size() > 0) {
+                            l = session
+                                    .createCriteria(UMLClass.class)
+                                    .add(Restrictions.ne("id", example.getId()))
+                                            // not from the same model
+                                    .add(
+                                            Restrictions.ne("model.id", example
+                                                    .getModel().getId()))
+                                    .addOrder(Order.asc("className"))
+                                    .createCriteria("semanticMetadata").add(
+                                    Restrictions.in("conceptCode",
+                                            codes))
+                                    .setResultTransformer(
+                                            Criteria.DISTINCT_ROOT_ENTITY)
+                                    .list();
+                        }
+                        return l;
+                    }
+                });
 
-	public List<UMLClass> getSemanticallyEquivalentClassesBasedOnAssociations(
-			final UMLClass umlClass) {
-		Map<Integer, UMLClass> classes = new HashMap<Integer, UMLClass>();
+        List<UMLClass> classes = new ArrayList<UMLClass>();
 
-		for (UMLAssociationEdge edge : umlClass.getAssociations()) {
-			if (edge instanceof SourceUMLAssociationEdge) {
-				UMLAssociation assoc = ((SourceUMLAssociationEdge) edge)
-						.getAssociation();
-				TargetUMLAssociationEdge target = assoc.getTarget();
-				final String code;
-				try {
-					code = target.getType().getSemanticMetadata().get(0)
-							.getConceptCode();
+        try {
+            for (UMLClass kclass : resultSet) {
+                if (kclass.getModel().getService().getCurrentStatus().equals(ServiceStatus.ACTIVE))
+                    classes.add(kclass);
+                if (classes.size() >= maxResultSize)
+                    break;
+            }
+        } catch (Exception e) {
+            logger.error("Error filtering resultset" + e.getMessage());
+        }
 
-					List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
-							.execute(new HibernateCallback() {
-								public Object doInHibernate(Session session)
-										throws HibernateException, SQLException {
+        return classes;
+    }
 
-									return session
-											.createCriteria(UMLClass.class)
-											.createAlias("model", "m")
-											.add(
-													Restrictions.ne("id",
-															umlClass.getId()))
-											.add(
-													Restrictions.ne("m.id",
-															umlClass.getModel()
-																	.getId()))
-											.add(
-													Restrictions
-															.eq(
-																	"allowableAsTarget",
-																	Boolean.TRUE))
-											.createCriteria("associations")
-											.createCriteria("type")
-											.createCriteria("semanticMetadata")
-											.add(
-													Restrictions
-															.eq("conceptCode",
-																	code))
-											.setResultTransformer(
-													Criteria.DISTINCT_ROOT_ENTITY)
-											.setMaxResults(maxResultSize)
-											.list();
-								}
-							});
+    public List<UMLClass> getSemanticallyEquivalentClassesBasedOnAssociations(
+            final UMLClass umlClass) {
+        Map<Integer, UMLClass> classes = new HashMap<Integer, UMLClass>();
 
-					logger.debug("Retreived " + resultSet.size()
-							+ " attributes   for concept code '" + code + "'.");
+        for (UMLAssociationEdge edge : umlClass.getAssociations()) {
+            if (edge instanceof SourceUMLAssociationEdge) {
+                UMLAssociation assoc = ((SourceUMLAssociationEdge) edge)
+                        .getAssociation();
+                TargetUMLAssociationEdge target = assoc.getTarget();
+                final String code;
+                try {
+                    code = target.getType().getSemanticMetadata().get(0)
+                            .getConceptCode();
 
-					for (UMLClass kclass : resultSet) {
-						if (!classes.containsKey(kclass.getId()))
-							classes.put(kclass.getId(), kclass);
-					}
-				} catch (Exception e) {
-					logger.warn("Exception getting concept code for UMLClass:"
-							+ umlClass.getId());
-				}
-			}
-		}
-		List<UMLClass> returnList = new ArrayList<UMLClass>();
-		returnList.addAll(classes.values());
-		return returnList;
-	}
+                    List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
+                            .execute(new HibernateCallback() {
+                                public Object doInHibernate(Session session)
+                                        throws HibernateException, SQLException {
 
-	public List<UMLClass> getSemanticalyEquivalentClassesBasedOnAtrributes(
-			final UMLClass umlClass) {
+                                    return session
+                                            .createCriteria(UMLClass.class)
+                                            .createAlias("model", "m")
+                                            .add(
+                                                    Restrictions.ne("id",
+                                                            umlClass.getId()))
+                                            .add(
+                                                    Restrictions.ne("m.id",
+                                                            umlClass.getModel()
+                                                                    .getId()))
+                                            .add(
+                                                    Restrictions
+                                                            .eq(
+                                                            "allowableAsTarget",
+                                                            Boolean.TRUE))
+                                            .createCriteria("associations")
+                                            .createCriteria("type")
+                                            .createCriteria("semanticMetadata")
+                                            .add(
+                                                    Restrictions
+                                                            .eq("conceptCode",
+                                                            code))
+                                            .setResultTransformer(
+                                                    Criteria.DISTINCT_ROOT_ENTITY)
+                                            .setMaxResults(maxResultSize)
+                                            .list();
+                                }
+                            });
 
-		List<UMLClass> classes = new ArrayList<UMLClass>();
+                    logger.debug("Retreived " + resultSet.size()
+                            + " attributes   for concept code '" + code + "'.");
 
-		for (UMLAttribute attr : umlClass.getUmlAttributeCollection()) {
-			final String code = attr.getSemanticMetadata().get(0)
-					.getConceptCode();
-			List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
-					.execute(new HibernateCallback() {
-						public Object doInHibernate(Session session)
-								throws HibernateException, SQLException {
+                    for (UMLClass kclass : resultSet) {
+                        if (!classes.containsKey(kclass.getId()))
+                            classes.put(kclass.getId(), kclass);
+                    }
+                } catch (Exception e) {
+                    logger.warn("Exception getting concept code for UMLClass:"
+                            + umlClass.getId());
+                }
+            }
+        }
+        List<UMLClass> returnList = new ArrayList<UMLClass>();
+        returnList.addAll(classes.values());
+        return returnList;
+    }
 
-							return session.createCriteria(UMLClass.class)
-									.createAlias("model", "m").add(
-											Restrictions.ne("id", umlClass
-													.getId())).add(
-											Restrictions.ne("m.id", umlClass
-													.getModel().getId())).add(
-											Restrictions.eq(
-													"allowableAsTarget",
-													Boolean.TRUE))
-									.createCriteria("umlAttributeCollection")
-									.createCriteria("semanticMetadata").add(
-											Restrictions
-													.eq("conceptCode", code))
-									.setResultTransformer(
-											Criteria.DISTINCT_ROOT_ENTITY)
-									.setMaxResults(maxResultSize).list();
-						}
-					});
-			logger.debug("Retreived " + resultSet.size()
-					+ " attributes   for concept code '" + code + "'.");
-			classes.addAll(resultSet);
-		}
-		return classes;
-	}
+    public List<UMLClass> getSemanticalyEquivalentClassesBasedOnAtrributes(
+            final UMLClass umlClass) {
 
-	public UMLClass getUmlClassFromModel(final DomainModel domainModel,
-			final String umlClassName) {
-		return (UMLClass) getHibernateTemplate().execute(
-				new HibernateCallback() {
-					public Object doInHibernate(Session session)
-							throws HibernateException, SQLException {
-						UMLClass umlClass = null;
-						for (UMLClass klass : domainModel.getClasses()) {
-							String className = klass.getPackageName() + "."
-									+ klass.getClassName();
-							if (className.equals(umlClassName)) {
-								umlClass = klass;
-								break;
-							}
-						}
-						return umlClass;
-					}
-				});
-	}
+        List<UMLClass> classes = new ArrayList<UMLClass>();
 
-	public int getMaxResultSize() {
-		return maxResultSize;
-	}
+        for (UMLAttribute attr : umlClass.getUmlAttributeCollection()) {
+            final String code = attr.getSemanticMetadata().get(0)
+                    .getConceptCode();
+            List<UMLClass> resultSet = (List<UMLClass>) getHibernateTemplate()
+                    .execute(new HibernateCallback() {
+                        public Object doInHibernate(Session session)
+                                throws HibernateException, SQLException {
 
-	public void setMaxResultSize(int maxResultSize) {
-		this.maxResultSize = maxResultSize;
-	}
+                            return session.createCriteria(UMLClass.class)
+                                    .createAlias("model", "m").add(
+                                    Restrictions.ne("id", umlClass
+                                            .getId())).add(
+                                    Restrictions.ne("m.id", umlClass
+                                            .getModel().getId())).add(
+                                    Restrictions.eq(
+                                            "allowableAsTarget",
+                                            Boolean.TRUE))
+                                    .createCriteria("umlAttributeCollection")
+                                    .createCriteria("semanticMetadata").add(
+                                    Restrictions
+                                            .eq("conceptCode", code))
+                                    .setResultTransformer(
+                                            Criteria.DISTINCT_ROOT_ENTITY)
+                                    .setMaxResults(maxResultSize).list();
+                        }
+                    });
+            logger.debug("Retreived " + resultSet.size()
+                    + " attributes   for concept code '" + code + "'.");
+            classes.addAll(resultSet);
+        }
+        return classes;
+    }
+
+    public UMLClass getUmlClassFromModel(final DomainModel domainModel,
+                                         final String umlClassName) {
+        return (UMLClass) getHibernateTemplate().execute(
+                new HibernateCallback() {
+                    public Object doInHibernate(Session session)
+                            throws HibernateException, SQLException {
+                        UMLClass umlClass = null;
+                        for (UMLClass klass : domainModel.getClasses()) {
+                            String className = klass.getPackageName() + "."
+                                    + klass.getClassName();
+                            if (className.equals(umlClassName)) {
+                                umlClass = klass;
+                                break;
+                            }
+                        }
+                        return umlClass;
+                    }
+                });
+    }
+
+    public int getMaxResultSize() {
+        return maxResultSize;
+    }
+
+    public void setMaxResultSize(int maxResultSize) {
+        this.maxResultSize = maxResultSize;
+    }
 }
