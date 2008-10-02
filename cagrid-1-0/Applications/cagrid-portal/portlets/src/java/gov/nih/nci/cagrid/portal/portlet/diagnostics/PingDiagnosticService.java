461ee9309923f1c402a578b47e9623ecca792f89
kherm <kherm>
2008-10-02 18:22:24 +0000

    updated text
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-2-1_release@11750 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/diagnostics/PingDiagnosticService.java
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/diagnostics/PingDiagnosticService.java
@@ -24,7 +24,7 @@ public class PingDiagnosticService extends StatusDiagnosticService {
             _result.setMessage("Sucessfully pinged service");
         else {
             _result.setMessage("Failed to PING service");
-            _result.setDetail("Failed to get service WSDL from <i><b>" + Url + "?wsdl </i></b><br/> Please make sure that the URL is correct and is reachable from the internet.");
+            _result.setDetail("Failed to retreive a <b>valid</b> service WSDL from <i><b>" + Url + "?wsdl </i></b><br/> Please make sure that the URL is correct and is reachable from the internet.");
         }
 
         return _result;
