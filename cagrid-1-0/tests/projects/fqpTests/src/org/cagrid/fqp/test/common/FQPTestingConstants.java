daa55771e73307c09588022c3d488ebe2915bd3a
dervin <dervin>
2008-10-09 16:34:40 +0000

    refactoring out useful constants
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11839 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/fqpTests/src/org/cagrid/fqp/test/common/FQPTestingConstants.java
+++ cagrid-1-0/tests/projects/fqpTests/src/org/cagrid/fqp/test/common/FQPTestingConstants.java
@@ -9,5 +9,20 @@ public interface FQPTestingConstants {
         "resources" + File.separator + "queries" + File.separator;
     public static final String GOLD_LOCATION = 
         "resources" + File.separator + "gold" + File.separator;
+    
+    /**
+     * Controled by a property in jndi-config.xml
+     */
+    public static final long RESOURCE_SWEEPER_DELAY = 2000;
+    
+    /**
+     * Number of times to try the isProcessingComplete() method
+     */
+    public static final int PROCESSING_WAIT_RETRIES = 20;
+    
+    /**
+     * ms delay between successive calls to isProcessingComplete()
+     */
+    public static final long PROCESSING_RETRY_DELAY = 500;
 
 }
