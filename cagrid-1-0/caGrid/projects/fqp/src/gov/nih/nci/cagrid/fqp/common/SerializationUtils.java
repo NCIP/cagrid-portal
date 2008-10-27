44fd0cd7f2c2fa9ea525f517f1c15c6275e53032
dervin <dervin>
2008-10-27 19:09:30 +0000

    added some helper methods for serializing [D]CQL query results (feature request #14933)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@11961 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/fqp/src/gov/nih/nci/cagrid/fqp/common/SerializationUtils.java
+++ cagrid-1-0/caGrid/projects/fqp/src/gov/nih/nci/cagrid/fqp/common/SerializationUtils.java
@@ -2,7 +2,10 @@ package gov.nih.nci.cagrid.fqp.common;
 
 import gov.nih.nci.cagrid.common.Utils;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
+import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
+import gov.nih.nci.cagrid.data.DataServiceConstants;
 import gov.nih.nci.cagrid.dcql.DCQLQuery;
+import gov.nih.nci.cagrid.dcqlresult.DCQLQueryResultsCollection;
 
 import java.io.InputStream;
 import java.io.Writer;
@@ -12,13 +15,16 @@ import org.globus.wsrf.encoding.ObjectDeserializer;
 
 
 /**
- * @author oster
+ * SerializationUtils
+ * Utilities for serializing and deserializing CQL and DCQL queries and results
  * 
+ * @author oster
+ * @author dervin
  */
 public class SerializationUtils {
 
 	/**
-	 * Write the XML representation of the specified metadata to the specified
+	 * Write the XML representation of the specified query to the specified
 	 * writer. If either are null, an IllegalArgumentException will be thown.
 	 * 
 	 * @param cqlQuery
@@ -34,8 +40,8 @@ public class SerializationUtils {
 
 
 	/**
-	 * Create an instance of CQLQuery from the specified inputstream. The stream
-	 * must contains an XML representation of the CQLQuery. If the reader is
+	 * Create an instance of CQLQuery from the specified input stream. The stream
+	 * must contain an XML representation of the CQLQuery. If the reader is
 	 * null, an IllegalArgumentException will be thown.
 	 * 
 	 * @param xmlStream
@@ -54,7 +60,7 @@ public class SerializationUtils {
 
 
 	/**
-	 * Write the XML representation of the specified metadata to the specified
+	 * Write the XML representation of the specified query to the specified
 	 * writer. If either are null, an IllegalArgumentException will be thown.
 	 * 
 	 * @param dcqlQuery
@@ -70,8 +76,8 @@ public class SerializationUtils {
 
 
 	/**
-	 * Create an instance of DCQLQuery from the specified inputstream. The
-	 * stream must contains an XML representation of the DCQLQuery. If the
+	 * Create an instance of DCQLQuery from the specified input stream. The
+	 * stream must contain an XML representation of the DCQLQuery. If the
 	 * reader is null, an IllegalArgumentException will be thown.
 	 * 
 	 * @param xmlStream
@@ -87,4 +93,74 @@ public class SerializationUtils {
 		org.w3c.dom.Document doc = XMLUtils.newDocument(xmlStream);
 		return (DCQLQuery) ObjectDeserializer.toObject(doc.getDocumentElement(), DCQLQuery.class);
 	}
+    
+    
+    /**
+     * Write the XML representation of the specified query results to the specified
+     * writer. If either are null, an IllegalArgumentException will be thown.
+     * 
+     * @param results
+     * @param writer
+     * @throws Exception
+     */
+    public static void serializeCQLQueryResults(CQLQueryResults results, Writer writer) throws Exception {
+        serializeCQLQueryResults(results, writer, null);
+    }
+    
+    
+    /**
+     * Write the XML representation of the specified query results to the specified
+     * writer, using the configuration specified by the WSDD configuration.
+     * If either the results or writer are null, an IllegalArgumentException will be thown.
+     * 
+     * @param results
+     * @param writer
+     * @param wsddStream
+     * @throws Exception
+     */
+    public static void serializeCQLQueryResults(CQLQueryResults results, Writer writer, InputStream wsddStream) throws Exception {
+        if (results == null || writer == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        if (wsddStream == null) {
+            Utils.serializeObject(results, DataServiceConstants.CQL_RESULT_SET_QNAME, writer);
+        } else {
+            Utils.serializeObject(results, DataServiceConstants.CQL_RESULT_SET_QNAME, writer, wsddStream);
+        }
+    }
+    
+    
+    /**
+     * Write the XML representation of the specified query results to the specified
+     * writer. If either are null, an IllegalArgumentException will be thown.
+     * 
+     * @param results
+     * @param writer
+     * @throws Exception
+     */
+    public static void serializeDCQLQueryResults(DCQLQueryResultsCollection results, Writer writer) throws Exception {
+        serializeDCQLQueryResults(results, writer, null);
+    }
+    
+    
+    /**
+     * Write the XML representation of the specified query results to the specified
+     * writer, using the configuration specified by the WSDD configuration.
+     * If either the results or writer are null, an IllegalArgumentException will be thown.
+     * 
+     * @param results
+     * @param writer
+     * @param wsddStream
+     * @throws Exception
+     */
+    public static void serializeDCQLQueryResults(DCQLQueryResultsCollection results, Writer writer, InputStream wsddStream) throws Exception {
+        if (results == null || writer == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        if (wsddStream == null) {
+            Utils.serializeObject(results, DCQLConstants.DCQL_RESULTS_QNAME, writer);
+        } else {
+            Utils.serializeObject(results, DCQLConstants.DCQL_RESULTS_QNAME, writer, wsddStream);
+        }
+    }
 }
