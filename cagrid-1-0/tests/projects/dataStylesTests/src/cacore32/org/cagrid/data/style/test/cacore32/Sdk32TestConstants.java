0fc46905aedd4ac31013385c54c163406a81ae72
dervin <dervin>
2008-11-04 16:14:53 +0000

    some testing constants
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12018 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/tests/projects/dataStylesTests/src/cacore32/org/cagrid/data/style/test/cacore32/Sdk32TestConstants.java
@@ -0,0 +1,10 @@
+package org.cagrid.data.style.test.cacore32;
+
+import java.io.File;
+
+public interface Sdk32TestConstants {
+
+    public static final String RESOURCES_DIR = "resources" + File.separator + "sdk3.2.1";
+    public static final String SDK_PACKAGE_ZIP = RESOURCES_DIR + File.separator + "sdk_321_package.zip";
+    public static final String EXTENSION_DATA_DOCUMENT = RESOURCES_DIR + File.separator + "extensionData.xml";
+}
