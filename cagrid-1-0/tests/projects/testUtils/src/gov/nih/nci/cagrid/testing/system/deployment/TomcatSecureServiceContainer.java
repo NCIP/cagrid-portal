7b88b5b7cf9a6f15272d49ac0ef2f64af81f1484
dervin <dervin>
2008-11-07 19:03:14 +0000

    reconfigures the containers security descriptor to use the testing credentials
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12071 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/testUtils/src/gov/nih/nci/cagrid/testing/system/deployment/TomcatSecureServiceContainer.java
+++ cagrid-1-0/tests/projects/testUtils/src/gov/nih/nci/cagrid/testing/system/deployment/TomcatSecureServiceContainer.java
@@ -1,10 +1,17 @@
 package gov.nih.nci.cagrid.testing.system.deployment;
 
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.common.XMLUtilities;
+
 import java.io.File;
+
+import org.jdom.Document;
+import org.jdom.Element;
 
 
 /**
- * TomcatServiceContainer Service container implementation for tomcat
+ * TomcatServiceContainer 
+ * Service container implementation for tomcat with security
  * 
  * @author David Ervin
  * @created Oct 19, 2007 12:01:22 PM
@@ -12,14 +19,62 @@ import java.io.File;
  *          Exp $
  */
 public class TomcatSecureServiceContainer extends TomcatServiceContainer implements SecureContainer {
-
+    public static final String SECURITY_DESCRIPTOR_PLACEHOLDER = "<!-- @CONTAINER_SECURITY_DESCRIPTOR@ -->";
+    public static final String DESCRIPTOR_FILE_PLACEHOLDER = "@@LOCATION@@";
+    public static final String SECURITY_DESCRIPTOR_PARAMETER_TEMPLATE = "" +
+            "<parameter name=\"containerSecDesc\"\n" +
+            "\t\tvalue=\"" + DESCRIPTOR_FILE_PLACEHOLDER + "\"/>";
+    
     public TomcatSecureServiceContainer(ContainerProperties properties) {
         super(properties);
-
+    }
+    
+    
+    public void unpackContainer() throws ContainerException {
+        super.unpackContainer();
+        File wsrfCoreDir = new File(getProperties().getContainerDirectory(), 
+            "webapps/wsrf/WEB-INF/etc/globus_wsrf_core/");
+        // locate the security descriptor file
+        File globalSecurityDescriptorFile = new File(wsrfCoreDir, "global_security_descriptor.xml");
+        Document descriptorDocument = null;
+        try {
+            descriptorDocument = XMLUtilities.fileNameToDocument(
+                globalSecurityDescriptorFile.getCanonicalPath());
+        } catch (Exception ex) {
+            throw new ContainerException("Error loading global security descriptor: " + ex.getMessage(), ex);
+        }
+        // fix the security configuration
+        Element securityConfigElement = descriptorDocument.getRootElement();
+        Element credentialElement = securityConfigElement.getChild("credential", securityConfigElement.getNamespace());
+        Element keyFileElement = credentialElement.getChild("key-file", credentialElement.getNamespace());
+        keyFileElement.setAttribute("value", getCertificatesDirectory().getAbsolutePath() + File.separator + "ca" + File.separator + "testing_ca_key.pem");
+        Element certFileElement = credentialElement.getChild("cert-file", credentialElement.getNamespace());
+        certFileElement.setAttribute("value", getCertificatesDirectory().getAbsolutePath() + File.separator + "ca" + File.separator + "testing_ca_cert.pem");
+        // write the config back to disk
+        try {
+            String fixedConfig = XMLUtilities.formatXML(XMLUtilities.documentToString(descriptorDocument));
+            Utils.stringBufferToFile(new StringBuffer(fixedConfig), globalSecurityDescriptorFile.getCanonicalPath());
+        } catch (Exception ex) {
+            throw new ContainerException("Error writting edited global security descriptor: " + ex.getMessage(), ex);
+        }
+        
+        // edit the server-config.wsdd to use the modified global security descriptor.
+        File serverWsdd = new File(wsrfCoreDir, "server-config.wsdd");
+        StringBuffer wsddContents = null;
+        try {
+            wsddContents = Utils.fileToStringBuffer(serverWsdd);
+            String parameter = SECURITY_DESCRIPTOR_PARAMETER_TEMPLATE.replace(
+                DESCRIPTOR_FILE_PLACEHOLDER, globalSecurityDescriptorFile.getCanonicalPath());
+            int placeholderIndex = wsddContents.indexOf(SECURITY_DESCRIPTOR_PLACEHOLDER);
+            wsddContents.replace(placeholderIndex, placeholderIndex + SECURITY_DESCRIPTOR_PLACEHOLDER.length(), parameter);
+            Utils.stringBufferToFile(wsddContents, serverWsdd.getCanonicalPath());
+        } catch (Exception ex) {
+            throw new ContainerException("Error editing server-config.wsdd for editing");
+        }
     }
 
 
-    public File getCertificatesDirectory() throws Exception {
+    public File getCertificatesDirectory() {
         return new File(this.getProperties().getContainerDirectory() + File.separator + "certificates");
     }
 
