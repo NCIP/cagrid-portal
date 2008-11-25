af3882421e0d6b49ef3d97c22e17a03eb1764f00
oster <oster>
2008-11-25 21:36:44 +0000

    shell of mms service
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12276 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/mms/src/org/cagrid/mms/service/MetadataModelServiceImpl.java
@@ -0,0 +1,19 @@
+package org.cagrid.mms.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class MetadataModelServiceImpl extends MetadataModelServiceImplBase {
+
+	
+	public MetadataModelServiceImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
---
+++ cagrid-1-0/caGrid/projects/mms/src/org/cagrid/mms/service/globus/resource/MetadataModelServiceResource.java
@@ -0,0 +1,17 @@
+package org.cagrid.mms.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this MetadataModelServiceResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class MetadataModelServiceResource extends MetadataModelServiceResourceBase {
+
+}
