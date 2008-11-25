4a5b3f445f19a770c4b846db307862de312b81ae
oster <oster>
2008-11-25 22:04:54 +0000

    start of migration of domainmodel generation to mms project
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12279 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/mms/test/src/ApplicationServiceExample.java
@@ -0,0 +1,26 @@
+import gov.nih.nci.cadsr.umlproject.domain.Project;
+import gov.nih.nci.system.applicationservice.ApplicationService;
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+
+import java.util.Iterator;
+import java.util.List;
+
+
+public class ApplicationServiceExample {
+
+    public static void main(String[] args) {
+        try {
+            ApplicationService appService = ApplicationServiceProvider
+                .getApplicationServiceFromUrl("http://cadsrapi-prod2.nci.nih.gov/cadsrapi40/");
+
+            List rList = appService.search(Project.class, new Project());
+            for (Iterator resultsIterator = rList.iterator(); resultsIterator.hasNext();) {
+                Project project = (Project) resultsIterator.next();
+                System.out.println(project.getShortName());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
