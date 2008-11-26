1ead32cc7812c89ec52fbd7b89f7a0afb4e7d36f
dervin <dervin>
2008-11-26 14:56:50 +0000

    notification moved to Introduce ClientTools project
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12283 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/tools/src/java/notification/org/cagrid/notification/SubscriptionListener.java
+++
@@ -1,9 +0,0 @@
-package org.cagrid.notification;
-
-
-import org.oasis.wsrf.properties.ResourcePropertyValueChangeNotificationType;
-
-public interface SubscriptionListener {
-
-    public void subscriptionValueChanged(ResourcePropertyValueChangeNotificationType notification);
-}
