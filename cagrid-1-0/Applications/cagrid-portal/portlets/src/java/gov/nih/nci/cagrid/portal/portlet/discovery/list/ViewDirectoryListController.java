fcd513da332c12978030c404689fa185a84dbcd6
kherm <kherm>
2008-12-04 19:58:28 +0000

    bug fix #18103
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-2-1_release@12354 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/discovery/list/ViewDirectoryListController.java
+++ cagrid-1-0/Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/discovery/list/ViewDirectoryListController.java
@@ -1,9 +1,7 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.discovery.list;
-
-import java.util.List;
 
 import gov.nih.nci.cagrid.portal.portlet.discovery.DiscoveryResults;
 import gov.nih.nci.cagrid.portal.portlet.discovery.dir.AbstractDirectoryBean;
@@ -12,79 +10,76 @@ import gov.nih.nci.cagrid.portal.portlet.discovery.dir.DiscoveryDirectory;
 
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import java.util.List;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
+ * @author <a href="mailto:manav.kher@semanticbits.com">Manav Kher</a>
  */
 public class ViewDirectoryListController extends BaseDiscoveryViewController {
 
-	private String listBeanSessionAttributeName;
+    private String listBeanSessionAttributeName;
 
-	/**
-	 * 
-	 */
-	public ViewDirectoryListController() {
+    /**
+     *
+     */
+    public ViewDirectoryListController() {
 
-	}
+    }
 
-	protected AbstractDirectoryBean newDirectoryBean() {
-		return (AbstractDirectoryBean) getApplicationContext().getBean(
-				"listBeanPrototype");
-	}
+    protected AbstractDirectoryBean newDirectoryBean() {
+        return (AbstractDirectoryBean) getApplicationContext().getBean(
+                "listBeanPrototype");
+    }
 
-	@Override
-	protected AbstractDirectoryBean doHandleDirectory(RenderRequest request,
-			RenderResponse response, DiscoveryDirectory dir) throws Exception {
-		ListBean listBean = getListBean(request, dir.getType().toString(), dir.getId(), dir.getObjects());
-		listBean.setSelectedDirectory(dir.getId());
-		return listBean;
-	}
+    @Override
+    protected AbstractDirectoryBean doHandleDirectory(RenderRequest request,
+                                                      RenderResponse response, DiscoveryDirectory dir) throws Exception {
+        return getListBean(request, dir.getType().toString(), dir.getId(), dir.getObjects());
+    }
 
-	private ListBean getListBean(RenderRequest request, String type, String id, List objects) {
-		ListBean listBean = (ListBean) request.getPortletSession()
-				.getAttribute(getListBeanSessionAttributeName());
-		
-		boolean createNewListBean = false;
-		
-		if (listBean != null){
-			String oldId = listBean.getSelectedResults();
-			if(oldId == null){
-				oldId = listBean.getSelectedDirectory();
-			}
-			if(!oldId.equals(id)){
-				createNewListBean = true;
-			}
-		}
-		if(listBean == null || createNewListBean){
+    private ListBean getListBean(RenderRequest request, String type, String id, List objects) {
+        ListBean listBean = (ListBean) request.getPortletSession()
+                .getAttribute(getListBeanSessionAttributeName());
 
-			listBean = (ListBean) newDirectoryBean();
-			listBean.setType(type);
-			listBean.getScroller().setObjects(objects);
-			
-			
-			request.getPortletSession().setAttribute(
-					getListBeanSessionAttributeName(), listBean);
-		}
-		listBean.refresh();
-		return listBean;
-	}
+        boolean createNewListBean = false;
 
-	@Override
-	protected AbstractDirectoryBean doHandleResults(RenderRequest request,
-			RenderResponse response, DiscoveryResults results) throws Exception {
-		ListBean listBean = getListBean(request, results.getType().toString(), results.getId(), results.getObjects());
-		listBean.setSelectedResults(results.getId());
-		return listBean;
-	}
+        if (listBean != null) {
+            String oldId = listBean.getSelectedResults();
+            if (oldId == null) {
+                oldId = listBean.getSelectedDirectory();
+            }
+            if (!oldId.equals(id)) {
+                createNewListBean = true;
+            }
+        }
+        if (listBean == null || createNewListBean) {
 
-	public String getListBeanSessionAttributeName() {
-		return listBeanSessionAttributeName;
-	}
+            listBean = (ListBean) newDirectoryBean();
+            listBean.setType(type);
+            listBean.getScroller().setObjects(objects);
 
-	public void setListBeanSessionAttributeName(
-			String listBeanSessionAttributeName) {
-		this.listBeanSessionAttributeName = listBeanSessionAttributeName;
-	}
+
+            request.getPortletSession().setAttribute(
+                    getListBeanSessionAttributeName(), listBean);
+        }
+        listBean.refresh();
+        return listBean;
+    }
+
+    @Override
+    protected AbstractDirectoryBean doHandleResults(RenderRequest request,
+                                                    RenderResponse response, DiscoveryResults results) throws Exception {
+        return getListBean(request, results.getType().toString(), results.getId(), results.getObjects());
+    }
+
+    public String getListBeanSessionAttributeName() {
+        return listBeanSessionAttributeName;
+    }
+
+    public void setListBeanSessionAttributeName(
+            String listBeanSessionAttributeName) {
+        this.listBeanSessionAttributeName = listBeanSessionAttributeName;
+    }
 
 }
