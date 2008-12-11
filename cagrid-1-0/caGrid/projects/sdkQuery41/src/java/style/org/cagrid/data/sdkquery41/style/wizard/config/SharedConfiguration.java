45a22135c3cb62ca11ed0c462082eeaa118a26ce
dervin <dervin>
2008-12-11 17:54:52 +0000

    shared configuration info I'll need in multiple wizard panels
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12433 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/sdkQuery41/src/java/style/org/cagrid/data/sdkquery41/style/wizard/config/SharedConfiguration.java
@@ -0,0 +1,57 @@
+package org.cagrid.data.sdkquery41.style.wizard.config;
+
+import gov.nih.nci.cagrid.introduce.common.ServiceInformation;
+
+import java.io.File;
+import java.util.Properties;
+
+public class SharedConfiguration {
+    
+    private static SharedConfiguration instance = null;
+    
+    private ServiceInformation serviceInfo = null;
+    private File sdkDirectory = null;
+    private Properties sdkDeployProperties = null;
+
+    private SharedConfiguration() {
+        
+    }
+    
+        
+    public static SharedConfiguration getInstance() {
+        if (instance == null) {
+            instance = new SharedConfiguration();
+        }
+        return instance;
+    }
+
+
+    public Properties getSdkDeployProperties() {
+        return sdkDeployProperties;
+    }
+
+
+    public void setSdkDeployProperties(Properties sdkDeployProperties) {
+        this.sdkDeployProperties = sdkDeployProperties;
+    }
+
+
+    public ServiceInformation getServiceInfo() {
+        return serviceInfo;
+    }
+
+
+    public void setServiceInfo(ServiceInformation serviceInfo) {
+        this.serviceInfo = serviceInfo;
+    }
+
+
+    public File getSdkDirectory() {
+        return sdkDirectory;
+    }
+
+
+    public void setSdkDirectory(File sdkDirectory) {
+        this.sdkDirectory = sdkDirectory;
+    }
+}
