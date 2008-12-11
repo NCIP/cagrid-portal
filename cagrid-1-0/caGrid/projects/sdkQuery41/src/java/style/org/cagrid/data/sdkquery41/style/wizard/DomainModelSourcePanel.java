33f4f6e6ba8ae4525ed4784dc202d965d81cf078
dervin <dervin>
2008-12-11 19:28:20 +0000

    internal panel to domain model wizard panel for various ways to create a domain model
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12436 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-1-0/caGrid/projects/sdkQuery41/src/java/style/org/cagrid/data/sdkquery41/style/wizard/DomainModelSourcePanel.java
@@ -0,0 +1,23 @@
+package org.cagrid.data.sdkquery41.style.wizard;
+
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
+
+import javax.swing.JPanel;
+
+/**
+ * JPanel which is required to produce a caGrid Domain Model
+ * 
+ * @author David
+ */
+public abstract class DomainModelSourcePanel extends JPanel {
+    
+    public DomainModelSourcePanel() {
+        super();
+    }
+    
+    
+    public abstract void populateFromConfiguration();
+    
+    
+    public abstract DomainModel getDomainModel() throws Exception;
+}
