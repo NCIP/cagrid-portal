01277e6e074c94b355a0579f5bda9392b05e2339
langella <langella>
2009-01-08 21:03:42 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12675 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/service/GTSImplBase.java
+++ cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/service/GTSImplBase.java
@@ -1,12 +1,21 @@
 package gov.nih.nci.cagrid.gts.service;
 
+import gov.nih.nci.cagrid.gts.service.globus.resource.GTSResource;
+import  gov.nih.nci.cagrid.gts.service.GTSConfiguration;
+
 import java.rmi.RemoteException;
 
 import javax.naming.InitialContext;
+import javax.xml.namespace.QName;
 
 import org.apache.axis.MessageContext;
 import org.globus.wsrf.Constants;
+import org.globus.wsrf.ResourceContext;
+import org.globus.wsrf.ResourceContextException;
+import org.globus.wsrf.ResourceException;
 import org.globus.wsrf.ResourceHome;
+import org.globus.wsrf.ResourceProperty;
+import org.globus.wsrf.ResourcePropertySet;
 
 
 /** 
--- cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/service/globus/GTSAuthorization.java
+++ cagrid-1-0/caGrid/projects/gts/src/gov/nih/nci/cagrid/gts/service/globus/GTSAuthorization.java
@@ -1,21 +1,22 @@
 package gov.nih.nci.cagrid.gts.service.globus;
 
 
-import java.io.File;
+import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.io.File;
 import javax.security.auth.Subject;
 import javax.xml.namespace.QName;
 import javax.xml.rpc.handler.MessageContext;
 
-import org.globus.wsrf.config.ContainerConfig;
+import gov.nih.nci.cagrid.introduce.servicetools.security.AuthorizationExtension;
 import org.globus.wsrf.impl.security.authorization.exceptions.AuthorizationException;
 import org.globus.wsrf.impl.security.authorization.exceptions.CloseException;
 import org.globus.wsrf.impl.security.authorization.exceptions.InitializeException;
 import org.globus.wsrf.impl.security.authorization.exceptions.InvalidPolicyException;
 import org.globus.wsrf.security.authorization.PDP;
 import org.globus.wsrf.security.authorization.PDPConfig;
+import org.globus.wsrf.config.ContainerConfig;
 import org.w3c.dom.Node;
 
 
