683b6a8de39c31cc12e4180b8f6fe5828337eade
langella <langella>
2009-01-09 21:30:27 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12705 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/dorian/src/org/cagrid/gaards/dorian/test/system/DorianSystemTestStoryBook.java
+++ cagrid-1-0/tests/projects/dorian/src/org/cagrid/gaards/dorian/test/system/DorianSystemTestStoryBook.java
@@ -14,25 +14,25 @@ public class DorianSystemTestStoryBook {
     public static File DORIAN_PROPERTIES_FILE = new File("resources/dorian.properties");
 
     @Test
-    public void dorianIdentityProviderAuthenticationTest() throws IOException {
+    public void dorianIdentityProviderAuthenticationTest() throws Throwable {
         Story test = new DorianIdentityProviderAuthenticationTest(ServiceContainerFactory
             .createContainer(ServiceContainerType.SECURE_TOMCAT_CONTAINER), DORIAN_PROPERTIES_FILE);
-        test.run();
+        test.runBare();
     }
 
     @Test
-    public void dorianLocalIdentityProviderTest() throws IOException {
+    public void dorianLocalIdentityProviderTest() throws Throwable {
     	Story test = new DorianLocaIdentityProviderTest(ServiceContainerFactory
             .createContainer(ServiceContainerType.SECURE_TOMCAT_CONTAINER), DORIAN_PROPERTIES_FILE);
-    	test.run();
+    	test.runBare();
     }
             
     @Test
-    public void dorianRemoteIdentityProviderTest() throws IOException {
+    public void dorianRemoteIdentityProviderTest() throws Throwable {
     	Story test = new DorianRemoteIdentityProviderTest(ServiceContainerFactory
                 .createContainer(ServiceContainerType.SECURE_TOMCAT_CONTAINER), null, DORIAN_PROPERTIES_FILE, new File(
                 "resources/authentication-config.xml"));
-    	test.run();
+    	test.runBare();
 
     }
 
