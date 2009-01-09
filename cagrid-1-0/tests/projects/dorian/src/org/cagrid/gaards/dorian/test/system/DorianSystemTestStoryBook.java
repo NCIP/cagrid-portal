3337efc0db70a7eddb3bd772ae7984814d92039d
langella <langella>
2009-01-09 21:31:18 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12706 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/dorian/src/org/cagrid/gaards/dorian/test/system/DorianSystemTestStoryBook.java
+++ cagrid-1-0/tests/projects/dorian/src/org/cagrid/gaards/dorian/test/system/DorianSystemTestStoryBook.java
@@ -5,7 +5,6 @@ import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainerType;
 import gov.nih.nci.cagrid.testing.system.haste.Story;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.junit.Test;
 
