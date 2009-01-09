e13a3a6ded502bed2b37083e7bde91e5df706075
langella <langella>
2009-01-09 21:39:53 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12707 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/authentication-service/src/org/cagrid/gaards/authentication/test/system/AuthenticationServiceTestsStoryBook.java
+++ cagrid-1-0/tests/projects/authentication-service/src/org/cagrid/gaards/authentication/test/system/AuthenticationServiceTestsStoryBook.java
@@ -5,7 +5,6 @@ import gov.nih.nci.cagrid.testing.system.deployment.ServiceContainerType;
 import gov.nih.nci.cagrid.testing.system.haste.Story;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.junit.Test;
 
@@ -14,13 +13,13 @@ public class AuthenticationServiceTestsStoryBook {
 	
 
     @Test
-    public void authenticationServiceTests() throws IOException {
+    public void authenticationServiceTests() throws Throwable {
     	Story s = new AuthenticationServiceTest(
 							ServiceContainerFactory
 									.createContainer(ServiceContainerType.SECURE_TOMCAT_CONTAINER),
 			new File("resources/authentication-config.xml"));
 
-    	s.run();
+    	s.runBare();
 	}
 
 }
