f993960987024e72e8350b9c6301a9aecf01c369
garmillas <garmillas>
2009-01-15 20:02:29 +0000

    code formatting
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12810 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/websso/src/java/org/cagrid/gaards/websso/exception/AuthenticationErrorException.java
+++ cagrid-1-0/caGrid/projects/websso/src/java/org/cagrid/gaards/websso/exception/AuthenticationErrorException.java
@@ -2,14 +2,12 @@ package org.cagrid.gaards.websso.exception;
 
 import org.jasig.cas.authentication.handler.BadCredentialsAuthenticationException;
 
-public class AuthenticationErrorException extends BadCredentialsAuthenticationException
-{
+public class AuthenticationErrorException extends
+		BadCredentialsAuthenticationException {
 
 	private static final long serialVersionUID = 1L;
-	
-	public AuthenticationErrorException(String message)
-	{
+
+	public AuthenticationErrorException(String message) {
 		super(message);
 	}
-
 }
