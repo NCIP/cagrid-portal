9d727c88a8d5f7ff59f9be53ab9d0b0467f046f8
langella <langella>
2009-01-15 17:54:52 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12799 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/cds/src/org/cagrid/gaards/cds/service/PropertyManager.java
+++ cagrid-1-0/caGrid/projects/cds/src/org/cagrid/gaards/cds/service/PropertyManager.java
@@ -7,6 +7,7 @@ import org.cagrid.tools.database.Database;
 import org.cagrid.tools.database.Metadata;
 import org.cagrid.tools.database.MetadataManager;
 
+
 /**
  * @author <A href="mailto:langella@bmi.osu.edu">Stephen Langella </A>
  * @author <A href="mailto:oster@bmi.osu.edu">Scott Oster </A>
@@ -16,96 +17,97 @@ import org.cagrid.tools.database.MetadataManager;
  */
 public class PropertyManager {
 
-	private static String TABLE = "properties";
-	private static String VERSION_PROPERTY = "version";
-	private static String KEY_MANAGER = "Key Manager";
-	public static float CDS_VERSION_1_2 = 1.2F;
-	public static float CURRENT_VERSION = CDS_VERSION_1_2;
-	private MetadataManager manager;
-	private Metadata version;
-	private Metadata keyManager;
-	private Log log;
+    private static String TABLE = "properties";
+    private static String VERSION_PROPERTY = "version";
+    private static String KEY_MANAGER = "Key Manager";
+    public static String CDS_VERSION_1_2 = "1.2";
+    public static String CDS_VERSION_1_3 = "1.3";
+    public static String CURRENT_VERSION = CDS_VERSION_1_3;
+    private MetadataManager manager;
+    private Metadata version;
+    private Metadata keyManager;
+    private Log log;
 
-	public PropertyManager(Database db) throws CDSInternalFault {
-		try {
-			this.log = LogFactory.getLog(this.getClass().getName());
-			this.manager = new MetadataManager(db, TABLE);
-			version = manager.get(VERSION_PROPERTY);
-			if (version == null) {
-				version = new Metadata();
-				version.setName(VERSION_PROPERTY);
-				version.setDescription("The software version of this CDS.");
-				version.setValue(String.valueOf(CURRENT_VERSION));
-				this.manager.update(version);
-			}
-			keyManager = manager.get(KEY_MANAGER);
-			if (keyManager == null) {
-				keyManager = new Metadata();
-				keyManager.setName(KEY_MANAGER);
-				keyManager
-						.setDescription("The key manager class used to manage keys.");
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			CDSInternalFault fault = new CDSInternalFault();
-			fault
-					.setFaultString("Unexpected error initializing the property manager.");
-			throw fault;
-		}
-	}
 
-	public void setKeyManager(String keyManagerClass) throws CDSInternalFault {
-		try {
-			this.keyManager.setValue(keyManagerClass);
-			this.manager.update(this.keyManager);
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			CDSInternalFault fault = new CDSInternalFault();
-			fault.setFaultString("Unexpected database error.");
-			throw fault;
-		}
-	}
+    public PropertyManager(Database db) throws CDSInternalFault {
+        try {
+            this.log = LogFactory.getLog(this.getClass().getName());
+            this.manager = new MetadataManager(db, TABLE);
+            version = manager.get(VERSION_PROPERTY);
+            if (version == null) {
+                version = new Metadata();
+                version.setName(VERSION_PROPERTY);
+                version.setDescription("The software version of this CDS.");
+                version.setValue(String.valueOf(CURRENT_VERSION));
+                this.manager.update(version);
+            }
+            keyManager = manager.get(KEY_MANAGER);
+            if (keyManager == null) {
+                keyManager = new Metadata();
+                keyManager.setName(KEY_MANAGER);
+                keyManager.setDescription("The key manager class used to manage keys.");
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            CDSInternalFault fault = new CDSInternalFault();
+            fault.setFaultString("Unexpected error initializing the property manager.");
+            throw fault;
+        }
+    }
 
-	public String getKeyManager() {
-		return this.keyManager.getValue();
-	}
 
-	public void setCurrentVersion() throws CDSInternalFault {
-		this.setVersion(CURRENT_VERSION);
-	}
+    public void setKeyManager(String keyManagerClass) throws CDSInternalFault {
+        try {
+            this.keyManager.setValue(keyManagerClass);
+            this.manager.update(this.keyManager);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            CDSInternalFault fault = new CDSInternalFault();
+            fault.setFaultString("Unexpected database error.");
+            throw fault;
+        }
+    }
 
-	public void setVersion(float version) throws CDSInternalFault {
-		try {
-			this.version.setValue(String.valueOf(version));
-			this.manager.update(this.version);
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			CDSInternalFault fault = new CDSInternalFault();
-			fault.setFaultString("Unexpected database error.");
-			throw fault;
-		}
-	}
 
-	public float getVersion() {
-		String s = this.version.getValue();
-		if (s == null) {
-			return 1.0F;
-		} else {
-			return Float.valueOf(s).floatValue();
-		}
-	}
+    public String getKeyManager() {
+        return this.keyManager.getValue();
+    }
 
-	public void clearAllProperties() throws CDSInternalFault {
-		try {
-			this.version.setValue(null);
-			this.keyManager.setValue(null);
-			this.manager.clearDatabase();
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			CDSInternalFault fault = new CDSInternalFault();
-			fault.setFaultString("Unexpected database error.");
-			throw fault;
-		}
-	}
+
+    public void setCurrentVersion() throws CDSInternalFault {
+        this.setVersion(CURRENT_VERSION);
+    }
+
+
+    public void setVersion(String version) throws CDSInternalFault {
+        try {
+            this.version.setValue(version);
+            this.manager.update(this.version);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            CDSInternalFault fault = new CDSInternalFault();
+            fault.setFaultString("Unexpected database error.");
+            throw fault;
+        }
+    }
+
+
+    public String getVersion() {
+        return this.version.getValue();
+    }
+
+
+    public void clearAllProperties() throws CDSInternalFault {
+        try {
+            this.version.setValue(null);
+            this.keyManager.setValue(null);
+            this.manager.clearDatabase();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            CDSInternalFault fault = new CDSInternalFault();
+            fault.setFaultString("Unexpected database error.");
+            throw fault;
+        }
+    }
 
 }
