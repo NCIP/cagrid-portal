871ead33cfeeed8b8ab68e94234f423f1fd4834d
sulakhed <sulakhed>
2009-01-23 20:31:09 +0000

    added Taverna build process
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12917 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/tests/projects/TavernaWorkflowServicesTests/src/gov/nih/nci/cagrid/workflow/test/system/TavernaWorkflowServiceStory.java
+++ cagrid-1-0/tests/projects/TavernaWorkflowServicesTests/src/gov/nih/nci/cagrid/workflow/test/system/TavernaWorkflowServiceStory.java
@@ -9,7 +9,7 @@ import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.Vector;
-
+import org.apache.tools.ant.*;
 import org.apache.axis.message.addressing.EndpointReferenceType;
 import org.apache.axis.types.URI.MalformedURIException;
 
@@ -27,71 +27,113 @@ import gov.nih.nci.cagrid.workflow.test.system.steps.CheckTavernaWorkflowService
 
 public class TavernaWorkflowServiceStory extends ServiceStoryBase {
 
-    private static final String SERVICE_TEMP_PATH = "tmp/TempGME";
-    private static final String RESULTS_TEMP_PATH = "tmp/results";
-    private static final String TWS_URL_PATH = "cagrid/TavernaWorkflowService";
-    private static final String PATH_TO_MMS_PROJECT = "../../../caGrid/projects/TavernaWorkflowServices";
-    public static final String TWS_DIR_PROPERTY = "tws.service.dir";
+	private static final String SERVICE_TEMP_PATH = "tmp/TempGME";
+	private static final String RESULTS_TEMP_PATH = "tmp/results";
+	private static final String TWS_URL_PATH = "cagrid/TavernaWorkflowService";
+	private static final String PATH_TO_MMS_PROJECT = "../../../caGrid/projects/TavernaWorkflowServices";
+	public static final String TWS_DIR_PROPERTY = "tws.service.dir";
 
 
-    public TavernaWorkflowServiceStory(ServiceContainer container) {
-        super(container);
-    }
+	public TavernaWorkflowServiceStory(ServiceContainer container) {
+		super(container);
+	}
 
-    public TavernaWorkflowServiceStory() {
+	public TavernaWorkflowServiceStory() {
 
-        // init the container
-        try {
-            this.setContainer(ServiceContainerFactory.createContainer(ServiceContainerType.GLOBUS_CONTAINER));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            fail("Failed to create container: " + ex.getMessage());
-        }
-    }
-
-
-    @Override
-    public String getName() {
-        return getDescription();
-    }
+		// init the container
+		try {
+			this.setContainer(ServiceContainerFactory.createContainer(ServiceContainerType.GLOBUS_CONTAINER));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			fail("Failed to create container: " + ex.getMessage());
+		}
+	}
 
 
-    @Override
-    public String getDescription() {
-        return "Taverna Workflow Service Test";
-    }
+	@Override
+	public String getName() {
+		return getDescription();
+	}
 
 
-    protected File getTWSDir() {
-        String value = System.getProperty(TWS_DIR_PROPERTY, PATH_TO_MMS_PROJECT);
-        assertNotNull("System property " + TWS_DIR_PROPERTY + " was not set!", value);
-        File dir = new File(value);
-        return dir;
-    }
-    
-    private class ChangeServicePropertyFile extends Step {
-    	
-    	public ChangeServicePropertyFile (){
-    		
-    	}
-    	
+	@Override
+	public String getDescription() {
+		return "Taverna Workflow Service Test";
+	}
+
+
+	protected File getTWSDir() {
+		String value = System.getProperty(TWS_DIR_PROPERTY, PATH_TO_MMS_PROJECT);
+		assertNotNull("System property " + TWS_DIR_PROPERTY + " was not set!", value);
+		File dir = new File(value);
+		return dir;
+	}
+
+
+
+
+	@Override
+	protected Vector steps() {
+		Vector<Step> steps = new Vector<Step>();
+		File tempGMEServiceDir = new File(SERVICE_TEMP_PATH);
+
+		// SETUP
+		steps.add(new UnpackContainerStep(getContainer()));
+		steps.add(new CopyServiceStep(getTWSDir(), tempGMEServiceDir));
+
+
+
+		// CONFIGURE
+		// steps.add(new SetDatabasePropertiesStep(tempGMEServiceDir));
+		// steps.add(new CreateDatabaseStep(tempGMEServiceDir));
+		steps.add (new ChangeServicePropertyFile());
+		steps.add(new BuildTaverna());
+
+
+		DeployServiceStep deployStep = new DeployServiceStep(getContainer(), tempGMEServiceDir.getAbsolutePath(),
+				Arrays.asList(new String[]{"-Dno.deployment.validation=true"}));
+		steps.add(deployStep);
+		steps.add(new StartContainerStep(getContainer()));
+
+		EndpointReferenceType epr = null;
+		try {
+			epr = getContainer().getServiceEPR(TWS_URL_PATH);
+		} catch (MalformedURIException e) {
+			e.printStackTrace();
+			fail("Error constructing client:" + e.getMessage());
+		}
+
+		// TEST
+		steps.add(new CheckTavernaWorkflowServiceStep(epr));
+
+		// retrieve failures
+
+		return steps;
+	}
+
+	private class ChangeServicePropertyFile extends Step {
+
+		public ChangeServicePropertyFile (){
+
+		}
+
 		@Override
 		public void runStep() throws Throwable {
 
 			Properties props = new Properties();
-	        try {
-	        	File path = new File (SERVICE_TEMP_PATH);
-	        	String absPath = path.getAbsolutePath();
+			try {
+				File path = new File (SERVICE_TEMP_PATH);
+				String absPath = path.getAbsolutePath();
 				props.load(new FileInputStream(absPath + "/service.properties"));
-	        	System.out.println(" >> Service Properites File : \n >> " + absPath + "/service.properties");
+				System.out.println(" >> Service Properites File : \n >> " + absPath + "/service.properties");
 				props.setProperty("tavernaDir", absPath + "/taverna");
 				props.setProperty("baseRepositoryDir", absPath + "/taverna/target/repository");
 				props.store(new FileOutputStream(absPath + "/service.properties"), null);
-	
+
 				System.out.println(" >> File Changed to:\n");
 				System.out.println(" >> " + props.getProperty("tavernaDir"));
 				System.out.println(" >> " + props.getProperty("baseRepositoryDir"));
-				
+
 				Thread.sleep(5000);
 			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
@@ -103,50 +145,34 @@ public class TavernaWorkflowServiceStory extends ServiceStoryBase {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
-    	
-    }
+
+	}
+
+	private class BuildTaverna extends Step
+	{
+		public BuildTaverna(){
+		}
+		@Override
+		public void runStep() throws Throwable {
+			Project project = new Project();
+			project.init();
+			DefaultLogger logger = new DefaultLogger();
+			logger.setMessageOutputLevel(Project.MSG_INFO);
+			logger.setErrorPrintStream(System.err);
+			logger.setOutputPrintStream(System.out);
+			project.addBuildListener(logger);
+
+			File buildFile = new File(SERVICE_TEMP_PATH + "/taverna-build.xml");
+			ProjectHelper.configureProject(project, buildFile);
+
+			try {
+				project.executeTarget("taverna-package");
+			} catch(Exception e) {System.err.println(e.getMessage());}
 
 
-
-	@Override
-	protected Vector steps() {
-        Vector<Step> steps = new Vector<Step>();
-        File tempGMEServiceDir = new File(SERVICE_TEMP_PATH);
-
-        // SETUP
-        steps.add(new UnpackContainerStep(getContainer()));
-        steps.add(new CopyServiceStep(getTWSDir(), tempGMEServiceDir));
-
-
-        
-        // CONFIGURE
-        // steps.add(new SetDatabasePropertiesStep(tempGMEServiceDir));
-        // steps.add(new CreateDatabaseStep(tempGMEServiceDir));
-        steps.add (new ChangeServicePropertyFile());
-
-        
-        
-        DeployServiceStep deployStep = new DeployServiceStep(getContainer(), tempGMEServiceDir.getAbsolutePath(),
-            Arrays.asList(new String[]{"-Dno.deployment.validation=true"}));
-        steps.add(deployStep);
-        steps.add(new StartContainerStep(getContainer()));
-
-        EndpointReferenceType epr = null;
-        try {
-            epr = getContainer().getServiceEPR(TWS_URL_PATH);
-        } catch (MalformedURIException e) {
-            e.printStackTrace();
-            fail("Error constructing client:" + e.getMessage());
-        }
-
-        // TEST
-        steps.add(new CheckTavernaWorkflowServiceStep(epr));
-
-        // retrieve failures
-
-        return steps;
+		}
 	}
 
 }
