0e91cacbee12bdbfef505a24d28888a51f4e4eb9
dervin <dervin>
2009-01-28 13:22:46 +0000

    org'd imports
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk@12955 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/caGrid/projects/fqp/src/gov/nih/nci/cagrid/fqp/service/FederatedQueryProcessorImpl.java
+++ cagrid-1-0/caGrid/projects/fqp/src/gov/nih/nci/cagrid/fqp/service/FederatedQueryProcessorImpl.java
@@ -14,7 +14,6 @@ import java.rmi.RemoteException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
