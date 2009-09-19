fffd0ddd699b861a7c7e6c955087362c2a0e93b2
kherm <kherm>
2009-09-19 14:21:37 +0000

    update cadsr URL
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-2-2_release@14358 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-1-0/Applications/cagrid-portal/aggr/test/src/java/gov/nih/nci/cagrid/portal/aggr/status/WSDLServiceStatusProviderTest.java
+++ cagrid-1-0/Applications/cagrid-portal/aggr/test/src/java/gov/nih/nci/cagrid/portal/aggr/status/WSDLServiceStatusProviderTest.java
@@ -18,12 +18,14 @@ public class WSDLServiceStatusProviderTest extends TestCase {
 
 
         WSDLServiceStatusProvider _provider = new WSDLServiceStatusProvider();
-        assertEquals("Should not get wsdl from this url", ServiceStatus.INACTIVE, _provider.getStatus(_nonCagridService));
-        assertEquals("Should get wsdl from URL", ServiceStatus.ACTIVE, _provider.getStatus(_cagridservice));
-
-        _provider.setTimeout(100000);
 
         assertEquals(ServiceStatus.INACTIVE, _provider.getStatus("http://wsdl"));
         assertEquals(ServiceStatus.INACTIVE, _provider.getStatus("http://"));
+
+        _provider.setTimeout(100000);
+
+        assertEquals("Should not get wsdl from this url", ServiceStatus.INACTIVE, _provider.getStatus(_nonCagridService));
+        assertEquals("Should get wsdl from URL", ServiceStatus.ACTIVE, _provider.getStatus(_cagridservice));
+
     }
 }
