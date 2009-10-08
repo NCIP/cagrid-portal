85c57c60b306bcfcef3114c705adac4d321e9184
kherm <kherm>
2009-10-08 14:15:47 +0000

    datasets summary
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/portal@14613 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-portal/portlets/test/src/java/gov/nih/nci/cagrid/portal/portlet/latestcontent/LatestContentContextInitTest.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.cagrid.portal.portlet.latestcontent;
+
+import gov.nih.nci.cagrid.portal.portlet.SpringPortletContextTestBase;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.springframework.mock.web.portlet.MockRenderRequest;
+import org.springframework.mock.web.portlet.MockRenderResponse;
+
+import javax.portlet.PortletMode;
+
+/**
+ * User: kherm
+ *
+ * @author kherm manav.kher@semanticbits.com
+ */
+public class LatestContentContextInitTest extends SpringPortletContextTestBase<LatestContextContext> {
+
+    public LatestContentContextInitTest() {
+        super(LatestContextContext.class);
+    }
+
+    /**
+     * Make sure we get a view back
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testView() throws Exception {
+        MockRenderRequest request = new MockRenderRequest();
+        MockRenderResponse response = new MockRenderResponse();
+
+        request.setPortletMode(PortletMode.VIEW);
+        doRender(request, response);
+        assertNotNull(response.getIncludedUrl());
+
+    }
+
+}
