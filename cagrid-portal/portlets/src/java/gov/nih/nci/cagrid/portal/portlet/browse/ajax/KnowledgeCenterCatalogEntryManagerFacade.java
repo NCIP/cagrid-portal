94fb4c5ad69458406fb9aedad9bfc5e2909408e9
kherm <kherm>
2009-11-03 02:45:58 +0000

    related items
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/portal@14986 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/browse/ajax/KnowledgeCenterCatalogEntryManagerFacade.java
+++ cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/browse/ajax/KnowledgeCenterCatalogEntryManagerFacade.java
@@ -7,7 +7,7 @@ package gov.nih.nci.cagrid.portal.portlet.browse.ajax;
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
  */
 public class KnowledgeCenterCatalogEntryManagerFacade extends
-        CommunityCatalogEntryManagerFacade {
+		CatalogEntryManagerFacade { 
 
     /**
      *
