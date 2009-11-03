50c8cc771e3c198a124219cfa27675e14060126f
kherm <kherm>
2009-11-03 03:30:47 +0000

    renamed to conform to scheme
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/portal@14988 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-portal/service/test/src/java/gov/nih/nci/cagrid/portal/service/LiferayServiceTestSuite.java
+++ cagrid-portal/service/test/src/java/gov/nih/nci/cagrid/portal/service/LiferayServiceTestSuite.java
@@ -1,13 +1,5 @@
 package gov.nih.nci.cagrid.portal.service;
 
-import gov.nih.nci.cagrid.portal.util.DBLoader;
-
-import java.io.File;
-
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
 import com.liferay.portal.jcr.JCRFactoryUtil;
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
 import com.liferay.portal.kernel.messaging.MessageBus;
@@ -23,95 +15,100 @@ import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.util.InitUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.TestPropsValues;
+import gov.nih.nci.cagrid.portal.util.DBLoader;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-//@Suite.SuiteClasses( { UserServiceTestCase.class,
-//CommunityServiceTestCase.class, PersonServiceTestCase.class })
+import java.io.File;
+
+//@Suite.SuiteClasses( { UserServiceTest.class,
+//CommunityServiceTest.class, PersonServiceTest.class })
 /**
- * 
+ *
  */
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { UserServiceTestCase.class,
-		CommunityServiceTestCase.class })
+@Suite.SuiteClasses({UserServiceTest.class,
+        CommunityServiceTest.class})
 public class LiferayServiceTestSuite {
 
-	@BeforeClass
-	public static void setUp() {
+    @BeforeClass
+    public static void setUp() {
 
-		File derbyDBDir = new File("./lportal");
-		if (derbyDBDir.exists()) {
-			deleteDir(derbyDBDir);
-		}
+        File derbyDBDir = new File("./lportal");
+        if (derbyDBDir.exists()) {
+            deleteDir(derbyDBDir);
+        }
 
-		new DBLoader("derby", "lportal", "test/data/create-derby.sql", true);
-		new DBLoader("derby", "lportal", "test/data/indexes.sql", false);
+        new DBLoader("derby", "lportal", "test/data/create-derby.sql", true);
+        new DBLoader("derby", "lportal", "test/data/indexes.sql", false);
 
-		InitUtil.initWithSpring();
+        InitUtil.initWithSpring();
 
-		FileUtil.deltree(PropsValues.LIFERAY_HOME + "/data");
+        FileUtil.deltree(PropsValues.LIFERAY_HOME + "/data");
 
-		// JCR
+        // JCR
 
-		try {
-			JCRFactoryUtil.prepare();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            JCRFactoryUtil.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// Lucene
+        // Lucene
 
-		LuceneUtil.checkLuceneDir(TestPropsValues.COMPANY_ID);
+        LuceneUtil.checkLuceneDir(TestPropsValues.COMPANY_ID);
 
-		// Messaging
+        // Messaging
 
-		MessageBus messageBus = (MessageBus) PortalBeanLocatorUtil
-				.locate(MessageBus.class.getName());
-		MessageSender messageSender = (MessageSender) PortalBeanLocatorUtil
-				.locate(MessageSender.class.getName());
-		SynchronousMessageSender synchronousMessageSender = (SynchronousMessageSender) PortalBeanLocatorUtil
-				.locate(SynchronousMessageSender.class.getName());
+        MessageBus messageBus = (MessageBus) PortalBeanLocatorUtil
+                .locate(MessageBus.class.getName());
+        MessageSender messageSender = (MessageSender) PortalBeanLocatorUtil
+                .locate(MessageSender.class.getName());
+        SynchronousMessageSender synchronousMessageSender = (SynchronousMessageSender) PortalBeanLocatorUtil
+                .locate(SynchronousMessageSender.class.getName());
 
-		MessageBusUtil
-				.init(messageBus, messageSender, synchronousMessageSender);
+        MessageBusUtil
+                .init(messageBus, messageSender, synchronousMessageSender);
 
-		// Scheduler
+        // Scheduler
 
-		SchedulerEngine schedulerEngine = (SchedulerEngine) PortalBeanLocatorUtil
-				.locate(SchedulerEngine.class.getName());
+        SchedulerEngine schedulerEngine = (SchedulerEngine) PortalBeanLocatorUtil
+                .locate(SchedulerEngine.class.getName());
 
-		SchedulerEngineUtil.init(new SchedulerEngineProxy());
+        SchedulerEngineUtil.init(new SchedulerEngineProxy());
 
-		try {
-			schedulerEngine.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            schedulerEngine.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// Company
+        // Company
 
-		try {
-			CompanyLocalServiceUtil
-					.checkCompany(TestPropsValues.COMPANY_WEB_ID);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            CompanyLocalServiceUtil
+                    .checkCompany(TestPropsValues.COMPANY_WEB_ID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public static boolean deleteDir(File dir) {
-		if (dir.isDirectory()) {
-			String[] children = dir.list();
-			for (int i = 0; i < children.length; i++) {
-				boolean success = deleteDir(new File(dir, children[i]));
-				if (!success) {
-					return false;
-				}
-			}
-		}
-		return dir.delete();
-	}
+    public static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
 }
