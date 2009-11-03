23379406101f25b8684b57d113a1ee58876819fe
kherm <kherm>
2009-11-03 05:46:36 +0000

    fix KC catalogs
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/portal@14991 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/browse/DeleteCatalogEntryController.java
+++ cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/browse/DeleteCatalogEntryController.java
@@ -1,80 +1,75 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.browse;
 
 import gov.nih.nci.cagrid.portal.dao.PortalUserDao;
 import gov.nih.nci.cagrid.portal.dao.catalog.CatalogEntryDao;
 import gov.nih.nci.cagrid.portal.dao.catalog.CatalogEntryRelationshipInstanceDao;
-import gov.nih.nci.cagrid.portal.domain.catalog.CatalogEntry;
-import gov.nih.nci.cagrid.portal.domain.catalog.CatalogEntryRelationshipInstance;
-import gov.nih.nci.cagrid.portal.domain.catalog.CatalogEntryRoleInstance;
-import gov.nih.nci.cagrid.portal.domain.catalog.CommunityCatalogEntry;
-import gov.nih.nci.cagrid.portal.domain.catalog.PersonCatalogEntry;
+import gov.nih.nci.cagrid.portal.domain.catalog.*;
 import gov.nih.nci.cagrid.portal.portlet.UserModel;
 import gov.nih.nci.cagrid.portal.service.CatalogEntryService;
 import gov.nih.nci.cagrid.portal.service.CommunityService;
 import gov.nih.nci.cagrid.portal.service.PersonService;
+import org.springframework.web.portlet.mvc.AbstractController;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
-import org.springframework.web.portlet.mvc.AbstractController;
-
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com>Joshua Phillips</a>
- * 
  */
 public class DeleteCatalogEntryController extends AbstractController {
 
-	private UserModel userModel;
-	private CatalogEntryDao catalogEntryDao;
-	private PortalUserDao portalUserDao;
-	private CatalogEntryRelationshipInstanceDao catalogEntryRelationshipInstanceDao;
-	
-	private CommunityService communityService;
-	private PersonService personService;
-	private CatalogEntryService catalogEntryService;
+    private UserModel userModel;
+    private CatalogEntryDao catalogEntryDao;
+    private PortalUserDao portalUserDao;
+    private CatalogEntryRelationshipInstanceDao catalogEntryRelationshipInstanceDao;
 
-	public PortalUserDao getPortalUserDao() {
-		return portalUserDao;
-	}
+    private CommunityService communityService;
+    private PersonService personService;
+    private CatalogEntryService catalogEntryService;
 
-	public void setPortalUserDao(PortalUserDao portalUserDao) {
-		this.portalUserDao = portalUserDao;
-	}
+    public PortalUserDao getPortalUserDao() {
+        return portalUserDao;
+    }
 
-	/**
-	 * 
-	 */
-	public DeleteCatalogEntryController() {
+    public void setPortalUserDao(PortalUserDao portalUserDao) {
+        this.portalUserDao = portalUserDao;
+    }
 
-	}
+    /**
+     *
+     */
+    public DeleteCatalogEntryController() {
 
-	protected void handleActionRequestInternal(ActionRequest request,
-			ActionResponse response) throws Exception {
+    }
 
-		Integer entryId = null;
-		try {
-			entryId = Integer.valueOf(request.getParameter("entryId"));
-		} catch (Exception ex) {
-			throw new RuntimeException("Error getting entryId from request: "
-					+ ex.getMessage(), ex);
-		}
-		CatalogEntry catalogEntry = getCatalogEntryDao().getById(entryId);
-		if (catalogEntry == null) {
-			throw new RuntimeException("Couldn't find catalog entry for id: "
-					+ entryId);
-		}
+    protected void handleActionRequestInternal(ActionRequest request,
+                                               ActionResponse response) throws Exception {
 
-		if(catalogEntry instanceof CommunityCatalogEntry){
-			getCommunityService().deleteCommunity(getUserModel().getPortalUser(), (CommunityCatalogEntry)catalogEntry);
-		}else if(catalogEntry instanceof PersonCatalogEntry){
-			getPersonService().deletePerson(getUserModel().getPortalUser(), (PersonCatalogEntry)catalogEntry);
-		}else{
-			getCatalogEntryService().deleteCatalogEntry(getUserModel().getPortalUser(), catalogEntry);
-		}
-		
+        Integer entryId = null;
+        try {
+            entryId = Integer.valueOf(request.getParameter("entryId"));
+        } catch (Exception ex) {
+            throw new RuntimeException("Error getting entryId from request: "
+                    + ex.getMessage(), ex);
+        }
+        CatalogEntry catalogEntry = getCatalogEntryDao().getById(entryId);
+        if (catalogEntry == null) {
+            throw new RuntimeException("Couldn't find catalog entry for id: "
+                    + entryId);
+        }
+
+        //only if Community CE delete community
+        if (catalogEntry.getClass().equals(CommunityCatalogEntry.class)) {
+            getCommunityService().deleteCommunity(getUserModel().getPortalUser(), (CommunityCatalogEntry) catalogEntry);
+        } else if (catalogEntry instanceof PersonCatalogEntry) {
+            getPersonService().deletePerson(getUserModel().getPortalUser(), (PersonCatalogEntry) catalogEntry);
+        } else {
+            getCatalogEntryService().deleteCatalogEntry(getUserModel().getPortalUser(), catalogEntry);
+        }
+
 //		deleteRelationships(catalogEntry);
 //		if (catalogEntry instanceof PersonCatalogEntry) {
 //			PersonCatalogEntry personCatalogEntry = (PersonCatalogEntry) catalogEntry;
@@ -87,66 +82,66 @@ public class DeleteCatalogEntryController extends AbstractController {
 //		}
 //		getCatalogEntryDao().delete(catalogEntry);
 
-		getUserModel().setCurrentCatalogEntry(null);
+        getUserModel().setCurrentCatalogEntry(null);
 
-		response.setRenderParameter("operation", "view");
-	}
+        response.setRenderParameter("operation", "view");
+    }
 
-	private void deleteRelationships(CatalogEntry catalogEntry) {
-		for (CatalogEntryRoleInstance roleInst : catalogEntry.getRoles()) {
-			CatalogEntryRelationshipInstance relInst = roleInst
-					.getRelationship();
-			getCatalogEntryRelationshipInstanceDao().delete(relInst);
-		}
-	}
+    private void deleteRelationships(CatalogEntry catalogEntry) {
+        for (CatalogEntryRoleInstance roleInst : catalogEntry.getRoles()) {
+            CatalogEntryRelationshipInstance relInst = roleInst
+                    .getRelationship();
+            getCatalogEntryRelationshipInstanceDao().delete(relInst);
+        }
+    }
 
-	public UserModel getUserModel() {
-		return userModel;
-	}
+    public UserModel getUserModel() {
+        return userModel;
+    }
 
-	public void setUserModel(UserModel userModel) {
-		this.userModel = userModel;
-	}
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
+    }
 
-	public CatalogEntryDao getCatalogEntryDao() {
-		return catalogEntryDao;
-	}
+    public CatalogEntryDao getCatalogEntryDao() {
+        return catalogEntryDao;
+    }
 
-	public void setCatalogEntryDao(CatalogEntryDao catalogEntryDao) {
-		this.catalogEntryDao = catalogEntryDao;
-	}
+    public void setCatalogEntryDao(CatalogEntryDao catalogEntryDao) {
+        this.catalogEntryDao = catalogEntryDao;
+    }
 
-	public CatalogEntryRelationshipInstanceDao getCatalogEntryRelationshipInstanceDao() {
-		return catalogEntryRelationshipInstanceDao;
-	}
+    public CatalogEntryRelationshipInstanceDao getCatalogEntryRelationshipInstanceDao() {
+        return catalogEntryRelationshipInstanceDao;
+    }
 
-	public void setCatalogEntryRelationshipInstanceDao(
-			CatalogEntryRelationshipInstanceDao catalogEntryRelationshipInstanceDao) {
-		this.catalogEntryRelationshipInstanceDao = catalogEntryRelationshipInstanceDao;
-	}
+    public void setCatalogEntryRelationshipInstanceDao(
+            CatalogEntryRelationshipInstanceDao catalogEntryRelationshipInstanceDao) {
+        this.catalogEntryRelationshipInstanceDao = catalogEntryRelationshipInstanceDao;
+    }
 
-	public CommunityService getCommunityService() {
-		return communityService;
-	}
+    public CommunityService getCommunityService() {
+        return communityService;
+    }
 
-	public void setCommunityService(CommunityService communityService) {
-		this.communityService = communityService;
-	}
+    public void setCommunityService(CommunityService communityService) {
+        this.communityService = communityService;
+    }
 
-	public PersonService getPersonService() {
-		return personService;
-	}
+    public PersonService getPersonService() {
+        return personService;
+    }
 
-	public void setPersonService(PersonService personService) {
-		this.personService = personService;
-	}
+    public void setPersonService(PersonService personService) {
+        this.personService = personService;
+    }
 
-	public CatalogEntryService getCatalogEntryService() {
-		return catalogEntryService;
-	}
+    public CatalogEntryService getCatalogEntryService() {
+        return catalogEntryService;
+    }
 
-	public void setCatalogEntryService(CatalogEntryService catalogEntryService) {
-		this.catalogEntryService = catalogEntryService;
-	}
+    public void setCatalogEntryService(CatalogEntryService catalogEntryService) {
+        this.catalogEntryService = catalogEntryService;
+    }
 
 }
