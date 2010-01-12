25d1a564c7550f4d3bcc7e762581880eb9ec33c1
kherm <kherm>
2010-01-12 23:02:17 +0000

    fix query btn
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/portal@15503 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/browse/ajax/GridDataServiceEndPointCatalogEntryManagerFacade.java
@@ -0,0 +1,12 @@
+package gov.nih.nci.cagrid.portal.portlet.browse.ajax;
+
+/**
+ * User: kherm
+ *
+ * @author kherm manav.kher@semanticbits.com
+ */
+public class GridDataServiceEndPointCatalogEntryManagerFacade  extends GridServiceEndPointCatalogEntryManagerFacade{
+
+    public GridDataServiceEndPointCatalogEntryManagerFacade() {
+    }
+}
