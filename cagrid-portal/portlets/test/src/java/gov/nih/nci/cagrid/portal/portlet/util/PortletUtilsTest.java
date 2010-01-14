4f967ec44b3de593879477f26b3e18773c4df330
kherm <kherm>
2010-01-14 22:53:07 +0000

    update
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/portal@15533 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid-portal/portlets/test/src/java/gov/nih/nci/cagrid/portal/portlet/util/PortletUtilsTest.java
+++ cagrid-portal/portlets/test/src/java/gov/nih/nci/cagrid/portal/portlet/util/PortletUtilsTest.java
@@ -9,17 +9,22 @@ import gov.nih.nci.cagrid.portal.domain.StatusChange;
 import gov.nih.nci.cagrid.portal.domain.metadata.ServiceMetadata;
 import gov.nih.nci.cagrid.portal.domain.metadata.service.Service;
 import gov.nih.nci.cagrid.portal.portlet.discovery.filter.BaseServiceFilter;
+import gov.nih.nci.cagrid.portal.PortalTestUtils;
 import junit.framework.TestCase;
+import static junit.framework.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.io.IOException;
+
+import org.junit.Test;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
  * @author <a href="mailto:manav.kher@semanticbits.com">Manav Kher</a>
  */
-public class PortletUtilsTest extends TestCase {
+public class PortletUtilsTest {
 
     /**
      *
@@ -29,14 +34,27 @@ public class PortletUtilsTest extends TestCase {
     }
 
 
-    public PortletUtilsTest(String name) {
-        super(name);
+
+
+    @Test
+    public void validate(){
+        try {
+            String dcqlXML = PortalTestUtils.readFileASString("test/data/microArrayLargeDataDCQL.xml");
+            String cqlXML = PortalTestUtils.readFileASString("test/data/sampleCQL1.xml");
+
+            assertFalse("Not a CQL query",PortletUtils.isCQL(dcqlXML));
+            assertTrue("Is a DCQL Query",PortletUtils.isDCQL(dcqlXML));
+
+            assertFalse("Not a DCQL Query",PortletUtils.isDCQL(cqlXML));
+            assertTrue("Is a CQL Query",PortletUtils.isCQL(cqlXML));
+
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
 
     }
 
-
-
-
+    @Test
     public void testFilterInvalidMetadataServices() {
         GridService svc1 = new GridService();
         svc1.setUrl("http://one");
