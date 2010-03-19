82ff96320fda472f7b4cc96535b7a767b1cdea54
kherm <kherm>
2010-03-19 00:15:22 +0000

    refactor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-3-0_release@16094 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid/Software/portal/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/util/filter/BaseServiceFilter.java
@@ -0,0 +1,79 @@
+package gov.nih.nci.cagrid.portal.util.filter;
+
+import gov.nih.nci.cagrid.portal.domain.GridService;
+import gov.nih.nci.cagrid.portal.domain.ServiceStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Default filter of services that
+ * is not dependent on the web users
+ * privileges. Will filter invalid, banned and dormant services
+ * <p/>
+ * User: kherm
+ *
+ * @author kherm manav.kher@semanticbits.com
+ */
+public class BaseServiceFilter implements ServiceFilter {
+
+    public static List<GridService> filterBannedServices(List<GridService> in) {
+        return filterServicesByStatus(in, ServiceStatus.BANNED);
+    }
+
+    public static List<GridService> filterDormantServices(List<GridService> in) {
+        return filterServicesByStatus(in, ServiceStatus.DORMANT);
+
+    }
+
+    public static List<GridService> filterServicesByInvalidMetadata(
+            List<GridService> in) {
+        List<GridService> out = new ArrayList<GridService>();
+        for (GridService svc : in) {
+            boolean filter = false;
+            try {
+                if (svc.getServiceMetadata() == null || svc.getServiceMetadata().getServiceDescription() == null) {
+                    filter = true;
+                }
+            } catch (Exception e) {
+                filter = true;
+            }
+            if (!filter) {
+                out.add(svc);
+            }
+        }
+        return out;
+    }
+
+    public static List<GridService> filterServicesByStatus(
+            List<GridService> in, ServiceStatus... statuses) {
+        List<GridService> out = new ArrayList<GridService>();
+        for (GridService svc : in) {
+            boolean filter = false;
+            for (ServiceStatus status : statuses) {
+                if (status.equals(svc.getCurrentStatus())) {
+                    filter = true;
+                    break;
+                }
+            }
+            if (!filter) {
+                out.add(svc);
+            }
+        }
+        return out;
+    }
+
+    public List<GridService> filter(List<GridService> services) {
+        return filterServicesByStatus(filterServicesByInvalidMetadata((services)), ServiceStatus.BANNED, ServiceStatus.DORMANT);
+    }
+
+    public boolean willBeFiltered(final GridService service) {
+        List _prefilter = new ArrayList<GridService>() {
+            {
+                add(service);
+            }
+        };
+
+        return filter(_prefilter).size() == 0;
+    }
+}
---
+++ cagrid/Software/portal/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/util/filter/ServiceFilter.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.cagrid.portal.util.filter;
+
+import gov.nih.nci.cagrid.portal.domain.GridService;
+
+import java.util.List;
+
+/**
+ * Interface for filtering services.
+ * Implementation classes will filter services
+ * based on some criteria
+ * <p/>
+ * Used to filter services in the View
+ * <p/>
+ * User: kherm
+ *
+ * @author kherm manav.kher@semanticbits.com
+ */
+public interface ServiceFilter {
+
+    /**
+     * Filter a List of services based on
+     * some pre-defined criteria.
+     * Implementations can define their filter criterias
+     *
+     * @param services
+     * @return
+     */
+    public List<GridService> filter(List<GridService> services);
+
+    /**
+     * Will return boolean flag if a service will be
+     * filtered by implementing class
+     *
+     * @param service
+     * @return
+     */
+    public boolean willBeFiltered(GridService service);
+}
