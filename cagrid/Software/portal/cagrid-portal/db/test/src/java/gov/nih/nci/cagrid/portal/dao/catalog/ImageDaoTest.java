8b6e5c6e76a6c9ed3d5ad98f793ec49aca7491b7
barcielaj <barcielaj>
2010-05-20 15:59:41 +0000

    changes to address bug [#28518] Upload photo corrupts the uploaded photo
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-3-0_release@16186 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid/Software/portal/cagrid-portal/db/test/src/java/gov/nih/nci/cagrid/portal/dao/catalog/ImageDaoTest.java
+++ cagrid/Software/portal/cagrid-portal/db/test/src/java/gov/nih/nci/cagrid/portal/dao/catalog/ImageDaoTest.java
@@ -13,45 +13,63 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import javax.imageio.ImageIO;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author joshua
- *
+ * 
  */
 public class ImageDaoTest extends DaoTestBase<ImageDao> {
 
-	@Test
-	public void save(){
-		try{
-			String inFile = "test/data/person_placeholder_180px.png";
-			String outFile = "test/build/person_placeholder_180px.png";
-			
-			FileInputStream in = new FileInputStream(inFile);
-			BufferedImage bImage = ImageIO.read(in);
-			in.close();
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ImageIO.write(bImage, ImageFormat.PNG.toString(), bos);
-			Image image = new Image();
-			image.setData(bos.toByteArray());
-			image.setHeight(bImage.getHeight());
-			image.setWidth(bImage.getWidth());
-			image.setType(bImage.getType());
-			image.setFormat(ImageFormat.PNG);
-			getDao().save(image);
+    private String originalImgFolder = "test/data/";
+    private String originalImgName = "photo-person-cc"; // creative commons licensed photo from:
+                                                        // http://www.flickr.com/photos/daexus/329687468
+    private String originalImgType = "jpg";
 
-			FileOutputStream out = new FileOutputStream(outFile);
-			Image image2 = getDao().getById(image.getId());
-			BufferedImage bImage2 = ImageIO.read(new ByteArrayInputStream(image2.getData()));
-			ImageIO.write(bImage2, ImageFormat.PNG.toString(), out);
-			out.flush();
-			out.close();
-			
-		}catch(Exception ex){
-			fail("Error saving image: " + ex.getMessage());
-		}
-	}
+    private String suffix;
+
+    @Before
+    public void init() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        suffix = dateFormat.format(new java.util.Date());
+    }
+
+    @Test
+    public void save() {
+        try {
+            // String inFile = "test/data/person_placeholder_180px.png";
+            // String outFile = "test/build/person_placeholder_180px.png";
+            String inFile = originalImgFolder + originalImgName + "." + originalImgType;
+            String outFile = "test/build/" + originalImgName + "_save_" + suffix + "." + originalImgType;
+
+            FileInputStream in = new FileInputStream(inFile);
+            BufferedImage bImage = ImageIO.read(in);
+            in.close();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ImageIO.write(bImage, ImageFormat.JPEG.toString(), bos);
+            Image image = new Image();
+            image.setData(bos.toByteArray());
+            // image.setHeight(bImage.getHeight());
+            // image.setWidth(bImage.getWidth());
+            image.setType(bImage.getType());
+            image.setFormat(ImageFormat.JPEG);
+            getDao().save(image);
+
+            FileOutputStream out = new FileOutputStream(outFile);
+            Image image2 = getDao().getById(image.getId());
+            BufferedImage bImage2 = ImageIO.read(new ByteArrayInputStream(image2.getData()));
+            ImageIO.write(bImage2, ImageFormat.JPEG.toString(), out);
+            out.flush();
+            out.close();
+
+        } catch (Exception ex) {
+            fail("Error saving image: " + ex.getMessage());
+        }
+    }
 }
--- cagrid/Software/portal/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/util/ImageUtils.java
+++ cagrid/Software/portal/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/util/ImageUtils.java
@@ -1,96 +1,138 @@
 package gov.nih.nci.cagrid.portal.portlet.util;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.HeadlessException;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.awt.image.PixelGrabber;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.ImageIcon;
 
 public class ImageUtils {
-	
-	public static BufferedImage scaleImage(BufferedImage source, int maxHeight, int maxWidth){
-		BufferedImage result = source;
-		if(result.getHeight() > maxHeight){
-			result = toBufferedImage(result.getScaledInstance(-1, maxHeight, -1));
-		}
-		if(result.getWidth() > maxWidth){
-			result = toBufferedImage(result.getScaledInstance(maxWidth, -1, -1));
-		}
-		return result;
-	}
-	
-	public static BufferedImage toBufferedImage(Image image) {
-		if (image instanceof BufferedImage) {
-			return (BufferedImage) image;
-		}
 
-		// This code ensures that all the pixels in the image are loaded
-		image = new ImageIcon(image).getImage();
+    public static BufferedImage scaleImage(BufferedImage source, int maxHeight, int maxWidth) 
+    {
+        float srcWidth = (float) source.getWidth();
+        float srcHeight = (float) source.getHeight();
+        
+        if ((srcWidth <= maxWidth) && (srcHeight <= maxHeight))
+        {
+            return source;
+        }
 
-		// Determine if the image has transparent pixels
-		boolean hasAlpha = hasAlpha(image);
+        boolean hasAlpha = hasAlpha(source);
+        int imgType = (hasAlpha == true) ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB;
 
-		// Create a buffered image with a format that's compatible with the
-		// screen
-		BufferedImage bimage = null;
-		GraphicsEnvironment ge = GraphicsEnvironment
-				.getLocalGraphicsEnvironment();
-		try {
-			// Determine the type of transparency of the new buffered image
-			int transparency = Transparency.OPAQUE;
-			if (hasAlpha == true) {
-				transparency = Transparency.BITMASK;
-			}
+        int newWidth = 0;
+        int newHeight = 0;
+        
+        float srcRatio = srcHeight / srcWidth;
+        
+        if (srcRatio >= 1) //portrait
+        {
+            newHeight = maxHeight;
+            newWidth = Math.round(newHeight / srcRatio);
+        }
+        else //landscape 
+        {
+            newWidth = maxWidth;
+            newHeight = Math.round(newWidth * srcRatio);
+        }
+        
+        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, imgType);
 
-			// Create the buffered image
-			GraphicsDevice gs = ge.getDefaultScreenDevice();
-			GraphicsConfiguration gc = gs.getDefaultConfiguration();
-			bimage = gc.createCompatibleImage(image.getWidth(null), image
-					.getHeight(null), transparency);
-		} catch (HeadlessException e) {
-		} // No screen
+        Graphics2D g = resizedImage.createGraphics();
 
-		if (bimage == null) {
-			// Create a buffered image using the default color model
-			int type = BufferedImage.TYPE_INT_RGB;
-			if (hasAlpha == true) {
-				type = BufferedImage.TYPE_INT_ARGB;
-			}
-			bimage = new BufferedImage(image.getWidth(null), image
-					.getHeight(null), type);
-		}
+        Map<RenderingHints.Key, Object> hints = new HashMap<RenderingHints.Key, Object>();
+        hints.put(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.addRenderingHints(hints);
 
-		// Copy image to buffered image
-		Graphics g = bimage.createGraphics();
+        g.drawImage(source, 0, 0, newWidth, newHeight, null);
+        g.dispose();
 
-		// Paint the image onto the buffered image
-		g.drawImage(image, 0, 0, null);
-		g.dispose();
+        return resizedImage;
+    }
 
-		return bimage;
-	}
+    /*
+     * public static BufferedImage scaleImage(BufferedImage source, int maxHeight, int maxWidth){ BufferedImage result =
+     * source; if(result.getHeight() > maxHeight){ result = toBufferedImage(result.getScaledInstance(-1, maxHeight,
+     * -1)); } if(result.getWidth() > maxWidth){ result = toBufferedImage(result.getScaledInstance(maxWidth, -1, -1)); }
+     * return result; }
+     */
 
-	public static boolean hasAlpha(Image image) {
-		// If buffered image, the color model is readily available
-		if (image instanceof BufferedImage) {
-			return ((BufferedImage) image).getColorModel().hasAlpha();
-		}
+    public static BufferedImage toBufferedImage(Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage) image;
+        }
 
-		// Use a pixel grabber to retrieve the image's color model;
-		// grabbing a single pixel is usually sufficient
-		PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
-		try {
-			pg.grabPixels();
-		} catch (InterruptedException e) {
-		}
+        // This code ensures that all the pixels in the image are loaded
+        image = new ImageIcon(image).getImage();
 
-		// Get the image's color model
-		return pg.getColorModel().hasAlpha();
-	}
+        // Determine if the image has transparent pixels
+        boolean hasAlpha = hasAlpha(image);
+
+        // Create a buffered image with a format that's compatible with the
+        // screen
+        BufferedImage bimage = null;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            // Determine the type of transparency of the new buffered image
+            int transparency = Transparency.OPAQUE;
+            if (hasAlpha == true) {
+                transparency = Transparency.BITMASK;
+            }
+
+            // Create the buffered image
+            GraphicsDevice gs = ge.getDefaultScreenDevice();
+            GraphicsConfiguration gc = gs.getDefaultConfiguration();
+            bimage = gc.createCompatibleImage(image.getWidth(null), image.getHeight(null), transparency);
+        } catch (HeadlessException e) {
+        } // No screen
+
+        if (bimage == null) {
+            // Create a buffered image using the default color model
+            int type = BufferedImage.TYPE_INT_RGB;
+            if (hasAlpha == true) {
+                type = BufferedImage.TYPE_INT_ARGB;
+            }
+            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+        }
+
+        // Copy image to buffered image
+        Graphics g = bimage.createGraphics();
+
+        // Paint the image onto the buffered image
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+
+        return bimage;
+    }
+
+    public static boolean hasAlpha(Image image) {
+        // If buffered image, the color model is readily available
+        if (image instanceof BufferedImage) {
+            return ((BufferedImage) image).getColorModel().hasAlpha();
+        }
+
+        // Use a pixel grabber to retrieve the image's color model;
+        // grabbing a single pixel is usually sufficient
+        PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+        try {
+            pg.grabPixels();
+        } catch (InterruptedException e) {
+        }
+
+        // Get the image's color model
+        return pg.getColorModel().hasAlpha();
+    }
 }
