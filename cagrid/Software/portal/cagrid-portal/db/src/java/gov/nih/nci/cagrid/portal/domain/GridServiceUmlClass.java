be6a7cc7d81e00104521ad082ba7cedf0e180bb3
asrinivasa <asrinivasa>
2010-11-02 20:46:01 +0000

    CAGPRTL-136
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-3-0_release@16854 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ cagrid/Software/portal/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/domain/GridServiceUmlClass.java
@@ -0,0 +1,52 @@
+package gov.nih.nci.cagrid.portal.domain;
+
+import gov.nih.nci.cagrid.portal.domain.metadata.dataservice.UMLClass;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+@Table(name = "grid_svc_uml_class")
+@GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "seq_grid_svc_uml_class") })
+public class GridServiceUmlClass extends AbstractDomainObject {
+	private UMLClass umlClass;
+	private GridService gridService;
+	private int objectCount;
+	
+	public GridServiceUmlClass() {
+		
+	}
+	
+    @ManyToOne
+    @JoinColumn(name = "grid_svc_id", nullable = false)
+	public GridService getGridService() {
+		return gridService;
+	}
+	public void setGridService(GridService gridService) {
+		this.gridService = gridService;
+	}
+	
+    @ManyToOne
+    @JoinColumn(name = "uml_class_id", nullable = false)
+	public UMLClass getUmlClass() {
+		return umlClass;
+	}
+	public void setUmlClass(UMLClass umlClass) {
+		this.umlClass = umlClass;
+	}
+	
+	@Column(name="object_count")
+	public int getObjectCount() {
+		return objectCount;
+	}
+	public void setObjectCount(int objectCount) {
+		this.objectCount = objectCount;
+	}
+	
+}
