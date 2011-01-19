4316af3b04b44f3d66f9e9ed78d7bd4c1445f0c8
chadaram <chadaram>
2011-01-19 22:47:24 +0000

    Loading catalog items during server startup issue resolved.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/branches/cagrid-portal-3-0_release@17213 dd7b0c16-3c94-4492-92de-414b6a06f0b1

--- cagrid/Software/portal/cagrid-portal/aggr/src/java/gov/nih/nci/cagrid/portal/aggr/catalog/GridServiceEndPointCatalogCreator.java
+++ cagrid/Software/portal/cagrid-portal/aggr/src/java/gov/nih/nci/cagrid/portal/aggr/catalog/GridServiceEndPointCatalogCreator.java
@@ -1,9 +1,5 @@
 package gov.nih.nci.cagrid.portal.aggr.catalog;
 
-import gov.nih.nci.cagrid.portal.dao.GridServiceDao;
-import gov.nih.nci.cagrid.portal.dao.catalog.GridServiceEndPointCatalogEntryDao;
-import gov.nih.nci.cagrid.portal.domain.GridService;
-import gov.nih.nci.cagrid.portal.util.filter.ServiceFilter;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -14,10 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class GridServiceEndPointCatalogCreator extends AbstractCatalogCreator {
 
-    private GridServiceDao gridServiceDao;
-    private GridServiceEndPointCatalogEntryDao gridServiceEndPointCatalogEntryDao;
-    private ServiceMetadataCatalogEntryBuilder serviceMetadataCatalogEntryBuilder;
-    private ServiceFilter baseServiceFilter;
+    private GridServiceEndPointCatalogCreatorDelegate gridServiceEndPointCatalogCreatorDelegate;
 
     /**
      * Will make sure catalog items exist for all services Run on container
@@ -26,52 +19,17 @@ public class GridServiceEndPointCatalogCreator extends AbstractCatalogCreator {
      * @throws Exception
      */
     public void afterPropertiesSet() throws Exception {
-
-        for (GridService g : gridServiceDao.getAll()) {
-            if (gridServiceEndPointCatalogEntryDao.isAbout(g) == null) {
-                if (!baseServiceFilter.willBeFiltered(g)) {
-
-                    logger
-                            .debug("GridService Endpoint catalog not found. Will create for id "
-                                    + g.getId());
-                    serviceMetadataCatalogEntryBuilder.build(g);
-                }
-            }
-
-        }
+    	//call delegate to load catalog items
+    	gridServiceEndPointCatalogCreatorDelegate.loadCatalogItems();    	
     }
 
     // spring getters and setters
+   	public GridServiceEndPointCatalogCreatorDelegate getGridServiceEndPointCatalogCreatorDelegate() {
+		return gridServiceEndPointCatalogCreatorDelegate;
+	}
 
-    public GridServiceDao getGridServiceDao() {
-        return gridServiceDao;
-    }
-
-    public void setGridServiceDao(GridServiceDao gridServiceDao) {
-        this.gridServiceDao = gridServiceDao;
-    }
-
-    public GridServiceEndPointCatalogEntryDao getGridServiceEndPointCatalogEntryDao() {
-        return gridServiceEndPointCatalogEntryDao;
-    }
-
-    public void setGridServiceEndPointCatalogEntryDao(GridServiceEndPointCatalogEntryDao gridServiceEndPointCatalogEntryDao) {
-        this.gridServiceEndPointCatalogEntryDao = gridServiceEndPointCatalogEntryDao;
-    }
-
-    public ServiceMetadataCatalogEntryBuilder getServiceMetadataCatalogEntryBuilder() {
-        return serviceMetadataCatalogEntryBuilder;
-    }
-
-    public void setServiceMetadataCatalogEntryBuilder(ServiceMetadataCatalogEntryBuilder serviceMetadataCatalogEntryBuilder) {
-        this.serviceMetadataCatalogEntryBuilder = serviceMetadataCatalogEntryBuilder;
-    }
-
-    public ServiceFilter getBaseServiceFilter() {
-        return baseServiceFilter;
-    }
-
-    public void setBaseServiceFilter(ServiceFilter baseServiceFilter) {
-        this.baseServiceFilter = baseServiceFilter;
-    }
+	public void setGridServiceEndPointCatalogCreatorDelegate(
+			GridServiceEndPointCatalogCreatorDelegate gridServiceEndPointCatalogCreatorDelegate) {
+		this.gridServiceEndPointCatalogCreatorDelegate = gridServiceEndPointCatalogCreatorDelegate;
+	}
 }
